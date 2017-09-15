59072dfe7215642ed85c24f16d9135d9f51a8306
zhennann <zhennann@qq.com>
2017-09-15 23:22:50 +0800

    backend-dev

---
+++ packages/egg-born-bin/lib/cmd/backend-dev.js
@@ -0,0 +1,32 @@
+/*
+* @Author: zhennann
+* @Date:   2017-09-15 22:51:21
+* @Last Modified by:   zhennann
+* @Last Modified time: 2017-09-15 23:05:56
+*/
+
+const path = require('path');
+const DevCommand = require('egg-bin').DevCommand;
+
+class BackendDevCommand extends DevCommand {
+
+  constructor(rawArgv) {
+    super(rawArgv);
+    this.usage = 'Usage: egg-born-bin backend-dev';
+  }
+
+  * run(context) {
+
+    if (!context.argv.baseDir) context.argv.baseDir = 'src/backend';
+
+    yield super.run(context);
+
+  }
+
+  description() {
+    return 'backend dev';
+  }
+
+}
+
+module.exports = BackendDevCommand;
