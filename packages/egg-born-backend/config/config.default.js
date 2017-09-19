9ce297c8a4da926992144d1a81d65f8752b75eab
zhennann <zhennann@qq.com>
2017-09-19 23:41:35 +0800

    structure

--- packages/egg-born-backend/config/config.default.js
+++ packages/egg-born-backend/config/config.default.js
@@ -3,14 +3,5 @@
 module.exports = appInfo => {
   const config = {};
 
-  /**
-   * some description
-   * @member Config#test
-   * @property {String} key - some description
-   */
-  config.test = {
-    key: appInfo.name + '_123456',
-  };
-
   return config;
 };
--- packages/egg-born-backend/config/plugin.js
+++ packages/egg-born-backend/config/plugin.js
@@ -1,8 +1,32 @@
-'use strict';
+/*
+* @Author: zhennann
+* @Date:   2017-09-19 23:32:28
+* @Last Modified by:   zhennann
+* @Last Modified time: 2017-09-19 23:33:20
+*/
 
-// add you build-in plugin here, example:
-// exports.nunjucks = {
-//   enable: true,
-//   package: 'egg-view-nunjucks',
-// };
+module.exports = {
 
+  /**
+   * `app/public` dir static serve
+   * @member {Object} Plugin#static
+   * @property {Boolean} enable - `true` by default
+   * @since 1.0.0
+   */
+  static: {
+    enable: false,
+    package: 'egg-static',
+  },
+
+  /**
+   * view plugin
+   * @member {Function} Plugin#view
+   * @property {Boolean} enable - `true` by default
+   * @since 1.0.0
+   */
+  view: {
+    enable: false,
+    package: 'egg-view',
+  },
+
+};
