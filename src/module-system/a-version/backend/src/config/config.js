727d40d5c3c4b48fb69c84253e15696ac7d07182
zhennann <zhennann@qq.com>
2017-10-11 20:38:09 +0800

    lint

--- src/module-system/a-version/backend/src/config/config.js
+++ src/module-system/a-version/backend/src/config/config.js
@@ -2,7 +2,7 @@
 * @Author: zhennann
 * @Date:   2017-09-21 11:30:15
 * @Last Modified by:   zhennann
-* @Last Modified time: 2017-10-10 15:37:07
+* @Last Modified time: 2017-10-11 20:36:47
 */
 
 // eslint-disable-next-line
