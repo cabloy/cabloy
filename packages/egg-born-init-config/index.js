d4d6331a70be740e8bbdca585d8c0de8a9a4c867
zhennann <zhennann@qq.com>
2017-10-12 19:03:20 +0800

    package

---
+++ packages/egg-born-init-config/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: zhennann
+* @Date:   2017-10-11 21:38:05
+* @Last Modified by:   zhennann
+* @Last Modified time: 2017-10-12 16:03:45
+*/
+
+console.error('should not require egg-born-init-config');
+process.exit(1);
