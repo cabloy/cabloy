5f7d7ba468ce0127b39d3b3d2e3ee490d4153bdd
zhennann <zhennann@qq.com>
2017-10-13 16:49:47 +0800

    milestone2

---
+++ packages/egg-born-template-module/boilerplate/backend/src/services.js
@@ -0,0 +1,5 @@
+const version = require('./service/version.js');
+
+module.exports = {
+  version,
+};
---
+++ packages/egg-born-template-module/index.js
@@ -0,0 +1,18 @@
+/*
+* @Author: zhennann
+* @Date:   2017-10-12 16:37:55
+* @Last Modified by:   zhennann
+* @Last Modified time: 2017-10-13 00:46:26
+*/
+
+module.exports = {
+  name: {
+    desc: 'module name',
+  },
+  description: {
+    desc: 'module description',
+  },
+  author: {
+    desc: 'module author',
+  },
+};
