a21076b06b0e203b6c58573fa1325f6b1815c731
zhennann <zhen.nann@icloud.com>
2017-11-21 16:53:27 +0800

    refactor: middlewares

--- packages/egg-born-template-module/boilerplate/backend/src/routes.js
+++ packages/egg-born-template-module/boilerplate/backend/src/routes.js
@@ -1,5 +1,5 @@
 const version = require('./controller/version.js');
 
 module.exports = [
-  { method: 'post', path: 'version/update', controller: version },
+  { method: 'post', path: 'version/update', controller: version, middlewares: 'safeAccess' },
 ];
