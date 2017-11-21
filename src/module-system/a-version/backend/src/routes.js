8037416e7eb33160c6ba6cfde61246666ae4e8e9
zhennann <zhen.nann@icloud.com>
2017-11-21 16:51:34 +0800

    refactor: middlewares

--- src/module-system/a-version/backend/src/routes.js
+++ src/module-system/a-version/backend/src/routes.js
@@ -1,8 +1,8 @@
 const version = require('./controller/version.js');
 
 module.exports = [
-  { method: 'post', path: 'version/check', controller: version },
-  { method: 'post', path: 'version/updateModule', controller: version, transaction: true },
-  { method: 'post', path: 'version/update', controller: version },
-  { method: 'get', path: 'version/result', controller: version },
+  { method: 'post', path: 'version/check', controller: version, middlewares: 'safeAccess' },
+  { method: 'post', path: 'version/updateModule', controller: version, middlewares: 'safeAccess,transaction' },
+  { method: 'post', path: 'version/update', controller: version, middlewares: 'safeAccess' },
+  { method: 'get', path: 'version/result', controller: version, middlewares: 'safeAccess' },
 ];
