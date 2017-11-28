1dbca5cdf7cee667346a436532c960c5787d7004
zhennann <zhen.nann@icloud.com>
2017-11-28 10:45:45 +0800

    refactor: eslint

--- packages/require3/index.js
+++ packages/require3/index.js
@@ -1,10 +1,3 @@
-/*
-* @Author: zhennann
-* @Date:   2017-10-11 21:38:05
-* @Last Modified by:   zhennann
-* @Last Modified time: 2017-10-11 21:41:12
-*/
-
 module.exports = function(path) {
   return require(path);
 };
