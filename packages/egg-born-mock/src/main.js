92837992e621fd6b2a16efb49f63d22cf51f0627
zhennann <zhen.nann@icloud.com>
2017-11-28 09:47:29 +0800

    refactor: runSchedule in test

--- packages/egg-born-mock/src/main.js
+++ packages/egg-born-mock/src/main.js
@@ -1,7 +1,10 @@
 const { assert, app, mock, mm } = require('egg-mock/bootstrap');
 
 before(function(done) {
-  app.on(app.meta.constants['egg-born-module-a-version'].event.checkReady, done);
+  app.messenger.once('egg-ready', () => {
+    app.meta.runSchedule('egg-born-module-a-version:versionCheck').then(done);
+  });
+  // app.on(app.meta.constants['egg-born-module-a-version'].event.checkReady, done);
 });
 
 module.exports = function(dirname) {
