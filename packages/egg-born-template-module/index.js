f4f373eb721ecd3e1d9246ce451f3d3c9fbd1248
zhennann <zhen.nann@icloud.com>
2017-11-28 10:32:33 +0800

    refactor: eslint

--- packages/egg-born-template-module/index.js
+++ packages/egg-born-template-module/index.js
@@ -1,10 +1,3 @@
-/*
-* @Author: zhennann
-* @Date:   2017-10-12 16:37:55
-* @Last Modified by:   zhennann
-* @Last Modified time: 2017-10-13 00:46:26
-*/
-
 module.exports = {
   name: {
     desc: 'module name',
