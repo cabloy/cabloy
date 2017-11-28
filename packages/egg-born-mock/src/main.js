31d3bd514549a197248520f84c547f6995c1f65c
zhennann <zhen.nann@icloud.com>
2017-11-28 11:35:09 +0800

    feat: dynamic database

--- packages/egg-born-mock/src/main.js
+++ packages/egg-born-mock/src/main.js
@@ -1,12 +1,34 @@
+const moment = require('moment');
+const chalk = require('chalk');
 const { assert, app, mock, mm } = require('egg-mock/bootstrap');
 
+const database = `egg-born-test-${moment().format('YYYYMMDD-HHmmss')}`;
+
 before(function(done) {
-  app.meta.runSchedule('egg-born-module-a-version:versionCheck').then(done);
-  // app.on(app.meta.constants['egg-born-module-a-version'].event.checkReady, done);
+  const ctx = app.mockContext({ mockUrl: '/api/a/version/' });
+
+  // create database
+  ctx.db.query(`CREATE DATABASE \`${database}\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`).then(() => {
+    // use database
+    ctx.db.query(`use \`${database}\``).then(() => {
+      console.log(chalk.cyan(`  database: ${database}`));
+      app.meta.runSchedule('egg-born-module-a-version:versionCheck').then(() => {
+        mock.restore();
+        done();
+      });
+    });
+  });
+});
+
+after(function(done) {
+  const ctx = app.mockContext({ mockUrl: '/api/a/version/' });
+  // drop database
+  ctx.db.query(`DROP DATABASE \`${database}\``).then(() => {
+    done();
+  });
 });
 
 module.exports = function(dirname) {
-
   return {
     assert,
     app,
@@ -17,5 +39,4 @@ module.exports = function(dirname) {
       return url ? `${prefix}/${url}` : `${prefix}/`;
     },
   };
-
 };
