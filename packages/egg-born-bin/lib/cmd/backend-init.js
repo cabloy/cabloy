eb5f1ce47150d0dec2959e01596829d3ac111264
zhennann <zhen.nann@icloud.com>
2017-12-14 11:11:16 +0800

    feat: backend-init

---
+++ packages/egg-born-bin/lib/cmd/backend-init.js
@@ -0,0 +1,56 @@
+const Command = require('egg-bin').Command;
+const path = require('path');
+const chalk = require('chalk');
+const mock = require('egg-mock');
+
+class BackendInitCommand extends Command {
+
+  constructor(rawArgv) {
+    super(rawArgv);
+    this.usage = 'Usage: egg-born-bin backend-init --subdomain=\'\' --password=123456';
+  }
+
+  * run(context) {
+
+    // override argv
+    const params = Object.assign({}, context.argv);
+    Object.keys(params).forEach(key => {
+      params[key] = context.env[`npm_config_${key}`] || params[key];
+    });
+
+    // options
+    const options = {};
+    options.baseDir = context.argv.baseDir || path.join(context.cwd, 'src/backend');
+
+    // app
+    const app = mock.app(options);
+    yield app.ready();
+
+    // ctx
+    const ctx = app.mockContext({ mockUrl: '/api/a/version/' });
+
+    // version check
+    yield app.meta.runSchedule('egg-born-module-a-version:versionCheck');
+
+    // version init
+    yield ctx.performAction({
+      method: 'post',
+      url: 'version/check',
+      body: {
+        ...params,
+        scene: 'init',
+      },
+    });
+
+    // done
+    console.log(chalk.cyan('  backend-init successfully!'));
+    process.exit(0);
+  }
+
+  description() {
+    return 'backend init';
+  }
+
+}
+
+module.exports = BackendInitCommand;
