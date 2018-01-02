1b0530f7068017859720dd6b76cf47c0de0225d7
zhennann <zhen.nann@icloud.com>
2018-01-02 22:24:31 +0800

    refactor: runSchedule

--- packages/egg-born-backend/lib/module/schedule/app.js
+++ packages/egg-born-backend/lib/module/schedule/app.js
@@ -18,16 +18,17 @@ module.exports = function(loader, modules) {
   });
 
   // for test purpose
-  loader.app.meta.runSchedule = key => {
-    const schedule = ebSchedules[key];
+  loader.app.meta.runSchedule = (module, key) => {
+    const fullKey = `${module}:${key}`;
+    const schedule = ebSchedules[fullKey];
     if (!schedule) {
-      throw new Error(`Cannot find schedule ${key}`);
+      throw new Error(`Cannot find schedule ${fullKey}`);
     }
 
     // run with anonymous context
     const ctx = loader.app.createAnonymousContext({
       method: 'SCHEDULE',
-      url: `/__schedule?path=${key}&${qs.stringify(schedule.schedule)}`,
+      url: `/__schedule?path=${fullKey}&${qs.stringify(schedule.schedule)}`,
     });
 
     return schedule.task(ctx);
@@ -39,7 +40,7 @@ module.exports = function(loader, modules) {
       // module schedules
       if (module.main.schedules) {
         Object.keys(module.main.schedules).forEach(scheduleKey => {
-          const fullKey = `${module.info.fullName}:${scheduleKey}`;
+          const fullKey = `${module.info.relativeName}:${scheduleKey}`;
           const scheduleTask = module.main.schedules[scheduleKey];
           const scheduleConfig = loader.app.meta.configs[module.info.relativeName].schedules[scheduleKey];
           ebSchedules[fullKey] = {
