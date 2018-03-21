7daa2fd52f6afd10f52c95d65866cb1c69a51a37
zhennann <zhen.nann@icloud.com>
2018-03-21 18:49:24 +0800

    refactor: test db

--- packages/egg-born-backend/app.js
+++ packages/egg-born-backend/app.js
@@ -1,4 +1,7 @@
 const eventWorkStartReady = 'eb:event:work-start:ready';
+const moment = require('moment');
+const chalk = require('chalk');
+
 module.exports = app => {
   app.beforeStart(async () => {
     // dev/debug db
@@ -11,8 +14,30 @@ module.exports = app => {
         const db = dbs[0];
         mysqlConfig.database = db[Object.keys(db)[0]];
         app.mysql.__ebdb_test = app.mysql.createInstance(mysqlConfig);
+        // database ready
+        console.log(chalk.cyan(`  database: ${mysqlConfig.database}`));
       }
     }
+    // test db
+    if (app.meta.isTest) {
+      // drop old databases
+      const mysql = app.mysql.get('__ebdb');
+      const dbs = await mysql.query('show databases like \'egg-born-test-%\'');
+      for (const db of dbs) {
+        const name = db[Object.keys(db)[0]];
+        await mysql.query(`drop database \`${name}\``);
+      }
+      // create database
+      const database = `egg-born-test-${moment().format('YYYYMMDD-HHmmss')}`;
+      await mysql.query(`CREATE DATABASE \`${database}\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`);
+      // create test mysql
+      const mysqlConfig = app.config.mysql.clients.__ebdb;
+      mysqlConfig.database = database;
+      app.mysql.__ebdb_test = app.mysql.createInstance(mysqlConfig);
+      // database ready
+      console.log(chalk.cyan(`  database: ${database}`));
+    }
+    // ready
     app.emit(eventWorkStartReady);
   });
 };
