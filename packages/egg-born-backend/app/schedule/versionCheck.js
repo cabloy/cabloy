0552549917da1d77f47d37c752b1928bd9a704d5
zhennann <zhen.nann@icloud.com>
2018-03-21 11:57:00 +0800

    feat: versioncheck

--- packages/egg-born-backend/app/schedule/versionCheck.js
+++ packages/egg-born-backend/app/schedule/versionCheck.js
@@ -11,6 +11,28 @@ module.exports = {
       method: 'post',
       url: '/a/version/version/start',
     });
+    // test
+    if (ctx.app.meta.isTest) {
+      // version init
+      await ctx.performAction({
+        method: 'post',
+        url: '/a/version/version/check',
+        body: {
+          subdomain: '',
+          password: '',
+          scene: 'init',
+        },
+      });
+      // version test
+      await ctx.performAction({
+        method: 'post',
+        url: '/a/version/version/check',
+        body: {
+          scene: 'test',
+        },
+      });
+    }
+    // emit checkReady
     ctx.app.emit(eventCheckReady);
   },
 };
