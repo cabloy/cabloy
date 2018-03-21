218788e18fe6fd72cd9af1ade40ccca8916e46d7
zhennann <zhen.nann@icloud.com>
2018-03-21 18:48:17 +0800

    refactor: test db

--- packages/egg-born-mock/src/main.js
+++ packages/egg-born-mock/src/main.js
@@ -1,36 +1,12 @@
-const moment = require('moment');
-const chalk = require('chalk');
 const path = require('path');
 const { assert, app, mock, mm } = require('egg-mock/bootstrap');
 
 before(async () => {
-  // drop old databases
-  const mysql = app.mysql.get('__ebdb');
-  const dbs = await mysql.query('show databases like \'egg-born-test-%\'');
-  for (const db of dbs) {
-    const name = db[Object.keys(db)[0]];
-    await mysql.query(`drop database \`${name}\``);
-  }
-
-  // create database
-  const database = `egg-born-test-${moment().format('YYYYMMDD-HHmmss')}`;
-  await mysql.query(`CREATE DATABASE \`${database}\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`);
-
-  // create test mysql
-  const mysqlConfig = app.config.mysql.clients.__ebdb;
-  mysqlConfig.database = database;
-  app.mysql.__ebdb_test = app.mysql.createInstance(mysqlConfig);
-
-  // database ready
-  console.log(chalk.cyan(`  database: ${database}`));
-
   // session
   app.mockSession({});
-
   // version check
   const pathVersionCheck = path.join(__dirname, '../../egg-born-backend/app/schedule/versionCheck.js');
   await app.runSchedule(pathVersionCheck);
-
   // restore
   mock.restore();
 });
