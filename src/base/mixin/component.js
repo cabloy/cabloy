dc39a1cec9889352a95ecbfe98c97667c3d50c7f
zhennann <zhen.nann@icloud.com>
2018-04-19 22:43:02 +0800

    feat: $page

--- src/base/mixin/component.js
+++ src/base/mixin/component.js
@@ -7,6 +7,13 @@ export default function() {
         return ctx.$f7Router.view.$el[0].__vue__;
       },
     });
+    Object.defineProperty(ctx, '$page', {
+      get() {
+        const page = ctx.$$(ctx.$el).parents('.page');
+        if (page.length > 0 && page[0].__vue__) return page[0].__vue__;
+        return null;
+      },
+    });
   };
 
   return { beforeCreate };
