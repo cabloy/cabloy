6b6229d185cf7410e7adcb4254326c5561ce35b9
zhennann <zhen.nann@icloud.com>
2018-04-25 15:36:16 +0800

    fix: $module

--- src/base/mixin.js
+++ src/base/mixin.js
@@ -35,7 +35,7 @@ export default function(Vue) {
 
   function mixinModule(ctx) {
     if (!ctx.$module) {
-      const relativeName = ctx.$options.__proto__.__ebModuleRelativeName;
+      const relativeName = ctx.$options.__proto__.__ebModuleRelativeName || ctx.$options.__ebModuleRelativeName;
       if (relativeName) {
         ctx.$module = ctx.$meta.module.get(relativeName);
         if (!ctx.$module.adjustHref) {
