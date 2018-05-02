2efd182bda4304bef806c2048ceb154bf2cdb8c2
zhennann <zhen.nann@icloud.com>
2018-05-02 21:09:36 +0800

    refactor: safeAccess=>inner

--- packages/egg-born-template-module/boilerplate/backend/src/routes.js
+++ packages/egg-born-template-module/boilerplate/backend/src/routes.js
@@ -1,7 +1,7 @@
 const version = require('./controller/version.js');
 
 module.exports = [
-  { method: 'post', path: 'version/update', controller: version, middlewares: 'safeAccess' },
-  { method: 'post', path: 'version/init', controller: version, middlewares: 'safeAccess' },
-  { method: 'post', path: 'version/test', controller: version, middlewares: 'safeAccess' },
+  { method: 'post', path: 'version/update', controller: version, middlewares: 'inner' },
+  { method: 'post', path: 'version/init', controller: version, middlewares: 'inner' },
+  { method: 'post', path: 'version/test', controller: version, middlewares: 'inner' },
 ];
