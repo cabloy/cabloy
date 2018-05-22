74829ed176f9a5f7957d51672d816082b62db8c7
zhennann <zhen.nann@icloud.com>
2018-05-22 22:54:05 +0800

    fix: test transaction

--- src/module-system/a-version/dist/backend.js
+++ src/module-system/a-version/dist/backend.js
@@ -483,7 +483,7 @@ module.exports = [
   { method: 'post', path: 'version/check', controller: version, middlewares: 'inner' },
   { method: 'post', path: 'version/updateModule', controller: version, middlewares: 'inner,transaction' },
   { method: 'post', path: 'version/initModule', controller: version, middlewares: 'inner,transaction' },
-  { method: 'post', path: 'version/testModule', controller: version, middlewares: 'inner,transaction' },
+  { method: 'post', path: 'version/testModule', controller: version, middlewares: 'inner' },
   { method: 'post', path: 'version/update', controller: version, middlewares: 'inner' },
 ];
 
