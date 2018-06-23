96d71f9589f6831ab5fc13ecc48b1a3e57f566cb
zhennann <zhen.nann@icloud.com>
2018-06-24 01:38:10 +0800

    feat: support no tests

--- packages/egg-born-bin/lib/cmd/backend-test.js
+++ packages/egg-born-bin/lib/cmd/backend-test.js
@@ -1,5 +1,9 @@
 const path = require('path');
+const chalk = require('chalk');
+const globby = require('globby');
+const mock = require('egg-mock');
 const TestCommand = require('egg-bin').TestCommand;
+const eventAppReady = 'eb:event:appReady';
 
 class BackendTestCommand extends TestCommand {
 
@@ -8,14 +12,51 @@ class BackendTestCommand extends TestCommand {
     this.usage = 'Usage: egg-born-bin backend-test';
   }
 
-  * run(context) {
+  async run(context) {
 
     if (!context.env.EGG_BASE_DIR) context.env.EGG_BASE_DIR = path.join(process.cwd(), 'src/backend');
 
     if (!context.argv._ || context.argv._.length === 0) context.argv._ = [ 'src/**/test/**/*.test.js' ];
 
-    yield super.run(context);
+    const testArgv = Object.assign({}, context.argv);
 
+    // collect test files
+    let files = testArgv._.slice();
+    if (!files.length) {
+      files = [ process.env.TESTS || 'test/**/*.test.js' ];
+    }
+
+    // expand glob and skip node_modules and fixtures
+    files = globby.sync(files.concat('!test/**/{fixtures, node_modules}/**/*.test.js'));
+
+    if (!files.length) {
+      // options
+      const options = {};
+      options.baseDir = context.env.EGG_BASE_DIR;
+
+      // app
+      const app = mock.app(options);
+      await app.ready();
+
+      // check app ready
+      await this.checkAppReady(app);
+
+      // done
+      console.log(chalk.cyan('  backend-test successfully!'));
+      process.exit(0);
+    } else {
+      // run
+      await super.run(context);
+    }
+
+  }
+
+  checkAppReady(app) {
+    return new Promise((resolve, reject) => {
+      app.on(eventAppReady, () => {
+        resolve();
+      });
+    });
   }
 
   description() {
