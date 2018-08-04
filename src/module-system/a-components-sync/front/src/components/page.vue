bc02ee16644cda1d4b5a031a47460456f337ec44
zhennann <zhen.nann@icloud.com>
2018-08-04 21:46:59 +0800

    feat: page title

--- src/module-system/a-components-sync/front/src/components/page.vue
+++ src/module-system/a-components-sync/front/src/components/page.vue
@@ -10,8 +10,33 @@ export default {
       this.dispatchEvent('page:init pageInit', event, event.detail);
       // title
       const title = this.$$(event.target).find('.navbar .title').text();
-      this.$view.$emit('view:title', { page: this, title });
+      this.onTitleChange(title, true);
     },
+    onPageReinit(event) {
+      this.dispatchEvent('page:reinit pageReinit', event, event.detail);
+      // title
+      const title = this.$$(event.target).find('.navbar .title').text();
+      this.onTitleChange(title, true);
+    },
+    onTitleChange(title, force) {
+      if (force || this.$$(this.$el).hasClass('page-current')) {
+        this.$view.$emit('view:title', { page: this, title });
+      }
+    },
+  },
+  mounted() {
+    const navbar = this.$$(this.$el).find('.navbar');
+    if (navbar.length > 0) {
+      this._watchTitle = navbar[0].__vue__.$watch('title', value => {
+        this.onTitleChange(value, false);
+      });
+    }
+  },
+  beforeDestroy() {
+    if (this._watchTitle) {
+      this._watchTitle();
+      this._watchTitle = null;
+    }
   },
 };
 
