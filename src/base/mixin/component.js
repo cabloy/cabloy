d5da45a1f624ab39d3a9f9c84bd3af222ff1ff62
zhennann <zhen.nann@icloud.com>
2018-08-08 18:46:40 +0800

    fix: $page

--- src/base/mixin/component.js
+++ src/base/mixin/component.js
@@ -9,9 +9,14 @@ export default function() {
     });
     Object.defineProperty(ctx, '$page', {
       get() {
-        const page = ctx.$$(ctx.$el).parents('.page');
-        if (page.length > 0 && page[0].__vue__) return page[0].__vue__;
-        return null;
+        const pageEl = ctx.$$(ctx.$el).parents('.page');
+        let page = pageEl.length > 0 && pageEl[0].__vue__;
+        if (!page) return null;
+        if (page.$options._componentTag !== 'eb-page') {
+          page = page.$children[0];
+          if (page.$options._componentTag !== 'eb-page') throw new Error('not found eb-page');
+        }
+        return page.$parent;
       },
     });
   };
