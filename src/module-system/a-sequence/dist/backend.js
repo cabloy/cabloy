fed38076ae947b9d60af6ca59727e3b6fd3b6cd7
zhennann <zhen.nann@icloud.com>
2018-08-09 18:51:21 +0800

    f7 3.0

--- src/module-system/a-sequence/dist/backend.js
+++ src/module-system/a-sequence/dist/backend.js
@@ -82,147 +82,83 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 17);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
+const services = __webpack_require__(1);
+const config = __webpack_require__(4);
+const locales = __webpack_require__(5);
+const errors = __webpack_require__(7);
+const middlewares = __webpack_require__(8);
 
-module.exports = (iterable, mapper, opts) => new Promise((resolve, reject) => {
-	opts = Object.assign({
-		concurrency: Infinity
-	}, opts);
+// eslint-disable-next-line
+module.exports = app => {
 
-	if (typeof mapper !== 'function') {
-		throw new TypeError('Mapper function is required');
-	}
+  // meta
+  const meta = __webpack_require__(11)(app);
+  const routes = __webpack_require__(12)(app);
 
-	const concurrency = opts.concurrency;
+  return {
+    routes,
+    services,
+    config,
+    locales,
+    errors,
+    middlewares,
+    meta,
+  };
 
-	if (!(typeof concurrency === 'number' && concurrency >= 1)) {
-		throw new TypeError(`Expected \`concurrency\` to be a number from 1 and up, got \`${concurrency}\` (${typeof concurrency})`);
-	}
-
-	const ret = [];
-	const iterator = iterable[Symbol.iterator]();
-	let isRejected = false;
-	let iterableDone = false;
-	let resolvingCount = 0;
-	let currentIdx = 0;
-
-	const next = () => {
-		if (isRejected) {
-			return;
-		}
-
-		const nextItem = iterator.next();
-		const i = currentIdx;
-		currentIdx++;
-
-		if (nextItem.done) {
-			iterableDone = true;
-
-			if (resolvingCount === 0) {
-				resolve(ret);
-			}
-
-			return;
-		}
-
-		resolvingCount++;
-
-		Promise.resolve(nextItem.value)
-			.then(el => mapper(el, i))
-			.then(
-				val => {
-					ret[i] = val;
-					resolvingCount--;
-					next();
-				},
-				err => {
-					isRejected = true;
-					reject(err);
-				}
-			);
-	};
-
-	for (let i = 0; i < concurrency; i++) {
-		next();
-
-		if (iterableDone) {
-			break;
-		}
-	}
-});
+};
 
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-module.exports = require("require3");
+const version = __webpack_require__(2);
+const sequence = __webpack_require__(3);
+module.exports = {
+  version,
+  sequence,
+};
+
 
 /***/ }),
 /* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
-const require3 = __webpack_require__(1);
-const assert = require3('assert');
+/***/ (function(module, exports) {
 
 module.exports = app => {
-  const pMap = __webpack_require__(0);
-  class TestController extends app.Controller {
 
-    async sequence() {
+  class Version extends app.Service {
 
-      // current
-      let current = await this.ctx.meta.sequence.current('test');
-      assert(current === 0);
-
-      // next
-      let next = await this.ctx.meta.sequence.next('test');
-      assert(next === 1);
-
-      // current
-      current = await this.ctx.meta.sequence.current('test');
-      assert(current === 1);
-
-      // reset
-      await this.ctx.meta.sequence.reset('test');
-
-      // other module's sequence
-      const moduleSequence = this.ctx.meta.sequence.module(this.ctx.module.info.relativeName);
-
-      // next
-      next = await moduleSequence.next('test');
-      assert(next === 1);
-
-      // current
-      current = await moduleSequence.current('test');
-      assert(current === 1);
-
-      // reset
-      await moduleSequence.reset('test');
-
-      // concurrency
-      const results = await pMap([ 1, 2, 3, 4, 5 ], async () => {
-        return await moduleSequence.next('test');
-      });
-      assert(results.join(',') === '1,2,3,4,5');
-
-      // reset
-      await moduleSequence.reset('test');
-
-      this.ctx.success();
+    async update(options) {
+      if (options.version === 1) {
+        // create table: aSequence
+        const sql = `
+          CREATE TABLE aSequence (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            module varchar(255) DEFAULT NULL,
+            name varchar(255) DEFAULT NULL,
+            value json DEFAULT NULL,
+            PRIMARY KEY (id)
+          )
+        `;
+        await this.ctx.model.query(sql);
+      }
     }
 
   }
-  return TestController;
-};
 
+  return Version;
+};
 
 
 /***/ }),
@@ -231,16 +167,17 @@ module.exports = app => {
 
 module.exports = app => {
 
-  class SequenceController extends app.Controller {
+  class Sequence extends app.Service {
 
-    async next() {
-      const res = await this.ctx.service.sequence.next(this.ctx.request.body);
-      this.ctx.success(res);
+    // next
+    async next({ module, name }) {
+      const res = await this.ctx.meta.sequence.module(module)._next(name);
+      return res;
     }
 
   }
 
-  return SequenceController;
+  return Sequence;
 };
 
 
@@ -248,16 +185,26 @@ module.exports = app => {
 /* 4 */
 /***/ (function(module, exports) {
 
-module.exports = app => {
-  class VersionController extends app.Controller {
+// eslint-disable-next-line
+module.exports = appInfo => {
+  const config = {};
 
-    async update() {
-      await this.service.version.update(this.ctx.request.body);
-      this.ctx.success();
-    }
+  // middlewares
+  config.middlewares = {
+    sequence: {
+      global: true,
+      dependencies: 'instance',
+    },
+  };
 
-  }
-  return VersionController;
+  // queues
+  config.queues = {
+    sequence: {
+      path: 'sequence/next',
+    },
+  };
+
+  return config;
 };
 
 
@@ -265,21 +212,8 @@ module.exports = app => {
 /* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const version = __webpack_require__(4);
-const sequence = __webpack_require__(3);
-const test = __webpack_require__(2);
-
-module.exports = app => {
-  let routes = [
-    { method: 'post', path: 'version/update', controller: version, middlewares: 'inner' },
-    { method: 'post', path: 'sequence/next', controller: sequence, middlewares: 'inner' },
-  ];
-  if (app.meta.isTest || app.meta.isLocal) {
-    routes = routes.concat([
-      { method: 'get', path: 'test/sequence', controller: test, middlewares: 'test' },
-    ]);
-  }
-  return routes;
+module.exports = {
+  'zh-cn': __webpack_require__(6),
 };
 
 
@@ -287,29 +221,57 @@ module.exports = app => {
 /* 6 */
 /***/ (function(module, exports) {
 
-module.exports = app => {
-  const meta = {};
-  if (app.meta.isTest || app.meta.isLocal) {
-    // meta
-    Object.assign(meta, {
-      sequence: {
-        providers: {
-          test: {
-            start: 0,
-            expression({ ctx, value }) {
-              return ++value;
-            },
-          },
-        },
-      },
-    });
-  }
-  return meta;
+module.exports = {
 };
 
 
 /***/ }),
 /* 7 */
+/***/ (function(module, exports) {
+
+// error code should start from 1001
+module.exports = {
+};
+
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const sequence = __webpack_require__(9);
+
+module.exports = {
+  sequence,
+};
+
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const SequenceFn = __webpack_require__(10);
+const SEQUENCE = Symbol('CTX#__SEQUENCE');
+
+module.exports = () => {
+  return async function sequence(ctx, next) {
+    ctx.meta = ctx.meta || {};
+    Object.defineProperty(ctx.meta, 'sequence', {
+      get() {
+        if (ctx.meta[SEQUENCE] === undefined) {
+          ctx.meta[SEQUENCE] = new (SequenceFn(ctx))();
+        }
+        return ctx.meta[SEQUENCE];
+      },
+    });
+
+    // next
+    await next();
+  };
+};
+
+
+/***/ }),
+/* 10 */
 /***/ (function(module, exports) {
 
 const Fn = module.exports = ctx => {
@@ -411,55 +373,27 @@ const Fn = module.exports = ctx => {
 
 
 /***/ }),
-/* 8 */
-/***/ (function(module, exports, __webpack_require__) {
-
-const SequenceFn = __webpack_require__(7);
-const SEQUENCE = Symbol('CTX#__SEQUENCE');
-
-module.exports = () => {
-  return async function sequence(ctx, next) {
-    ctx.meta = ctx.meta || {};
-    Object.defineProperty(ctx.meta, 'sequence', {
-      get() {
-        if (ctx.meta[SEQUENCE] === undefined) {
-          ctx.meta[SEQUENCE] = new (SequenceFn(ctx))();
-        }
-        return ctx.meta[SEQUENCE];
-      },
-    });
-
-    // next
-    await next();
-  };
-};
-
-
-/***/ }),
-/* 9 */
-/***/ (function(module, exports, __webpack_require__) {
-
-const sequence = __webpack_require__(8);
-
-module.exports = {
-  sequence,
-};
-
-
-/***/ }),
-/* 10 */
-/***/ (function(module, exports) {
-
-// error code should start from 1001
-module.exports = {
-};
-
-
-/***/ }),
 /* 11 */
 /***/ (function(module, exports) {
 
-module.exports = {
+module.exports = app => {
+  const meta = {};
+  if (app.meta.isTest || app.meta.isLocal) {
+    // meta
+    Object.assign(meta, {
+      sequence: {
+        providers: {
+          test: {
+            start: 0,
+            expression({ ctx, value }) {
+              return ++value;
+            },
+          },
+        },
+      },
+    });
+  }
+  return meta;
 };
 
 
@@ -467,8 +401,21 @@ module.exports = {
 /* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = {
-  'zh-cn': __webpack_require__(11),
+const version = __webpack_require__(13);
+const sequence = __webpack_require__(14);
+const test = __webpack_require__(15);
+
+module.exports = app => {
+  let routes = [
+    { method: 'post', path: 'version/update', controller: version, middlewares: 'inner' },
+    { method: 'post', path: 'sequence/next', controller: sequence, middlewares: 'inner' },
+  ];
+  if (app.meta.isTest || app.meta.isLocal) {
+    routes = routes.concat([
+      { method: 'get', path: 'test/sequence', controller: test, middlewares: 'test' },
+    ]);
+  }
+  return routes;
 };
 
 
@@ -476,26 +423,16 @@ module.exports = {
 /* 13 */
 /***/ (function(module, exports) {
 
-// eslint-disable-next-line
-module.exports = appInfo => {
-  const config = {};
+module.exports = app => {
+  class VersionController extends app.Controller {
 
-  // middlewares
-  config.middlewares = {
-    sequence: {
-      global: true,
-      dependencies: 'instance',
-    },
-  };
+    async update() {
+      await this.service.version.update(this.ctx.request.body);
+      this.ctx.success();
+    }
 
-  // queues
-  config.queues = {
-    sequence: {
-      path: 'sequence/next',
-    },
-  };
-
-  return config;
+  }
+  return VersionController;
 };
 
 
@@ -505,94 +442,157 @@ module.exports = appInfo => {
 
 module.exports = app => {
 
-  class Sequence extends app.Service {
+  class SequenceController extends app.Controller {
 
-    // next
-    async next({ module, name }) {
-      const res = await this.ctx.meta.sequence.module(module)._next(name);
-      return res;
+    async next() {
+      const res = await this.ctx.service.sequence.next(this.ctx.request.body);
+      this.ctx.success(res);
     }
 
   }
 
-  return Sequence;
+  return SequenceController;
 };
 
 
 /***/ }),
 /* 15 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+const require3 = __webpack_require__(16);
+const assert = require3('assert');
 
 module.exports = app => {
+  const pMap = __webpack_require__(17);
+  class TestController extends app.Controller {
 
-  class Version extends app.Service {
+    async sequence() {
 
-    async update(options) {
-      if (options.version === 1) {
-        // create table: aSequence
-        const sql = `
-          CREATE TABLE aSequence (
-            id int(11) NOT NULL AUTO_INCREMENT,
-            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            deleted int(11) DEFAULT '0',
-            iid int(11) DEFAULT '0',
-            module varchar(255) DEFAULT NULL,
-            name varchar(255) DEFAULT NULL,
-            value json DEFAULT NULL,
-            PRIMARY KEY (id)
-          )
-        `;
-        await this.ctx.model.query(sql);
-      }
+      // current
+      let current = await this.ctx.meta.sequence.current('test');
+      assert(current === 0);
+
+      // next
+      let next = await this.ctx.meta.sequence.next('test');
+      assert(next === 1);
+
+      // current
+      current = await this.ctx.meta.sequence.current('test');
+      assert(current === 1);
+
+      // reset
+      await this.ctx.meta.sequence.reset('test');
+
+      // other module's sequence
+      const moduleSequence = this.ctx.meta.sequence.module(this.ctx.module.info.relativeName);
+
+      // next
+      next = await moduleSequence.next('test');
+      assert(next === 1);
+
+      // current
+      current = await moduleSequence.current('test');
+      assert(current === 1);
+
+      // reset
+      await moduleSequence.reset('test');
+
+      // concurrency
+      const results = await pMap([ 1, 2, 3, 4, 5 ], async () => {
+        return await moduleSequence.next('test');
+      });
+      assert(results.join(',') === '1,2,3,4,5');
+
+      // reset
+      await moduleSequence.reset('test');
+
+      this.ctx.success();
     }
 
   }
-
-  return Version;
+  return TestController;
 };
+
 
 
 /***/ }),
 /* 16 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-const version = __webpack_require__(15);
-const sequence = __webpack_require__(14);
-module.exports = {
-  version,
-  sequence,
-};
-
+module.exports = require("require3");
 
 /***/ }),
 /* 17 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const services = __webpack_require__(16);
-const config = __webpack_require__(13);
-const locales = __webpack_require__(12);
-const errors = __webpack_require__(10);
-const middlewares = __webpack_require__(9);
+"use strict";
 
-// eslint-disable-next-line
-module.exports = app => {
+module.exports = (iterable, mapper, opts) => new Promise((resolve, reject) => {
+	opts = Object.assign({
+		concurrency: Infinity
+	}, opts);
 
-  // meta
-  const meta = __webpack_require__(6)(app);
-  const routes = __webpack_require__(5)(app);
+	if (typeof mapper !== 'function') {
+		throw new TypeError('Mapper function is required');
+	}
 
-  return {
-    routes,
-    services,
-    config,
-    locales,
-    errors,
-    middlewares,
-    meta,
-  };
+	const concurrency = opts.concurrency;
 
-};
+	if (!(typeof concurrency === 'number' && concurrency >= 1)) {
+		throw new TypeError(`Expected \`concurrency\` to be a number from 1 and up, got \`${concurrency}\` (${typeof concurrency})`);
+	}
+
+	const ret = [];
+	const iterator = iterable[Symbol.iterator]();
+	let isRejected = false;
+	let iterableDone = false;
+	let resolvingCount = 0;
+	let currentIdx = 0;
+
+	const next = () => {
+		if (isRejected) {
+			return;
+		}
+
+		const nextItem = iterator.next();
+		const i = currentIdx;
+		currentIdx++;
+
+		if (nextItem.done) {
+			iterableDone = true;
+
+			if (resolvingCount === 0) {
+				resolve(ret);
+			}
+
+			return;
+		}
+
+		resolvingCount++;
+
+		Promise.resolve(nextItem.value)
+			.then(el => mapper(el, i))
+			.then(
+				val => {
+					ret[i] = val;
+					resolvingCount--;
+					next();
+				},
+				err => {
+					isRejected = true;
+					reject(err);
+				}
+			);
+	};
+
+	for (let i = 0; i < concurrency; i++) {
+		next();
+
+		if (iterableDone) {
+			break;
+		}
+	}
+});
 
 
 /***/ })
