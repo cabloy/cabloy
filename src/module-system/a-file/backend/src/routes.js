13b597a8157c50e99b9eba3ae4be0ef5a913a210
zhennann <zhen.nann@icloud.com>
2018-08-17 17:46:13 +0800

    first commit

---
+++ src/module-system/a-file/backend/src/routes.js
@@ -0,0 +1,11 @@
+const version = require('./controller/version.js');
+
+module.exports = app => {
+  const routes = [
+    // version
+    { method: 'post', path: 'version/update', controller: version, middlewares: 'inner' },
+    { method: 'post', path: 'version/init', controller: version, middlewares: 'inner' },
+    { method: 'post', path: 'version/test', controller: version, middlewares: 'test' },
+  ];
+  return routes;
+};
