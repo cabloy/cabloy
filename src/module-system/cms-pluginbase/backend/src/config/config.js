137720e76ca79ce85a029b81a41f1234bd902148
zhennann <zhen.nann@icloud.com>
2018-08-22 22:49:56 +0800

    feat: config

--- src/module-system/cms-pluginbase/backend/src/config/config.js
+++ src/module-system/cms-pluginbase/backend/src/config/config.js
@@ -1,5 +1,15 @@
 // eslint-disable-next-line
 module.exports = appInfo => {
   const config = {};
+
+  // plugin
+  config.plugin = {
+    track: {
+      google: '',
+      baidu: '',
+    },
+    icp: '',
+  };
+
   return config;
 };
