281245c3d54d26ce9ec290d14d373ee8aa05d7c2
zhennann <zhen.nann@icloud.com>
2018-09-03 23:01:53 +0800

    feat: content

---
+++ src/module-system/a-cms/front/src/components/article/itemEdit.vue
@@ -0,0 +1,117 @@
+<template>
+  <f7-list>
+    <f7-list-group>
+      <f7-list-item group-title :title="$text('Title')"></f7-list-item>
+      <f7-list-item>
+        <f7-label floating>{{$text('Title')}}</f7-label>
+        <eb-input type="text" clear-button v-model="item.atomName" dataPath="atomName"></eb-input>
+      </f7-list-item>
+    </f7-list-group>
+    <f7-list-group>
+      <f7-list-item group-title :title="$text('Content')"></f7-list-item>
+      <eb-list-item-choose eb-href="#" dataPath="content" :title="$text('Content')" :onPerform="onPerformEditContent">
+      </eb-list-item-choose>
+    </f7-list-group>
+    <f7-list-group>
+      <f7-list-item group-title :title="$text('Basic Info')"></f7-list-item>
+      <f7-list-item smartSelect :title="$text('Language')" :smartSelectParams="{openIn: 'page', closeOnSelect: true}">
+        <eb-select name="language" v-model="item.language" dataPath="language" :options="languages"></eb-select>
+      </f7-list-item>
+      <eb-list-item-choose ref="category" eb-href="#" dataPath="categoryId" :title="$text('Category')" :onPerform="onPerformChooseCategory">
+        <div slot="after">{{item.categoryName}}</div>
+      </eb-list-item-choose>
+      <f7-list-item>
+        <span>{{$text('Sticky')}}</span>
+        <eb-toggle v-model="item.sticky" dataPath="sticky"></eb-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label floating>{{$text('Keywords')}}</f7-label>
+        <eb-input type="text" clear-button v-model="item.keywords" dataPath="keywords"></eb-input>
+      </f7-list-item>
+      <f7-list-item>
+        <f7-label floating>{{$text('Description')}}</f7-label>
+        <eb-input type="text" clear-button v-model="item.description" dataPath="description"></eb-input>
+      </f7-list-item>
+    </f7-list-group>
+  </f7-list>
+</template>
+<script>
+export default {
+  props: {
+    // mode: edit/view
+    mode: {
+      type: String,
+    },
+    item: {
+      type: Object,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    languages() {
+      let _languages = this.$local.getters('languages');
+      if (!_languages) return [];
+      _languages = _languages.map(item => {
+        return {
+          title: item,
+          value: item,
+        };
+      });
+      _languages.unshift({ title: '', value: '' });
+      return _languages;
+    },
+  },
+  created() {
+    this.$local.dispatch('getLanguages');
+  },
+  methods: {
+    onSave() {
+      this.$emit('save');
+    },
+    onPerformChooseCategory() {
+      if (!this.item.language) {
+        return this.$view.dialog.alert(this.$text('Please specify the language'));
+      }
+      this.$view.navigate('/a/cms/category/select', {
+        context: {
+          params: {
+            language: this.item.language,
+            categoryIdStart: 0,
+            leafOnly: true,
+          },
+          callback: (code, data) => {
+            if (code === 200) {
+              this.item.categoryId = data.id;
+              this.item.categoryName = data.categoryName;
+              this.$refs.category.clearValidateError();
+            }
+          },
+        },
+      });
+    },
+    onPerformEditContent() {
+      this.$view.navigate('/a/cms/article/contentEdit', {
+        context: {
+          params: {
+            ctx: this,
+            item: this.item,
+            mode: this.mode,
+          },
+          callback: (code, data) => {
+            if (code === 200) {
+              this.item.content = data.content;
+            }
+          },
+        },
+      });
+    },
+  },
+};
+
+</script>
+<style scoped>
+
+
+</style>
--- src/module-system/a-cms/front/src/pages/article/contentEdit.vue
+++ src/module-system/a-cms/front/src/pages/article/contentEdit.vue
@@ -1,13 +1,13 @@
 <template>
   <eb-page>
-    <eb-navbar :title="$text('Content')" eb-back-link="Back">
+    <eb-navbar :title="title" eb-back-link="Back">
       <f7-nav-right>
         <eb-link iconMaterial="save" :onPerform="onPerformSave"></eb-link>
         <eb-link iconMaterial="visibility" :onPerform="onPerformPreview"></eb-link>
       </f7-nav-right>
     </eb-navbar>
     <template v-if="module">
-      <mavon-editor ref="editor" v-model="item.content" />
+      <mavon-editor ref="editor" :value="item.content" @change="onChange" @save="onSave" @imgAdd="onImgAdd" :language="language" :subfield="subfield" :editable="editable" :defaultOpen="defaultOpen" :toolbarsFlag="toolbarsFlag" :navigation="navigation" :toolbars="toolbars" />
     </template>
   </eb-page>
 </template>
@@ -15,29 +15,86 @@
 import Vue from 'vue';
 const ebPageContext = Vue.prototype.$meta.module.get('a-components').options.components.ebPageContext;
 export default {
+  meta: {
+    size: 'large',
+  },
   mixins: [ ebPageContext ],
   data() {
     return {
+      dirty: false,
       module: null,
       _unwatch: null,
     };
   },
   computed: {
+    title() {
+      return `${this.dirty ? '* ' : ''}${this.item.atomName}`;
+    },
+    mode() {
+      return this.contextParams.mode;
+    },
     item() {
       return this.contextParams.item;
     },
+    language() {
+      const locale = this.$meta.util.cookies.get('locale') || 'en-us';
+      return locale === 'zh-cn' ? 'zh-CN' : 'en';
+    },
+    subfield() {
+      return this.editable && this.$view.size !== 'small';
+    },
+    editable() {
+      return this.mode === 'edit';
+    },
+    defaultOpen() {
+      return this.editable ? '' : 'preview';
+    },
+    toolbarsFlag() {
+      return this.editable;
+    },
+    navigation() {
+      return !this.editable;
+    },
+    toolbars() {
+      return {
+        bold: true, // 粗体
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: true, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        /* 1.3.5 */
+        undo: true, // 上一步
+        redo: true, // 下一步
+        trash: true, // 清空
+        /* 1.4.2 */
+        navigation: true, // 导航目录
+        /* 2.1.8 */
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+        /* 2.2.1 */
+        subfield: true, // 单双栏模式
+      };
+    },
   },
-  created() {
+  mounted() {
     this.$meta.module.use('a-mavoneditor', module => {
       this.module = module;
       this.onSize();
     });
-  },
-  mounted() {
     this._unwatch = this.$view.$watch('sizeExtent', () => {
       this.onSize();
     });
-    this.onSize();
   },
   beforeDestroy() {
     if (this._unwatch) {
@@ -51,24 +108,28 @@ export default {
         const size = this.$view.sizeExtent;
         if (size) {
           this.$$(this.$refs.editor.$el).css({
-            height: `${size.height - 56}px`,
+            height: `${size.height - 64}px`,
             width: `${size.width}px`,
           });
         }
       });
     },
-    onInput(event) {
-      this.content = event.target.value;
+    onChange(data) {
+      this.dirty = true;
+      this.contextCallback(200, { content: data });
+    },
+    onSave() {
+      this.onPerformSave();
     },
     onPerformSave() {
-      const data = JSON.parse(this.content);
-      return this.$api.post('site/setConfigSite', { data }).then(() => {
-        this.$local.commit('setConfigSite', data);
-        return true;
-      });
+      this.contextParams.ctx.onSave();
+      this.dirty = false;
     },
     onPerformPreview() {
 
+    },
+    onImgAdd(pos, file) {
+      console.log(pos, file);
     },
   },
 };
