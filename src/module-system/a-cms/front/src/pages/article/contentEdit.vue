0d50d60f460a78764f143af1076ae35ee7416c6c
zhennann <zhen.nann@icloud.com>
2018-09-10 16:57:00 +0800

    feat: contentEdit

--- src/module-system/a-cms/front/src/pages/article/contentEdit.vue
+++ src/module-system/a-cms/front/src/pages/article/contentEdit.vue
@@ -8,7 +8,7 @@
     </eb-navbar>
     <template v-if="module">
       <eb-box>
-        <eb-mde ref="editor" :value="item.content" @change="onChange" @save="onSave" @imgAdd="onImgAdd" :editable="editable" />
+        <mavon-editor ref="editor" :value="item.content" @change="onChange" @save="onSave" :onImageUpload="onImageUpload" :language="language" :subfield="subfield" :editable="editable" :defaultOpen="defaultOpen" :toolbarsFlag="toolbarsFlag" :navigation="navigation" :toolbars="toolbars" />
       </eb-box>
     </template>
   </eb-page>
@@ -41,16 +41,63 @@ export default {
     editable() {
       return !this.readOnly;
     },
+    language() {
+      const locale = this.$meta.util.cookies.get('locale') || 'en-us';
+      return locale === 'zh-cn' ? 'zh-CN' : 'en';
+    },
+    subfield() {
+      return this.editable && this.$view.size !== 'small';
+    },
+    defaultOpen() {
+      return this.editable ? '' : 'preview';
+    },
+    toolbarsFlag() {
+      return this.editable;
+    },
+    navigation() {
+      return !this.editable;
+    },
+    toolbars() {
+      return {
+        bold: true, // 粗体
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: true, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        /* 1.3.5 */
+        undo: true, // 上一步
+        redo: true, // 下一步
+        trash: true, // 清空
+        /* 1.4.2 */
+        navigation: true, // 导航目录
+        /* 2.1.8 */
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+        /* 2.2.1 */
+        subfield: true, // 单双栏模式
+      };
+    },
   },
   mounted() {
-    this.$meta.module.use('a-mde', module => {
+    this.$meta.module.use('a-mavoneditor', module => {
       this.module = module;
     });
   },
   methods: {
     onChange(data) {
-      console.log(data);
       if (this.readOnly) return;
+      if (this.item.content === data) return;
       this.dirty = true;
       this.contextCallback(200, { content: data });
     },
@@ -66,8 +113,26 @@ export default {
     onPerformPreview() {
 
     },
-    onImgAdd(pos, file) {
-      console.log(pos, file);
+    onImageUpload() {
+      return new Promise((resolve, reject) => {
+        this.$view.navigate('/a/file/file/upload', {
+          context: {
+            params: {
+              mode: 1,
+              atomId: this.item.atomId,
+            },
+            callback: (code, data) => {
+              if (code === 200) {
+                resolve({ text: data.realName, addr: data.downloadUrl });
+              }
+              if (code === false) {
+                reject();
+              }
+            },
+          },
+        });
+      });
+
     },
   },
 };
