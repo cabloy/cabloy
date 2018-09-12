05a5362c9267b5ff0f6f72cbb6b62fba4f808bb8
zhennann <zhen.nann@icloud.com>
2018-09-12 23:53:21 +0800

    feat: util

---
+++ src/module-system/cms-pluginbase/backend/cms/plugin/util.js
@@ -0,0 +1,21 @@
+const util = {
+  parseUrlQuery(url) {
+    const query = {};
+    let urlToParse = url || window.location.href;
+    let i;
+    let params;
+    let param;
+    let length;
+    if (typeof urlToParse === 'string' && urlToParse.length) {
+      urlToParse = urlToParse.indexOf('?') > -1 ? urlToParse.replace(/\S*\?/, '') : '';
+      params = urlToParse.split('&');
+      length = params.length;
+
+      for (i = 0; i < length; i += 1) {
+        param = params[i].replace(/#\S+/g, '').split('=');
+        query[decodeURIComponent(param[0])] = typeof param[1] === 'undefined' ? undefined : decodeURIComponent(param[1]) || '';
+      }
+    }
+    return query;
+  },
+};
