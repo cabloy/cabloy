2ad3454a5750d27ec581356d63d521ea06114d28
zhennann <zhen.nann@icloud.com>
2018-09-13 22:50:48 +0800

    feat: layout

--- src/module-system/cms-themeblog/backend/cms/theme/assets/js/site.js
+++ src/module-system/cms-themeblog/backend/cms/theme/assets/js/site.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   // menu active
   const query = util.parseUrlQuery();
   if (query.categoryId) {
-    const li = $(`.category-${query.categoryId}`);
-    li.addClass('active');
-    li.parents('li').addClass('active');
+    const link = $(`.category-${query.categoryId}`);
+    link.parents('li').addClass('active');
   }
 });
 
