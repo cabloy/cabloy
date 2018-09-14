a2d810c775fae47cd869f74a756938ccc7a0bd11
zhennann <zhen.nann@icloud.com>
2018-09-14 21:38:02 +0800

    feat: ajax

--- src/module-system/cms-themeblog/backend/cms/theme/assets/js/site.js
+++ src/module-system/cms-themeblog/backend/cms/theme/assets/js/site.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
+
   // menu active
   const query = util.parseUrlQuery();
   if (query.categoryId) {
     const link = $(`.category-${query.categoryId}`);
     link.parents('li').addClass('active');
+  }
+
+  // load more
+  if (env.path === 'main/index/index') {
+    util.loadMore({
+      container: '.article-list',
+      index: 0,
+      onFetch({ index }) {
+        return util.ajax({
+          url: 'a/cms/jsonp/articleList',
+          body: { index },
+        });
+      },
+      onParse(item) {
+        return `<div style="height:150px;">${item}</div>`;
+      },
+    });
   }
 });
 
