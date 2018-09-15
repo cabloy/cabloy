d7f8e3f28c7e88aef392dff5d2ba09c62765813f
zhennann <zhen.nann@icloud.com>
2018-09-15 18:18:50 +0800

    feat: article list

--- src/module-system/cms-themeblog/backend/cms/theme/assets/js/site.js
+++ src/module-system/cms-themeblog/backend/cms/theme/assets/js/site.js
@@ -1,27 +1,78 @@
 $(document).ready(function() {
 
-  // menu active
+  // query
   const query = util.parseUrlQuery();
+
+  // menu active
   if (query.categoryId) {
     const link = $(`.category-${query.categoryId}`);
     link.parents('li').addClass('active');
   }
 
   // load more
-  if (env.path === 'main/index/index') {
-    util.loadMore({
-      container: '.article-list',
-      index: 0,
-      onFetch({ index }) {
-        return util.ajax({
-          url: 'a/cms/jsonp/articleList',
-          body: { index },
-        });
-      },
-      onParse(item) {
-        return `<div style="height:150px;">${item}</div>`;
-      },
-    });
+  if (env.site.path === 'main/index/index') {
+    loadMore({});
+  } else if (env.site.path === 'static/category') {
+    loadMore({
+      categoryId: query.categoryId,
+      search: query.search });
   }
+
+  // search text
+  if (query.search) {
+    $('form.search input').val(query.search);
+  }
+
 });
 
+function loadMore({ categoryId, search }) {
+  util.loadMore({
+    container: '.article-list',
+    index: (env.index && env.index[env.site.path]) || 0,
+    onFetch({ index }) {
+      // options
+      const options = {
+        where: {
+          'f.language': env.language.current,
+        },
+        orders: [
+          [ 'a.createdAt', 'desc' ],
+        ],
+        page: { index },
+        mode: 'list',
+      };
+      // categoryId
+      if (categoryId) {
+        options.where['f.categoryId'] = categoryId;
+      }
+      // search
+      if (search) {
+        options.where['f.content'] = { val: search, op: 'like' };
+        options.mode = 'search';
+      }
+      // select
+      return util.ajax({
+        url: '/a/cms/jsonp/articleList',
+        body: { options: encodeURIComponent(JSON.stringify(options)) },
+      });
+    },
+    onParse(item) {
+      const media = !item.imageFirst ? '' : `
+<div class="media-right">
+      <a target="_blank" href="${env.site.rootUrl}/${item.url}">
+        <img class="media-object" src="${util.combineImageUrl(item.imageFirst, 125, 100)}">
+      </a>
+</div>
+        `;
+      return `
+<li class="media">
+    <div class="media-body">
+      <h4 class="media-heading"><a target="_blank" href="${env.site.rootUrl}/${item.url}">${item.atomName}</a></h4>
+      ${item.description || item.summary}
+    </div>
+    ${media}
+</li>
+        `;
+    },
+  });
+}
