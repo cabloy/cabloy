fae9a53c45192ef16dfa40d9b5415b20da802cc1
zhennann <zhen.nann@icloud.com>
2018-09-25 22:22:52 +0800

    feat: comments

--- src/module-system/a-base-sync/backend/src/controller/comment.js
+++ src/module-system/a-base-sync/backend/src/controller/comment.js
@@ -13,6 +13,18 @@ module.exports = app => {
       this.ctx.successMore(items, options.page.index, options.page.size);
     }
 
+    async listP() {
+      // data
+      const data = JSON.parse(this.ctx.request.query.data);
+      // select
+      const res = await this.ctx.performAction({
+        method: 'post',
+        url: 'comment/list',
+        body: data,
+      });
+      this.ctx.success(res);
+    }
+
     async item() {
       const res = await this.ctx.service.comment.item({
         key: this.ctx.request.body.key,
@@ -40,11 +52,35 @@ module.exports = app => {
       this.ctx.success(res);
     }
 
+    async deleteP() {
+      // data
+      const data = JSON.parse(this.ctx.request.query.data);
+      // delete
+      const res = await this.ctx.performAction({
+        method: 'post',
+        url: 'comment/delete',
+        body: data,
+      });
+      this.ctx.success(res);
+    }
+
     async heart() {
       const res = await this.ctx.service.comment.heart({
         key: this.ctx.request.body.key,
         data: this.ctx.request.body.data,
         user: this.ctx.user.op,
+      });
+      this.ctx.success(res);
+    }
+
+    async heartP() {
+      // data
+      const data = JSON.parse(this.ctx.request.query.data);
+      // heart
+      const res = await this.ctx.performAction({
+        method: 'post',
+        url: 'comment/heart',
+        body: data,
       });
       this.ctx.success(res);
     }
