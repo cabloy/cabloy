a5729b3b83ab1b3fd99c3c3949957587ad284ec1
zhennann <zhen.nann@icloud.com>
2018-09-29 17:34:06 +0800

    feat: limit

--- packages/egg-born-template-module/boilerplate/build/front/webpack.base.conf.js
+++ packages/egg-born-template-module/boilerplate/build/front/webpack.base.conf.js
@@ -50,7 +50,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1000,
           name: utils.assetsPath('img/[name].[hash].[ext]'),
         },
       },
@@ -58,7 +58,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1000,
           name: utils.assetsPath('fonts/[name].[hash].[ext]'),
         },
       },
