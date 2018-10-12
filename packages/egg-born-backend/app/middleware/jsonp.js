2898ad01a993da135fb279ae89b0b8b653b0dea6
zhennann <zhen.nann@icloud.com>
2018-10-12 20:42:10 +0800

    feat: jsonp

--- packages/egg-born-backend/app/middleware/jsonp.js
+++ packages/egg-born-backend/app/middleware/jsonp.js
@@ -1,5 +1,7 @@
 module.exports = options => {
   return async function jsonp(ctx, next) {
+    options = options || {};
+    options.whiteList = ctx.instance.meta && ctx.instance.meta.jsonp && ctx.instance.meta.jsonp.whiteList;
     const fn = ctx.app.jsonp(options);
     await fn(ctx, next);
   };
