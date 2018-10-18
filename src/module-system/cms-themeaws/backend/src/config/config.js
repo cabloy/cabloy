b28fb40185bdb8d7c249df52a28bd17c38f2ef3d
zhennann <zhen.nann@icloud.com>
2018-10-18 16:28:12 +0800

    feat: extend cms-themeblog

--- src/module-system/cms-themeaws/backend/src/config/config.js
+++ src/module-system/cms-themeaws/backend/src/config/config.js
@@ -1,5 +1,14 @@
 // eslint-disable-next-line
 module.exports = appInfo => {
   const config = {};
+
+  // theme
+  config.theme = {
+    _theme: {
+      name: 'cms-themeaws',
+      url: 'https://github.com/zhennann/egg-born-module-cms-themeaws',
+    },
+  };
+
   return config;
 };
