e1679362c8d0edf67eb6589ea7d126f32e3cd2e4
zhennann <zhen.nann@icloud.com>
2018-10-23 11:17:45 +0800

    feat: atomClassNameCapitalize

--- packages/egg-born-template-module-business/index.js
+++ packages/egg-born-template-module-business/index.js
@@ -14,10 +14,16 @@ module.exports = {
       return vars.name.split('-')[0];
     },
   },
-  atomClassName:{
+  atomClassName: {
     desc: 'atomClassName',
     default(vars) {
       return vars.name.split('-')[1];
     },
-  }
+  },
+  atomClassNameCapitalize: {
+    desc: 'atomClassNameCapitalize',
+    default(vars) {
+      return vars.atomClassName.replace(/^\S/,function(s){return s.toUpperCase();});
+    },
+  },
 };
