992884b679d28ddea23115d19dc238aefa5a76f6
zhennann <zhen.nann@icloud.com>
2018-10-23 16:56:25 +0800

    feat: jsonp for test

--- packages/egg-born-backend/app/middleware/jsonp.js
+++ packages/egg-born-backend/app/middleware/jsonp.js
@@ -1,16 +1,23 @@
 module.exports = options => {
   return async function jsonp(ctx, next) {
+    // options
     options = options || {};
-    options.whiteList = ctx.instance.meta && ctx.instance.meta.jsonp && ctx.instance.meta.jsonp.whiteList;
-    const hostSelf = ctx.host.split(':')[0];
-    if (options.whiteList) {
-      if (!Array.isArray(options.whiteList)) {
-        options.whiteList = [ options.whiteList ];
-      }
-      options.whiteList.push(hostSelf);
+    // whiteList
+    if (ctx.app.meta.isTest) {
+      options.whiteList = false;
     } else {
-      options.whiteList = [ hostSelf ];
+      options.whiteList = ctx.instance.meta && ctx.instance.meta.jsonp && ctx.instance.meta.jsonp.whiteList;
+      const hostSelf = ctx.host.split(':')[0];
+      if (options.whiteList) {
+        if (!Array.isArray(options.whiteList)) {
+          options.whiteList = [ options.whiteList ];
+        }
+        options.whiteList.push(hostSelf);
+      } else {
+        options.whiteList = [ hostSelf ];
+      }
     }
+    // jsonp
     const fn = ctx.app.jsonp(options);
     await fn(ctx, next);
   };
