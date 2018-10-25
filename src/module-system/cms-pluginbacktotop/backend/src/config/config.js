70499859048005a2c84af8286f4c59fb735b863f
zhennann <zhen.nann@icloud.com>
2018-10-25 18:49:21 +0800

    first commit

---
+++ src/module-system/cms-pluginbacktotop/backend/src/config/config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+module.exports = appInfo => {
+  const config = {};
+
+  // plugin
+  config.plugin = {
+    animateSpeed: 500,
+  };
+
+  return config;
+};
---
+++ src/module-system/cms-pluginbacktotop/build/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  productionSourceMap: true,
+  uglify: false,
+};
