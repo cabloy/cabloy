a645ac7400bad1ca944edcc97cec4ec1cb7f0da7
zhennann <zhen.nann@icloud.com>
2018-10-25 22:53:41 +0800

    feat: socialshare

--- src/module-system/cms-pluginsocialshare/dist/backend.js
+++ src/module-system/cms-pluginsocialshare/dist/backend.js
@@ -129,7 +129,12 @@ module.exports = appInfo => {
 
   // plugin
   config.plugin = {
-    animateSpeed: 500,
+    wechatQrcodeTitle: '微信扫一扫：分享',
+    wechatQrcodeHelper: '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>',
+    wechatQrcodeSize: 100,
+    sites: 'wechat,weibo,qq,douban,linkedin,facebook,twitter,google',
+    mobileSites: '',
+    disabled: '',
   };
 
   return config;
