9f10bc97b3c333d018402169cd08bc1a2b582cdc
zhennann <zhen.nann@icloud.com>
2018-10-28 17:24:27 +0800

    first commit

---
+++ src/module-system/cms-pluginrss/backend/src/config/config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+module.exports = appInfo => {
+  const config = {};
+
+  // plugin
+  config.plugin = {
+    track: {
+      google: '',
+      baidu: '',
+      qq: '',
+    },
+  };
+
+  return config;
+};
