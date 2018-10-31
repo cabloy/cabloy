8a2f82c3e85ef451c1e5fe44ff2e5adf4a97fe62
zhennann <zhen.nann@icloud.com>
2018-10-31 16:49:02 +0800

    feat: header

--- src/module-system/cms-themedocs/backend/src/config/config.js
+++ src/module-system/cms-themedocs/backend/src/config/config.js
@@ -1,5 +1,14 @@
 // eslint-disable-next-line
 module.exports = appInfo => {
   const config = {};
+
+  // theme
+  config.theme = {
+    _theme: {
+      name: 'cms-themedocs',
+      url: 'https://github.com/zhennann/egg-born-module-cms-themedocs',
+    },
+  };
+
   return config;
 };
