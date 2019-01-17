7605781acfebec4f4d1d0397068af13303fc7b64
zhennann <zhen.nann@icloud.com>
2019-01-17 16:29:56 +0800

    fix: meta.auth.enable=false

--- packages/egg-born-template-module-business/boilerplate/backend/src/routes.js
+++ packages/egg-born-template-module-business/boilerplate/backend/src/routes.js
@@ -8,13 +8,13 @@ module.exports = app => {
     { method: 'post', path: 'version/init', controller: version, middlewares: 'inner' },
     { method: 'post', path: 'version/test', controller: version, middlewares: 'test' },
     // {{atomClassName}}
-    { method: 'post', path: '{{atomClassName}}/create', controller: {{atomClassName}}, middlewares: 'inner' },
-    { method: 'post', path: '{{atomClassName}}/read', controller: {{atomClassName}}, middlewares: 'inner' },
-    { method: 'post', path: '{{atomClassName}}/select', controller: {{atomClassName}}, middlewares: 'inner' },
-    { method: 'post', path: '{{atomClassName}}/write', controller: {{atomClassName}}, middlewares: 'inner' },
-    { method: 'post', path: '{{atomClassName}}/delete', controller: {{atomClassName}}, middlewares: 'inner' },
-    { method: 'post', path: '{{atomClassName}}/action', controller: {{atomClassName}}, middlewares: 'inner' },
-    { method: 'post', path: '{{atomClassName}}/enable', controller: {{atomClassName}}, middlewares: 'inner' },
+    { method: 'post', path: '{{atomClassName}}/create', controller: {{atomClassName}}, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: '{{atomClassName}}/read', controller: {{atomClassName}}, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: '{{atomClassName}}/select', controller: {{atomClassName}}, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: '{{atomClassName}}/write', controller: {{atomClassName}}, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: '{{atomClassName}}/delete', controller: {{atomClassName}}, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: '{{atomClassName}}/action', controller: {{atomClassName}}, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: '{{atomClassName}}/enable', controller: {{atomClassName}}, middlewares: 'inner', meta: { auth: { enable: false } } },
   ];
   return routes;
 };
