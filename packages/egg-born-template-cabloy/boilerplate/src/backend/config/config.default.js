d8217a5b689576d5b5f2fa4f99147b9f55509d31
zhennann <zhen.nann@icloud.com>
2019-03-01 16:15:03 +0800

    refactor: i18n

--- packages/egg-born-template-cabloy/boilerplate/src/backend/config/config.default.js
+++ packages/egg-born-template-cabloy/boilerplate/src/backend/config/config.default.js
@@ -4,5 +4,10 @@ module.exports = appInfo => {
   // keys
   config.keys = appInfo.name + '_{{keys}}';
 
+  // i18n
+  // config.i18n = {
+  //   defaultLocale: 'zh-cn',
+  // };
+
   return config;
 };
