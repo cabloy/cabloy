d623a3f800dba64f1aee73fa6ec03e07d1c6fe9d
zhennann <zhen.nann@icloud.com>
2019-03-30 14:25:39 +0800

    feat: cms:site

--- src/module-system/a-validation/dist/backend.js
+++ src/module-system/a-validation/dist/backend.js
@@ -325,11 +325,11 @@ const Fn = module.exports = ctx => {
 module.exports = (options, app) => {
   return async function validate(ctx, next) {
     // ignore
-    const validator = options.validator || (ctx.request.body.validate && ctx.request.body.validate.validator);
+    const validator = options.validator || (ctx.meta._validator && ctx.meta._validator.validator);
     if (!validator) return await next();
     // params
-    const module = options.module || (ctx.request.body.validate && ctx.request.body.validate.module) || ctx.module.info.relativeName;
-    const schema = options.schema || (ctx.request.body.validate && ctx.request.body.validate.schema);
+    const module = options.module || (ctx.meta._validator && ctx.meta._validator.module) || ctx.module.info.relativeName;
+    const schema = options.schema || (ctx.meta._validator && ctx.meta._validator.schema);
     const data = ctx.request.body[options.data || 'data'];
     // if error throw 422
     await ctx.meta.validation.validate({
