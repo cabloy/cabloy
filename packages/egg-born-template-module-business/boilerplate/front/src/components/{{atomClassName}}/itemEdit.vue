7a4924bd61922d3016afa2d2e8676c68ad56cf8e
zhennann <zhen.nann@icloud.com>
2019-04-28 23:36:58 +0800

    feat: form.onsubmit

--- packages/egg-born-template-module-business/boilerplate/front/src/components/{{atomClassName}}/itemEdit.vue
+++ packages/egg-born-template-module-business/boilerplate/front/src/components/{{atomClassName}}/itemEdit.vue
@@ -1,8 +1,8 @@
 <template>
-  <f7-list>
+  <eb-list form no-hairlines-md @submit="onSubmit">
     <eb-list-item-validate dataKey="atomName"></eb-list-item-validate>
     <eb-list-item-validate dataKey="description"></eb-list-item-validate>
-  </f7-list>
+  </eb-list>
 </template>
 <script>
 export default {
@@ -17,7 +17,11 @@ export default {
       type: Function,
     },
   },
-  methods: {},
+  methods: {
+    onSubmit(event) {
+      this.$emit('submit', event);
+    },
+  },
 };
 
 </script>
