22d35db1d280d2ef158759ce9356eac416f16929
zhennann <zhen.nann@icloud.com>
2019-05-08 17:56:27 +0800

    feat: done

--- packages/markdown-it-block/index.js
+++ packages/markdown-it-block/index.js
@@ -3,67 +3,47 @@
 'use strict';
 
 
-module.exports = function container_plugin(md, name, options) {
+module.exports = function cabloycmstag_plugin(md, options) {
 
-  function validateDefault(params) {
-    return params.trim().split(' ', 2)[0] === name;
-  }
-
-  function renderDefault(tokens, idx, _options, env, slf) {
-
-    // add a class to the opening tag
-    if (tokens[idx].nesting === 1) {
-      tokens[idx].attrJoin('class', name);
+  function render(tokens, idx /*_options, env, self*/) {
+    // tag
+    var token = tokens[idx];
+    var tagName = token.info.trim().split(' ', 2)[0];
+    var tag = options.tags[tagName];
+    // content: for safe
+    var content = token.content ? JSON.parse(token.content) : {};
+    if (!tag || !tag.render){
+      // default
+      var res = JSON.stringify(content, null, 2);
+      return `<div>\n${md.utils.escapeHtml(res)}\n</div>\n`;
     }
-
-    return slf.renderToken(tokens, idx, _options, env, slf);
+    return tag.render({ md, token, content, tag });
   }
 
-  options = options || {};
-
-  var min_markers = 3,
-      marker_str  = options.marker || ':',
-      marker_char = marker_str.charCodeAt(0),
-      marker_len  = marker_str.length,
-      validate    = options.validate || validateDefault,
-      render      = options.render || renderDefault;
-
-  function container(state, startLine, endLine, silent) {
-    var pos, nextLine, marker_count, markup, params, token,
-        old_parent, old_line_max,
-        auto_closed = false,
-        start = state.bMarks[startLine] + state.tShift[startLine],
+  function cabloycmstag(state, startLine, endLine, silent) {
+    var marker, len, params, nextLine, mem, token, markup,
+        haveEndMarker = false,
+        pos = state.bMarks[startLine] + state.tShift[startLine],
         max = state.eMarks[startLine];
-
-    // Check out the first character quickly,
-    // this should filter out most of non-containers
-    //
-    if (marker_char !== state.src.charCodeAt(start)) { return false; }
-
-    // Check out the rest of the marker string
-    //
-    for (pos = start + 1; pos <= max; pos++) {
-      if (marker_str[(pos - start) % marker_len] !== state.src[pos]) {
-        break;
-      }
+    // if it's indented more than 3 spaces, it should be a code block
+    if (state.sCount[startLine] - state.blkIndent >= 4) { return false; }
+    if (pos + 3 > max) { return false; }
+    marker = state.src.charCodeAt(pos);
+    if (marker !== 0x24/* $ */) {
+      return false;
     }
-
-    marker_count = Math.floor((pos - start) / marker_len);
-    if (marker_count < min_markers) { return false; }
-    pos -= (pos - start) % marker_len;
-
-    markup = state.src.slice(start, pos);
+    // scan marker length
+    mem = pos;
+    pos = state.skipChars(pos, marker);
+    len = pos - mem;
+    if (len < 3) { return false; }
+    markup = state.src.slice(mem, pos);
     params = state.src.slice(pos, max);
-    if (!validate(params)) { return false; }
-
+    if (params.indexOf(String.fromCharCode(marker)) >= 0) { return false; }
     // Since start is found, we can report success here in validation mode
-    //
     if (silent) { return true; }
-
-    // Search for the end of the block
-    //
+    // search end of block
     nextLine = startLine;
-
     for (;;) {
       nextLine++;
       if (nextLine >= endLine) {
@@ -71,73 +51,42 @@ module.exports = function container_plugin(md, name, options) {
         // also block seems to be autoclosed by end of parent
         break;
       }
-
-      start = state.bMarks[nextLine] + state.tShift[nextLine];
+      pos = mem = state.bMarks[nextLine] + state.tShift[nextLine];
       max = state.eMarks[nextLine];
-
-      if (start < max && state.sCount[nextLine] < state.blkIndent) {
+      if (pos < max && state.sCount[nextLine] < state.blkIndent) {
         // non-empty line with negative indent should stop the list:
         // - ```
         //  test
         break;
       }
-
-      if (marker_char !== state.src.charCodeAt(start)) { continue; }
-
+      if (state.src.charCodeAt(pos) !== marker) { continue; }
       if (state.sCount[nextLine] - state.blkIndent >= 4) {
         // closing fence should be indented less than 4 spaces
         continue;
       }
-
-      for (pos = start + 1; pos <= max; pos++) {
-        if (marker_str[(pos - start) % marker_len] !== state.src[pos]) {
-          break;
-        }
-      }
-
+      pos = state.skipChars(pos, marker);
       // closing code fence must be at least as long as the opening one
-      if (Math.floor((pos - start) / marker_len) < marker_count) { continue; }
-
+      if (pos - mem < len) { continue; }
       // make sure tail has spaces only
-      pos -= (pos - start) % marker_len;
       pos = state.skipSpaces(pos);
-
       if (pos < max) { continue; }
-
+      haveEndMarker = true;
       // found!
-      auto_closed = true;
       break;
     }
-
-    old_parent = state.parentType;
-    old_line_max = state.lineMax;
-    state.parentType = 'container';
-
-    // this will prevent lazy continuations from ever going past our end marker
-    state.lineMax = nextLine;
-
-    token        = state.push('container_' + name + '_open', 'div', 1);
-    token.markup = markup;
-    token.block  = true;
-    token.info   = params;
-    token.map    = [ startLine, nextLine ];
-
-    state.md.block.tokenize(state, startLine + 1, nextLine);
-
-    token        = state.push('container_' + name + '_close', 'div', -1);
-    token.markup = state.src.slice(start, pos);
-    token.block  = true;
-
-    state.parentType = old_parent;
-    state.lineMax = old_line_max;
-    state.line = nextLine + (auto_closed ? 1 : 0);
-
+    // If a fence has heading spaces, they should be removed from its inner block
+    len = state.sCount[startLine];
+    state.line = nextLine + (haveEndMarker ? 1 : 0);
+    token         = state.push('cabloycmstag', 'div', 0);
+    token.info    = params;
+    token.content = state.getLines(startLine + 1, nextLine, len, true);
+    token.markup  = markup;
+    token.map     = [ startLine, state.line ];
     return true;
   }
 
-  md.block.ruler.before('fence', 'container_' + name, container, {
+  md.block.ruler.before('fence', 'cabloycmstag', cabloycmstag, {
     alt: [ 'paragraph', 'reference', 'blockquote', 'list' ]
   });
-  md.renderer.rules['container_' + name + '_open'] = render;
-  md.renderer.rules['container_' + name + '_close'] = render;
+  md.renderer.rules.cabloycmstag = render;
 };
--- packages/markdown-it-block/test/default.js
+++ packages/markdown-it-block/test/default.js
@@ -6,9 +6,20 @@ var generate = require('markdown-it-testgen');
 
 /*eslint-env mocha*/
 
+var options = {
+  tags:{
+    iframe:{
+      render:function ({ md, token, content, tag }){
+        var src = md.utils.escapeHtml(content.src);
+        return `<div><iframe src="${src}"></iframe></div>\n`;
+      }
+    }
+  }
+};
+
 describe('default container', function () {
   var md = require('markdown-it')()
-              .use(require('../'), 'name');
+              .use(require('../'), options);
 
   generate(path.join(__dirname, 'fixtures/default.txt'), md);
 });
