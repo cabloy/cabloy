7ca12c45db1ee3bf5be4b329e7901144d4313d8c
zhennann <zhen.nann@icloud.com>
2019-05-16 15:52:05 +0800

    feat: update to framework7-v4

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Framework7 from '@zhennann/framework7/packages/core/framework7.esm.bundle.js';
 import Framework7Vue from '@zhennann/framework7/packages/vue/framework7-vue.esm.bundle.js';
 
-import '@zhennann/framework7/packages/core/css/framework7.md.css';
+import '@zhennann/framework7/packages/core/css/framework7.bundle.css';
 import './assets/css/iconfont/material-icons.css';
 
 // use
