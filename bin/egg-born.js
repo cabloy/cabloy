e9850732a7871e8e63581fed87470521af26e3a7
zhennann <zhen.nann@icloud.com>
2019-07-08 18:30:43 +0800

    feat: move module:test-cook to project

--- bin/egg-born.js
+++ bin/egg-born.js
@@ -3,9 +3,7 @@
 const co = require('co');
 const Command = require('egg-init');
 const path = require('path');
-const glob = require('glob');
 const fse = require('fs-extra');
-const isTextOrBinary = require('istextorbinary');
 
 co(function* () {
 
@@ -43,27 +41,27 @@ co(function* () {
     return locals;
   };
 
-  // 'mem-fs-editor' may cause a problem on windows
+  const processFiles = command.processFiles;
   command.processFiles = function* (targetDir, templateDir) {
-    const src = path.join(templateDir, 'boilerplate');
-    const locals = yield this.askForVariable(targetDir, templateDir);
-    const files = glob.sync('**/*', { cwd: src, dot: true, nodir: true });
-    files.forEach(file => {
-      const from = path.join(src, file);
-      const to = path.join(targetDir, this.replaceTemplate(this.fileMapping[file] || file, locals));
-      this.log('write to %s', to);
-      let content = fse.readFileSync(from);
-      if (isTextOrBinary.isTextSync(from, content)) {
-        content = this.replaceTemplate(content, locals);
-      }
-      fse.outputFileSync(to, content);
-    });
-    return files;
+    yield processFiles.call(command, targetDir, templateDir);
+    // download egg-born-module-test-cook
+    const pkg = require(path.join(templateDir, 'package.json'));
+    if (pkg.name === 'egg-born-template-cabloy') {
+      // download
+      const testCookDir = yield this.downloadBoilerplate('egg-born-module-test-cook');
+      // move
+      const destDir = path.join(targetDir, 'src/module');
+      fse.moveSync(testCookDir, path.join(destDir, 'test-cook'));
+      // delete .gitkeep
+      fse.removeSync(path.join(destDir, '.gitkeep'));
+    }
   };
 
+  // run
   yield command.run(process.cwd(), process.argv.slice(2));
 
 }).catch(err => {
   console.error(err.stack);
   process.exit(1);
 });
+
