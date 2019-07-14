240dba274c6f6362536b313646509d8585fc4241
zhennann <zhen.nann@icloud.com>
2019-07-14 10:04:29 +0800

    refactor: move test to test-party

--- src/module-system/a-cache/backend/src/routes.js
+++ src/module-system/a-cache/backend/src/routes.js
@@ -1,19 +1,12 @@
 const version = require('./controller/version.js');
 const db = require('./controller/db.js');
-const test = require('./controller/test.js');
 
 module.exports = app => {
-  let routes = [
+  const routes = [
     { method: 'post', path: 'version/update', controller: version, middlewares: 'inner' },
     { method: 'post', path: 'db/set', controller: db, middlewares: 'inner',
       meta: { auth: { enable: false } },
     },
   ];
-  if (app.meta.isTest || app.meta.isLocal) {
-    routes = routes.concat([
-      { method: 'get', path: 'test/cachedb', controller: test, middlewares: 'test', meta: { auth: { enable: false } } },
-      { method: 'get', path: 'test/cachemem', controller: test, middlewares: 'test', meta: { auth: { enable: false } } },
-    ]);
-  }
   return routes;
 };
--- src/module-system/a-cache/dist/backend.js
+++ src/module-system/a-cache/dist/backend.js
@@ -465,21 +465,14 @@ const Fn = module.exports = ctx => {
 
 const version = __webpack_require__(14);
 const db = __webpack_require__(15);
-const test = __webpack_require__(16);
 
 module.exports = app => {
-  let routes = [
+  const routes = [
     { method: 'post', path: 'version/update', controller: version, middlewares: 'inner' },
     { method: 'post', path: 'db/set', controller: db, middlewares: 'inner',
       meta: { auth: { enable: false } },
     },
   ];
-  if (app.meta.isTest || app.meta.isLocal) {
-    routes = routes.concat([
-      { method: 'get', path: 'test/cachedb', controller: test, middlewares: 'test', meta: { auth: { enable: false } } },
-      { method: 'get', path: 'test/cachemem', controller: test, middlewares: 'test', meta: { auth: { enable: false } } },
-    ]);
-  }
   return routes;
 };
 
@@ -519,115 +512,6 @@ module.exports = app => {
   return DbController;
 };
 
-
-/***/ }),
-/* 16 */
-/***/ (function(module, exports, __webpack_require__) {
-
-const require3 = __webpack_require__(17);
-const assert = require3('assert');
-
-module.exports = app => {
-  class TestController extends app.Controller {
-
-    async cachemem() {
-
-      // set
-      this.ctx.cache.mem.set('name', 'zhennann');
-
-      // has
-      let res = this.ctx.cache.mem.has('name');
-      assert(res);
-
-      // get
-      let value = this.ctx.cache.mem.get('name');
-      assert(value === 'zhennann');
-
-      // remove
-      this.ctx.cache.mem.remove('name');
-      res = this.ctx.cache.mem.has('name');
-      assert(!res);
-
-      // set with timeout
-      this.ctx.cache.mem.set('name', 'zhennann', 1000);
-
-      // get
-      value = this.ctx.cache.mem.get('name');
-      assert(value === 'zhennann');
-
-      // other module's cache
-      const moduleCache = this.ctx.cache.mem.module(this.ctx.module.info.relativeName);
-      value = moduleCache.get('name');
-      assert(value === 'zhennann');
-
-      // get after timeout
-      await sleep(1000);
-      value = this.ctx.cache.mem.get('name');
-      assert(!value);
-
-      // clear
-      //   not clear, hold other caches
-      // this.ctx.cache.mem.clear();
-
-      this.ctx.success();
-    }
-
-    async cachedb() {
-
-      // set
-      await this.ctx.cache.db.set('name', 'zhennann');
-
-      // has
-      let res = await this.ctx.cache.db.has('name');
-      assert(res);
-
-      // get
-      let value = await this.ctx.cache.db.get('name');
-      assert(value === 'zhennann');
-
-      // remove
-      await this.ctx.cache.db.remove('name');
-      res = await this.ctx.cache.db.has('name');
-      assert(!res);
-
-      // set with timeout
-      await this.ctx.cache.db.set('name', 'zhennann', 1000);
-
-      // get
-      value = await this.ctx.cache.db.get('name');
-      assert(value === 'zhennann');
-
-      // other module's cache
-      const moduleCache = this.ctx.cache.db.module(this.ctx.module.info.relativeName);
-      value = await moduleCache.get('name');
-      assert(value === 'zhennann');
-
-      // get after timeout
-      await sleep(1000);
-      value = await this.ctx.cache.db.get('name');
-      assert(!value);
-
-      // clear
-      //   not clear, hold other caches
-      // await this.ctx.cache.db.clear();
-
-      this.ctx.success();
-    }
-
-  }
-  return TestController;
-};
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-/***/ }),
-/* 17 */
-/***/ (function(module, exports) {
-
-module.exports = require("require3");
 
 /***/ })
 /******/ ]);
