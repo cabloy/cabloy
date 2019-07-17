d105d9f0f388457541292787d090270e79e5e37b
zhennann <zhen.nann@icloud.com>
2019-07-18 01:55:44 +0800

    fix: view

--- src/module-system/a-components-sync/front/src/components/view.vue
+++ src/module-system/a-components-sync/front/src/components/view.vue
@@ -44,18 +44,30 @@ export default {
     const el = self.$refs.el;
     self.$f7ready(f7Instance => {
       if (self.props.init) return;
-      self.routerData = {
-        el,
-        component: self,
-        instance: null,
-      };
-      self.$vuef7.routers.views.push(self.routerData);
-      self.routerData.instance = f7Instance.views.create(el, self.$options.propsData || {});
+      self.routerData.instance = f7Instance.views.create(el, Object.assign({
+        on: {
+          init: self.onViewInit,
+        },
+      }, noUndefinedProps(self.$options.propsData || {})));
       self.f7View = self.routerData.instance;
+      self.f7View.on('swipebackMove', self.onSwipeBackMove);
+      self.f7View.on('swipebackBeforeChange', self.onSwipeBackBeforeChange);
+      self.f7View.on('swipebackAfterChange', self.onSwipeBackAfterChange);
+      self.f7View.on('swipebackBeforeReset', self.onSwipeBackBeforeReset);
+      self.f7View.on('swipebackAfterReset', self.onSwipeBackAfterReset);
+      // viewReady
       this.$emit('view:ready', self);
     });
   },
 };
+
+function noUndefinedProps(obj) {
+  const o = {};
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] !== 'undefined') o[key] = obj[key];
+  });
+  return o;
+}
 
 </script>
 <style scoped>
