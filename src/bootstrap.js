6e6285091a8ff33ae0803fcaebe7c0329a5fc5c4
zhennann <zhen.nann@icloud.com>
2019-07-19 13:25:43 +0800

    update: framework7

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Framework7 from '@zhennann/framework7/packages/core/framework7.esm.bundle.js';
 import Framework7Vue from '@zhennann/framework7/packages/vue/framework7-vue.esm.bundle.js';
 
+import 'framework7-icons';
 import '@zhennann/framework7/packages/core/framework7.bundle.less';
 import './assets/css/iconfont/material-icons.css';
 
