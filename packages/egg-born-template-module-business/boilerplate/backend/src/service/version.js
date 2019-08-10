de5d83b2d154dd1fd62e2323cb085f0d34709634
zhennann <zhen.nann@icloud.com>
2019-08-10 20:49:26 +0800

    update

--- packages/egg-born-template-module-business/boilerplate/backend/src/service/version.js
+++ packages/egg-born-template-module-business/boilerplate/backend/src/service/version.js
@@ -23,38 +23,15 @@ module.exports = app => {
 
     async init(options) {
       if (options.version === 1) {
-        // atomClassName
-        const atomClassName = '{{atomClassName}}';
         // add role rights
         const roleRights = [
           { roleName: 'authenticated', action: 'create' },
           { roleName: 'authenticated', action: 'write', scopeNames: 0 },
           { roleName: 'authenticated', action: 'delete', scopeNames: 0 },
           { roleName: 'authenticated', action: 'read', scopeNames: 0 },
-          { roleName: 'superuser', action: 'read', scopeNames: 'authenticated' },
+          { roleName: 'system', action: 'read', scopeNames: 'authenticated' },
         ];
-        const module = this.ctx.app.meta.modules[this.ctx.module.info.relativeName];
-        const atomClass = await this.ctx.meta.atomClass.get({ atomClassName });
-        for (const roleRight of roleRights) {
-          // role
-          const role = await this.ctx.meta.role.get({ roleName: roleRight.roleName });
-          // scope
-          let scope;
-          if (!roleRight.scopeNames) {
-            scope = 0;
-          } else {
-            const roleScope = await this.ctx.meta.role.get({ roleName: roleRight.scopeNames });
-            scope = [ roleScope.id ];
-          }
-          // add role right
-          await this.ctx.meta.role.addRoleRight({
-            roleId: role.id,
-            atomClassId: atomClass.id,
-            action: this.ctx.constant.module('a-base').atom.action[roleRight.action] || module.main.meta.base.atoms.{{atomClassName}}
-              .actions[roleRight.action].code,
-            scope,
-          });
-        }
+        await this.ctx.meta.role.addRoleRightBatch({ atomClassName: '{{atomClassName}}', roleRights });
       }
     }
 
