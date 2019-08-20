439a79e0a60989892711d3d0f29efd9f3c03de98
zhennann <zhen.nann@icloud.com>
2019-08-20 21:49:44 +0800

    fix: role.addRoleFunctionBatch

--- src/module-system/a-baseadmin/backend/src/service/version.js
+++ src/module-system/a-baseadmin/backend/src/service/version.js
@@ -10,17 +10,15 @@ module.exports = app => {
     async init(options) {
       if (options.version === 1) {
         // roleFunctions
-        const role = await this.ctx.meta.role.getSystemRole({ roleName: 'system' });
-        const functions = [ 'user', 'role', 'atomRight', 'menuRight', 'functionRight', 'auth' ];
-        for (const functionName of functions) {
-          const func = await this.ctx.meta.function.get({
-            name: functionName,
-          });
-          await this.ctx.meta.role.addRoleFunction({
-            roleId: role.id,
-            functionId: func.id,
-          });
-        }
+        const roleFunctions = [
+          { roleName: 'system', name: 'user' },
+          { roleName: 'system', name: 'role' },
+          { roleName: 'system', name: 'atomRight' },
+          { roleName: 'system', name: 'menuRight' },
+          { roleName: 'system', name: 'functionRight' },
+          { roleName: 'system', name: 'auth' },
+        ];
+        await this.ctx.meta.role.addRoleFunctionBatch({ roleFunctions });
       }
     }
 
