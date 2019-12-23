60061f282311ae9b668a360f74143c89c8b4c9d5
zhennann <zhen.nann@icloud.com>
2019-12-23 11:38:54 +0800

    feat: js config

--- src/module-system/test-wechat/front/src/pages/test/index.vue
+++ src/module-system/test-wechat/front/src/pages/test/index.vue
@@ -2,7 +2,7 @@
   <eb-page>
     <eb-navbar :title="$text('Test')" eb-back-link="Back"></eb-navbar>
     <eb-list no-hairlines-md>
-      <eb-list-item :title="微信扫一扫" :onPerform="onPerformScanQRCode"></eb-list-item>
+      <eb-list-item title="微信扫一扫" link="#" :onPerform="onPerformScanQRCode"></eb-list-item>
     </eb-list>
   </eb-page>
 </template>
@@ -10,10 +10,20 @@
 export default {
   data() {
     return {
-
+      moduleWechat: null,
     };
   },
-  created() {},
+  created() {
+    const action = {
+      actionModule: 'a-wechat',
+      actionComponent: 'jssdk',
+      name: 'config',
+    };
+    const item = { flag: 'test' };
+    this.$meta.util.performAction({ ctx: this, action, item }).then(res => {
+      console.log(res);
+    });
+  },
   methods: {
     onPerformScanQRCode() {
       console.log('0000');
