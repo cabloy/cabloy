c4f20fb48ff35a3b446fdec39ee7a52527d39bb3
zhennann <zhen.nann@icloud.com>
2019-12-23 16:38:43 +0800

    feat: jssdk scanQrcode

--- src/module-system/a-wechat/front/src/components/jssdk.js
+++ src/module-system/a-wechat/front/src/components/jssdk.js
@@ -1,17 +1,31 @@
+let _wxInstance = null;
+
 export default {
   meta: {
     global: false,
   },
   methods: {
     onAction({ ctx, action, item }) {
-      if (action.name === 'config') {
-        return new Promise((resolve, reject) => {
-          const url = location.href.split('#')[0];
-          this.$api.post('jssdk/jsconfig', { url }).then(res => {
-            resolve(res);
+      if (action.name === 'config') return this._createConfig({ ctx, item });
+    },
+    _createConfig(/* { ctx, item }*/) {
+      if (!this.$device.wechat) return null;
+      if (_wxInstance) return { wx: _wxInstance };
+      return new Promise((resolve, reject) => {
+        // load jweixin-1.4.0.js
+        this.$meta.util.loadScript(this.$config.jssdk.url.jweixin, () => {
+          this.$api.post('jssdk/jsconfig', { url: location.href.split('#')[0] }).then(params => {
+            window.wx.config(params);
+            window.wx.error(e => {
+              reject(e);
+            });
+            window.wx.ready(() => {
+              _wxInstance = window.wx;
+              resolve({ wx: window.wx });
+            });
           });
         });
-      }
+      });
     },
   },
 };
