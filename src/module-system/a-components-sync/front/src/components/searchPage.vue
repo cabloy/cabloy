65fb8c0897976fdebabf7a291aec116f28bcbfed
zhennann <zhen.nann@icloud.com>
2020-02-17 20:34:28 +0800

    feat: advanced search

--- src/module-system/a-components-sync/front/src/components/searchPage.vue
+++ src/module-system/a-components-sync/front/src/components/searchPage.vue
@@ -5,7 +5,7 @@
       </f7-searchbar>
       <eb-link v-if="advanced" slot="title-large" class="search-advanced" @click="onClickAdvanced">{{$text('Advanced Search')}}</eb-link>
     </eb-navbar>
-    <slot ref="list" name="list"></slot>
+    <slot name="list"></slot>
   </eb-page>
 </template>
 <script>
@@ -22,26 +22,21 @@ export default {
       default: false,
     }
   },
-  computed: {
-    list() {
-      return this.$slots.list[0].componentInstance;
-    },
-  },
   mounted() {
     this.$refs.searchbar.f7Searchbar.enable(true);
   },
   methods: {
     onSearch: Vue.prototype.$meta.util.debounce(function(searchbar, query) {
-      this.list.onSearch(query);
+      this.$emit('onSearch', query);
     }, 300),
     onDisable() {
-      this.$f7router.back();
+      this.$emit('onDisable');
     },
     onInfinite() {
-      this.list.loadMore();
+      this.$emit('onLoadMore');
     },
     onClickAdvanced() {
-      this.list.onSearchAdvanced();
+      this.$emit('onSearchAdvanced');
     }
   },
 };
