c51793e682107656a6a9f87294a65b667c8ab766
zhennann <zhen.nann@icloud.com>
2020-02-24 19:48:18 +0800

    feat: eb-box with toolbar

--- src/module/test-party/front/src/kitchen-sink/pages/box.vue
+++ src/module/test-party/front/src/kitchen-sink/pages/box.vue
@@ -1,9 +1,14 @@
 <template>
   <eb-page>
     <eb-navbar :title="$text('Box Container')" eb-back-link="Back"> </eb-navbar>
-    <eb-box @size="onSize">
+    <eb-box @size="onSize" toolbar>
       <textarea ref="textarea" type="textarea" :value="content" class="json-textarea"></textarea>
     </eb-box>
+    <f7-toolbar bottom-md>
+      <f7-button>left</f7-button>
+      <f7-button>middle</f7-button>
+      <eb-button>right</eb-button>
+    </f7-toolbar>
   </eb-page>
 </template>
 <script>
