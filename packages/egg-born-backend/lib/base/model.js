25ede68e1a9edc8bb796361fb94bae775c8e007f
zhennann <zhen.nann@icloud.com>
2020-03-04 09:04:31 +0800

    enhance: model

--- packages/egg-born-backend/lib/base/model.js
+++ packages/egg-born-backend/lib/base/model.js
@@ -46,12 +46,13 @@ module.exports = app => {
     Object.defineProperty(Model.prototype, method, {
       get() {
         return function() {
-          const args = [ this.table ];
+          const args = [ ];
+          if (this.table) args.push(this.table);
           for (const arg of arguments) args.push(arg);
-          if (!this.disableInstance && !args[1].iid) {
+          if ((!this.table || !this.disableInstance) && !args[1].iid) {
             args[1].iid = this.ctx.instance.id;
           }
-          if (!this.disableDeleted) {
+          if (this.table && !this.disableDeleted) {
             args[1].deleted = 0;
           }
           return this.ctx.db[method].apply(this.ctx.db, args);
@@ -66,7 +67,8 @@ module.exports = app => {
     Object.defineProperty(Model.prototype, method, {
       get() {
         return function() {
-          const args = [ this.table ];
+          const args = [ ];
+          if (this.table) args.push(this.table);
           for (const arg of arguments) args.push(arg);
           return this.ctx.db[method].apply(this.ctx.db, args);
         };
@@ -80,13 +82,14 @@ module.exports = app => {
     Object.defineProperty(Model.prototype, method, {
       get() {
         return function() {
-          const args = [ this.table ];
+          const args = [ ];
+          if (this.table) args.push(this.table);
           for (const arg of arguments) args.push(arg);
           args[1] = args[1] || {};
-          if (!this.disableInstance && !args[1].iid) {
+          if ((!this.table || !this.disableInstance) && !args[1].iid) {
             args[1].iid = this.ctx.instance.id;
           }
-          if (!this.disableDeleted) {
+          if (this.table && !this.disableDeleted) {
             const sql = this.ctx.db.format('UPDATE ?? SET deleted=1 ', [ args[0] ]) +
               this.ctx.db._where(args[1]);
             return this.ctx.db.query(sql);
@@ -103,13 +106,14 @@ module.exports = app => {
     Object.defineProperty(Model.prototype, method, {
       get() {
         return function() {
-          const args = [ this.table ];
+          const args = [ ];
+          if (this.table) args.push(this.table);
           for (const arg of arguments) args.push(arg);
           args[1] = args[1] || {};
-          if (!this.disableInstance && !args[1].iid) {
+          if ((!this.table || !this.disableInstance) && !args[1].iid) {
             args[1].iid = this.ctx.instance.id;
           }
-          if (!this.disableDeleted) {
+          if (this.table && !this.disableDeleted) {
             args[1].deleted = 0;
           }
           return this.ctx.db[method].apply(this.ctx.db, args);
@@ -124,16 +128,17 @@ module.exports = app => {
     Object.defineProperty(Model.prototype, method, {
       get() {
         return function() {
-          const args = [ this.table ];
+          const args = [ ];
+          if (this.table) args.push(this.table);
           for (const arg of arguments) args.push(arg);
           args[1] = args[1] || {};
           // if (args[1].id) {
           //   return this.ctx.db[method].apply(this.ctx.db, args);
           // }
-          if (!this.disableInstance && !args[1].iid) {
+          if ((!this.table || !this.disableInstance) && !args[1].iid) {
             args[1].iid = this.ctx.instance.id;
           }
-          if (!this.disableDeleted) {
+          if (this.table && !this.disableDeleted) {
             args[1].deleted = 0;
           }
           return this.ctx.db[method].apply(this.ctx.db, args);
@@ -148,14 +153,15 @@ module.exports = app => {
     Object.defineProperty(Model.prototype, method, {
       get() {
         return function() {
-          const args = [ this.table ];
+          const args = [ ];
+          if (this.table) args.push(this.table);
           for (const arg of arguments) args.push(arg);
           args[1] = args[1] || {};
           args[1].where = args[1].where || {};
-          if (!this.disableInstance && !args[1].where.iid) {
+          if ((!this.table || !this.disableInstance) && !args[1].iid) {
             args[1].where.iid = this.ctx.instance.id;
           }
-          if (!this.disableDeleted) {
+          if (this.table && !this.disableDeleted) {
             args[1].where.deleted = 0;
           }
           return this.ctx.db[method].apply(this.ctx.db, args);
