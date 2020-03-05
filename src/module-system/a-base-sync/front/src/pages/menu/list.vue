7fc5e222d76e2c0859e8bdd58f352d7ce70dad3a
zhennann <zhen.nann@icloud.com>
2020-03-05 23:02:46 +0800

    enhance: sidebar

--- src/module-system/a-base-sync/front/src/pages/menu/list.vue
+++ src/module-system/a-base-sync/front/src/pages/menu/list.vue
@@ -4,23 +4,15 @@
       <f7-nav-right>
         <eb-link iconMaterial="search" eb-target="_self" eb-href="menu/search"></eb-link>
       </f7-nav-right>
-      <f7-subnavbar>
+      <f7-subnavbar v-if="tabActive">
         <f7-toolbar top tabbar>
-          <f7-link :tab-link="`#${tabIdScenes}`" tab-link-active>{{$text('Scenes')}}</f7-link>
-          <f7-link :tab-link="`#${tabIdModules}`">{{$text('Modules')}}</f7-link>
-          <f7-link :tab-link="`#${tabIdStars}`">{{$text('Stars')}}</f7-link>
+          <f7-link v-for="tab of tabs" :key="tab.id" :tabLink="`#${tab.id}`" :tabLinkActive="tabActive===tab.mode">{{$text(tab.title)}}</f7-link>
         </f7-toolbar>
       </f7-subnavbar>
     </eb-navbar>
-    <f7-tabs>
-      <eb-tab-page-content :id="tabIdScenes" tab-active>
-        <menus slot="list" mode="scenes"></menus>
-      </eb-tab-page-content>
-      <eb-tab-page-content :id="tabIdModules">
-        <menus slot="list" mode="modules"></menus>
-      </eb-tab-page-content>
-      <eb-tab-page-content :id="tabIdStars">
-        <menus slot="list" mode="stars"></menus>
+    <f7-tabs v-if="tabActive">
+      <eb-tab-page-content v-for="tab of tabs" :key="tab.id" :id="tab.id" :tabActive="tabActive===tab.mode" @tab:show="onTabShow(tab.mode)">
+        <menus slot="list" :mode="tab.mode"></menus>
       </eb-tab-page-content>
     </f7-tabs>
   </eb-page>
@@ -36,22 +28,49 @@ export default {
     menus,
   },
   data() {
+    const tabs = [
+      { id: Vue.prototype.$meta.util.nextId('tab'), mode: 'scenes', title: 'Scenes' },
+      { id: Vue.prototype.$meta.util.nextId('tab'), mode: 'modules', title: 'Modules' },
+      { id: Vue.prototype.$meta.util.nextId('tab'), mode: 'stars', title: 'Stars' },
+    ];
     return {
       pageTitle: null,
-      tabIdScenes: Vue.prototype.$meta.util.nextId('tab'),
-      tabIdModules: Vue.prototype.$meta.util.nextId('tab'),
-      tabIdStars: Vue.prototype.$meta.util.nextId('tab'),
+      tabs,
+      tabActive: '',
     };
   },
+  created() {
+    // tab active
+    this.__initTabActive();
+  },
   mounted() {
-    const $el = this.$$(this.$el);
-    const $view = $el.parents('.eb-layout-view');
-    if ($view.is('.eb-layout-panel-view')) {
-      this.pageTitle = this.$text('Menu');
-    } else {
-      this.pageTitle = this.$text('Home');
-    }
-  }
+    // title
+    this.__initPageTitle();
+  },
+  methods: {
+    __initPageTitle() {
+      const $el = this.$$(this.$el);
+      const $view = $el.parents('.eb-layout-view');
+      if ($view.is('.eb-layout-panel-view')) {
+        this.pageTitle = this.$text('Menu');
+      } else {
+        this.pageTitle = this.$text('Home');
+      }
+    },
+    __initTabActive() {
+      // layoutConfig
+      this.$store.dispatch('a/base/getLayoutConfig', 'a-base').then(layoutConfig => {
+        this.tabActive = layoutConfig.menuTabActive || this.tabs[0].mode;
+      });
+    },
+    onTabShow(tabMode) {
+      if (this.tabActive !== tabMode) {
+        this.tabActive = tabMode;
+        // save
+        this.$store.commit('a/base/setLayoutConfigKey', { module: 'a-base', key: 'menuTabActive', value: this.tabActive });
+      }
+    },
+  },
 };
 
 </script>
