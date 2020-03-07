5623b429e17119c26aeb4b1aeadc90d3349c1b02
zhennann <zhen.nann@icloud.com>
2020-03-07 22:26:38 +0800

    feat: dashboard

--- src/module-system/a-chartjs/front/src/main.js
+++ src/module-system/a-chartjs/front/src/main.js
@@ -2,11 +2,16 @@ let Vue;
 
 import './assets/css/module.less';
 
+import chartjs from 'chart.js';
+
 // install
 function install(_Vue, cb) {
   if (Vue) return console.error('already installed.');
 
   Vue = _Vue;
+
+  // utils
+  const utils = { chartjs };
 
   return cb({
     routes: require('./routes.js').default,
@@ -14,6 +19,7 @@ function install(_Vue, cb) {
     config: require('./config/config.js').default,
     locales: require('./config/locales.js').default,
     components: require('./components.js').default,
+    utils,
   });
 }
 
