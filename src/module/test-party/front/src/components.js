4e9df0afb023befb5a2b5d2db3427d2121424c7e
zhennann <zhen.nann@icloud.com>
2020-03-07 22:27:28 +0800

    feat: dashboard

--- src/module/test-party/front/src/components.js
+++ src/module/test-party/front/src/components.js
@@ -1,5 +1,7 @@
 import helloGlobal from './kitchen-sink/components/helloGlobal.vue';
+import widgetProducts from './kitchen-sink/components/widgets/products.vue';
 
 export default {
   helloGlobal,
+  widgetProducts,
 };
---
+++ src/module/test-party/front/src/kitchen-sink/components/widgets/products.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="data-table card">
+    <table>
+      <thead>
+        <tr>
+          <th class="label-cell">Time</th>
+          <th class="numeric-cell">Calories</th>
+          ...
+          <th class="medium-only">Comment</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="label-cell">Frozen yogurt</td>
+          <td class="numeric-cell">159</td>
+          ...
+          <td class="medium-only">I like it!</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  meta: {
+    global: false,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    signIn() {
+      const info = this.$module.info;
+      const url = `/api/${info.url}/passport/${info.relativeName}/authgithub`;
+      this.$meta.vueApp.toLogin({ url });
+    },
+  },
+};
+
+</script>
+<style scoped>
+</style>
