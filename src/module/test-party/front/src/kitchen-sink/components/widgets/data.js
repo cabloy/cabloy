558b3108346686c91ee194055947fc27e36e2438
zhennann <zhen.nann@icloud.com>
2020-03-07 23:07:47 +0800

    feat: dashboard

---
+++ src/module/test-party/front/src/kitchen-sink/components/widgets/data.js
@@ -0,0 +1,10 @@
+export default {
+  cols: [ 'Apples', 'Pears', 'All' ],
+  rows: [ 'Spring', 'Summer', 'Autumn', 'Winter' ],
+  dataset: [
+    [ 1000, 800, 1800 ],
+    [ 1300, 850, 2150 ],
+    [ 1200, 1000, 2200 ],
+    [ 1500, 1200, 2700 ],
+  ],
+};
--- src/module/test-party/front/src/kitchen-sink/components/widgets/products.vue
+++ src/module/test-party/front/src/kitchen-sink/components/widgets/products.vue
@@ -1,39 +1,48 @@
 <template>
-  <div class="data-table card">
-    <table>
-      <thead>
-        <tr>
-          <th class="label-cell">Time</th>
-          <th class="numeric-cell">Calories</th>
-          ...
-          <th class="medium-only">Comment</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="label-cell">Frozen yogurt</td>
-          <td class="numeric-cell">159</td>
-          ...
-          <td class="medium-only">I like it!</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <f7-card>
+    <f7-card-header>Fruit Sales</f7-card-header>
+    <f7-card-content>
+      <div class="data-table">
+        <table>
+          <thead>
+            <tr>
+              <th class="label-cell"></th>
+              <th v-for="(col,index) of data.cols" :key="index" class="numeric-cell">{{col}}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(row,index) of data.rows" :key="index">
+              <th class="label-cell">{{row}}</th>
+              <td v-for="(col,colIndex) of data.cols" :key="colIndex" class="numeric-cell">{{data.dataset[index][colIndex]}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </f7-card-content>
+    <f7-card-footer>
+      <span></span>
+      <span></span>
+      <span>Amount: {{getAmount()}}</span>
+    </f7-card-footer>
+  </f7-card>
 </template>
 <script>
+import data from './data.js';
 export default {
   meta: {
     global: false,
   },
   data() {
-    return {};
+    return {
+      data,
+    };
   },
   methods: {
-    signIn() {
-      const info = this.$module.info;
-      const url = `/api/${info.url}/passport/${info.relativeName}/authgithub`;
-      this.$meta.vueApp.toLogin({ url });
-    },
+    getAmount() {
+      return this.data.dataset.reduce((total, row) => {
+        return total + row[2];
+      }, 0);
+    }
   },
 };
 
