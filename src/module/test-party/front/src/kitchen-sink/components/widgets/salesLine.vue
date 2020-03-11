2359bf345ca054c0c20590854bf498e9c5e36f83
zhennann <zhen.nann@icloud.com>
2020-03-11 09:01:29 +0800

    feat: dashboard

--- src/module/test-party/front/src/kitchen-sink/components/widgets/salesLine.vue
+++ src/module/test-party/front/src/kitchen-sink/components/widgets/salesLine.vue
@@ -2,18 +2,79 @@
   <f7-card>
     <f7-card-header>Fruit Sales(Line Chart)</f7-card-header>
     <f7-card-content>
+      <canvas ref="chart"></canvas>
     </f7-card-content>
   </f7-card>
 </template>
 <script>
+import dataSource from './data.js';
 export default {
   meta: {
     global: false,
   },
   data() {
-    return {};
+    return {
+      chartjs: null,
+      chart: null,
+    };
   },
-  methods: {},
+  mounted() {
+    this.__init();
+  },
+  methods: {
+    __init() {
+      this.$meta.module.use('a-chartjs', module => {
+        this.chartjs = module.options.utils.chartjs;
+        this.__fillChart();
+      });
+    },
+    __fillChart() {
+      // canvas
+      const chartCanvas = this.$refs.chart.getContext('2d');
+      // data
+      const chartData = {
+        labels: dataSource.rows,
+        datasets: [{
+          label: 'Apple',
+          fill: false,
+          data: dataSource.dataset.map(item => item[0]),
+        }, ],
+      };
+      // options
+      const chartOptions = {
+        maintainAspectRatio: false,
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        scales: {
+          xAxes: [{
+            gridLines: {
+              display: false,
+            },
+            ticks: {
+              fontColor: 'rgba(255, 255, 255, 0.4)',
+            },
+          }],
+          yAxes: [{
+            gridLines: {
+              display: true,
+            },
+            ticks: {
+              fontColor: 'rgba(255, 255, 255, 0.4)',
+              stepSize: 200,
+            },
+          }],
+        },
+      };
+      // fill
+      this.chart = new this.chartjs(chartCanvas, {
+        type: 'line',
+        data: chartData,
+        options: chartOptions,
+      })
+    }
+  },
 };
 
 </script>
