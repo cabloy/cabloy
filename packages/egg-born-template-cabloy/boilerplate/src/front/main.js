c09ae33b0f69b4cffffe78de7536b796c4c6c373
zhennann <zhen.nann@icloud.com>
2020-03-17 16:47:35 +0800

    feat: scene

--- packages/egg-born-template-cabloy/boilerplate/src/front/main.js
+++ packages/egg-born-template-cabloy/boilerplate/src/front/main.js
@@ -2,7 +2,6 @@ import 'egg-born-front/src/bootstrap.js';
 
 import './assets/css/app.less';
 
-import config from './config/config.js';
 import locales from './locales.js';
 
 let Vue;
@@ -12,6 +11,13 @@ function install(_Vue, cb) {
   if (Vue) return console.error('already installed.');
 
   Vue = _Vue;
+
+  // config
+  const config = require('./config/config.default.js').default;
+  if (process.env.SCENE) {
+    const configScene = require(`./config/config.${process.env.SCENE}.js`).default;
+    Vue.prototype.$meta.util.extend(config, configScene);
+  }
 
   // options
   return cb({
