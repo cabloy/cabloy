a4dee618884e0607aaccabf461d978cfc1267a2c
zhennann <zhen.nann@icloud.com>
2020-03-19 10:02:03 +0800

    feat: function scene

--- src/module-system/a-components-sync/front/src/common/dragdrop.js
+++ src/module-system/a-components-sync/front/src/common/dragdrop.js
@@ -26,7 +26,10 @@ export default function(Vue) {
     if (!context.onDragStart) return { size: _windowSize, tooltip: undefined };
     const res = context.onDragStart({ $el, context, dragElement });
     if (res === undefined) return { size: _windowSize, tooltip: undefined };
-    if (res) return res;
+    if (res) {
+      if (!res.size) res.size = _windowSize;
+      return res;
+    }
     return null;
   }
 
