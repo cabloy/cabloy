7eabe4ee5495f3a894480d8df39101a81c994892
zhennann <zhen.nann@icloud.com>
2020-03-23 10:47:32 +0800

    feat: sms

--- src/module-system/a-base-sync/backend/src/config/errors.js
+++ src/module-system/a-base-sync/backend/src/config/errors.js
@@ -1,10 +1,10 @@
 // error code should start from 1001
 module.exports = {
-  1001: 'Element exists',
+  1001: 'Element Exists',
   1002: 'Element does not exist',
   1003: 'Operation failed',
   1004: 'User does not exist',
-  1005: 'User is disabled',
+  1005: 'User is Disabled',
   1006: 'Agent user does not exist',
   1007: 'Incomplete information',
   1008: 'Should delete children first',
--- src/module-system/a-base-sync/backend/src/config/validation/keywords.js
+++ src/module-system/a-base-sync/backend/src/config/validation/keywords.js
@@ -9,11 +9,11 @@ module.exports = app => {
         const ctx = this;
         const res = await ctx.meta.user.exists({ [name]: data });
         if (res && res.id !== ctx.user.agent.id) {
-          const errors = [{ keyword: 'x-exists', params: [], message: ctx.text('Element exists') }];
+          const errors = [{ keyword: 'x-exists', params: [], message: ctx.text('Element Exists') }];
           throw new app.meta.ajv.ValidationError(errors);
         }
         if (!res && data.indexOf('__') > -1) {
-          const errors = [{ keyword: 'x-exists', params: [], message: ctx.text('Cannot contain __') }];
+          const errors = [{ keyword: 'x-exists', params: [], message: ctx.text('Cannot Contain __') }];
           throw new app.meta.ajv.ValidationError(errors);
         }
         return true;
