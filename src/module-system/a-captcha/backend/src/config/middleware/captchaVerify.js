756da3502af43002236041022a6cb2efa7f9cc27
zhennann <zhen.nann@icloud.com>
2020-03-23 22:02:50 +0800

    feat: sms

--- src/module-system/a-captcha/backend/src/config/middleware/captchaVerify.js
+++ src/module-system/a-captcha/backend/src/config/middleware/captchaVerify.js
@@ -5,28 +5,43 @@ module.exports = (options2, app) => {
   return async function captchaVerify(ctx, next, options) {
     // must exists
     const scene = options.scene;
-    if (!scene) ctx.throw.module(moduleInfo.relativeName, 1001);
-    // params
-    const module = scene.module || ctx.module.info.relativeName;
-    const sceneName = scene.name;
-    const captchaData = ctx.request.body[options.data || 'captcha'];
-    const providerInstanceId = captchaData.providerInstanceId;
-    const dataInput = captchaData.data;
-    // verify
-    try {
-      const _captcha = new (CaptchaFn(ctx))();
-      await _captcha.verify({ module, sceneName, providerInstanceId, dataInput });
-    } catch (err) {
-      throw combineCaptchaError({
-        message: err.message,
-      });
+    const scenes = options.scenes;
+    if (!scene && !scenes) ctx.throw.module(moduleInfo.relativeName, 1001);
+
+    // scene
+    if (scene) {
+      await sceneVerify({ ctx, scene });
+    } else if (scenes) {
+      for (const scene of scenes) {
+        await sceneVerify({ ctx, scene });
+      }
     }
+
     // next
     await next();
   };
 };
 
-function combineCaptchaError({ message }) {
+async function sceneVerify({ ctx, scene }) {
+  // params
+  const module = scene.module || ctx.module.info.relativeName;
+  const sceneName = scene.name;
+  const captchaData = ctx.request.body[scene.dataKey || 'captcha'];
+  const providerInstanceId = captchaData.providerInstanceId;
+  const dataInput = captchaData.data;
+  // verify
+  try {
+    const _captcha = new (CaptchaFn(ctx))();
+    await _captcha.verify({ module, sceneName, providerInstanceId, dataInput });
+  } catch (err) {
+    throw combineCaptchaError({
+      fieldKey: scene.fieldKey || 'token',
+      message: err.message,
+    });
+  }
+}
+
+function combineCaptchaError({ fieldKey, message }) {
   // error
   const error = new Error();
   error.code = 422;
@@ -35,8 +50,8 @@ function combineCaptchaError({ message }) {
       keyword: 'x-captcha',
       params: [],
       message,
-      dataPath: '/captcha/token',
-      schemaPath: '#/properties/captcha/token/x-captcha',
+      dataPath: `/captcha/${fieldKey}`,
+      schemaPath: `#/properties/captcha/${fieldKey}/x-captcha`,
     },
   ];
   return error;
