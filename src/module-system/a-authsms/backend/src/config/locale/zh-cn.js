d0bc6d255b5734b400103eaf6a917af8de814d2d
zhennann <zhen.nann@icloud.com>
2020-03-24 15:41:10 +0800

    feat: sms

--- src/module-system/a-authsms/backend/src/config/locale/zh-cn.js
+++ src/module-system/a-authsms/backend/src/config/locale/zh-cn.js
@@ -1,8 +1,8 @@
 module.exports = {
   SMS: '短信',
   smsProviderNonePrompt: '请指定SMS Provider',
-  SMSCodeInvalid: '验证码已失效，请重新获取',
-  SMSCodeMismatch: '验证码不匹配',
+  SMSCodeInvalid: '认证码已失效，请重新获取',
+  SMSCodeMismatch: '认证码不匹配',
   'Element Exists': '元素已存在',
   'Cannot Contain __': '不能包含__',
   'SMS Verification': '短信认证',
