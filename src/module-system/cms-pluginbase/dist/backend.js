d4fd0222ffc1368ca8ab3716a47365e3ffaaed9f
zhennann <zhen.nann@icloud.com>
2020-04-13 09:19:58 +0800

    feat: cluster and more

--- src/module-system/cms-pluginbase/dist/backend.js
+++ src/module-system/cms-pluginbase/dist/backend.js
@@ -150,7 +150,7 @@ module.exports = {
 
 module.exports = {
   'Load More': '加载更多',
-  'Load error, try again': '加载失败，请重试',
+  'Load Error, Try Again': '加载失败，请重试',
   'Load Completed': '加载完毕',
 };
 
