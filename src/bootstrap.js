193e8f31da3b6068a75069c09dc519076552ef73
zhennann <zhen.nann@icloud.com>
2020-04-16 11:34:23 +0800

    fix: require f7

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,15 +7,15 @@ import './assets/css/app.less';
 let Framework7;
 let Framework7Vue;
 if (process.env.NODE_ENV === 'production') {
-  Framework7 = require('@zhennann/framework7/packages/core/js/framework7-lite.bundle.min.js').default;
-  Framework7Vue = require('@zhennann/framework7/packages/vue/framework7-vue.bundle.min.js').default;
+  Framework7 = require('@zhennann/framework7/packages/core/js/framework7-lite.bundle.min.js');
+  Framework7Vue = require('@zhennann/framework7/packages/vue/framework7-vue.bundle.min.js');
   // eslint-disable-next-line
-  const Framework7Style = require('@zhennann/framework7/packages/core/css/framework7.bundle.min.css').default;
+  const Framework7Style = require('@zhennann/framework7/packages/core/css/framework7.bundle.min.css');
 } else {
   Framework7 = require('@zhennann/framework7/packages/core/framework7-lite.esm.bundle.js').default;
   Framework7Vue = require('@zhennann/framework7/packages/vue/framework7-vue.esm.bundle.js').default;
   // eslint-disable-next-line
-  const Framework7Style = require('@zhennann/framework7/packages/core/framework7.bundle.less').default;
+  const Framework7Style = require('@zhennann/framework7/packages/core/framework7.bundle.less');
 }
 
 // use
