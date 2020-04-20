6b92d818e516c70ed6d62b3305df7b1b2b6f2d58
zhennann <zhen.nann@icloud.com>
2020-04-20 17:15:45 +0800

    feat: uglify backend build

--- src/module-system/a-authsms/build/backend/webpack.base.conf.js
+++ src/module-system/a-authsms/build/backend/webpack.base.conf.js
@@ -25,7 +25,29 @@ module.exports = {
     extensions: [ '.js', '.json' ],
   },
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            // presets: [ '@babel/preset-env' ],
+            plugins: [
+              '@babel/plugin-transform-arrow-functions',
+              '@babel/plugin-transform-for-of',
+              '@babel/plugin-transform-parameters',
+              '@babel/plugin-transform-shorthand-properties',
+              '@babel/plugin-transform-spread',
+              '@babel/plugin-transform-template-literals',
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-transform-async-to-generator',
+            ],
+          },
+        },
+      },
+    ],
   },
   node: {
     console: false,
--- src/module-system/a-authsms/build/config.js
+++ src/module-system/a-authsms/build/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   productionSourceMap: true,
-  uglify: false,
+  uglify: true,
 };
