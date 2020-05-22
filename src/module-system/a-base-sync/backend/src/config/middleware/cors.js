522712b63afb506f63db4aa878c6d4e61d55abbe
zhennann <zhen.nann@icloud.com>
2020-05-23 00:03:28 +0800

    feat: socketio

--- src/module-system/a-base-sync/backend/src/config/middleware/cors.js
+++ src/module-system/a-base-sync/backend/src/config/middleware/cors.js
@@ -1,7 +1,5 @@
 const require3 = require('require3');
-const URL = require('url').URL;
 const extend = require3('extend2');
-const isSafeDomainUtil = require3('egg-security').utils.isSafeDomain;
 const koaCors = require3('@koa/cors');
 
 const optionsDefault = {
@@ -15,44 +13,19 @@ const optionsDefault = {
 };
 
 module.exports = (options, app) => {
-  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   return async function cors(ctx, next) {
+    // not cors (safari not send sec-fetch-mode)
+    // if (ctx.headers['sec-fetch-mode'] !== 'cors') return await next();
+
     // options
     const optionsCors = extend(true, {}, optionsDefault, options);
-
-    // whiteList
-    let whiteListCors;
-    const _config = ctx.config.module(moduleInfo.relativeName);
-    const _whiteList = (_config && _config.cors && _config.cors.whiteList) || [];
-    if (!Array.isArray(_whiteList)) {
-      whiteListCors = _whiteList.split(',');
-    } else {
-      whiteListCors = _whiteList.concat();
-    }
-    // inherits from jsonp
-    let _whiteListJsonp = _config && _config.jsonp && _config.jsonp.whiteList;
-    if (_whiteListJsonp) {
-      if (!Array.isArray(_whiteListJsonp)) {
-        _whiteListJsonp = _whiteListJsonp.split(',');
-      }
-      whiteListCors = whiteListCors.concat(_whiteListJsonp);
-    }
 
     // origin
     // if security plugin enabled, and origin config is not provided, will only allow safe domains support CORS.
     optionsCors.origin = optionsCors.origin || function corsOrigin(ctx) {
       // origin is {protocol}{hostname}{port}...
       const origin = ctx.get('origin');
-      if (!origin) return '';
-
-      let parsedUrl;
-      try {
-        parsedUrl = new URL(origin);
-      } catch (err) {
-        return '';
-      }
-
-      if (isSafeDomainUtil(parsedUrl.hostname, whiteListCors) || isSafeDomainUtil(origin, whiteListCors)) {
+      if (app.meta.util.isSafeDomain(ctx, origin)) {
         return origin;
       }
       return '';
