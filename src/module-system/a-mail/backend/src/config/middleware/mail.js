1f14d6815aae9cc1bf39f3735070e91be2dbecaa
zhennann <zhen.nann@icloud.com>
2020-05-24 16:35:32 +0800

    feat: socketio

--- src/module-system/a-mail/backend/src/config/middleware/mail.js
+++ src/module-system/a-mail/backend/src/config/middleware/mail.js
@@ -2,7 +2,6 @@
 const MailFn = require('./adapter/mail.js');
 const MAIL = Symbol('CTX#__MAIL');
 
-
 module.exports = () => {
   return async function mail(ctx, next) {
     ctx.meta = ctx.meta || {};
