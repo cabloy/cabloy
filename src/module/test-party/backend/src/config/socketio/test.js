6408d4d197a4a6db322e992e6aab88951b8e2925
zhennann <zhen.nann@icloud.com>
2020-05-26 23:03:33 +0800

    feat: socketio

--- src/module/test-party/backend/src/config/socketio/test.js
+++ src/module/test-party/backend/src/config/socketio/test.js
@@ -1,6 +1,31 @@
 
-async function onPublish({ ctx, path, message, options, user }) {
+async function onPublish({ io, ctx, path, message, options, user }) {
   // donothing
+}
+
+async function onProcess({ io, path, options, message, messageSync, messageClass }) {
+  // options
+  const messageScene = (options && options.scene) || '';
+  // ignore self
+  if (message.userIdFrom === messageSync.userId) return;
+  // send back
+  if (messageSync.userId === 0) {
+    const content = JSON.parse(message.content);
+    const _message = {
+      messageType: message.messageType,
+      messageFilter: message.messageFilter,
+      messageGroup: message.messageGroup,
+      messageScene,
+      userIdTo: message.userIdFrom,
+      content: {
+        text: `Reply: ${content.text}`,
+      },
+    };
+    return await io.publish({ path, message: _message, messageClass, options, user: { id: 0 } });
+  }
+  // send by socket
+
+
 }
 
 module.exports = app => {
@@ -10,6 +35,7 @@ module.exports = app => {
     },
     callbacks: {
       onPublish,
+      onProcess,
     },
   };
   return test;
