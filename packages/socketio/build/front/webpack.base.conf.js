0261dc7827013c5b0364faaec502737b37055690
zhennann <zhen.nann@icloud.com>
2020-06-01 16:37:08 +0800

    first commit

---
+++ packages/socketio/build/front/webpack.base.conf.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const utils = require('./utils');
+const config = require('./config.js');
+
+function resolve(dir) {
+  return path.join(__dirname, '../..', dir);
+}
+
+module.exports = {
+  entry: {
+    socketio: resolve('src/main.js'),
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].min.js',
+    publicPath: config.build.assetsPublicPath,
+    library: 'socketio',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    vue: 'vue',
+  },
+  resolve: {
+    extensions: [ '.js', '.vue', '.json' ],
+    alias: {
+      '@': resolve('src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.esm\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.esm\.bundle\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.module\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [ resolve('src') ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash].[ext]'),
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash].[ext]'),
+        },
+      },
+    ],
+  },
+};
