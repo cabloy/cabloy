e3c07270f078b465749d913ff73fc7d13e2c4fda
zhennann <zhen.nann@icloud.com>
2020-06-09 19:02:29 +0800

    refactor: instance appReady

--- src/module-system/a-instance/backend/src/config/middleware/appReady.js
+++ src/module-system/a-instance/backend/src/config/middleware/appReady.js
@@ -1,11 +1,22 @@
-const eventAppReady = 'eb:event:appReady';
+// MaxListenersExceededWarning
+// const eventAppReady = 'eb:event:appReady';
 
-function checkAppReady(app) {
-  return new Promise(resolve => {
-    app.once(eventAppReady, () => {
-      resolve();
-    });
-  });
+// function checkAppReady(app) {
+//   return new Promise(resolve => {
+//     app.once(eventAppReady, () => {
+//       resolve();
+//     });
+//   });
+// }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function checkAppReady(app) {
+  while (!app.meta.appReady) {
+    await sleep(300);
+  }
 }
 
 module.exports = (options, app) => {
