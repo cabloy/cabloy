1f2c830d180f70345d58bcc8f16a7e32de03e83f
zhennann <zhen.nann@icloud.com>
2020-06-16 22:42:42 +0800

    feat: device.wxwork

--- src/inject/patch/device.js
+++ src/inject/patch/device.js
@@ -1,7 +1,10 @@
 export default function(Vue) {
   // device
   const device = Vue.prototype.$device;
+  // ua
+  const ua = navigator.userAgent.toLowerCase() || '';
+  // wxwork
+  device.wxwork = ua.indexOf('wxwork') > -1;
   // wechat
-  const ua = navigator.userAgent.toLowerCase();
-  device.wechat = ua && (ua.indexOf('micromessenger') > -1 || ua.indexOf('micromessage') > -1);
+  device.wechat = !device.wxwork && (ua.indexOf('micromessenger') > -1 || ua.indexOf('micromessage') > -1);
 }
