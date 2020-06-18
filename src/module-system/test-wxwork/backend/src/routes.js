ddf6ccd69ce258b9d5205c0632eb33c6d9b5bc41
zhennann <zhen.nann@icloud.com>
2020-06-18 10:24:18 +0800

    feat: wxwork

--- src/module-system/test-wxwork/backend/src/routes.js
+++ src/module-system/test-wxwork/backend/src/routes.js
@@ -9,7 +9,7 @@ module.exports = app => {
     { method: 'post', path: 'version/init', controller: version, middlewares: 'inner' },
     { method: 'post', path: 'version/test', controller: version, middlewares: 'test' },
     // event
-    { method: 'post', path: 'event/wxworkMessage', controller: event, middlewares: 'inner', meta: { auth: { enable: false } } },
+    { method: 'post', path: 'event/wxworkMessage', controller: event, middlewares: 'inner,wxwork', meta: { auth: { enable: false } } },
     { method: 'post', path: 'event/loginInfo', controller: event, middlewares: 'inner', meta: { auth: { enable: false } } },
     // test
     { method: 'post', path: 'test/getMemberId', controller: test, middlewares: 'inWxwork',
