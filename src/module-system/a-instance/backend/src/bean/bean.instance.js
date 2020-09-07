cd9f6c0d82ef948bb2b96215927a44a0d1d58d5f
zhennann <zhen.nann@icloud.com>
2020-09-07 10:47:24 +0800

    refactor: instance middleware

--- src/module-system/a-instance/backend/src/bean/bean.instance.js
+++ src/module-system/a-instance/backend/src/bean/bean.instance.js
@@ -1,5 +1,9 @@
 const require3 = require('require3');
 const extend = require3('extend2');
+const chalk = require3('chalk');
+const boxen = require3('boxen');
+
+const boxenOptions = { padding: 1, margin: 1, align: 'center', borderColor: 'yellow', borderStyle: 'round' };
 
 module.exports = ctx => {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
@@ -19,7 +23,17 @@ module.exports = ctx => {
       // cache
       const cacheMem = ctx.cache.mem.module(moduleInfo.relativeName);
       const instance = await ctx.db.get('aInstance', { name: subdomain });
-      if (!instance) throw new Error(`instance not found: ${subdomain}`);
+      if (!instance) {
+        // prompt
+        if (ctx.app.meta.isLocal) {
+          const urlInfo = ctx.locale === 'zh-cn' ? 'https://cabloy.com/zh-cn/articles/multi-instance.html' : 'https://cabloy.com/articles/multi-instance.html';
+          let message = `Please add instance in ${chalk.keyword('cyan')('src/backend/config/config.local.js')}`;
+          message += '\n' + chalk.keyword('orange')(`{ subdomain: '${ctx.subdomain}', password: '', title: '' }`);
+          message += `\nMore info: ${chalk.keyword('cyan')(urlInfo)}`;
+          console.log('\n' + boxen(message, boxenOptions));
+        }
+        return ctx.throw(423);
+      }
       // config
       instance.config = JSON.parse(instance.config) || {};
       // cache configs
--- src/module-system/a-instance/backend/src/config/middleware/instance.js
+++ src/module-system/a-instance/backend/src/config/middleware/instance.js
@@ -1,32 +1,18 @@
-const require3 = require('require3');
-const extend = require3('extend2');
-const chalk = require3('chalk');
-const boxen = require3('boxen');
-
-const boxenOptions = { padding: 1, margin: 1, align: 'center', borderColor: 'yellow', borderStyle: 'round' };
-
 module.exports = ctx => {
-  const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Middleware {
     async execute(options, next) {
-      // cache
-      const cacheMem = ctx.cache.mem.module(moduleInfo.relativeName);
-      let instance = cacheMem.get('instance');
-      if (!instance) {
-        instance = await ctx.db.get('aInstance', { name: ctx.subdomain });
-        if (instance) {
-        // config
-          instance.config = JSON.parse(instance.config) || {};
-          // cache configs
-          const instanceConfigs = extend(true, {}, ctx.app.meta.configs, instance.config);
-          cacheMem.set('instanceConfigs', instanceConfigs);
-          // cache instance
-          cacheMem.set('instance', instance);
-        }
+      // instance
+      const instance = await ctx.bean.instance.get({ subdomain: ctx.subdomain });
+
+      // check if disabled
+      if (!ctx.innerAccess && instance.disabled) {
+        // locked
+        return ctx.throw(423);
       }
 
       // try to save host/protocol to config
-      if (instance && !instance.disabled && ctxHostValid(ctx)) {
+      if (ctxHostValid(ctx)) {
         if (!instance.config['a-base']) instance.config['a-base'] = {};
         const aBase = instance.config['a-base'];
         if (aBase.host !== ctx.host || aBase.protocol !== ctx.protocol) {
@@ -46,19 +32,7 @@ module.exports = ctx => {
         }
       }
 
-      if (!ctx.innerAccess && (!instance || instance.disabled)) {
-        // prompt
-        if (!instance && ctx.app.meta.isLocal) {
-          const urlInfo = ctx.locale === 'zh-cn' ? 'https://cabloy.com/zh-cn/articles/multi-instance.html' : 'https://cabloy.com/articles/multi-instance.html';
-          let message = `Please add instance in ${chalk.keyword('cyan')('src/backend/config/config.local.js')}`;
-          message += '\n' + chalk.keyword('orange')(`{ subdomain: '${ctx.subdomain}', password: '', title: '' }`);
-          message += `\nMore info: ${chalk.keyword('cyan')(urlInfo)}`;
-          console.log('\n' + boxen(message, boxenOptions));
-        }
-        // locked
-        ctx.throw(423);
-      }
-
+      // ok
       ctx.instance = instance;
 
       // next
