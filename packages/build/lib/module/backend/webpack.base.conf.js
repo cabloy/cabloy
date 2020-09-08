1619a4f4c74a75d5a3f128d7534a629c50e169db
zhennann <zhen.nann@icloud.com>
2020-09-08 15:30:51 +0800

    feat: module build:backend uglify

--- packages/build/lib/module/backend/webpack.base.conf.js
+++ packages/build/lib/module/backend/webpack.base.conf.js
@@ -6,6 +6,31 @@ module.exports = context => {
     require3: 'commonjs2 require3',
   };
 
+  const rules = [];
+  if (context.config.build.uglify) {
+    rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          // presets: [ '@babel/preset-env' ],
+          plugins: [
+            '@babel/plugin-transform-arrow-functions',
+            '@babel/plugin-transform-for-of',
+            '@babel/plugin-transform-parameters',
+            '@babel/plugin-transform-shorthand-properties',
+            '@babel/plugin-transform-spread',
+            '@babel/plugin-transform-template-literals',
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-transform-async-to-generator',
+          ],
+        },
+      },
+    });
+  }
+
   return {
     entry: {
       backend: path.join(context.modulePath, 'backend/src/main.js'),
@@ -22,7 +47,7 @@ module.exports = context => {
       extensions: [ '.js', '.json' ],
     },
     module: {
-      rules: [],
+      rules,
     },
     node: {
       console: false,
