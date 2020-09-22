c88b4b0b1b7b193f4b84f8856caa47f757eb75d4
zhennann <zhen.nann@icloud.com>
2020-09-22 17:09:07 +0800

    feat: a-flow

---
+++ src/module-system/a-flow/backend/src/bean/version.manager.js
@@ -0,0 +1,16 @@
+module.exports = app => {
+
+  class Version extends app.meta.BeanBase {
+
+    async update(options) {
+      if (options.version === 1) {
+
+      }
+    }
+
+    async init(options) { }
+
+  }
+
+  return Version;
+};
