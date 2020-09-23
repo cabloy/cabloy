b29ff43d97f3338c769a2412237f05d4363cbb14
zhennann <zhen.nann@icloud.com>
2020-09-23 20:30:30 +0800

    feat: a-flownode

---
+++ src/module-system/a-flownode/backend/src/meta.js
@@ -0,0 +1,19 @@
+module.exports = app => {
+  const schemas = require('./config/validation/schemas.js')(app);
+  const meta = {
+    base: {
+      atoms: {
+      },
+      functions: {
+      },
+    },
+    validation: {
+      validators: {
+      },
+      keywords: {},
+      schemas: {
+      },
+    },
+  };
+  return meta;
+};
