94d584f5eecc69bd1d4ca44c017f916615a6625f
zhennann <zhen.nann@icloud.com>
2020-11-05 23:10:07 +0800

    feat: remove custom component

--- src/module-system/a-cms/front/src/components.js
+++ src/module-system/a-cms/front/src/components.js
@@ -1,11 +1,9 @@
 import categoryList from './components/category/list.vue';
-import articleItem from './components/article/item.vue';
 import articleSearch from './components/article/articleSearch.vue';
 import renderArticleLanguage from './components/article/renderLanguage.jsx';
 
 export default {
   categoryList,
-  articleItem,
   articleSearch,
   renderArticleLanguage,
 };
