57eedc010cf23c80e4a8892c3d0a45ec9a5854a1
zhennann <zhen.nann@icloud.com>
2020-11-06 20:38:48 +0800

    feat: community post

--- src/module-system/cms-sitecommunity/backend/src/config/validation/schemas.js
+++ src/module-system/cms-sitecommunity/backend/src/config/validation/schemas.js
@@ -1,66 +1,119 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   const schemas = {};
   // post
   schemas.post = {
     type: 'object',
-    meta: {
-      custom: {
-        component: 'postItem',
-      },
-    },
     properties: {
+      atomId: {
+        type: 'number',
+      },
+      // title
+      groupTitle: {
+        type: 'null',
+        ebType: 'group-flatten',
+        ebTitle: 'Title',
+      },
       atomName: {
         type: 'string',
         ebType: 'text',
         ebTitle: 'Atom Name',
         notEmpty: true,
       },
+      // content
+      groupContent: {
+        type: 'null',
+        ebType: 'group-flatten',
+        ebTitle: 'Content',
+      },
+      content: {
+        type: 'string',
+        ebType: 'component',
+        ebTitle: 'Content',
+        ebRender: {
+          module: 'a-cms',
+          name: 'renderArticleContent',
+        },
+      },
+      // Basic Info
+      groupBasicInfo: {
+        type: 'null',
+        ebType: 'group-flatten',
+        ebTitle: 'Basic Info',
+        ebGroupWhole: true,
+      },
       language: {
         type: 'string',
-        ebType: 'select',
+        ebType: 'component',
         ebTitle: 'Language',
         ebMultiple: false,
         ebOptionsBlankAuto: true,
         notEmpty: true,
+        ebRender: {
+          module: 'a-cms',
+          name: 'renderArticleLanguage',
+        },
       },
       categoryId: {
         type: 'number',
-        ebType: 'text',
+        ebType: 'component',
         ebTitle: 'Category',
         notEmpty: true,
+        ebRender: {
+          module: 'a-cms',
+          name: 'renderArticleCategory',
+        },
       },
       tags: {
         type: 'string',
-        ebType: 'text',
+        ebType: 'component',
         ebTitle: 'Tags',
-      },
-      content: {
-        type: 'string',
-        ebType: 'text',
-        ebTitle: 'Content',
+        ebRender: {
+          module: 'a-cms',
+          name: 'renderArticleTags',
+        },
       },
     },
   };
   // post search
   schemas.postSearch = {
     type: 'object',
-    meta: {
-      custom: {
-        component: 'postSearch',
-      },
-    },
     properties: {
       language: {
         type: 'string',
-        ebType: 'select',
+        ebType: 'component',
         ebTitle: 'Language',
         ebMultiple: false,
         ebOptionsBlankAuto: true,
+        ebRender: {
+          module: 'a-cms',
+          name: 'renderArticleSearchLanguage',
+          options: {
+            props: {
+              atomClass: {
+                module: moduleInfo.relativeName,
+                atomClassName: 'post',
+              },
+            },
+          },
+        },
       },
       categoryId: {
         type: 'number',
-        ebType: 'text',
+        ebType: 'component',
         ebTitle: 'Category',
+        ebRender: {
+          module: 'a-cms',
+          name: 'renderArticleSearchCategory',
+          options: {
+            props: {
+              atomClass: {
+                module: moduleInfo.relativeName,
+                atomClassName: 'post',
+              },
+            },
+          },
+        },
       },
       content: {
         type: 'string',
--- src/module-system/cms-sitecommunity/front/src/components.js
+++ src/module-system/cms-sitecommunity/front/src/components.js
@@ -1,6 +1,2 @@
-import postItem from './components/post/item.vue';
-import postSearch from './components/post/search.vue';
 export default {
-  postItem,
-  postSearch,
 };
