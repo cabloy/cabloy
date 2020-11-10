3448428b75c255e7ac58d0c550945cf414b7dbd4
zhennann <zhen.nann@icloud.com>
2020-11-10 10:30:35 +0800

    feat: transaction

--- src/module-system/a-flownode/backend/src/routes.js
+++ src/module-system/a-flownode/backend/src/routes.js
@@ -2,8 +2,8 @@ module.exports = app => {
   const routes = [
     { method: 'post', path: 'task/select', controller: 'flowTask' },
     { method: 'post', path: 'task/count', controller: 'flowTask' },
-    { method: 'post', path: 'task/claim', controller: 'flowTask' },
-    { method: 'post', path: 'task/complete', controller: 'flowTask' },
+    { method: 'post', path: 'task/claim', controller: 'flowTask', middlewares: 'transaction' },
+    { method: 'post', path: 'task/complete', controller: 'flowTask', middlewares: 'transaction' },
   ];
   return routes;
 };
