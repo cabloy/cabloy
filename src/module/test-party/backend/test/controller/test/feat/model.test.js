5032a0af3825853d62480525140f54268a69af13
zhennann <zhen.nann@icloud.com>
2020-11-22 22:06:43 +0800

    update

--- src/module/test-party/backend/test/controller/test/feat/model.test.js
+++ src/module/test-party/backend/test/controller/test/feat/model.test.js
@@ -1,6 +1,6 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe.only('test/controller/test/feat/model.test.js', () => {
+describe('test/controller/test/feat/model.test.js', () => {
 
   it('action:model', async () => {
     const result = await app.httpRequest().post(mockUrl('test/feat/model'));
