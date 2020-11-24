a45608d2e84fb0833d019f19960190c087fb64ef
zhennann <zhen.nann@icloud.com>
2020-11-24 11:18:50 +0800

    feat: dashboard profile

--- src/module-system/a-dashboard/backend/src/bean/version.manager.js
+++ src/module-system/a-dashboard/backend/src/bean/version.manager.js
@@ -20,6 +20,65 @@ module.exports = app => {
         `;
         await this.ctx.model.query(sql);
       }
+
+      if (options.version === 2) {
+        // alter table: aDashboardProfile
+        // remove: userId/profileName/profileValue
+        // add: atomId description
+        let sql = `
+          ALTER TABLE aDashboardProfile
+            DROP COLUMN userId,
+            DROP COLUMN profileName,
+            DROP COLUMN profileValue,
+            ADD COLUMN atomId int(11) DEFAULT '0',
+            ADD COLUMN description varchar(255) DEFAULT NULL
+          `;
+        await this.ctx.model.query(sql);
+
+        // create table: aDashboardProfileContent
+        sql = `
+          CREATE TABLE aDashboardProfileContent (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            atomId int(11) DEFAULT '0',
+            itemId int(11) DEFAULT '0',
+            content JSON DEFAULT NULL,
+            PRIMARY KEY (id)
+          )
+        `;
+        await this.ctx.model.query(sql);
+
+        // create table: aDashboardProfileUser
+        sql = `
+          CREATE TABLE aDashboardProfileUser (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            userId int(11) DEFAULT '0',
+            profileDefault int(11) DEFAULT '0',
+            profileAtomId int(11) DEFAULT '0',
+            profileName varchar(255) DEFAULT NULL,
+            content JSON DEFAULT NULL,
+            PRIMARY KEY (id)
+          )
+        `;
+        await this.ctx.model.query(sql);
+
+        // create view: aDashboardProfileViewFull
+        sql = `
+          CREATE VIEW aDashboardProfileViewFull as
+            select a.*,b.content from aDashboardProfile a
+              left join aDashboardProfileContent b on a.id=b.itemId
+        `;
+        await this.ctx.model.query(sql);
+
+      }
+
     }
 
     async init(options) {
--- src/module/test-party/backend/test/controller/test.test.js
+++ src/module/test-party/backend/test/controller/test.test.js
@@ -1,6 +1,6 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe('action: atom: party', () => {
+describe.only('action: atom: party', () => {
   it('[atom]', async () => {
     app.mockSession({});
 
