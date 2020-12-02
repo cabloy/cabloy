08b7f6108072395c353830549c4ba04598db6931
zhennann <zhen.nann@icloud.com>
2020-12-02 20:57:38 +0800

    feat: renderCategory

--- src/module-system/a-basefront/front/src/components/render/renderCategory.jsx
+++ src/module-system/a-basefront/front/src/components/render/renderCategory.jsx
@@ -19,27 +19,46 @@ export default {
   created() {
   },
   methods: {
-    onChooseCategory() {
+    combineAtomClassAndLanguage(locale) {
+      const queries = {
+        module: this.atomClass.module,
+        atomClassName: this.atomClass.atomClassName,
+      };
+      if (locale) {
+        queries.language = locale.value;
+      }
+      return queries;
+    },
+    async onChooseCategory(event) {
       const { data } = this.context;
-
-      if (!data.language) {
-        this.$view.dialog.alert(this.$text('Please specify the language'));
-        return false;
+      const action = {
+        actionModule: 'a-base',
+        actionComponent: 'action',
+        name: 'selectLocale',
+        targetEl: event.target,
+      };
+      let locale;
+      if (data.atomLanguage) {
+        locale = data.atomLanguage;
+      } else {
+        locale = await this.$meta.util.performAction({ ctx: this, action, item: data });
+        if (locale) {
+          data.atomLanguage = locale.value;
+        }
       }
       return new Promise(resolve => {
-        const url = this.combineAtomClass('/a/cms/category/select');
+        const url = this.$meta.util.combineQueries('/a/basefront/category/select', this.combineAtomClassAndLanguage(locale));
         this.$view.navigate(url, {
           target: '_self',
           context: {
             params: {
-              language: data.language,
               categoryIdStart: 0,
               leafOnly: true,
             },
             callback: (code, node) => {
               if (code === 200) {
-                data.categoryId = node.id;
-                data.categoryName = node.data.categoryName;
+                data.atomCategoryId = node.id;
+                data.atomCategoryName = node.data.categoryName;
                 resolve(true);
               } else if (code === false) {
                 resolve(false);
@@ -56,14 +75,14 @@ export default {
     if (validate.readOnly || property.ebReadOnly) {
       return (
         <f7-list-item title={title}>
-          <div slot="after">{data.categoryName}</div>
+          <div slot="after">{data.atomCategoryName}</div>
         </f7-list-item>
       );
     }
     return (
       <eb-list-item-choose
         link="#" dataPath={dataPath} title={title} propsOnChoose={this.onChooseCategory}>
-        <div slot="after">{data.categoryName}</div>
+        <div slot="after">{data.atomCategoryName}</div>
       </eb-list-item-choose>
     );
   },
