99e19672f9a31c2781345bdb31ef9b0486d2fc2f
zhennann <zhen.nann@icloud.com>
2020-12-05 10:34:50 +0800

    feat: remove only

--- src/module-system/a-cms/backend/test/controller/test.test.js
+++ src/module-system/a-cms/backend/test/controller/test.test.js
@@ -1,6 +1,6 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe.only('test/controller/test.test.js', () => {
+describe('test/controller/test.test.js', () => {
 
   it('action:set config', async () => {
     app.mockSession({});
