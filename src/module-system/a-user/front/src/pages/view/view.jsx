dca7d99e8e5cf19a363a0d79c543a85f270a99a5
zhennann <zhen.nann@icloud.com>
2020-12-12 16:19:01 +0800

    feat: button style

--- src/module-system/a-user/front/src/pages/view/view.jsx
+++ src/module-system/a-user/front/src/pages/view/view.jsx
@@ -99,7 +99,7 @@ export default {
           <eb-list-item title={this.$text('Sidebar (Left)')} link="#" propsOnPerform={event => this.onPerformResources(event, 'left', 'panels', 'a-layoutpc:sidebarPanel')}></eb-list-item>
           <eb-list-item title={this.$text('Sidebar (Right)')} link="#" propsOnPerform={event => this.onPerformResources(event, 'right', 'panels', 'a-layoutpc:sidebarPanel')}></eb-list-item>
           <eb-list-item title={this.$text('Statusbar (Left)')} link="#" propsOnPerform={event => this.onPerformResources(event, 'bottom', 'panels', 'a-layoutpc:sidebarPanel')}></eb-list-item>
-          <eb-list-item title={this.$text('Statusbar (Right)')} link="#" propsOnPerform={event => this.onPerformResources(event, 'bottom', 'sections', 'a-layoutpc:statusbarSection')}></eb-list-item>
+          <eb-list-item title={this.$text('Statusbar (Right)')} link="#" propsOnPerform={event => this.onPerformResources(event, 'bottom', 'buttons', 'a-layoutpc:headerButton')}></eb-list-item>
         </f7-list>
       </eb-page>
     );
