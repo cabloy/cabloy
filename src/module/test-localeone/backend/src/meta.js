49123cb8b60a71a24f1ef72cdd3337191f566c17
zhennann <zhen.nann@icloud.com>
2020-12-13 21:53:49 +0800

    feat: locals

---
+++ src/module/test-localeone/backend/src/meta.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+  const schemas = require('./config/validation/schemas.js')(app);
+  const meta = {
+    base: {
+      atoms: {
+      },
+    },
+    validation: {
+      validators: {
+      },
+      keywords: {},
+      schemas: {
+      },
+    },
+  };
+  return meta;
+};
---
+++ src/module/test-localetwo/README.md
@@ -0,0 +1,4 @@
+# egg-born-module-a-themehyacinth
+
+This is a cabloy theme named as `hyacinth`
+
