f6a36baf16fda57aa7fbe7ebc8e215b491efbab1
zhennann <zhen.nann@icloud.com>
2020-12-23 22:52:31 +0800

    chore: github actions

--- src/backend/_config/config.unittest.js
+++ src/backend/_config/config.unittest.js
@@ -13,7 +13,7 @@ module.exports = appInfo => {
     clients: {
       // donnot change the name
       __ebdb: {
-        host: 'mysql',
+        host: '127.0.0.1',
         port: '3306',
         user: 'root',
         password: '',
@@ -29,7 +29,7 @@ module.exports = appInfo => {
 
   // redis
   const __redisConnectionDefault = {
-    host: 'redis',
+    host: '127.0.0.1',
     port: 6379,
     password: '',
     db: 0,
