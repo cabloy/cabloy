06bcf177898c50d3a740fd432d41c743ecff29dd
zhennann <zhen.nann@icloud.com>
2021-01-06 09:35:51 +0800

    Publish
     - cabloy@4.0.33-alpha.0
     - egg-born-bin@3.0.9
     - egg-born-front@8.0.6
     - egg-born-module-a-base-sync@4.0.19
     - egg-born-module-a-login@4.0.7

--- src/module-system/a-base-sync/dist/backend.js
+++ src/module-system/a-base-sync/dist/backend.js
@@ -3504,10 +3504,9 @@ module.exports = ctx => {
           await ctx.bean.role.deleteUserRole({ userId, roleId: roleRegistered.id });
         }
         // add to activated
-        const roleNames = this.config.account.activatedRoles.split(',');
-        for (const roleName of roleNames) {
-          if (!map[roleName]) {
-            const role = await ctx.bean.role.get({ roleName });
+        const rolesActivated = await ctx.bean.role.parseRoleNames({ roleNames: this.config.account.activatedRoles });
+        for (const role of rolesActivated) {
+          if (!map[role.roleName]) {
             await ctx.bean.role.addUserRole({ userId, roleId: role.id });
           }
         }
@@ -7807,7 +7806,7 @@ module.exports = appInfo => {
     needActivation: true,
     activationWays: 'mobile,email',
     activationProviders: {
-      mobile: '', // a-authsms recommended
+      mobile: 'a-authsms',
       email: 'a-authsimple',
     },
     url: {
--- src/module-system/a-login/dist/backend.js
+++ src/module-system/a-login/dist/backend.js
@@ -16,6 +16,11 @@ module.exports = appInfo => {
       provider: 'authsimple',
     },
     {
+      module: 'a-authsms',
+      provider: 'authsms',
+    },
+    {
+      // disable: true,
       module: 'a-authgithub',
       provider: 'authgithub',
     },
