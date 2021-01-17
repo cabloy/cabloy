7f80d509957f7a3df86d9014e69bb508fef4efab
zhennann <zhen.nann@icloud.com>
2021-01-17 19:06:50 +0800

    fix: checkAppReadyInstance({ startup: true })

--- src/module-system/a-socketio/backend/src/bean/middleware.connection.js
+++ src/module-system/a-socketio/backend/src/bean/middleware.connection.js
@@ -2,7 +2,7 @@ module.exports = ctx => {
   const app = ctx.app;
   class Middleware {
     async execute(options, next) {
-      const appReadyInstance = await ctx.bean.instance.checkAppReadyInstance({ startup: false });
+      const appReadyInstance = await ctx.bean.instance.checkAppReadyInstance({ startup: true });
       if (!appReadyInstance) return ctx.throw(403);
       // cache userId/socketId for disconnect
       const user = ctx.session.passport.user.op;
