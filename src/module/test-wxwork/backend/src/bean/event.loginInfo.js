a2aac77ad38e123664863cf862570afa5c2742d4
zhennann <zhen.nann@icloud.com>
2021-01-28 11:57:26 +0800

    feat: wxwork layout test

--- src/module/test-wxwork/backend/src/bean/event.loginInfo.js
+++ src/module/test-wxwork/backend/src/bean/event.loginInfo.js
@@ -12,16 +12,9 @@ module.exports = ctx => {
           modules: {
             'a-layoutmobile': {
               layout: {
-                login: '/a/login/login',
-                loginOnStart: true,
-                toolbar: {
-                  tabbar: true, labels: true, bottomMd: true,
+                scene: {
+                  web: 'test-wxwork:layoutTest',
                 },
-                tabs: [
-                  { name: 'Test', tabLinkActive: true, iconMaterial: 'group_work', url: '/test/wxwork/test/index' },
-                  { name: 'Home', tabLinkActive: false, iconMaterial: 'home', url: '/a/basefront/menu/list' },
-                  { name: 'Mine', tabLinkActive: false, iconMaterial: 'person', url: '/a/user/user/mine' },
-                ],
               },
             },
           },
