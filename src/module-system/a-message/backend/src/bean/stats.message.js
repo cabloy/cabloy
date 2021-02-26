d572332bc10d7890ba5137a1f89fef0c9ba113ab
zhennann <zhen.nann@icloud.com>
2021-02-26 18:12:59 +0800

    feat: stats.message

--- src/module-system/a-message/backend/src/bean/stats.message.js
+++ src/module-system/a-message/backend/src/bean/stats.message.js
@@ -5,6 +5,51 @@ module.exports = ctx => {
     async execute(context) {
       const { keys, user } = context;
       const fullName = keys.join('.');
+      if (keys.length === 2) {
+        // messageClass
+        const [ module, messageClassName ] = keys[1].split('_');
+        const messageClass = { module, messageClassName };
+        const messageClassBase = ctx.bean.io.messageClass.messageClass(messageClass);
+        // options
+        const options = {
+          where: {
+            messageRead: 0,
+          },
+        };
+        // count
+        const res = await ctx.bean.io.message.count({ messageClass, options, user });
+        const count = res.count;
+        // stat
+        const color = messageClassBase.info.uniform.stats.color;
+        return { [color]: count };
+      } else if (keys.length === 1) {
+        // message
+        const modelStats = ctx.model.module('a-stats').stats;
+        const items = await modelStats.select({
+          where: {
+            module: moduleInfo.relativeName,
+            name: {
+              op: 'likeRight',
+              val: 'message.',
+            },
+            userId: user.id,
+          },
+        });
+        // count
+        const stat = {
+          red: 0,
+          gray: 0,
+        };
+        for (const item of items) {
+          // only level 2
+          if (item.name.split('.').length !== 2) continue;
+          const value = JSON.parse(item.value);
+          if (value.red !== undefined) stat.red += value.red;
+          if (value.gray !== undefined) stat.gray += value.gray;
+        }
+        // ok
+        return stat;
+      }
     }
 
   }
