090b82b0f81fb74f80fa0cbbc8f69bfb3bdc5989
zhennann <zhen.nann@icloud.com>
2021-02-28 15:38:19 +0800

    feat: message publish for flow end

--- src/module-system/a-flow/backend/src/bean/local.flow.flow.js
+++ src/module-system/a-flow/backend/src/bean/local.flow.flow.js
@@ -150,11 +150,45 @@ module.exports = ctx => {
         await this._flowListener.onFlowEnd(options);
         // clear nodes
         await this._clearNodeRemains();
+        // publish uniform message
+        await this._endFlowPublish();
         // log
         // console.log(`--------flow end: ${flowId}`);
       });
       // notify
       this._notifyFlowInitiateds(this.context._flow.flowUserId);
+    }
+
+    async _endFlowPublish() {
+      // publish uniform message
+      const userOp = this._getOpUser();
+      const flowUserId = this.context._flow.flowUserId;
+      if (flowUserId !== userOp.id) {
+        const userFlow = await ctx.bean.user.get({ id: flowUserId });
+        const title = `${ctx.text.locale(userFlow.locale, 'FlowTitle')} - ${ctx.text.locale(userFlow.locale, this.context._flow.flowRemark || 'End')}`;
+        const actionPath = `/a/flowtask/flow?flowId=${this.context._flowId}`;
+        const message = {
+          userIdTo: flowUserId,
+          content: {
+            userId: userFlow.id,
+            userName: userFlow.userName,
+            userAvatar: userFlow.avatar,
+            title,
+            body: this.context._flow.flowName,
+            actionPath,
+            params: {
+              flowId: this.context._flowId,
+            },
+          },
+        };
+        await ctx.bean.io.publish({
+          message,
+          messageClass: {
+            module: 'a-flow',
+            messageClassName: 'workflow',
+          },
+        });
+      }
     }
 
     async _clearNodeRemains() {
