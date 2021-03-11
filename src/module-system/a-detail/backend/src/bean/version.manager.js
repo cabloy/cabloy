344d995fd6e7b143df5bee42bf269a8d358ce2c1
zhennann <zhen.nann@icloud.com>
2021-03-11 17:51:38 +0800

    feat: table: aDetail

--- src/module-system/a-detail/backend/src/bean/version.manager.js
+++ src/module-system/a-detail/backend/src/bean/version.manager.js
@@ -2,6 +2,28 @@ module.exports = app => {
   class Version extends app.meta.BeanBase {
 
     async update(options) {
+      if (options.version === 1) {
+        // create table: aDetail
+        const sql = `
+          CREATE TABLE aDetail (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            atomId int(11) DEFAULT '0',
+            detailClassId int(11) DEFAULT '0',
+            detailCodeId int(11) DEFAULT '0',
+            detailCode varchar(255) DEFAULT NULL,
+            detailName varchar(255) DEFAULT NULL,
+            detailLineNo int(11) DEFAULT '0',
+            userIdCreated int(11) DEFAULT '0',
+            userIdUpdated int(11) DEFAULT '0',
+            PRIMARY KEY (id)
+          )
+        `;
+        await this.ctx.model.query(sql);
+      }
     }
 
     async init(options) {
