927c0778848496a6256bab9f35d791c685ff1ee0
zhennann <zhen.nann@icloud.com>
2021-03-15 17:18:56 +0800

    feat: details render

--- src/module-system/a-components-sync/front/src/common/validate/render/renderDetails.js
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderDetails.js
@@ -1,36 +1,9 @@
 export default {
   methods: {
     renderDetails(c, context) {
-      const { data, pathParent, key, property, dataPath } = context;
-      const renderProps = this.$meta.util.extend(
-        { options: { props: {} } },
-        {
-          module: 'a-detail',
-          name: 'renderDetails',
-        });
-      renderProps.options.props.context = {
-        validate: this.validate,
-        validateItem: this,
-        data,
-        pathParent,
-        key,
-        schema: context.schema,
-        properties: context.properties,
-        property,
-        dataPath,
-        meta: context.meta,
-        getTitle: notHint => {
-          return this.getTitle(context, notHint);
-        },
-        getValue: () => {
-          return this.getValue(data, key, property);
-        },
-        setValue: value => {
-          this.setValue(data, key, value, property);
-        },
-      };
-      return c('eb-list-item-component', {
-        props: renderProps,
+      return this._renderComponent(c, context, {
+        module: 'a-detail',
+        name: 'renderDetails',
       });
     },
   },
