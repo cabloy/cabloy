099b48f15d173e27a83818d61256f0649eed8e17
zhennann <zhen.nann@icloud.com>
2021-03-17 15:05:34 +0800

    feat: detail item

---
+++ src/module-system/a-detail/front/src/pages/detail/item.jsx
@@ -0,0 +1,33 @@
+import itemLayoutManager from '../../common/itemLayoutManager/index.jsx';
+export default {
+  meta: {
+    size: 'medium',
+  },
+  mixins: [ itemLayoutManager ],
+  data() {
+    const query = this.$f7route.query;
+    const mode = query.mode;
+    const atomId = parseInt(query.atomId || 0);
+    const itemId = parseInt(query.itemId || 0);
+    const layout = query.layout;
+    return {
+      container: {
+        mode,
+        atomId,
+        itemId,
+        layout,
+      },
+    };
+  },
+  render() {
+    return (
+      <eb-page withSubnavbar={this.subnavbar.enable}>
+        <eb-navbar title={this.page_getTitle()} subtitle={this.page_getSubtitle()} eb-back-link="Back">
+          {this.layout_renderBlock({ blockName: 'title' })}
+          {this.layout.instance && this.subnavbar.enable && this.layout_renderBlock({ blockName: 'subnavbar' })}
+        </eb-navbar>
+        {this.layout_renderLayout()}
+      </eb-page>
+    );
+  },
+};
