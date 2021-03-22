87497082c03e9ef953fbbbe516fc09afaa72806c
zhennann <zhen.nann@icloud.com>
2021-03-22 21:21:22 +0800

    feat: details price

--- src/module/test-flow/front/src/config/config.js
+++ src/module/test-flow/front/src/config/config.js
@@ -14,4 +14,46 @@ export default {
       },
     },
   },
+  details: {
+    default: {
+      render: {
+        list: {
+          layouts: {
+            table: {
+              blocks: {
+                items: {
+                  columns: [
+                    {
+                      dataIndex: 'detailLineNo',
+                      title: '#',
+                      align: 'center',
+                      width: '50px',
+                      component: {
+                        module: 'a-detail',
+                        name: 'listLayoutTableCellDetailLineNo',
+                      },
+                    },
+                    {
+                      dataIndex: 'detailName',
+                      title: 'Name',
+                      align: 'left',
+                      component: {
+                        module: 'a-detail',
+                        name: 'listLayoutTableCellDetailName',
+                      },
+                    },
+                    {
+                      dataIndex: 'price',
+                      title: 'Price',
+                      align: 'left',
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 };
