7547c6a1c7cd449140d3378917ef76d4c41a2f39
zhennann <zhen.nann@icloud.com>
2021-04-09 19:58:45 +0800

    feat: EditInPCTip

--- src/module-system/a-flowchart/front/src/config/locale/en-us.js
+++ src/module-system/a-flowchart/front/src/config/locale/en-us.js
@@ -2,4 +2,5 @@ export default {
   flowDefDiagramTitle: 'Diagram',
   flowDefSourceTitle: 'Source',
   flowDefListenerTitle: 'Listener',
+  EditInPCTip: 'Please edit in PC',
 };
--- src/module-system/a-flowchart/front/src/config/locale/zh-cn.js
+++ src/module-system/a-flowchart/front/src/config/locale/zh-cn.js
@@ -2,5 +2,6 @@ export default {
   flowDefDiagramTitle: '流程图',
   flowDefSourceTitle: '源文件',
   flowDefListenerTitle: '监听器',
+  EditInPCTip: '请在PC中编辑',
   'Flow Chart': '流程图',
 };
