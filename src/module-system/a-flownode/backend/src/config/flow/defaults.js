42154e928bc80138c2bbcd94b9e46585fc90d877
zhennann <zhen.nann@icloud.com>
2021-04-10 20:01:59 +0800

    feat: defaults of node

--- src/module-system/a-flownode/backend/src/config/flow/defaults.js
+++ src/module-system/a-flownode/backend/src/config/flow/defaults.js
@@ -1,2 +1,22 @@
 module.exports = {
+  sequence: {
+    conditionExpression: null,
+  },
+  startEventTimer: {
+    repeat: {
+      every: 0,
+    },
+    bean: {
+      module: null,
+      name: null,
+    },
+    parameterExpression: null,
+  },
+  activityService: {
+    bean: {
+      module: null,
+      name: null,
+    },
+    parameterExpression: null,
+  },
 };
--- src/module-system/a-flownode/backend/src/config/flow/edges.js
+++ src/module-system/a-flownode/backend/src/config/flow/edges.js
@@ -1,6 +1,18 @@
-module.exports = {
+const defaults = require('./defaults.js');
+
+const edges = {
   sequence: {
     title: 'Sequence',
     bean: 'sequence',
   },
 };
+
+for (const key in edges) {
+  const node = edges[key];
+  node.options = {};
+  if (defaults[key]) {
+    node.options.default = defaults[key];
+  }
+}
+
+module.exports = edges;
