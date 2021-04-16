8130c0e33edeb81dd15faf4f5c8b8f5fba9884a9
zhennann <zhen.nann@icloud.com>
2021-04-16 19:20:30 +0800

    feat: assignees-vars

---
+++ src/module-system/a-flowchart/front/src/components/assignees/vars.jsx
@@ -0,0 +1,84 @@
+export default {
+  props: {
+    assignees: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    onPerformAdd() {
+      const flowUser = {
+        name: 'flowUser',
+        title: 'FlowInitiator',
+        titleLocale: this.$text('FlowInitiator'),
+      };
+      this.assignees.vars.push(flowUser);
+    },
+    onPerformRemove(event, item, index) {
+      this.assignees.vars.splice(index, 1);
+      this.$meta.util.swipeoutClose(event.target);
+    },
+    _renderAssignee(item, index) {
+      // domTitle
+      const domTitle = (
+        <div slot="title" class="title">
+          <div>{item.titleLocale}</div>
+        </div>
+      );
+      // ok
+      //   key: not use item.name
+      return (
+        <eb-list-item class="item" key={index} swipeout>
+          {domTitle}
+          {this._renderAssigneeContextMenu(item, index)}
+        </eb-list-item>
+      );
+    },
+    _renderAssigneeContextMenu(item, index) {
+      // domRight
+      const domActions = [];
+      domActions.push(
+        <div key='remove' color='red' propsOnPerform={event => this.onPerformRemove(event, item, index)}>
+          <f7-icon slot="media" material='delete'></f7-icon>
+          {this.$device.desktop && <div slot="title">{this.$text('Remove')}</div>}
+        </div>
+      );
+      const domRight = (
+        <div slot="right">
+          {domActions}
+        </div>
+      );
+
+      return (
+        <eb-context-menu>
+          {domRight}
+        </eb-context-menu>
+      );
+    },
+    renderAssignees() {
+      const children = [];
+      const items = this.assignees.vars;
+      for (let index = 0; index < items.length; index++) {
+        children.push(this._renderAssignee(items[index], index));
+      }
+      return children;
+    },
+  },
+  render() {
+    return (
+      <f7-list-group>
+        <f7-list-item group-title>
+          <div class="display-flex justify-content-space-between">
+            <div>{this.$text('Variables')}</div>
+            <eb-link iconMaterial='add' propsOnPerform={this.onPerformAdd}></eb-link>
+          </div>
+        </f7-list-item>
+        {this.renderAssignees()}
+      </f7-list-group>
+    );
+  },
+};
+
--- src/module-system/a-flowchart/front/src/pages/flowDef/assigneesEdit.jsx
+++ src/module-system/a-flowchart/front/src/pages/flowDef/assigneesEdit.jsx
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import assigneesVars from '../../components/assignees/vars.jsx';
 const ebPageContext = Vue.prototype.$meta.module.get('a-components').options.mixins.ebPageContext;
 export default {
   mixins: [ ebPageContext ],
+  components: {
+    assigneesVars,
+  },
   data() {
     return {
       flowDefId: parseInt(this.$f7route.query.flowDefId),
@@ -44,79 +48,12 @@ export default {
     onPerformDone() {
 
     },
-    onPerformAddVar() {
-      const flowUser = {
-        name: 'flowUser',
-        title: 'FlowInitiator',
-        titleLocale: this.$text('FlowInitiator'),
-      };
-      this.assignees.vars.push(flowUser);
-    },
-    onPerformRemoveVar(event, item, index) {
-      this.assignees.vars.splice(index, 1);
-      this.$meta.util.swipeoutClose(event.target);
-    },
-    _renderAssignees_var(item, index) {
-      // domTitle
-      const domTitle = (
-        <div slot="title" class="title">
-          <div>{item.titleLocale}</div>
-        </div>
-      );
-      // ok
-      //   key: not use item.name
-      return (
-        <eb-list-item class="item" key={index} swipeout>
-          {domTitle}
-          {this._renderAssignees_var_ContextMenu(item, index)}
-        </eb-list-item>
-      );
-    },
-    _renderAssignees_var_ContextMenu(item, index) {
-      // domRight
-      const domActions = [];
-      domActions.push(
-        <div key='remove' color='red' propsOnPerform={event => this.onPerformRemoveVar(event, item, index)}>
-          <f7-icon slot="media" material='delete'></f7-icon>
-          {this.$device.desktop && <div slot="title">{this.$text('Remove')}</div>}
-        </div>
-      );
-      const domRight = (
-        <div slot="right">
-          {domActions}
-        </div>
-      );
-
-      return (
-        <eb-context-menu>
-          {domRight}
-        </eb-context-menu>
-      );
-    },
-    renderAssignees_vars() {
-      const children = [];
-      const items = this.assignees.vars;
-      for (let index = 0; index < items.length; index++) {
-        children.push(this._renderAssignees_var(items[index], index));
-      }
-      return (
-        <f7-list-group>
-          <f7-list-item group-title>
-            <div class="display-flex justify-content-space-between">
-              <div>{this.$text('Variables')}</div>
-              <eb-link iconMaterial='add' propsOnPerform={this.onPerformAddVar}></eb-link>
-            </div>
-          </f7-list-item>
-          {children}
-        </f7-list-group>
-      );
-    },
     renderAssignees() {
       if (!this.ready) return;
       // list
       return (
         <eb-list form inline-labels no-hairlines-md>
-          {this.renderAssignees_vars()}
+          <assignees-vars assignees={this.assignees}></assignees-vars>
         </eb-list>
       );
     },
