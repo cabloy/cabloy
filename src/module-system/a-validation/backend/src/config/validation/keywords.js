e90fde29c7f3ba48f83019d288bab3175250cbbf
zhennann <zhen.nann@icloud.com>
2021-04-18 21:16:31 +0800

    refactor: remove test resources of a-validation

--- src/module-system/a-validation/backend/src/config/validation/keywords.js
+++ src/module-system/a-validation/backend/src/config/validation/keywords.js
@@ -1,22 +1,4 @@
 module.exports = app => {
   const keywords = {};
-  keywords.languages = {
-    async: true,
-    type: 'string',
-    errors: true,
-    compile() {
-      return async function(data) {
-        const ctx = this;
-        return new Promise((resolve, reject) => {
-          const res = [ 'zh-cn', 'en-us' ].indexOf(data) > -1;
-          if (!res) {
-            const errors = [{ keyword: 'x-languages', params: [], message: ctx.text('Not Expected Value') }];
-            return reject(new app.meta.ajv.ValidationError(errors));
-          }
-          return resolve(res);
-        });
-      };
-    },
-  };
   return keywords;
 };
