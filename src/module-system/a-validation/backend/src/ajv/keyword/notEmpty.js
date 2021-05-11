58b118a2004773ea81cb10f0e6cb521023cf988d
zhennann <zhen.nann@icloud.com>
2021-05-11 14:20:42 +0800

    feat: notEmpty.expression

--- src/module-system/a-validation/backend/src/ajv/keyword/notEmpty.js
+++ src/module-system/a-validation/backend/src/ajv/keyword/notEmpty.js
@@ -1,8 +1,18 @@
+const vm = require('vm');
+
 module.exports = {
   errors: true,
   compile(schema, schemaProperty) {
-    const fun = function(data) {
-      if (schema && checkIfEmpty(schemaProperty, data)) {
+    const fun = function(data, path, rootData) {
+      // notEmpty=false
+      if (!schema) return true;
+      // expression
+      const expression = schema && schema.expression;
+      if (expression) {
+        const res = evaluateExpression({ expression, rootData, ctx: this });
+        if (!res) return true;
+      }
+      if (checkIfEmpty(schemaProperty, data)) {
         fun.errors = [{ keyword: 'notEmpty', params: [], message: this.text('RequiredField') }];
         return false;
       }
@@ -11,6 +21,17 @@ module.exports = {
     return fun;
   },
 };
+
+function evaluateExpression({ expression, rootData, ctx }) {
+  const scope = {
+    ... rootData,
+    _meta: {
+      host: ctx.meta && ctx.meta.validateHost,
+      user: ctx.state.user && ctx.state.user.op,
+    },
+  };
+  return vm.runInContext(expression, vm.createContext(scope));
+}
 
 function checkIfEmpty(schemaProperty, value) {
   const type = schemaProperty.type;
