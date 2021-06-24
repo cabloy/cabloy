958505f32330293369f6f0ca7a1abe8fb3438e32
zhennann <zhen.nann@icloud.com>
2021-06-24 09:30:51 +0800

    refactor: egg-born-bin

---
+++ scripts/egg-born-bin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+
+// egg-born-bin
+const pathEggBornBin = __getPathEggBornBin();
+if (!pathEggBornBin) return;
+require(pathEggBornBin);
+
+function __getPathEggBornBin() {
+  const basePath = path.join(__dirname, '../');
+  let cabloyPath = path.join(basePath, 'node_modules/egg-born-bin/bin/egg-born-bin.js');
+  if (fs.existsSync(cabloyPath)) return cabloyPath;
+  cabloyPath = path.join(basePath, 'packages/egg-born-bin/bin/egg-born-bin.js');
+  if (fs.existsSync(cabloyPath)) return cabloyPath;
+  return null;
+}
