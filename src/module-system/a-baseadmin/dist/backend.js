57e3fec31c3592689ce6b760fd6787200cba905a
zhennann <zhen.nann@icloud.com>
2021-07-04 23:39:03 +0800

    Publish
     - @zhennann/build@1.1.9
     - cabloy@4.11.7
     - egg-born-bin@3.3.4
     - egg-born-template-cabloy@4.11.9
     - egg-born-module-a-baseadmin@4.1.8
     - egg-born-module-cms-themeaws@4.1.20
     - egg-born-module-cms-themeblog@4.1.21

--- src/module-system/a-baseadmin/dist/backend.js
+++ src/module-system/a-baseadmin/dist/backend.js
@@ -9,15 +9,15 @@ module.exports = app => {
   class Version extends app.meta.BeanBase {
 
     async update(options) {
-      if (options.version === 1) {}
+      if (options.version === 1) { }
     }
 
     async init(options) {
-      if (options.version === 1) {}
+      if (options.version === 1) { }
 
-      if (options.version === 2) {}
+      if (options.version === 2) { }
 
-      if (options.version === 3) {}
+      if (options.version === 3) { }
 
     }
 
@@ -1119,7 +1119,7 @@ module.exports = app => {
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("require3");;
+module.exports = require("require3");
 
 /***/ })
 
