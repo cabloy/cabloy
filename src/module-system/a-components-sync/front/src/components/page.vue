a85d29b00b331b440dc7e09d452851da3724a500
zhennann <zhen.nann@icloud.com>
2021-08-13 22:11:36 +0800

    feat: page title

--- src/module-system/a-components-sync/front/src/components/page.vue
+++ src/module-system/a-components-sync/front/src/components/page.vue
@@ -12,39 +12,6 @@ export default {
       pageTitle: null,
     };
   },
-  methods: {
-    setPageTitle(title) {
-      this.pageTitle = title;
-      this.onTitleChange(false);
-    },
-    onPageAfterIn(page) {
-      if (this.eventTargetEl !== page.el) return;
-      this.setState({
-        routerPositionClass: 'page-current',
-      });
-      this.dispatchEvent('page:afterin pageAfterIn', page);
-
-      // title
-      if (this.pageTitle === null) {
-        let title = page.$el.find('.navbar .title').text();
-        if (!title) {
-          // try get meta
-          const page = this.$page;
-          const _title = page && page.$options.meta && page.$options.meta.title;
-          if (_title) {
-            title = this.$text(_title);
-          }
-        }
-        this.pageTitle = title;
-      }
-      this.onTitleChange(true);
-    },
-    onTitleChange(force) {
-      if (force || this.$$(this.$el).hasClass('page-current')) {
-        this.$view.$emit('view:title', { page: this, title: this.pageTitle });
-      }
-    },
-  },
   mounted() {
     const navbar = this.$$(this.$el).find('.navbar');
     if (navbar.length > 0) {
@@ -59,6 +26,50 @@ export default {
       this._watchTitle();
       this._watchTitle = null;
     }
+  },
+  methods: {
+    setPageTitle(title) {
+      this.pageTitle = title;
+      this.onTitleChange(false);
+    },
+    _tryParsePageTitle() {
+      let title;
+      // navbar value
+      const navbar = this.$$(this.$el).find('.navbar');
+      if (navbar.length > 0) {
+        title = navbar[0].__vue__.title;
+        if (title) return title;
+      }
+      // navbar dom
+      title = this.$$(this.$el).find('.navbar .title').text();
+      if (title) return title;
+      // page meta
+      const page = this.$page;
+      const _title = page && page.$options.meta && page.$options.meta.title;
+      if (_title) {
+        title = this.$text(_title);
+      }
+      // ok
+      return title;
+    },
+    onPageAfterIn(page) {
+      if (this.eventTargetEl !== page.el) return;
+      this.setState({
+        routerPositionClass: 'page-current',
+      });
+      this.dispatchEvent('page:afterin pageAfterIn', page);
+
+      // title
+      if (this.pageTitle === null) {
+        this.pageTitle = this._tryParsePageTitle();
+      }
+      this.onTitleChange(true);
+    },
+    onTitleChange(force) {
+      if (force || this.$$(this.$el).hasClass('page-current')) {
+        this.$view.$emit('view:title', { page: this, title: this.pageTitle });
+      }
+    },
   },
 };
 </script>
