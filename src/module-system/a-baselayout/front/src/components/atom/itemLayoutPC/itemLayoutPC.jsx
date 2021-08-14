f3eed4335a6e3533274d3c40b2dbf999ef71ed79
zhennann <zhen.nann@icloud.com>
2021-08-14 09:34:59 +0800

    refactor: item layout

--- src/module-system/a-baselayout/front/src/components/atom/itemLayoutPC/itemLayoutPC.jsx
+++ src/module-system/a-baselayout/front/src/components/atom/itemLayoutPC/itemLayoutPC.jsx
@@ -14,26 +14,21 @@ export default {
     return {};
   },
   created() {
-    this.layoutManager.layout.instance = this;
-    this.layoutManager.subnavbar.enable = false;
+    this.init();
+  },
+  beforeDestroy() {
+    // eslint-disable-next-line
+    this.layoutManager.layout.instance = null;
   },
   methods: {
-    getBlockComponentOptions({ blockConfig }) {
-      return {
-        props: {
-          layoutManager: this.layoutManager,
-          layout: this,
-          blockConfig,
-        },
-      };
-    },
-    _renderBlock({ blockName }) {
-      const blockConfig = this.layoutConfig.blocks[blockName];
-      if (!blockConfig) return null;
-      return <eb-component module={blockConfig.component.module} name={blockConfig.component.name} options={this.getBlockComponentOptions({ blockConfig })}></eb-component>;
+    init() {
+      // eslint-disable-next-line
+      this.layoutManager.layout.instance = this;
+      // eslint-disable-next-line
+      this.layoutManager.subnavbar.enable = false;
     },
   },
   render() {
-    return <div>{this._renderBlock({ blockName: 'main' })}</div>;
+    return <div>{this.layoutManager.layout_renderBlock({ blockName: 'main' })}</div>;
   },
 };
