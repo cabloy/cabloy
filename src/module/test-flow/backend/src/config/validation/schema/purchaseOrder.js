aca6d5dfcb561e09110a52cc4ae6f75cc7b2fd05
zhennann <zhen.nann@icloud.com>
2021-08-15 15:16:13 +0800

    feat: Details Stats

--- src/module/test-flow/backend/src/config/validation/schema/purchaseOrder.js
+++ src/module/test-flow/backend/src/config/validation/schema/purchaseOrder.js
@@ -44,7 +44,7 @@ module.exports = app => {
       // Stats
       __groupStats: {
         ebType: 'group-flatten',
-        ebTitle: 'Stats',
+        ebTitle: 'Details Stats',
       },
       detailsCount: {
         type: 'number',
--- src/module/test-flow/front/src/config/locale/zh-cn.js
+++ src/module/test-flow/front/src/config/locale/zh-cn.js
@@ -4,6 +4,7 @@ export default {
   Price: '价格',
   Quantity: '数量',
   Amount: '金额',
+  'Details Stats': '明细统计',
   'Flow Definition': '流程定义',
   'Product Code': '产品编码',
   'Product Name': '产品名称',
