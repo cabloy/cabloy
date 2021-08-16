fb968f7f5fc222e5ea8c68f6dcc8bae3eba6fbab
zhennann <zhen.nann@icloud.com>
2021-08-16 21:15:47 +0800

    refactor: party over

--- src/module/test-party/front/src/components/action/actionPartyOver.js
+++ src/module/test-party/front/src/components/action/actionPartyOver.js
@@ -5,6 +5,7 @@ export default {
       const key = { atomId: item.atomId, itemId: item.itemId };
       await ctx.$api.post('/test/party/party/over', { key });
       ctx.$meta.eventHub.$emit('atom:action', { key, action: { name: 'save' } });
+      return true;
     },
   },
 };
