299310c072eecdfa638d76daa075817125b9b1c6
zhennann <zhen.nann@icloud.com>
2021-08-17 08:46:17 +0800

    refactor: actionHandleTask

--- src/module-system/a-flowtask/front/src/components/action/action.js
+++ src/module-system/a-flowtask/front/src/components/action/action.js
@@ -1,6 +1,7 @@
 import ActionViewAtom from './action/actionViewAtom.js';
 import ActionAssigneesConfirmation from './action/actionAssigneesConfirmation.js';
 import ActionCancelFlow from './action/actionCancelFlow.js';
+import ActionHandleTask from './action/actionHandleTask.js';
 import ActionRecall from './action/actionRecall.js';
 
 export default {
@@ -11,6 +12,7 @@ export default {
     ActionViewAtom, //
     ActionAssigneesConfirmation,
     ActionCancelFlow,
+    ActionHandleTask,
     ActionRecall,
   ],
   props: {
@@ -49,53 +51,10 @@ export default {
       } else if (action.name === 'cancelFlow') {
         return await this._onActionCancelFlow();
       } else if (action.name === 'handleTask') {
-        return await this._handleTask({ ctx, action, flowLayoutManager, task, flowTaskId });
+        return await this._onActionHandleTask();
       } else if (action.name === 'recall') {
         return await this._onActionRecall();
       }
-    },
-    async _handleTask({ ctx, action, flowLayoutManager, task, flowTaskId }) {
-      // claim first
-      if (!task.timeClaimed) {
-        const res = await ctx.$api.post('/a/flowtask/task/claim', {
-          flowTaskId,
-        });
-        task.timeClaimed = res.timeClaimed;
-      }
-      // load schema and item
-      if (task._editAtomData === undefined) {
-        const data = await ctx.$api.post('/a/flowtask/task/editAtom', {
-          flowTaskId,
-        });
-        Vue.set(task, '_editAtomData', data);
-      }
-      if (!task._editAtomData) {
-        // handle directly
-        ctx.$refs.actionHandleTask.open({
-          flowLayoutManager,
-          flowTaskId,
-          action,
-          callback: null,
-        });
-        return;
-      }
-      // navigate options
-      const navigateOptions = {
-        context: {
-          params: {
-            flowLayoutManager,
-            task,
-            data: task._editAtomData,
-            action,
-          },
-        },
-      };
-      if (ctx.$pageRoute.path === '/a/flowtask/flowTaskAtom') {
-        navigateOptions.target = '_self';
-        navigateOptions.reloadCurrent = true;
-      }
-      // navigate
-      ctx.$view.navigate(`/a/flowtask/flowTaskAtom?flowTaskId=${flowTaskId}&mode=edit`, navigateOptions);
     },
   },
 };
