9068da129749e730d49bf9bc8234c429dcbfc9e0
zhennann <zhen.nann@icloud.com>
2021-08-18 10:49:55 +0800

    refactor: actionSelectResourceType

--- src/module-system/a-base-sync/front/src/components/atom/action/actionSelectResourceType.js
+++ src/module-system/a-base-sync/front/src/components/atom/action/actionSelectResourceType.js
@@ -3,40 +3,26 @@ export default {
     async _onActionSelectResourceType() {
       const { ctx, action } = this.$props;
       const resourceTypes = await ctx.$store.dispatch('a/base/getResourceTypes');
+      const buttons = [
+        {
+          text: ctx.$text('SelectResourceTypeTip'),
+          label: true,
+        },
+      ];
+      for (const key in resourceTypes) {
+        const resourceType = resourceTypes[key];
+        buttons.push({
+          text: resourceType.titleLocale,
+          data: key,
+        });
+      }
       // choose
-      return new Promise((resolve, reject) => {
-        const hostEl = ctx.$view.getHostEl();
-        const targetEl = action.targetEl;
-        const buttons = [
-          {
-            text: ctx.$text('SelectResourceTypeTip'),
-            label: true,
-          },
-        ];
-        let resolved = false;
-        function onButtonClick(locale) {
-          resolved = true;
-          resolve(locale);
-        }
-        for (const key in resourceTypes) {
-          const resourceType = resourceTypes[key];
-          buttons.push({
-            text: resourceType.titleLocale,
-            onClick: () => {
-              onButtonClick(key);
-            },
-          });
-        }
-        const actions = ctx.$f7.actions.create({ hostEl, buttons, targetEl });
-        function onActionsClosed() {
-          actions.destroy();
-          if (!resolved) {
-            resolved = true;
-            reject(new Error());
-          }
-        }
-        actions.open().once('actionsClosed', onActionsClosed).once('popoverClosed', onActionsClosed);
-      });
+      const params = {
+        targetEl: action.targetEl,
+        buttons,
+      };
+      const button = await ctx.$view.actions.choose(params);
+      return button.data;
     },
   },
 };
