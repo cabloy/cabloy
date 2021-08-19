b78cf04f114bc03552066b1933a46c601b247bc7
zhennann <zhen.nann@icloud.com>
2021-08-19 10:44:57 +0800

    refactor: resource config

--- src/module-system/a-base-sync/front/src/config/config.js
+++ src/module-system/a-base-sync/front/src/config/config.js
@@ -1,4 +1,6 @@
+import configResourceRenderList from './config/configResourceRenderList.js';
 import avatarUser from '../assets/img/user.png';
+
 export default {
   stage: {
     draft: 0,
@@ -13,72 +15,7 @@ export default {
   atoms: {
     resource: {
       render: {
-        list: {
-          info: {
-            orders: [
-              { name: 'resourceType', title: 'Resource Type', by: 'asc' },
-              { name: 'resourceSorting', title: 'Resource Sorting', by: 'asc' },
-            ],
-          },
-          layouts: {
-            list: {},
-            table: {
-              blocks: {
-                items: {
-                  columns: [
-                    {
-                      dataIndex: 'atomName',
-                      title: 'Atom Name',
-                      align: 'left',
-                      component: {
-                        module: 'a-baselayout',
-                        name: 'listLayoutTableCellAtomName',
-                      },
-                    },
-                    {
-                      dataIndex: 'resourceType',
-                      title: 'Resource Type',
-                      align: 'left',
-                      component: {
-                        module: 'a-basefront',
-                        name: 'renderTableCellComputed',
-                        options: {
-                          props: {
-                            expression: 'record.resourceTypeLocale',
-                          },
-                        },
-                      },
-                    },
-                    {
-                      dataIndex: 'atomCategoryNameLocale',
-                      title: 'Category',
-                      align: 'left',
-                    },
-                    {
-                      dataIndex: 'userName',
-                      title: 'Creator',
-                      align: 'left',
-                      component: {
-                        module: 'a-baselayout',
-                        name: 'listLayoutTableCellUserName',
-                      },
-                    },
-                    {
-                      dataIndex: 'createdAt',
-                      title: 'Created Time',
-                      align: 'left',
-                    },
-                    {
-                      dataIndex: 'updatedAt',
-                      title: 'Modification Time',
-                      align: 'left',
-                    },
-                  ],
-                },
-              },
-            },
-          },
-        },
+        list: configResourceRenderList,
       },
     },
   },
---
+++ src/module-system/a-base-sync/front/src/config/config/configResourceRenderList.js
@@ -0,0 +1,67 @@
+const list = {
+  info: {
+    orders: [
+      { name: 'resourceType', title: 'Resource Type', by: 'asc' },
+      { name: 'resourceSorting', title: 'Resource Sorting', by: 'asc' },
+    ],
+  },
+  layouts: {
+    list: {},
+    table: {
+      blocks: {
+        items: {
+          columns: [
+            {
+              dataIndex: 'atomName',
+              title: 'Atom Name',
+              align: 'left',
+              component: {
+                module: 'a-baselayout',
+                name: 'listLayoutTableCellAtomName',
+              },
+            },
+            {
+              dataIndex: 'resourceType',
+              title: 'Resource Type',
+              align: 'left',
+              component: {
+                module: 'a-basefront',
+                name: 'renderTableCellComputed',
+                options: {
+                  props: {
+                    expression: 'record.resourceTypeLocale',
+                  },
+                },
+              },
+            },
+            {
+              dataIndex: 'atomCategoryNameLocale',
+              title: 'Category',
+              align: 'left',
+            },
+            {
+              dataIndex: 'userName',
+              title: 'Creator',
+              align: 'left',
+              component: {
+                module: 'a-baselayout',
+                name: 'listLayoutTableCellUserName',
+              },
+            },
+            {
+              dataIndex: 'createdAt',
+              title: 'Created Time',
+              align: 'left',
+            },
+            {
+              dataIndex: 'updatedAt',
+              title: 'Modification Time',
+              align: 'left',
+            },
+          ],
+        },
+      },
+    },
+  },
+};
+export default list;
