e9c7ad7095338f2d43aabe823e99627f959cf3f5
zhennann <zhen.nann@icloud.com>
2021-08-19 09:47:17 +0800

    refactor: configResourceRenderTree

--- src/module-system/a-basefront/front/src/config/config.js
+++ src/module-system/a-basefront/front/src/config/config.js
@@ -1,3 +1,5 @@
+import configResourceRenderTree from './config/configResourceRenderTree.js';
+
 export default {
   atom: {
     render: {
@@ -262,56 +264,7 @@ export default {
   },
   resource: {
     render: {
-      tree: {
-        info: {
-          layout: {
-            items: [
-              { name: 'accordion', title: 'LayoutAccordion' },
-              { name: 'tree', title: 'LayoutTree' },
-            ],
-          },
-        },
-        layouts: {
-          base: {
-            blocks: {
-              title: {
-                component: {
-                  module: 'a-baselayout',
-                  name: 'resourceTreeLayoutBlockAccordionTitle',
-                },
-              },
-            },
-          },
-          accordion: {
-            component: {
-              module: 'a-baselayout',
-              name: 'resourceTreeLayoutAccordion',
-            },
-            blocks: {
-              items: {
-                component: {
-                  module: 'a-baselayout',
-                  name: 'resourceTreeLayoutBlockAccordionItems',
-                },
-              },
-            },
-          },
-          tree: {
-            component: {
-              module: 'a-baselayout',
-              name: 'resourceTreeLayoutTree',
-            },
-            blocks: {
-              items: {
-                component: {
-                  module: 'a-baselayout',
-                  name: 'resourceTreeLayoutBlockTreeItems',
-                },
-              },
-            },
-          },
-        },
-      },
+      tree: configResourceRenderTree,
     },
   },
 };
---
+++ src/module-system/a-basefront/front/src/config/config/configResourceRenderTree.js
@@ -0,0 +1,51 @@
+const resource = {
+  info: {
+    layout: {
+      items: [
+        { name: 'accordion', title: 'LayoutAccordion' },
+        { name: 'tree', title: 'LayoutTree' },
+      ],
+    },
+  },
+  layouts: {
+    base: {
+      blocks: {
+        title: {
+          component: {
+            module: 'a-baselayout',
+            name: 'resourceTreeLayoutBlockAccordionTitle',
+          },
+        },
+      },
+    },
+    accordion: {
+      component: {
+        module: 'a-baselayout',
+        name: 'resourceTreeLayoutAccordion',
+      },
+      blocks: {
+        items: {
+          component: {
+            module: 'a-baselayout',
+            name: 'resourceTreeLayoutBlockAccordionItems',
+          },
+        },
+      },
+    },
+    tree: {
+      component: {
+        module: 'a-baselayout',
+        name: 'resourceTreeLayoutTree',
+      },
+      blocks: {
+        items: {
+          component: {
+            module: 'a-baselayout',
+            name: 'resourceTreeLayoutBlockTreeItems',
+          },
+        },
+      },
+    },
+  },
+};
+export default resource;
