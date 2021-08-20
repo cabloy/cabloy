2a20a7617b56f8638c8990bc41affef20768182d
zhennann <zhen.nann@icloud.com>
2021-08-21 07:17:00 +0800

    refactor: renderPanel

--- src/module-system/a-components-sync/front/src/common/validate/render/renderPanel.js
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderPanel.js
@@ -17,50 +17,57 @@ export default {
         },
         on: {
           click: () => {
-            // schemaSub
-            const metaSchema = this.validate.meta && this.validate.meta.schema;
-            const schemaSub = metaSchema
-              ? {
-                  module: metaSchema.module,
-                  validator: metaSchema.validator,
-                  schema: property.$ref,
-                }
-              : {
-                  module: this.validate.params.module,
-                  validator: this.validate.params.validator,
-                  schema: property.$ref,
-                };
-            // errors
-            const verrors = this.validate.verrors;
-            // target
-            let target = this.$meta.util.getProperty(property, 'ebParams.target');
-            if (target === undefined) target = '_self';
-            // navigate
-            this.$view.navigate('/a/validation/validate', {
-              target,
-              context: {
-                params: {
-                  host: this.validate.host,
-                  params: schemaSub,
-                  meta: panelMeta,
-                  title,
-                  data: value,
-                  dataPathRoot: this.adjustDataPath(dataPath),
-                  errors: verrors ? verrors.slice(0) : null,
-                  readOnly: this.validate.readOnly || property.ebReadOnly,
-                },
-                callback: (code, res) => {
-                  if (code === 200) {
-                    context.setValue(res.data);
-                    this.validate.verrors = res.errors;
-                    // submit
-                    if (property.ebAutoSubmit !== false) {
-                      this.validate.onSubmit();
-                    }
-                  }
-                },
-              },
-            });
+            this._renderPanel_onClick(context, panelMeta, title, value);
+          },
+        },
+      });
+    },
+    _renderPanel_onClick(context, panelMeta, title, value) {
+      const { property, dataPath } = context;
+      // schemaSub
+      let schemaSub;
+      const metaSchema = this.validate.meta && this.validate.meta.schema;
+      if (metaSchema) {
+        schemaSub = {
+          module: metaSchema.module,
+          validator: metaSchema.validator,
+          schema: property.$ref,
+        };
+      } else {
+        schemaSub = {
+          module: this.validate.params.module,
+          validator: this.validate.params.validator,
+          schema: property.$ref,
+        };
+      }
+      // errors
+      const verrors = this.validate.verrors;
+      // target
+      let target = this.$meta.util.getProperty(property, 'ebParams.target');
+      if (target === undefined) target = '_self';
+      // navigate
+      this.$view.navigate('/a/validation/validate', {
+        target,
+        context: {
+          params: {
+            host: this.validate.host,
+            params: schemaSub,
+            meta: panelMeta,
+            title,
+            data: value,
+            dataPathRoot: this.adjustDataPath(dataPath),
+            errors: verrors ? verrors.slice(0) : null,
+            readOnly: this.validate.readOnly || property.ebReadOnly,
+          },
+          callback: (code, res) => {
+            if (code === 200) {
+              context.setValue(res.data);
+              this.validate.verrors = res.errors;
+              // submit
+              if (property.ebAutoSubmit !== false) {
+                this.validate.onSubmit();
+              }
+            }
           },
         },
       });
