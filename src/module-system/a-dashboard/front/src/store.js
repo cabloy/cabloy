b7b2a60fb7c9a21d84383056256e4cd6486b67a8
zhennann <zhen.nann@icloud.com>
2021-08-21 11:42:05 +0800

    feat: dashboard store

--- src/module-system/a-dashboard/front/src/store.js
+++ src/module-system/a-dashboard/front/src/store.js
@@ -1,9 +1,44 @@
 // eslint-disable-next-line
 export default function (Vue) {
+  Vue.prototype.$meta.eventHub.$on('auth:login', data => {
+    Vue.prototype.$meta.store.commit('a/dashboard/authLogin', data);
+  });
   return {
-    state: {},
+    state: {
+      dashboardUsers: {},
+    },
     getters: {},
-    mutations: {},
-    actions: {},
+    mutations: {
+      authLogin(state) {
+        // clear user
+        state.dashboardUsers = null;
+      },
+      setDashboardUsers(state, { dashboardAtomId, dashboardUsers }) {
+        state.dashboardUsers = {
+          ...state.dashboardUsers,
+          [dashboardAtomId]: dashboardUsers,
+        };
+      },
+    },
+    actions: {
+      getDashboardUsers({ state, commit }, { dashboardAtomId }) {
+        return new Promise((resolve, reject) => {
+          const key = dashboardAtomId;
+          if (state.dashboardUsers[key]) return resolve(state.dashboardUsers[key]);
+          Vue.prototype.$meta.api
+            .post('/a/dashboard/dashboard/itemUsers', {
+              key: { atomId: key },
+            })
+            .then(data => {
+              const dashboardUsers = data;
+              commit('setDashboardUsers', { dashboardAtomId, dashboardUsers });
+              resolve(dashboardUsers);
+            })
+            .catch(err => {
+              reject(err);
+            });
+        });
+      },
+    },
   };
 }
