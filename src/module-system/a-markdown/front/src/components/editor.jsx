632119f1f79519109534cc4bf44214a14c472e40
zhennann <zhen.nann@icloud.com>
2021-08-29 09:20:05 +0800

    feat: editor

--- src/module-system/a-markdown/front/src/components/editor.jsx
+++ src/module-system/a-markdown/front/src/components/editor.jsx
@@ -1,9 +1,42 @@
+// import { schema } from 'prosemirror-schema-basic';
+import { EditorState } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { undo, redo, history } from 'prosemirror-history';
+import { keymap } from 'prosemirror-keymap';
+import { baseKeymap } from 'prosemirror-commands';
+import { schema, defaultMarkdownParser, defaultMarkdownSerializer } from 'prosemirror-markdown';
+// import { exampleSetup } from 'prosemirror-example-setup';
+
 export default {
   meta: {
     global: false,
   },
   data() {
-    return {};
+    return {
+      content: '## hello world',
+    };
+  },
+  mounted() {
+    const state = EditorState.create({
+      schema,
+      doc: defaultMarkdownParser.parse(this.content),
+      plugins: [
+        history(), //
+        keymap({ 'Mod-z': undo, 'Mod-y': redo }),
+        keymap(baseKeymap),
+      ],
+    });
+    const view = new EditorView(this.$el, {
+      state,
+      dispatchTransaction(transaction) {
+        console.log('Document size went from', transaction.before.content.size, 'to', transaction.doc.content.size);
+        const newState = view.state.apply(transaction);
+        view.updateState(newState);
+      },
+    });
   },
   methods: {},
+  render() {
+    return <div></div>;
+  },
 };
---
+++ src/module-system/a-markdown/front/src/pages/demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <eb-page>
+    <Editor></Editor>
+  </eb-page>
+</template>
+<script>
+import Editor from '../components/editor.jsx';
+export default {
+  components: {
+    Editor,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
