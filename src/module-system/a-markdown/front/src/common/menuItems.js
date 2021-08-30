3f9296f9b7c3dd63da3f3e243ee0bed5cb400582
zhennann <zhen.nann@icloud.com>
2021-08-30 16:03:44 +0800

    feat: block menu item

--- src/module-system/a-markdown/front/src/common/menuItems.js
+++ src/module-system/a-markdown/front/src/common/menuItems.js
@@ -58,10 +58,12 @@ export const ButtonsOptions = {
   paragraph: {
     title: 'EditorButtonTitleParagraph',
     icon: { text: '¶' },
+    onBuild: blockTypeItem,
   },
   code_block: {
     title: 'EditorButtonTitleCodeBlock',
     icon: { material: 'wysiwyg' },
+    onBuild: blockTypeItem,
   },
   horizontal_rule: {
     title: 'EditorButtonTitleHorizontalRule',
@@ -131,19 +133,24 @@ function markItem(markType, options) {
 
 function buildMenuItemsAll(ctx, schema) {
   const menuItems = {};
-  // marks
-  for (const key in schema.marks) {
-    const markType = schema.marks[key];
-    const buttonOptions = ButtonsOptions[key];
-    if (!buttonOptions.onBuild) continue;
-    const options = {
-      ...buttonOptions,
-      title: ctx.$text(buttonOptions.title),
-      key,
-    };
-    const menuItem = buttonOptions.onBuild(markType, options);
-    menuItems[key] = menuItem;
+  function __schemaElements(elements) {
+    for (const key in elements) {
+      const element = elements[key];
+      const buttonOptions = ButtonsOptions[key];
+      if (!buttonOptions || !buttonOptions.onBuild) continue;
+      const options = {
+        ...buttonOptions,
+        title: ctx.$text(buttonOptions.title),
+        key,
+      };
+      const menuItem = buttonOptions.onBuild(element, options);
+      menuItems[key] = menuItem;
+    }
   }
+  // marks
+  __schemaElements(schema.marks);
+  // nodes
+  __schemaElements(schema.nodes);
   // nodes
   return menuItems;
 }
@@ -160,90 +167,4 @@ export function buildMenuItems(ctx, schema, buttonsWant) {
   });
   menuItems = menuItems.filter(x => x && x.length > 0);
   return menuItems;
-}
-
-export function buildMenuItems1(schema, buttonsWant) {
-  let r = {},
-    type;
-  if ((type = schema.marks.strong)) {
-    r.toggleStrong = markItem(type, { title: 'Toggle strong style', icon: icons.strong });
-  }
-  if ((type = schema.marks.em)) {
-    r.toggleEm = markItem(type, { title: 'Toggle emphasis', icon: icons.em });
-  }
-  if ((type = schema.marks.code)) {
-    r.toggleCode = markItem(type, { title: 'Toggle code font', icon: icons.code });
-  }
-  if ((type = schema.marks.link)) {
-    r.toggleLink = linkItem(type);
-  }
-
-  if ((type = schema.nodes.image)) {
-    r.insertImage = insertImageItem(type);
-  }
-  if ((type = schema.nodes.bullet_list)) {
-    r.wrapBulletList = wrapListItem(type, {
-      title: 'Wrap in bullet list',
-      icon: icons.bulletList,
-    });
-  }
-  if ((type = schema.nodes.ordered_list)) {
-    r.wrapOrderedList = wrapListItem(type, {
-      title: 'Wrap in ordered list',
-      icon: icons.orderedList,
-    });
-  }
-  if ((type = schema.nodes.blockquote)) {
-    r.wrapBlockQuote = wrapItem(type, {
-      title: 'Wrap in block quote',
-      icon: icons.blockquote,
-    });
-  }
-  if ((type = schema.nodes.paragraph)) {
-    r.makeParagraph = blockTypeItem(type, {
-      title: 'Change to paragraph',
-      label: 'Plain',
-    });
-  }
-  if ((type = schema.nodes.code_block)) {
-    r.makeCodeBlock = blockTypeItem(type, {
-      title: 'Change to code block',
-      label: 'Code',
-    });
-  }
-  if ((type = schema.nodes.heading)) {
-    for (let i = 1; i <= 10; i++) {
-      r['makeHead' + i] = blockTypeItem(type, {
-        title: 'Change to heading ' + i,
-        label: 'Level ' + i,
-        attrs: { level: i },
-      });
-    }
-  }
-  if ((type = schema.nodes.horizontal_rule)) {
-    const hr = type;
-    r.insertHorizontalRule = new MenuItem({
-      title: 'Insert horizontal rule',
-      label: 'Horizontal rule',
-      enable(state) {
-        return canInsert(state, hr);
-      },
-      run(state, dispatch) {
-        dispatch(state.tr.replaceSelectionWith(hr.create()));
-      },
-    });
-  }
-
-  const cut = arr => arr.filter(x => x);
-  r.insertMenu = new Dropdown(cut([r.insertImage, r.insertHorizontalRule]), { label: 'Insert' });
-  r.typeMenu = new Dropdown(
-    cut([r.makeParagraph, r.makeCodeBlock, r.makeHead1 && new DropdownSubmenu(cut([r.makeHead1, r.makeHead2, r.makeHead3, r.makeHead4, r.makeHead5, r.makeHead6]), { label: 'Heading' })]),
-    { label: 'Type...' }
-  );
-
-  r.inlineMenu = [cut([r.toggleStrong, r.toggleEm, r.toggleCode, r.toggleLink])];
-  r.blockMenu = [cut([r.wrapBulletList, r.wrapOrderedList, r.wrapBlockQuote, joinUpItem, liftItem, selectParentNodeItem])];
-  r.fullMenu = r.inlineMenu.concat([[r.insertMenu, r.typeMenu]], [[undoItem, redoItem]], r.blockMenu);
-
-  return r;
 }
