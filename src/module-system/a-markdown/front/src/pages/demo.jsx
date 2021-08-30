26e0df16ac0cccb1c55e7976e057730b55cdfc62
zhennann <zhen.nann@icloud.com>
2021-08-30 16:06:05 +0800

    feat: block menu item

--- src/module-system/a-markdown/front/src/pages/demo.jsx
+++ src/module-system/a-markdown/front/src/pages/demo.jsx
@@ -13,7 +13,7 @@ export default {
     return (
       <eb-page>
         <Editor style={{ height: '300px' }} value={this.content} onInput={value => (this.content = value)}></Editor>
-        <textarea vModel={this.content}></textarea>
+        <textarea vModel={this.content} style="border:1px solid gray; width:100%;height:200px;"></textarea>
       </eb-page>
     );
   },
