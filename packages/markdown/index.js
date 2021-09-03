11f6dae15594b95bc61a0dc142d3cbff3e3c9d84
zhennann <zhen.nann@icloud.com>
2021-09-03 15:52:54 +0800

    feat: markdownit container

--- packages/markdown/index.js
+++ packages/markdown/index.js
@@ -22,29 +22,44 @@ const defaults = {
   typographer: true, // Enable smartypants and other sweet transforms
 };
 
+function get_options_container(name) {
+  return {
+    render(tokens, idx, _options, env, slf) {
+      // add a class to the opening tag
+      if (tokens[idx].nesting === 1) {
+        tokens[idx].attrJoin('class', `markdown-it-container ${name}`);
+      }
+
+      return slf.renderToken(tokens, idx, _options, env, slf);
+    },
+  };
+}
+
 module.exports = {
   create(ops) {
-    return markdown_it((ops && ops.defaults) || defaults)
-      .use(markdown_it_abbr)
-      .use(markdown_it_container)
-      .use(markdown_it_container, 'comment-quot')
-      .use(markdown_it_container, 'alert-success')
-      .use(markdown_it_container, 'alert-info')
-      .use(markdown_it_container, 'alert-warning')
-      .use(markdown_it_container, 'alert-danger')
-      .use(markdown_it_container, 'hljs-left')
-      .use(markdown_it_container, 'hljs-center')
-      .use(markdown_it_container, 'hljs-right')
-      .use(markdown_it_deflist)
-      .use(markdown_it_emoji)
-      .use(markdown_it_footnote)
-      .use(markdown_it_highlightjs)
-      .use(markdown_it_ins)
-      .use(markdown_it_katex)
-      .use(markdown_it_mark)
-      .use(markdown_it_sub)
-      .use(markdown_it_sup)
-      .use(markdown_it_task_lists)
-      .use(markdown_it_toc);
+    return (
+      markdown_it((ops && ops.defaults) || defaults)
+        .use(markdown_it_abbr)
+        // .use(markdown_it_container)
+        .use(markdown_it_container, 'comment-quot', get_options_container('comment-quot'))
+        .use(markdown_it_container, 'alert-success', get_options_container('alert-success'))
+        .use(markdown_it_container, 'alert-info', get_options_container('alert-info'))
+        .use(markdown_it_container, 'alert-warning', get_options_container('alert-warning'))
+        .use(markdown_it_container, 'alert-danger', get_options_container('alert-danger'))
+        .use(markdown_it_container, 'hljs-left', get_options_container('hljs-left'))
+        .use(markdown_it_container, 'hljs-center', get_options_container('hljs-center'))
+        .use(markdown_it_container, 'hljs-right', get_options_container('hljs-right'))
+        .use(markdown_it_deflist)
+        .use(markdown_it_emoji)
+        .use(markdown_it_footnote)
+        .use(markdown_it_highlightjs)
+        .use(markdown_it_ins)
+        .use(markdown_it_katex)
+        .use(markdown_it_mark)
+        .use(markdown_it_sub)
+        .use(markdown_it_sup)
+        .use(markdown_it_task_lists)
+        .use(markdown_it_toc)
+    );
   },
 };
