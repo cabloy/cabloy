9d0cd2727e05b642ad9adac5b2d8244280df6496
zhennann <zhen.nann@icloud.com>
2021-09-04 15:49:40 +0800

    feat: table menu item

--- src/module-system/a-markdown/front/src/common/buttons/table.js
+++ src/module-system/a-markdown/front/src/common/buttons/table.js
@@ -1,6 +1,7 @@
 import { MenuItem } from 'prosemirror-menu';
 import { TextSelection } from 'prosemirror-state';
 import { addRowAt, createTable, getCellsInColumn, moveRow } from '@zhennann/prosemirror-utils';
+import { isInTable } from 'prosemirror-tables';
 
 export const ButtonTable = {
   node: true,
@@ -10,22 +11,31 @@ export const ButtonTable = {
 };
 
 function insertTableMenu(nodeType, options) {
-  return new MenuItem({
+  const menuItem = new MenuItem({
     ...options,
+    // select(state) {
+    //   return !isInTable(state);
+    // },
     enable(state) {
       return true;
     },
-    run(state, dispatch) {
-      const rowsCount = 3;
-      const colsCount = 3;
-      const offset = state.tr.selection.anchor + 1;
-      const cell = state.schema.nodes.paragraph.create();
-      const nodes = createTable(state.schema, rowsCount, colsCount, true, cell);
-      const tr = state.tr.replaceSelectionWith(nodes).scrollIntoView();
-      const resolvedPos = tr.doc.resolve(offset);
-
-      tr.setSelection(TextSelection.near(resolvedPos));
-      dispatch(tr);
+    run(state, dispatch, view, event) {
+      if (!isInTable(state)) return _insertTable(state, dispatch);
+      options.onPopup(state, dispatch, view, event, menuItem);
     },
   });
+  return menuItem;
+}
+
+function _insertTable(state, dispatch) {
+  const rowsCount = 3;
+  const colsCount = 3;
+  const offset = state.tr.selection.anchor + 1;
+  const cell = state.schema.nodes.paragraph.create();
+  const nodes = createTable(state.schema, rowsCount, colsCount, true, cell);
+  const tr = state.tr.replaceSelectionWith(nodes).scrollIntoView();
+  const resolvedPos = tr.doc.resolve(offset);
+
+  tr.setSelection(TextSelection.near(resolvedPos));
+  dispatch(tr);
 }
