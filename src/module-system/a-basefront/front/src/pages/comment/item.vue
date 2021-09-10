9971cb5e9de13c39fb5ffb63cb625fda1d167a41
zhennann <zhen.nann@icloud.com>
2021-09-10 10:43:52 +0800

    refactor: new comment

--- src/module-system/a-basefront/front/src/pages/comment/item.vue
+++ src/module-system/a-basefront/front/src/pages/comment/item.vue
@@ -1,33 +1,22 @@
 <template>
   <eb-page>
-    <eb-navbar :title="$text('Comment')" eb-back-link="Back">
+    <eb-navbar :title="page_title" eb-back-link="Back">
       <f7-nav-right>
-        <eb-link iconMaterial="save" :onPerform="onPerformSave"></eb-link>
+        <eb-link ref="buttonSave" iconMaterial="save" :onPerform="onPerformSave"></eb-link>
       </f7-nav-right>
     </eb-navbar>
     <template v-if="ready">
       <eb-box>
-        <mavon-editor
-          ref="editor"
-          :value="content"
-          :onPreRender="onPreRender"
-          @change="onChange"
-          @save="onSave"
-          :onImageUpload="onImageUpload"
-          :language="language"
-          :subfield="subfield"
-          :editable="editable"
-          :defaultOpen="defaultOpen"
-          :toolbarsFlag="toolbarsFlag"
-          :navigation="navigation"
-          :toolbars="toolbars"
-        />
+        <eb-markdown-editor ref="editor" :readOnly="readOnly" :host="host" :value="content" @input="onInput" @save="onSave" />
       </eb-box>
     </template>
   </eb-page>
 </template>
 <script>
+import Vue from 'vue';
+const ebPageDirty = Vue.prototype.$meta.module.get('a-components').options.mixins.ebPageDirty;
 export default {
+  mixins: [ebPageDirty],
   meta: {
     size: 'medium',
   },
@@ -43,141 +32,73 @@ export default {
     };
   },
   computed: {
+    page_title() {
+      const title = this.$text('Comment');
+      return this.page_getDirtyTitle(title);
+    },
     ready() {
       return this.module && (this.commentId === 0 || this.comment) && (this.replyId === 0 || this.reply);
     },
     readOnly() {
       return false;
     },
-    editable() {
-      return !this.readOnly;
-    },
-    language() {
-      const locale = this.$meta.util.getLocale();
-      return locale === 'zh-cn' ? 'zh-CN' : 'en';
-    },
-    subfield() {
-      return this.editable && this.$view.size !== 'small';
-    },
-    defaultOpen() {
-      return this.editable ? '' : 'preview';
-    },
-    toolbarsFlag() {
-      return this.editable;
-    },
-    navigation() {
-      return !this.editable;
-    },
-    toolbars() {
+    host() {
       return {
-        bold: true, // 粗体
-        italic: true, // 斜体
-        // header: true, // 标题
-        underline: true, // 下划线
-        strikethrough: true, // 中划线
-        mark: true, // 标记
-        superscript: true, // 上角标
-        subscript: true, // 下角标
-        // quote: true, // 引用
-        ol: true, // 有序列表
-        ul: true, // 无序列表
-        link: true, // 链接
-        imagelink: true, // 图片链接
-        code: true, // code
-        table: true, // 表格
-        /* 1.3.5 */
-        undo: true, // 上一步
-        redo: true, // 下一步
-        trash: true, // 清空
-        /* 1.4.2 */
-        // navigation: true, // 导航目录
-        /* 2.1.8 */
-        alignleft: true, // 左对齐
-        aligncenter: true, // 居中
-        alignright: true, // 右对齐
-        /* 2.2.1 */
-        subfield: true, // 单双栏模式
+        atomId: this.atomId,
       };
     },
   },
   created() {
-    this.$meta.module.use('a-mavoneditor', module => {
+    this.$meta.module.use('a-markdown', module => {
       this.module = module;
     });
     this.loadComment(this.commentId);
     this.loadReply(this.replyId);
   },
   methods: {
-    loadComment(commentId) {
+    async loadComment(commentId) {
       if (!commentId) return;
-      this.$api
-        .post('/a/base/comment/item', {
-          key: { atomId: this.atomId },
-          data: { commentId },
-        })
-        .then(data => {
-          this.comment = data;
-          this.content = this.comment.content;
-        });
+      const data = await this.$api.post('/a/base/comment/item', {
+        key: { atomId: this.atomId },
+        data: { commentId },
+      });
+      this.comment = data;
+      this.content = this.comment.content;
     },
-    loadReply(replyId) {
+    async loadReply(replyId) {
       if (!replyId) return;
-      this.$api
-        .post('/a/base/comment/item', {
-          key: { atomId: this.atomId },
-          data: { commentId: replyId },
-        })
-        .then(data => {
-          this.reply = data;
-        });
+      const data = await this.$api.post('/a/base/comment/item', {
+        key: { atomId: this.atomId },
+        data: { commentId: replyId },
+      });
+      this.reply = data;
     },
-    onChange(data) {
+    onInput(data) {
+      if (this.content === data) return;
       this.content = data;
+      this.page_setDirty(true);
     },
     onSave() {
-      this.onPerformSave();
+      this.$refs.buttonSave.onClick();
     },
-    onPerformSave() {
+    async onPerformSave() {
       if (!this.content) return;
-      return this.$api
-        .post('/a/base/comment/save', {
-          key: { atomId: this.atomId },
-          data: {
-            commentId: this.commentId,
-            replyId: this.replyId,
-            content: this.content,
-          },
-        })
-        .then(data => {
-          this.$meta.eventHub.$emit('comment:action', data);
-          const returnTo = this.$f7route.query.returnTo;
-          if (returnTo) {
-            location.assign(returnTo);
-          } else {
-            this.$f7router.back();
-          }
-        });
-    },
-    onImageUpload() {
-      return new Promise((resolve, reject) => {
-        this.$view.navigate('/a/file/file/upload', {
-          context: {
-            params: {
-              mode: 1,
-              atomId: this.atomId,
-              flag: 'comment',
-            },
-            callback: (code, data) => {
-              if (code === 200) {
-                resolve({ text: data.realName, addr: data.downloadUrl });
-              }
-              if (code === false) {
-                reject(new Error());
-              }
-            },
-          },
-        });
+      const data = await this.$api.post('/a/base/comment/save', {
+        key: { atomId: this.atomId },
+        data: {
+          commentId: this.commentId,
+          replyId: this.replyId,
+          content: this.content,
+        },
       });
+      this.page_setDirty(false);
+      this.$meta.eventHub.$emit('comment:action', data);
+      const returnTo = this.$f7route.query.returnTo;
+      if (returnTo) {
+        location.assign(returnTo);
+      } else {
+        this.$f7router.back();
+      }
     },
     onPreRender(value) {
       if (this.comment) {
