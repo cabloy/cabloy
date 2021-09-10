ed655fd4ac7a0a096e97dc6a4f9dbf51501ab554
zhennann <zhen.nann@icloud.com>
2021-09-10 22:06:29 +0800

    feat: article markdown

--- src/module-system/cms-pluginarticle/backend/cms/plugin/assets/js/render.js
+++ src/module-system/cms-pluginarticle/backend/cms/plugin/assets/js/render.js
@@ -1,5 +1,10 @@
-console.log(requirejs);
 (function () {
+  $(document).ready(function () {
+    util.article.mountMarkdown($('.article-body'));
+  });
+
+  const BlockClasses = [];
+
   util.article.mountMarkdown = function ($container) {
     const $blocks = $('.markdown-it-cabloy-block', $container);
     for (let i = 0; i < $blocks.length; i++) {
@@ -8,24 +13,66 @@ console.log(requirejs);
   };
 
   function _mountBlock($blockContainer) {
-    // const blockParams = this._getBlockParams(blockContainer);
-    // const blockContent = this._getBlockContent(blockContainer);
-    // if (!blockParams || !blockContent) return;
-    // // Block Class
-    // // const BlockClass = await this._getBlockClass(blockParams);
-    // if (!BlockClass) {
-    //   // do nothing
-    //   return;
-    // }
-    // // host
-    // const host = this._getHost(blockContainer, blockContent);
-    // // Block Instance
-    // const blockInstance = new BlockClass(host);
-    // // mount
-    // if (blockInstance.mount) {
-    //   // await blockInstance.mount();
-    // }
+    const blockParams = _getBlockParams($blockContainer);
+    const blockContent = _getBlockContent($blockContainer);
+    if (!blockParams || !blockContent) return;
+    // Block Class
+    _getBlockClass(blockParams).then(BlockClass => {
+      if (!BlockClass) {
+        // do nothing
+        return;
+      }
+      // host
+      const host = _getHost($blockContainer, blockContent);
+      // Block Instance
+      const blockInstance = new BlockClass(host);
+      // mount
+      if (blockInstance.mount) {
+        blockInstance.mount();
+      }
+    });
+  }
+
+  function _getBlockParams($block) {
+    const params = $block.getAttribute('data-block-params');
+    if (!params) return null;
+    const [module, blockName] = params.split(':');
+    if (!module || !blockName) return null;
+    return { params, module, blockName };
+  }
+
+  function _getBlockContent($block) {
+    const content = $block.getAttribute('data-block-content');
+    if (!content) return null;
+    return window.JSON5.parse(decodeURIComponent(content));
+  }
+
+  function _getBlockClass(blockParams) {
+    const { params, module, blockName } = blockParams;
+    const BlockClass = BlockClasses[params];
+    if (BlockClass) return Promise.resolve(BlockClass);
+    return new Promise(resolve => {
+      const block_js = `api/static/${module.replace('-', '/')}/blocks/${blockName}/main`;
+      window.requirejs([block_js], BlockClass => {
+        BlockClasses[params] = BlockClass;
+        resolve(BlockClass);
+      });
+    });
+  }
+
+  function _getHost($blockContainer, blockContent) {
+    const $host = {
+      atom: env.article,
+      atomId: env.article.atomId,
+    };
+    const $util = window.util.hostUtil({
+      locale: $host.atom.atomLanguage,
+    });
+    return {
+      $host, // atomId/atom
+      $container: $blockContainer,
+      $content: blockContent,
+      $util,
+    };
   }
 })();
-
-$(document).ready(function () {});
