2a5fe97a25488a0d9234c78de5f9d545fc475e59
zhennann <zhen.nann@icloud.com>
2021-09-11 15:14:50 +0800

    feat: cms version

---
+++ src/module-system/a-cms/backend/src/bean/version/update_1.js
@@ -0,0 +1,89 @@
+module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class Version {
+    async _update_1(options) {
+      // create table: aCmsArticle
+      let sql = `
+          CREATE TABLE aCmsArticle (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            atomId int(11) DEFAULT '0',
+            categoryId int(11) DEFAULT '0',
+            language varchar(50) DEFAULT NULL,
+            sticky int(11) DEFAULT '0',
+            keywords varchar(255) DEFAULT NULL,
+            description text DEFAULT NULL,
+            summary text DEFAULT NULL,
+            url varchar(255) DEFAULT NULL,
+            editMode int(11) DEFAULT '0',
+            slug varchar(255) DEFAULT NULL,
+            sorting int(11) DEFAULT '0',
+            flag varchar(255) DEFAULT NULL,
+            extra json DEFAULT NULL,
+            imageFirst varchar(255) DEFAULT NULL,
+            PRIMARY KEY (id)
+          )
+        `;
+      await this.ctx.model.query(sql);
+
+      // create table: aCmsContent
+      sql = `
+          CREATE TABLE aCmsContent (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            atomId int(11) DEFAULT '0',
+            itemId int(11) DEFAULT '0',
+            content LONGTEXT DEFAULT NULL,
+            html LONGTEXT DEFAULT NULL,
+            PRIMARY KEY (id)
+          )
+        `;
+      await this.ctx.model.query(sql);
+
+      // create table: aCmsCategory
+      sql = `
+          CREATE TABLE aCmsCategory (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            categoryName varchar(50) DEFAULT NULL,
+            language varchar(50) DEFAULT NULL,
+            catalog int(11) DEFAULT '0',
+            hidden int(11) DEFAULT '0',
+            sorting int(11) DEFAULT '0',
+            flag varchar(255) DEFAULT NULL,
+            categoryIdParent int(11) DEFAULT '0',
+            PRIMARY KEY (id)
+          )
+        `;
+      await this.ctx.model.query(sql);
+
+      // create view: aCmsArticleView
+      sql = `
+          CREATE VIEW aCmsArticleView as
+            select a.*,b.categoryName from aCmsArticle a
+              left join aCmsCategory b on a.categoryId=b.id
+        `;
+      await this.ctx.model.query(sql);
+
+      // create view: aCmsArticleViewFull
+      sql = `
+          CREATE VIEW aCmsArticleViewFull as
+            select a.*,b.categoryName,c.content,c.html,concat(d.atomName,',',c.content) contentSearch from aCmsArticle a
+              left join aCmsCategory b on a.categoryId=b.id
+              left join aCmsContent c on a.id=c.itemId
+              left join aAtom d on a.atomId=d.id
+        `;
+      await this.ctx.model.query(sql);
+    }
+  }
+  return Version;
+};
