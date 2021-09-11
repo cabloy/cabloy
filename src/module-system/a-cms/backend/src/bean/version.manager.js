0f3f2838563bedc3a113028c7f06bf2cdffbbdeb
zhennann <zhen.nann@icloud.com>
2021-09-11 15:19:57 +0800

    feat: cms version

--- src/module-system/a-cms/backend/src/bean/version.manager.js
+++ src/module-system/a-cms/backend/src/bean/version.manager.js
@@ -1,9 +1,13 @@
 const version_0 = require('./version.manager_0.js');
 const update_1 = require('./version/update_1.js');
+const update_2 = require('./version/update_2.js');
+const update_3 = require('./version/update_3.js');
 
 module.exports = app => {
   const classes = [
     update_1, //
+    update_2,
+    update_3,
   ];
   return app.meta.util.mixinClasses(version_0, classes, app);
 };
