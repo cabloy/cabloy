bbf4e61642a2409782e797af25a0a9cea34da676
zhennann <zhen.nann@icloud.com>
2021-09-12 18:49:44 +0800

    feat: atom->data

--- src/module-system/a-baseadmin/front/src/config/locale/en-us.js
+++ src/module-system/a-baseadmin/front/src/config/locale/en-us.js
@@ -1,4 +1,11 @@
 export default {
   SelectedResources: 'Selected Resources',
   ActionObsoletedTitle: 'Obsoleted',
+  'Atom Action': 'Data Action',
+  'Atom Class': 'Data Class',
+  'Atom Right': 'Data Right',
+  'Atom Right Management': 'Data Right Management',
+  'New Atom Right': 'New Data Right',
+  'Atom Actions': 'Data Actions',
+  Atoms: 'Data',
 };
