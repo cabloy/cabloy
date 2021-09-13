b817fa39a5955b026959def8cd95a85079b06232
zhennann <zhen.nann@icloud.com>
2021-09-13 21:08:07 +0800

    feat: cms site

--- src/module-system/a-cms/front/src/pages/config/site.vue
+++ src/module-system/a-cms/front/src/pages/config/site.vue
@@ -1,67 +1,73 @@
 <template>
   <eb-page>
-    <eb-navbar :title="$text('Site Configuration')" eb-back-link="Back">
+    <eb-navbar :title="page_title" eb-back-link="Back">
       <f7-nav-right>
-        <eb-link iconMaterial="save" :onPerform="onPerformSave"></eb-link>
+        <eb-link ref="buttonSave" iconMaterial="save" :onPerform="onPerformSave"></eb-link>
         <eb-link iconMaterial="info" :eb-href="combineAtomClass('config/siteBase')"></eb-link>
       </f7-nav-right>
     </eb-navbar>
-    <eb-box @size="onSize">
-      <textarea ref="textarea" type="textarea" :value="content" @input="onInput" class="json-textarea json-textarea-margin"></textarea>
-    </eb-box>
+    <eb-json-editor v-if="ready" ref="jsonEditor" :readOnly="false" valueType="object" :value="content" @input="onInput" @save="onSaveEditor"></eb-json-editor>
   </eb-page>
 </template>
 <script>
+import Vue from 'vue';
 import utils from '../../common/utils.js';
+const ebPageDirty = Vue.prototype.$meta.module.get('a-components').options.mixins.ebPageDirty;
 export default {
+  mixins: [ebPageDirty],
   data() {
     const atomClass = utils.parseAtomClass(this.$f7route.query);
     return {
       atomClass,
-      content: '{}',
+      content: null,
+      ready: false,
     };
   },
+  computed: {
+    title() {
+      return this.$text('Site Configuration');
+    },
+    page_title() {
+      return this.page_getDirtyTitle(this.title);
+    },
+  },
   created() {
-    this.$local
-      .dispatch('getConfigSite', {
-        atomClass: this.atomClass,
-      })
-      .then(data => {
-        if (!data) {
-          this.content = '{}';
-        } else {
-          this.content = window.JSON5.stringify(data, null, 2);
-        }
-      });
+    this.init();
   },
   methods: {
+    async init() {
+      // json editor
+      await this.$meta.module.use('a-jsoneditor');
+      // get content
+      this.content = await this.$local.dispatch('getConfigSite', {
+        atomClass: this.atomClass,
+      });
+      // ok
+      this.ready = true;
+    },
     combineAtomClass(url) {
       return utils.combineAtomClass(this.atomClass, url);
     },
-    onSize(size) {
-      this.$$(this.$refs.textarea).css({
-        height: `${size.height - 20}px`,
-        width: `${size.width - 20}px`,
+    onInput(value) {
+      this.content = value;
+      this.page_setDirty(true);
+    },
+    onSaveEditor() {
+      this.$refs.buttonSave.onClick();
+    },
+    async onPerformSave() {
+      const data = this.content;
+      await this.$api.post('site/setConfigSite', {
+        atomClass: this.atomClass,
+        data,
       });
-    },
-    onInput(event) {
-      this.content = event.target.value;
-    },
-    onPerformSave() {
-      const data = window.JSON5.parse(this.content);
-      return this.$api
-        .post('site/setConfigSite', {
-          atomClass: this.atomClass,
-          data,
-        })
-        .then(() => {
-          // change
-          this.$local.commit('setConfigSite', { atomClass: this.atomClass, configSite: data });
-          // refetch languages
-          this.$local.commit('setLanguages', { atomClass: this.atomClass, languages: null });
-          this.$local.dispatch('getLanguages', { atomClass: this.atomClass });
-          return true;
-        });
+      this.page_setDirty(false);
+      // change
+      this.$local.commit('setConfigSite', { atomClass: this.atomClass, configSite: data });
+      // refetch languages
+      this.$local.commit('setLanguages', { atomClass: this.atomClass, languages: null });
+      this.$local.dispatch('getLanguages', { atomClass: this.atomClass });
+      return true;
     },
   },
 };
