d44ecaa8283d1dd8405ad1e9d1d0dc4f71859b88
zhennann <zhen.nann@icloud.com>
2021-09-15 22:35:02 +0800

    feat: renderMarkdown content cms

--- src/module-system/a-components-sync/front/src/common/validate/render/renderMarkdown.js
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderMarkdown.js
@@ -4,38 +4,43 @@ export default {
       return this._renderMarkdown_general(c, context);
     },
     renderMarkdownContent(c, context) {
-      let { property } = context;
-      property = Object.assign({}, property, {
-        ebParams: {
-          host: this._renderMarkdown_host(context),
-        },
-      });
-      context = {
-        ...context,
-        property,
-      };
+      context = this._renderMarkdown_patchContext(context);
       return this._renderMarkdown_general(c, context);
     },
     renderMarkdownContentCms(c, context) {
+      context = this._renderMarkdown_patchContext(context);
+      // action
+      const action = {
+        name: 'preview',
+        actionModule: 'a-cms',
+        actionComponent: 'action',
+        icon: { material: 'visibility' },
+      };
+      // actions
+      if (!context.property.ebParams.actions) context.property.ebParams.actions = [];
+      context.property.ebParams.actions.push(action);
+      return this._renderMarkdown_general(c, context);
+    },
+    _renderMarkdown_patchContext(context) {
       let { property } = context;
-      property = Object.assign({}, property, {
-        ebParams: {
-          host: this._renderMarkdown_host(context),
-          actions: [
-            {
-              name: 'preview',
-              actionModule: 'a-cms',
-              actionComponent: 'action',
-              icon: { material: 'visibility' },
-            },
-          ],
+      property = this.$meta.util.extend(
+        {
+          ebParams: {
+            actionSave: true,
+            actionDone: true,
+          },
         },
-      });
-      context = {
+        property,
+        {
+          ebParams: {
+            host: this._renderMarkdown_host(context),
+          },
+        }
+      );
+      return {
         ...context,
         property,
       };
-      return this._renderMarkdown_general(c, context);
     },
     _renderMarkdown_general(c, context) {
       return this._renderComponent(c, context, {
