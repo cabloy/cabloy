297d891d0837f77c65dbd8b3e337143f4eba41db
zhennann <zhen.nann@icloud.com>
2021-09-19 14:44:21 +0800

    fix: createComponentInstance

--- packages/egg-born-front/src/base/util.js
+++ packages/egg-born-front/src/base/util.js
@@ -210,7 +210,7 @@ export default function (Vue) {
       return `/${moduleInfo.url}/${arg}`;
     },
     createComponentInstance(component, options) {
-      const _component = Object.assign({}, component, options);
+      const _component = this.extend({}, component, options);
       return new Vue(_component);
     },
     _combineComponentsProps(parent, component) {
