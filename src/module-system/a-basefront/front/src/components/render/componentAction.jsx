c98f285fb3a261cfa4f390b48c80fec0e0031947
zhennann <zhen.nann@icloud.com>
2021-09-19 21:06:17 +0800

    feat: dateRange

--- src/module-system/a-basefront/front/src/components/render/componentAction.jsx
+++ src/module-system/a-basefront/front/src/components/render/componentAction.jsx
@@ -13,7 +13,37 @@ export default {
       if (this.action.name === 'dateRange') return this._renderDateRange();
     },
     _renderDateRange() {
-      return <eb-list-item title="title"></eb-list-item>;
+      const { ctx, item } = this.$props;
+      const { context } = item;
+      const { property } = context;
+      const children = [
+        this._renderDateRange_renderProperty({ ctx, context, property, mode: 'begin' }), //
+        this._renderDateRange_renderProperty({ ctx, context, property, mode: 'end' }),
+      ];
+      return children;
+    },
+    _renderDateRange_renderProperty({ ctx, context, mode }) {
+      const { key } = context;
+      const propertyDatepicker = this._renderDateRange_combineProperty({ ctx, context, mode });
+      context = {
+        ...context,
+        key: `${key}:${mode}`,
+        property: propertyDatepicker,
+      };
+      return ctx._renderItem(context);
+    },
+    _renderDateRange_combineProperty({ ctx, context, mode }) {
+      const { property } = context;
+      const propertyDatepicker = {
+        type: ['object', 'null'],
+        ebType: 'datepicker',
+        ebRender: null,
+        ebSearch: {
+          tableAlias: 'a',
+          operators: mode === 'begin' ? '>=' : '<=',
+        },
+      };
+      return ctx.$meta.util.extend({}, property, propertyDatepicker);
     },
   },
 };
