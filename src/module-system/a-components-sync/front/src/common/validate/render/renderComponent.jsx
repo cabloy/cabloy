0248381414922793297a1ae4f504420d5b6c5e15
zhennann <zhen.nann@icloud.com>
2021-09-19 10:49:17 +0800

    feat: validate jsx

--- src/module-system/a-components-sync/front/src/common/validate/render/renderComponent.jsx
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderComponent.jsx
@@ -6,9 +6,9 @@ export default {
     },
     _renderComponent(context, ebRender) {
       const { key } = context;
-      const renderProps = this.$meta.util.extend({ options: { props: {} } }, ebRender);
-      renderProps.options.props.context = context;
-      return <eb-list-item-component key={key} props={this.renderProps}></eb-list-item-component>;
+      const props = this.$meta.util.extend({ options: { props: {} } }, ebRender);
+      props.options.props.context = context;
+      return <eb-list-item-component key={key} {...{ props }}></eb-list-item-component>;
     },
   },
 };
