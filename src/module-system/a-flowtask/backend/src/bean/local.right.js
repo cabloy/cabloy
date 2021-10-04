5050607d4e8c0bdbce4af78e9b5abce794179bf2
zhennann <zhen.nann@icloud.com>
2021-10-04 14:52:26 +0800

    feat: flow task

--- src/module-system/a-flowtask/backend/src/bean/local.right.js
+++ src/module-system/a-flowtask/backend/src/bean/local.right.js
@@ -2,15 +2,17 @@ module.exports = ctx => {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Right {
     _check_notDone({ flowTask }) {
+      const flowTaskId = flowTask.flowTaskId || flowTask.id;
       // not complete
       if (flowTask.flowTaskStatus === 1) {
-        ctx.throw.module(moduleInfo.relativeName, 1002, flowTask.id);
+        ctx.throw.module(moduleInfo.relativeName, 1002, flowTaskId);
       }
     }
     _check_sameUser({ flowTask, user }) {
+      const flowTaskId = flowTask.flowTaskId || flowTask.id;
       // must be the same user
       if (user && user.id !== 0 && user.id !== flowTask.userIdAssignee) {
-        ctx.throw.module(moduleInfo.relativeName, 1002, flowTask.id);
+        ctx.throw.module(moduleInfo.relativeName, 1002, flowTaskId);
       }
     }
     async _getNodeOptions({ flowTask, nodeInstance }) {
@@ -31,6 +33,7 @@ module.exports = ctx => {
       }
     }
     async forward({ flowTask, user, nodeInstance }) {
+      const flowTaskId = flowTask.flowTaskId || flowTask.id;
       // not complete
       this._check_notDone({ flowTask });
       // must be the same user
@@ -38,7 +41,7 @@ module.exports = ctx => {
       // options
       const options = await this._getNodeOptions({ flowTask, nodeInstance });
       if (!options.allowForward || flowTask.userIdForwardTo) {
-        ctx.throw.module(moduleInfo.relativeName, 1012, flowTask.id);
+        ctx.throw.module(moduleInfo.relativeName, 1012, flowTaskId);
       }
     }
   }
