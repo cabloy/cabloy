24b1343c7453d20b59cad7d0224320f5ab7a4fce
zhennann <zhen.nann@icloud.com>
2021-10-04 14:24:54 +0800

    feat: flow task

--- src/module-system/a-flowtask/backend/src/bean/local.flow.task.js
+++ src/module-system/a-flowtask/backend/src/bean/local.flow.task.js
@@ -2,12 +2,13 @@ const flowTask_0 = require('./local.flow.task/local.flow.task_0.js');
 const flowTask_claim = require('./local.flow.task/local.flow.task_claim.js');
 const flowTask_complete = require('./local.flow.task/local.flow.task_complete.js');
 const flowTask_event = require('./local.flow.task/local.flow.task_event.js');
+const flowTask_init = require('./local.flow.task/local.flow.task_init.js');
 const flowTask_schema = require('./local.flow.task/local.flow.task_schema.js');
 
 module.exports = ctx => {
   return ctx.app.meta.util.mixinClasses(
     flowTask_0,
-    [flowTask_claim, flowTask_complete, flowTask_event, flowTask_schema],
+    [flowTask_claim, flowTask_complete, flowTask_event, flowTask_init, flowTask_schema],
     ctx
   );
 };
--- src/module-system/a-flowtask/backend/src/bean/local.flow.task/local.flow.task_0.js
+++ src/module-system/a-flowtask/backend/src/bean/local.flow.task/local.flow.task_0.js
@@ -1,6 +1,3 @@
-const VarsFn = require('../../common/vars.js');
-const UtilsFn = require('../../common/utils.js');
-
 module.exports = ctx => {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class FlowTask {
@@ -34,90 +31,6 @@ module.exports = ctx => {
       await this._contextInit({ flowTaskId, user });
       // event
       await this.created();
-    }
-
-    async _load({ flowTask, user, history }) {
-      // context init
-      await this._contextInit({ flowTaskId: flowTask.id, user, history });
-    }
-
-    async _createFlowTask({ userIdAssignee, user }) {
-      // flowTask
-      const data = {
-        flowId: this.context._flowId,
-        flowNodeId: this.contextNode._flowNodeId,
-        flowTaskStatus: 0,
-        userIdAssignee,
-        specificFlag: 0,
-        taskVars: '{}',
-      };
-      const res = await this.modelFlowTask.insert(data);
-      const flowTaskId = res.insertId;
-      // flowTaskHistory
-      data.flowTaskId = flowTaskId;
-      await this.modelFlowTaskHistory.insert(data);
-      // notify
-      this._notifyTaskClaimings(userIdAssignee);
-      // publish uniform message
-      if (userIdAssignee !== user.id) {
-        const userFlow = await ctx.bean.user.get({ id: this.context._flow.flowUserId });
-        const userAssignee = await ctx.bean.user.get({ id: userIdAssignee });
-        const title = `${ctx.text.locale(userAssignee.locale, 'Task')} - ${ctx.text.locale(
-          userAssignee.locale,
-          this.contextNode._flowNode.flowNodeName
-        )}`;
-        const actionPath = `/a/flowtask/flow?flowId=${this.context._flowId}&flowTaskId=${flowTaskId}`;
-        const message = {
-          userIdTo: userIdAssignee,
-          content: {
-            issuerId: userFlow.id,
-            issuerName: userFlow.userName,
-            issuerAvatar: userFlow.avatar,
-            title,
-            body: this.context._flow.flowName,
-            actionPath,
-            params: {
-              flowId: this.context._flowId,
-              flowTaskId,
-            },
-          },
-        };
-        // jump out of the transaction
-        ctx.tail(async () => {
-          await ctx.bean.io.publish({
-            message,
-            messageClass: {
-              module: 'a-flow',
-              messageClassName: 'workflow',
-            },
-          });
-        });
-      }
-      // ok
-      return flowTaskId;
-    }
-
-    async _contextInit({ flowTaskId, user, history }) {
-      // flowTaskId
-      this.contextTask._flowTaskId = flowTaskId;
-      // flowTask
-      if (!history) {
-        this.contextTask._flowTask = await this.modelFlowTask.get({ id: flowTaskId });
-      }
-      this.contextTask._flowTaskHistory = await this.modelFlowTaskHistory.get({ flowTaskId });
-      // taskVars
-      this.contextTask._taskVars = new (VarsFn())();
-      this.contextTask._taskVars._vars = this.contextTask._flowTaskHistory.taskVars
-        ? JSON.parse(this.contextTask._flowTaskHistory.taskVars)
-        : {};
-      // utils
-      this.contextTask._utils = new (UtilsFn({ ctx, flowInstance: this.flowInstance }))({
-        context: this.context,
-        contextNode: this.contextNode,
-        contextTask: this.contextTask,
-      });
-      // user
-      this.contextTask._user = user;
     }
 
     async _hidden({ hidden }) {
