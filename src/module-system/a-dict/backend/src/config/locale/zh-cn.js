6ba43990c79ea680a093575c53fcc290d52a8048
zhennann <zhen.nann@icloud.com>
2021-10-17 17:49:18 +0800

    feat: locales

--- src/module-system/a-dict/backend/src/config/locale/zh-cn.js
+++ src/module-system/a-dict/backend/src/config/locale/zh-cn.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'Create Dict': '新建Dict',
-  'Dict List': 'Dict列表',
+  'Create Dict': '新建字典',
+  'Dict List': '字典列表',
 };
--- src/module-system/a-dict/front/src/config/locale/zh-cn.js
+++ src/module-system/a-dict/front/src/config/locale/zh-cn.js
@@ -1,4 +1,6 @@
 export default {
   Name: '名称',
   Description: '描述',
+  'Dict Items': '字典项',
+  'Dict Locales': '字典本地化',
 };
