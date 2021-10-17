9f604174f927f1adaa82f1bdd86add9461a322cf
zhennann <zhen.nann@icloud.com>
2021-10-17 17:26:02 +0800

    feat: dict

--- src/module-system/a-dict/backend/src/bean/version.manager.js
+++ src/module-system/a-dict/backend/src/bean/version.manager.js
@@ -3,7 +3,7 @@ module.exports = app => {
     async update(options) {
       if (options.version === 1) {
         // create table: aDict
-        const sql = `
+        let sql = `
           CREATE TABLE aDict (
             id int(11) NOT NULL AUTO_INCREMENT,
             createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -16,6 +16,22 @@ module.exports = app => {
           )
         `;
         await this.ctx.model.query(sql);
+        // create table: aDictContent
+        sql = `
+          CREATE TABLE aDictContent (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updatedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            deleted int(11) DEFAULT '0',
+            iid int(11) DEFAULT '0',
+            atomId int(11) DEFAULT '0',
+            itemId int(11) DEFAULT '0',
+            dictContent JSON DEFAULT NULL,
+            dictLocales JSON DEFAULT NULL,
+            PRIMARY KEY (id)
+          )
+        `;
+        await this.ctx.model.query(sql);
       }
     }
 
@@ -23,14 +39,17 @@ module.exports = app => {
       if (options.version === 1) {
         // add role rights
         const roleRights = [
-          { roleName: 'authenticated', action: 'create' },
-          { roleName: 'authenticated', action: 'read', scopeNames: 0 },
-          { roleName: 'authenticated', action: 'write', scopeNames: 0 },
-          { roleName: 'authenticated', action: 'delete', scopeNames: 0 },
-          { roleName: 'authenticated', action: 'clone', scopeNames: 0 },
-          { roleName: 'authenticated', action: 'deleteBulk' },
-          { roleName: 'authenticated', action: 'exportBulk' },
-          { roleName: 'system', action: 'read', scopeNames: 'authenticated' },
+          { roleName: 'system', action: 'create' },
+          { roleName: 'system', action: 'read', scopeNames: 0 },
+          { roleName: 'system', action: 'read', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'write', scopeNames: 0 },
+          { roleName: 'system', action: 'write', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'delete', scopeNames: 0 },
+          { roleName: 'system', action: 'delete', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'clone', scopeNames: 0 },
+          { roleName: 'system', action: 'clone', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'deleteBulk' },
+          { roleName: 'system', action: 'exportBulk' },
         ];
         await this.ctx.bean.role.addRoleRightBatch({ atomClassName: 'dict', roleRights });
       }
