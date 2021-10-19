f861b7eb4fb271b4b814eeb5c4184e590e589f01
zhennann <zhen.nann@icloud.com>
2021-10-19 20:52:18 +0800

    feat: renderDict as select

--- src/module-system/a-dict/front/src/components/render/renderDict.jsx
+++ src/module-system/a-dict/front/src/components/render/renderDict.jsx
@@ -18,15 +18,33 @@ export default {
       const { property } = this.context;
       const dictKey = property.ebParams.dictKey;
       this.dict = await this.$store.dispatch('a/dict/getDict', { dictKey });
-      console.log(this.dict);
+    },
+    _renderAsSelect() {
+      if (!this.dict) return <div></div>;
+      const { parcel, key, property } = this.context;
+      const options = this.dict._dictItems.map(item => {
+        return {
+          title: item.titleLocale,
+          value: item.code,
+        };
+      });
+      const propertyNew = this.$utils.extend({}, property, {
+        ebType: 'select',
+        ebOptions: options,
+        ebParams: null,
+      });
+      return <eb-list-item-validate parcel={parcel} dataKey={key} property={propertyNew}></eb-list-item-validate>;
+    },
+    _renderAsTree() {
+      return <div></div>;
     },
   },
   render() {
-    const { parcel, key, property } = this.context;
-    const propertyNew = this.$utils.extend({}, property, {
-      ebType: 'select',
-      ebOptions: this.locales,
-    });
-    return <eb-list-item-validate parcel={parcel} dataKey={key} property={propertyNew}></eb-list-item-validate>;
+    const { property } = this.context;
+    const mode = property.ebParams && property.ebParams.mode;
+    if (!mode || mode === 'select') {
+      return this._renderAsSelect();
+    }
+    return this._renderAsTree();
   },
 };
