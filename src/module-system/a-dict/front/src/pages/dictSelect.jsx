93b3c43bc31d72dddced84898467477f99b695c7
zhennann <zhen.nann@icloud.com>
2021-10-20 21:44:49 +0800

    feat: dictSelect

---
+++ src/module-system/a-dict/front/src/pages/dictSelect.jsx
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+const ebPageContext = Vue.prototype.$meta.module.get('a-components').options.mixins.ebPageContext;
+export default {
+  mixins: [ebPageContext],
+  components: {
+    users,
+  },
+  computed: {
+    title() {
+      return this.contextParams.title;
+    },
+  },
+  methods: {
+    onSearch(query) {
+      this.$refs.list.onSearch(query);
+    },
+    onLoadMore() {
+      this.$refs.list.loadMore();
+    },
+    onDisable() {
+      this.$f7router.back();
+    },
+    onSelect(event, item) {
+      this.contextCallback(200, item);
+      this.$f7router.back();
+    },
+  },
+  render() {
+    return (
+      <eb-search-page
+        title={this.title}
+        onSearch={this.onSearch}
+        onLoadMore={this.onLoadMore}
+        onDisable={this.onDisable}
+      >
+        <div>sssss</div>
+      </eb-search-page>
+    );
+  },
+};
