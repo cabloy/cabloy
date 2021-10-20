3d571538a95bb1edd177eaeecacc9b8793d5cf52
zhennann <zhen.nann@icloud.com>
2021-10-20 20:47:52 +0800

    feat: findDictItem

--- src/module-system/a-dict/front/src/components/render/renderDict.jsx
+++ src/module-system/a-dict/front/src/components/render/renderDict.jsx
@@ -10,10 +10,17 @@ export default {
       dictItemTitle: null,
     };
   },
-  computed: {},
+  computed: {
+    value() {
+      return this.context.getValue();
+    },
+  },
   watch: {
     context() {
       this._loadDict();
+    },
+    value() {
+      this._loadDictItem();
     },
   },
   created() {
@@ -21,12 +28,23 @@ export default {
   },
   methods: {
     async _loadDict() {
+      const { property, validate } = this.context;
+      const dictKey = property.ebParams.dictKey;
+      if (!validate.readOnly && !property.ebReadOnly) {
+        this.dict = await this.$store.dispatch('a/dict/getDict', { dictKey });
+      }
+      // load dict item
+      await this._loadDictItem();
+    },
+    async _loadDictItem() {
       const { key, property, validate } = this.context;
+      const dictKey = property.ebParams.dictKey;
       if (validate.readOnly || property.ebReadOnly) {
         this.dictItemTitle = this.context.getValue(`_${key}TitleLocale`);
       } else {
-        const dictKey = property.ebParams.dictKey;
-        this.dict = await this.$store.dispatch('a/dict/getDict', { dictKey });
+        const code = this.context.getValue();
+        const options = property.ebParams && property.ebParams.options;
+        const dictItem = await this.$store.dispatch('a/dict/findItem', { dictKey, code, options });
       }
     },
     _renderAsSelect() {
--- src/module-system/a-dict/front/src/store.js
+++ src/module-system/a-dict/front/src/store.js
@@ -21,21 +21,45 @@ export default function (Vue) {
       },
     },
     actions: {
-      getDict({ state, commit }, { dictKey }) {
-        return new Promise((resolve, reject) => {
-          if (state.dicts[dictKey]) return resolve(state.dicts[dictKey]);
-          Vue.prototype.$meta.api
-            .post('/a/dict/dict/getDict', { dictKey })
-            .then(data => {
-              const dict = data;
-              _adjustDict({ dict });
-              commit('setDict', { dictKey, dict });
-              resolve(dict);
-            })
-            .catch(err => {
-              reject(err);
-            });
+      async getDict({ state, commit }, { dictKey }) {
+        if (state.dicts[dictKey]) return state.dicts[dictKey];
+        const data = await Vue.prototype.$meta.api.post('/a/dict/dict/getDict', { dictKey });
+        const dict = data;
+        _adjustDict({ dict });
+        commit('setDict', { dictKey, dict });
+        return dict;
+      },
+      async findItem({ state, dispatch }, { dictKey, code, options }) {
+        if (!code) return null;
+        code = String(code);
+        // options
+        options = options || { separator: '/' };
+        const separator = options.separator;
+        // dict
+        const dict = await dispatch('getDict', { dictKey });
+        if (!dict._cache) dict._cache = {};
+        let dictItemRes = dict._cache[code];
+        if (dictItemRes) return dictItemRes;
+        // find
+        const dictItemsRes = [];
+        const res = _findItem_loop({
+          dictItemsRes,
+          dictItemsMap: dict._dictItemsMap,
+          codes: code.split('/'),
         });
+        if (!res) return null;
+        const titleFull = dictItemsRes.map(item => item.title).join(separator);
+        const titleLocaleFull = dictItemsRes.map(item => item.titleLocale).join(separator);
+        dictItemRes = {
+          ...dictItemsRes[dictItemsRes.length - 1],
+          codeFull: code,
+          titleFull,
+          titleLocaleFull,
+        };
+        // cache
+        dict._cache[code] = dictItemRes;
+        // ok
+        return dictItemRes;
       },
     },
   };
@@ -65,4 +89,17 @@ function _adjustDict_loop({ dict, dictItemsMap, dictItems }) {
       });
     }
   }
+}
+
+function _findItem_loop({ dictItemsRes, dictItemsMap, codes }) {
+  const code = codes.shift();
+  const dictItem = dictItemsMap && dictItemsMap[code];
+  if (!dictItem) return false;
+  dictItemsRes.push(dictItem);
+  if (codes.length === 0) return true;
+  return _findItem_loop({
+    dictItemsRes,
+    dictItemsMap: dictItem._childrenMap,
+    codes,
+  });
 }
