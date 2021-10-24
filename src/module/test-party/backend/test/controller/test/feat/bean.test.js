d6f7e418d58a0f6c104c3ebff7273f458f732bfd
zhennann <zhen.nann@icloud.com>
2021-10-24 18:55:07 +0800

    test: bean

--- src/module/test-party/backend/test/controller/test/feat/bean.test.js
+++ src/module/test-party/backend/test/controller/test/feat/bean.test.js
@@ -1,8 +1,9 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe('test/controller/test/feat/bean.test.js', () => {
+describe.only('test/controller/test/feat/bean.test.js', () => {
   it('action:bean', async () => {
     const result = await app.httpRequest().get(mockUrl('test/feat/bean'));
+    console.log(result);
     assert.equal(result.body.code, 0);
   });
 
