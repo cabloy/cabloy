d6e79409631cab8599988e25eb31acb509f3c4ce
zhennann <zhen.nann@icloud.com>
2021-10-25 21:46:02 +0800

    feat: resource summary

--- src/module-system/a-base-sync/backend/src/bean/atom.resource.js
+++ src/module-system/a-base-sync/backend/src/bean/atom.resource.js
@@ -67,20 +67,25 @@ module.exports = app => {
       if (showSorting) {
         flags.push(item.resourceSorting);
       }
-      // layout
+      // typeCategory
+      let typeCategory;
       const layout = options && options.layout;
       if (layout === 'list' || layout === 'select' || layout === 'selecting') {
         // type/categary
         if (resourceType) {
-          const typeCategory = `${item.resourceTypeLocale} / ${item.atomCategoryNameLocale}`;
-          flags.push(typeCategory);
+          typeCategory = `${item.resourceTypeLocale} / ${item.atomCategoryNameLocale}`;
         }
       } else if (layout === 'table') {
         // donothing
       }
+      // summary
+      const summaries = [];
+      if (typeCategory) summaries.push(typeCategory);
+      if (item.description) summaries.push(item.description);
+      const summary = summaries.join(' / ');
       // meta
       const meta = {
-        summary: item.description,
+        summary,
         flags,
       };
       // ok
