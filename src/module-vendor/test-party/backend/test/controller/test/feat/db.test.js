69eb98d2f3a887a869cf731832e079a14dba1505
zhennann <zhen.nann@icloud.com>
2021-11-02 16:57:35 +0800

    feat: db test

--- src/module-vendor/test-party/backend/test/controller/test/feat/db.test.js
+++ src/module-vendor/test-party/backend/test/controller/test/feat/db.test.js
@@ -1,6 +1,6 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe.only('test/controller/test/feat/db.test.js', () => {
+describe('test/controller/test/feat/db.test.js', () => {
   it('action:db', async () => {
     let result;
     // iid
