9188c4c4d34f6e22179280ee4384563eeb011a22
zhennann <zhen.nann@icloud.com>
2021-11-04 10:23:16 +0800

    fix: dict menu right

--- src/module-system/a-dict/backend/src/config/static/resources.js
+++ src/module-system/a-dict/backend/src/config/static/resources.js
@@ -5,7 +5,7 @@ module.exports = app => {
     {
       atomName: 'Create Dict',
       atomStaticKey: 'createDict',
-      atomRevision: 0,
+      atomRevision: 1,
       atomCategoryId: 'a-base:menu.Create',
       resourceType: 'a-base:menu',
       resourceConfig: JSON.stringify({
@@ -13,12 +13,12 @@ module.exports = app => {
         atomClassName: 'dict',
         atomAction: 'create',
       }),
-      resourceRoles: 'authenticated',
+      resourceRoles: 'template.system',
     },
     {
       atomName: 'Dict List',
       atomStaticKey: 'listDict',
-      atomRevision: 0,
+      atomRevision: 1,
       atomCategoryId: 'a-base:menu.List',
       resourceType: 'a-base:menu',
       resourceConfig: JSON.stringify({
@@ -26,7 +26,7 @@ module.exports = app => {
         atomClassName: 'dict',
         atomAction: 'read',
       }),
-      resourceRoles: 'authenticated',
+      resourceRoles: 'template.system',
     },
   ];
   return resources;
