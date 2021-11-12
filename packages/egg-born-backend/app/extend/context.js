68d2834702639bf3dc73a959d11f293769b3711c
zhennann <zhen.nann@icloud.com>
2021-11-12 23:17:19 +0800

    enhance: performAction cookies

--- packages/egg-born-backend/app/extend/context.js
+++ packages/egg-born-backend/app/extend/context.js
@@ -355,9 +355,10 @@ function respond() {
 }
 
 function delegateCookies(ctx, ctxCaller) {
+  const _cookies = ctx.cookies;
   Object.defineProperty(ctx, 'cookies', {
     get() {
-      return ctxCaller.cookies;
+      return ctxCaller.cookies || _cookies;
     },
   });
 }
