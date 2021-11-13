0e23023f4adda5fc3da32755f1012dcdd9de1f78
zhennann <zhen.nann@icloud.com>
2021-11-13 20:50:08 +0800

    feat: markdown block $$

--- src/module-system/a-markdownrender/front/src/components/render.jsx
+++ src/module-system/a-markdownrender/front/src/components/render.jsx
@@ -148,6 +148,7 @@ export default {
         $container: blockContainer,
         $content: blockContent,
         $util,
+        $$: this.$$,
       };
     },
   },
--- src/module-system/cms-pluginarticle/backend/cms/plugin/assets/js/render.js
+++ src/module-system/cms-pluginarticle/backend/cms/plugin/assets/js/render.js
@@ -70,6 +70,7 @@
       $container: $blockContainer,
       $content: blockContent,
       $util,
+      $$: $,
     };
   }
 })();
