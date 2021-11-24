05440627f72ded16c3ec7a93924d5838dcb0f61e
zhennann <zhen.nann@icloud.com>
2021-11-24 11:41:23 +0800

    enhance: input validate error

--- src/module-system/a-components-sync/front/src/components/input.vue
+++ src/module-system/a-components-sync/front/src/components/input.vue
@@ -1,6 +1,7 @@
 <script>
 import Vue from 'vue';
 import validateCheck from '../common/validate/validateCheck.js';
+import validateCheckInput from '../common/validate/validateCheckInput.js';
 const f7Input = Vue.options.components['f7-input'].extendOptions;
 export default {
   meta: {
@@ -8,7 +9,7 @@ export default {
   },
   name: 'eb-input',
   extends: f7Input,
-  mixins: [validateCheck],
+  mixins: [validateCheck, validateCheckInput],
 };
 </script>
 <style scoped></style>
--- src/module-system/a-components-sync/front/src/components/listInput.vue
+++ src/module-system/a-components-sync/front/src/components/listInput.vue
@@ -1,6 +1,7 @@
 <script>
 import Vue from 'vue';
 import validateCheck from '../common/validate/validateCheck.js';
+import validateCheckInput from '../common/validate/validateCheckInput.js';
 const f7ListInput = Vue.options.components['f7-list-input'].extendOptions;
 export default {
   meta: {
@@ -8,7 +9,7 @@ export default {
   },
   name: 'eb-list-input',
   extends: f7ListInput,
-  mixins: [validateCheck],
+  mixins: [validateCheck, validateCheckInput],
 };
 </script>
 <style scoped></style>
