1e67af10e0905df928ab2e38fca7ed27d866a92a
zhennann <zhen.nann@icloud.com>
2021-11-30 13:18:30 +0800

    enhance: renderToggle

--- src/module-system/a-components-sync/front/src/common/validate/render/renderToggle.jsx
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderToggle.jsx
@@ -1,17 +1,32 @@
 export default {
   methods: {
+    _renderToggleOnClick(event, context) {
+      const { property } = context;
+      const disabled = this.validate.readOnly || property.ebReadOnly;
+      if (disabled) return;
+      const value = context.getValue();
+      context.setValue(!value);
+    },
     renderToggle(context) {
       const { key, property, dataPath } = context;
       const title = this.getTitle(context);
+      const disabled = this.validate.readOnly || property.ebReadOnly;
       // props
       const props = {
         dataPath,
         value: context.getValue(),
-        disabled: this.validate.readOnly || property.ebReadOnly,
+        disabled,
       };
+      // staticClass
+      let staticClass = '';
+      if (property.ebReadOnly) {
+        staticClass = 'text-color-gray';
+      } else if (!disabled) {
+        staticClass = 'eb-cursor-pointer';
+      }
       return (
         <f7-list-item key={key}>
-          <div slot="title" staticClass={property.ebReadOnly ? 'text-color-gray' : ''}>
+          <div slot="title" staticClass={staticClass} onClick={event => this._renderToggleOnClick(event, context)}>
             {title}
           </div>
           <eb-toggle
