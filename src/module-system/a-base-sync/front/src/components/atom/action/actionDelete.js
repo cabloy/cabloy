58d266b6b741405ac98789123c159197eda7d499
zhennann <zhen.nann@icloud.com>
2021-12-22 17:50:21 +0800

    feat: update formal when close draft

--- src/module-system/a-base-sync/front/src/components/atom/action/actionDelete.js
+++ src/module-system/a-base-sync/front/src/components/atom/action/actionDelete.js
@@ -7,6 +7,10 @@ export default {
       const key = { atomId: item.atomId, itemId: item.itemId };
       await ctx.$api.post('/a/base/atom/delete', { key });
       ctx.$meta.eventHub.$emit('atom:action', { key, action });
+      // update formal
+      if (item.atomStage === 0 && item.atomIdFormal) {
+        ctx.$meta.eventHub.$emit('atom:action', { key: { atomId: item.atomIdFormal }, action: { name: 'save' } });
+      }
       // back
       if (ctx.$pageRoute.path === '/a/basefront/atom/item') {
         ctx.$f7router.back();
