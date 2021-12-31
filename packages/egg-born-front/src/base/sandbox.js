8a650c3c88d216a5090014076623a7aabee27dba
zhennann <zhen.nann@icloud.com>
2021-12-31 10:26:01 +0800

    refactor: sandbox webworker

--- packages/egg-born-front/src/base/sandbox.js
+++ packages/egg-born-front/src/base/sandbox.js
@@ -1,39 +1,10 @@
-import workerJS from './sandbox.spec.js';
+import sandbox from 'sandbox-webworker';
 export default function (Vue) {
   return {
-    _idCounter: 0,
-    _callbacks: {},
-    _worker: null,
-    getWorker() {
-      if (this._worker) return this._worker;
-      const blobURL = Vue.prototype.$meta.util.fn2workerURL(workerJS.toString());
-      this._worker = new Worker(blobURL);
-      this._worker.onmessage = event => {
-        const { id, err, value } = event.data;
-        const cb = this._callbacks[id];
-        if (cb) {
-          delete this._callbacks[id];
-          cb.callback(err, value);
-        }
-      };
-      return this._worker;
-    },
-    evaluate(expression, scope) {
-      return new Promise((resolve, reject) => {
-        // id
-        const id = ++this._idCounter;
-        // callback
-        this._callbacks[id] = {
-          callback: (err, value) => {
-            if (err) return reject(new Error(err.message));
-            resolve(value);
-          },
-        };
-        // scope
-        scope = this._purgeScope(scope);
-        const worker = this.getWorker();
-        worker.postMessage({ id, expression, scope });
-      });
+    async evaluate(expression, scope) {
+      // scope
+      scope = this._purgeScope(scope);
+      return await sandbox.evaluate(expression, scope);
     },
     _purgeScope(scope) {
       scope = Vue.prototype.$meta.util.extend({}, scope);
