1fc8758839687f44fcdf9f94c08e0c7898170c31
zhennann <zhen.nann@icloud.com>
2022-01-04 21:52:17 +0800

    feat: move department of dingtalk

--- src/module-system/a-dingtalk/backend/src/service/contacts.js
+++ src/module-system/a-dingtalk/backend/src/service/contacts.js
@@ -426,7 +426,25 @@ module.exports = app => {
           this.ctx.throw(1004, department.departmentId);
         }
       }
-      // update role name/order
+      // update role
+      await this._updateRoleAndDepartment_role({ localDepartment, department });
+      // update department
+      department.id = localDepartment.id;
+      await this.ctx.model.department.update(department);
+    }
+
+    async _updateRoleAndDepartment_role({ localDepartment, department }) {
+      // change role parent
+      if (department.departmentParentId && department.departmentParentId !== localDepartment.departmentParentId) {
+        const departmentParent = await this.ctx.model.department.get({ departmentId: department.departmentParentId });
+        if (!departmentParent) {
+          this.ctx.throw(1004, department.departmentParentId);
+        }
+        const roleIdParent = departmentParent.roleId;
+        // move
+        await this.ctx.bean.role.move({ roleId: localDepartment.roleId, roleIdParent });
+      }
+      // update role: name/order
       const data = {};
       if (department.departmentName) {
         data.roleName = department.departmentName;
@@ -440,9 +458,6 @@ module.exports = app => {
           data,
         });
       }
-      // update department
-      department.id = localDepartment.id;
-      await this.ctx.model.department.update(department);
     }
 
     async _updateUserRoles({ userId, departmentIdsOld, departmentIdsNew }) {
