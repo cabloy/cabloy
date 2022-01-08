8f6ceaf61b47a3c957576ea82e0217b338ffb307
zhennann <zhen.nann@icloud.com>
2022-01-08 21:23:47 +0800

    Publish
     - egg-born-module-a-wxwork@4.1.10

--- src/module-system/a-wxwork/dist/backend.js
+++ src/module-system/a-wxwork/dist/backend.js
@@ -703,6 +703,7 @@ module.exports = appInfo => {
     department: {
       roleContainer: 'internal',
       roleTop: 'wxwork',
+      reverseOrder: true,
     },
   };
 
@@ -2390,11 +2391,15 @@ module.exports = app => {
     }
 
     _adjustFields(itemDest, itemSrc, fieldMap) {
+      const reverseOrder = this.ctx.config.sync.department.reverseOrder;
       for (const index in fieldMap[1]) {
         const field = fieldMap[1][index];
         if (itemSrc[field] !== undefined) {
           const fieldDest = fieldMap[0][index];
           itemDest[fieldDest] = this._adjustFieldType(itemSrc[field], fieldMap[2][index]);
+          if (fieldDest === 'departmentOrder' && reverseOrder) {
+            itemDest[fieldDest] = -itemDest[fieldDest];
+          }
         }
       }
     }
