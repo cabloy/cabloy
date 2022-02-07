f5e6ceac361bc1e88475e331b52480948b2cf6b5
zhennann <zhen.nann@icloud.com>
2022-02-07 22:27:16 +0800

    jsdoc: a-base front config

--- src/module-system/a-base-sync/front/src/config/config.js
+++ src/module-system/a-base-sync/front/src/config/config.js
@@ -1,7 +1,18 @@
+/** @module a-base/front/config */
+
 import configResourceRenderList from './config/configResourceRenderList.js';
 import avatarUser from '../assets/img/user.png';
 
-export default {
+/** The front config of a-base
+ * @property {object} stage - The stages of atom.
+ * @property {number} stage.draft - 0
+ * @property {number} stage.formal - 1
+ * @property {number} stage.history - 2
+ * @property {object} user
+ * @property {object} user.avatar
+ * @property {string} user.avatar.default - The default avatar
+ */
+const config = {
   stage: {
     draft: 0,
     formal: 1,
@@ -20,3 +31,4 @@ export default {
     },
   },
 };
+export default config;
