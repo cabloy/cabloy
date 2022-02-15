c1476d4482e371d8c7aa798e26b976330c25a8e6
zhennann <zhennann@users.noreply.github.com>
2022-02-15 04:22:28 +0000

    deploy: e48aa84c5187ac6ac7f1e72899a16d27bc144f92

--- scripts/collapse.js
+++ scripts/collapse.js
@@ -1,10 +1,4 @@
-function hideAllButCurrent(){
-    //by default all submenut items are hidden
-    //but we need to rehide them for search
-    document.querySelectorAll("nav > ul > li > ul li").forEach(function(parent) {
-        parent.style.display = "none";
-    });
-    
+function showCurrent(){
     //only current page (if it exists) should be opened
     var file = window.location.pathname.split("/").pop().replace(/\.html/, '');
     document.querySelectorAll("nav > ul > li > a").forEach(function(parent) {
@@ -13,8 +7,18 @@ function hideAllButCurrent(){
             parent.parentNode.querySelectorAll("ul li").forEach(function(elem) {
                 elem.style.display = "block";
             });
+            parent.parentNode.style.display="block";
         }
     });
+}
+function hideAllButCurrent(){
+    //by default all submenut items are hidden
+    //but we need to rehide them for search
+    document.querySelectorAll("nav > ul > li > ul li").forEach(function(parent) {
+        parent.style.display = "none";
+    });
+    
+    showCurrent();
 }
 
 hideAllButCurrent();
