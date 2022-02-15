1af12eb34443ab476d003aaf6c7fda35e6ef7319
zhennann <zhen.nann@icloud.com>
2022-02-15 18:19:10 +0800

    jsdoc: build/config.js

--- build/config.js
+++ build/config.js
@@ -1,10 +1,25 @@
-// backend
+/** @module project/build/config */
+
+/** backend
+ * @property {object} backend
+ * @property {string} backend.hostname='0.0.0.0'
+ * @property {number} backend.port=7102
+ */
 const backend = {
   port: 7102,
   hostname: '0.0.0.0',
 };
 
-// front
+/** front
+ * @property {object} front
+ * @property {object} front.build - npm run build:front
+ * @property {boolean} front.build.productionSourceMap=true
+ * @property {boolean} front.build.uglify=true
+ * @property {object} front.dev - npm run dev:front
+ * @property {string} front.dev.hostname='localhost'
+ * @property {number} front.dev.port=9192
+ * @property {string} front.dev.proxyBaseURL='http://localhost:7102'
+ */
 const front = {
   build: {
     productionSourceMap: true,
