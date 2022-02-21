a0a578c385d19bc3d51ebe4f2e778f50ecb95373
zhennann <zhen.nann@icloud.com>
2022-02-21 10:24:04 +0800

    feat: date lines

--- src/module-system/a-basefront/front/src/components/render/renderTableCellDatetime.jsx
+++ src/module-system/a-basefront/front/src/components/render/renderTableCellDatetime.jsx
@@ -25,23 +25,34 @@ export default {
   methods: {
     _renderDateDefault() {
       const { text } = this.info;
-      const { pattern } = this.dateFormat;
+      let _default = this.dateFormat.default;
+      if (_default === true) _default = null;
       let value;
       if (typeof text === 'object' && text instanceof Date) {
-        value = this.$meta.util.formatDateTime(text, pattern);
+        value = this.$meta.util.formatDateTime(text, _default);
       } else {
         value = text;
       }
       return { title: value, value };
     },
-    _renderDateLines() {},
+    _renderDateLines() {
+      const { text } = this.info;
+      const children = [];
+      const valueDate = this.$meta.util.formatDate(text, this.dateFormat.dateSeparator);
+      const valueTime = this.$meta.util.formatTime(text, this.dateFormat.timeSeparator);
+      children.push(<div key="date:one">{valueDate}</div>);
+      children.push(<div key="date:two">{valueTime}</div>);
+      const value = <div class="date-lines">{children}</div>;
+      const title = `${valueDate} ${valueTime}`;
+      return { title, value };
+    },
   },
   render() {
     let res;
-    if (this.dateFormat.pattern) {
-      res = this._renderDateDefault();
-    } else {
+    if (this.dateFormat.lines) {
       res = this._renderDateLines();
+    } else {
+      res = this._renderDateDefault();
     }
     const { title, value } = res;
     return (
