49b97ca8caf610e11b3233e3969d9842d2d917d2
zhennann <zhen.nann@icloud.com>
2022-02-23 21:19:53 +0800

    feat: icons generate

--- src/module-system/a-iconbooster/front/src/config/icons.js
+++ src/module-system/a-iconbooster/front/src/config/icons.js
@@ -1,5 +1,5 @@
-import iconDefault from './assets/icons/groups/default.svg';
-import iconFlow from './assets/icons/groups/flow.svg';
+import iconDefault from '../assets/icons/groups/default.svg';
+import iconFlow from '../assets/icons/groups/flow.svg';
 
 export default {
   default: iconDefault,
