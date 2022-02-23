c1cd25484e374b3012537507c0bef092a606c0b8
zhennann <zhen.nann@icloud.com>
2022-02-23 20:22:44 +0800

    feat: tools icons

--- packages/egg-born-bin/lib/cmd/tools-icons.js
+++ packages/egg-born-bin/lib/cmd/tools-icons.js
@@ -1,4 +1,7 @@
+const path = require('path');
 const chalk = require('chalk');
+const glob = require('glob');
+const bb = require('bluebird');
 const Command = require('@zhennann/egg-bin').Command;
 
 class ToolsIconsCommand extends Command {
@@ -7,15 +10,39 @@ class ToolsIconsCommand extends Command {
     this.usage = 'Usage: egg-born-bin tools-icons';
   }
 
-  *run({ cwd, argv }) {
+  async run({ cwd, argv }) {
     console.log('run tools icons at %s', cwd);
 
-    const modules = argv._;
-    for (const module of modules) {
-      console.log(module);
+    const moduleNames = argv._;
+    for (const moduleName of moduleNames) {
+      console.log(moduleName);
+      await this._generateIcons({ cwd, moduleName });
     }
     // done
     console.log(chalk.cyan('  tools-icons successfully!'));
+  }
+
+  async _generateIcons({ cwd, moduleName }) {
+    const modulePath = await this._resolveModulePath({ cwd, moduleName });
+    const iconsSrc = path.join(modulePath, 'icons/src');
+    // groups
+    const groups = await this._resolveGroups({ iconsSrc });
+    console.log(groups);
+  }
+
+  async _resolveModulePath({ cwd, moduleName }) {
+    const files = await bb.fromCallback(cb => {
+      glob(`${cwd}/src/**/${moduleName}/`, cb);
+    });
+    if (files.length === 0) throw new Error('module not found: ', moduleName);
+    return files[0];
+  }
+
+  async _resolveGroups({ iconsSrc }) {
+    const groupPaths = await bb.fromCallback(cb => {
+      glob(`${iconsSrc}/*`, cb);
+    });
+    return groupPaths.map(item => path.basename(item));
   }
 
   description() {
