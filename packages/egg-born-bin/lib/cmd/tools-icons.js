068177b6d6a8057072f3cf8b805ba05bb1a1032e
zhennann <zhen.nann@icloud.com>
2022-02-23 20:55:36 +0800

    feat: icons generate

--- packages/egg-born-bin/lib/cmd/tools-icons.js
+++ packages/egg-born-bin/lib/cmd/tools-icons.js
@@ -2,6 +2,8 @@ const path = require('path');
 const chalk = require('chalk');
 const glob = require('glob');
 const bb = require('bluebird');
+const fse = require('fs-extra');
+const xml2js = require('xml2js');
 const Command = require('@zhennann/egg-bin').Command;
 
 class ToolsIconsCommand extends Command {
@@ -15,7 +17,6 @@ class ToolsIconsCommand extends Command {
 
     const moduleNames = argv._;
     for (const moduleName of moduleNames) {
-      console.log(moduleName);
       await this._generateIcons({ cwd, moduleName });
     }
     // done
@@ -27,7 +28,47 @@ class ToolsIconsCommand extends Command {
     const iconsSrc = path.join(modulePath, 'icons/src');
     // groups
     const groups = await this._resolveGroups({ iconsSrc });
-    console.log(groups);
+    for (const group of groups) {
+      await this._generateIconsGroup({ modulePath, iconsSrc, group });
+    }
+  }
+
+  async _generateIconsGroup({ modulePath, iconsSrc, group }) {
+    // icons
+    const files = await bb.fromCallback(cb => {
+      glob(`${iconsSrc}/${group}/*.svg`, cb);
+    });
+    const iconNames = files.map(item => path.basename(item, '.svg'));
+    // symbols
+    const symbols = [];
+    for (let index = 0; index < files.length; index++) {
+      const file = files[index];
+      const iconName = iconNames[index];
+      const symbol = await this._combineSymbol({ file, iconName });
+      symbols.push(symbol);
+    }
+    // xml
+    const xml = `<svg xmlns="http://www.w3.org/2000/svg">
+  ${symbols}
+</svg>`;
+    // write
+    const pathDest = path.join(modulePath, 'front/src/assets/icons', 'groups');
+    await fse.ensureDir(pathDest);
+    const fileDest = path.join(pathDest, `${group}.svg`);
+    await fse.writeFile(fileDest, xml);
+  }
+
+  async _combineSymbol({ file, iconName }) {
+    // svg
+    const xml = await fse.readFile(file);
+    const svg = await this.parseXML({ xml });
+    // root
+    const domRoot = svg.$;
+    const attrs = { id: iconName };
+    if (domRoot.preserveAspectRatio) attrs.preserveAspectRatio = domRoot.preserveAspectRatio;
+    if (domRoot.viewBox) attrs.viewBox = domRoot.viewBox;
+    svg.$ = attrs;
+    return this.buildXML({ xml: svg, rootName: 'symbol' });
   }
 
   async _resolveModulePath({ cwd, moduleName }) {
@@ -43,6 +84,17 @@ class ToolsIconsCommand extends Command {
       glob(`${iconsSrc}/*`, cb);
     });
     return groupPaths.map(item => path.basename(item));
+  }
+
+  async parseXML({ xml, trim = true, explicitArray = false, explicitRoot = false }) {
+    const parser = new xml2js.Parser({ trim, explicitArray, explicitRoot });
+    return await bb.fromCallback(cb => {
+      parser.parseString(xml, cb);
+    });
+  }
+
+  buildXML({ xml, cdata = true, headless = true, rootName = 'xml' }) {
+    return new xml2js.Builder({ cdata, headless, rootName }).buildObject(xml);
   }
 
   description() {
