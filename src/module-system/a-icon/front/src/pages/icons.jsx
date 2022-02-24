0a09b200a1a50dd0569efe97df97131ba01f05cf
zhennann <zhen.nann@icloud.com>
2022-02-24 17:11:08 +0800

    feat: icons

--- src/module-system/a-icon/front/src/pages/icons.jsx
+++ src/module-system/a-icon/front/src/pages/icons.jsx
@@ -2,12 +2,44 @@ export default {
   data() {
     return {};
   },
-  created() {},
-  methods: {},
+  computed: {
+    pageTitle() {
+      return this.$text('Icons');
+    },
+  },
+  created() {
+    this.onSearchbarSearch = this.$meta.util.debounce((searchbar, query) => {
+      this._onSearch(query);
+    }, 300);
+  },
+  methods: {
+    onClickEnable() {
+      this.$refs.searchbar.f7Searchbar.enable(true);
+    },
+    _onSearch(query) {
+      console.log(query);
+    },
+  },
   render() {
     return (
       <eb-page>
-        <eb-navbar large largeTransparent title={this.$text('Icons')} eb-back-link="Back"></eb-navbar>
+        <eb-navbar title={this.pageTitle} eb-back-link="Back">
+          <f7-nav-right>
+            <f7-link icon-material="search" onClick={this.onClickEnable}></f7-link>
+          </f7-nav-right>
+          <f7-searchbar
+            ref="searchbar"
+            onSearchbarSearch={this.onSearchbarSearch}
+            expandable
+            search-container=".search-list"
+            search-in=".item-title"
+            placeholder={this.pageTitle}
+            backdrop={false}
+            disable-button={true}
+            clear-button={true}
+            custom-search={true}
+          ></f7-searchbar>
+        </eb-navbar>
       </eb-page>
     );
   },
