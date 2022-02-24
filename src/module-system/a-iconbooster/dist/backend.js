563c16985ea1380a0463cdd458d78ef48f5ee582
zhennann <zhen.nann@icloud.com>
2022-02-24 23:19:41 +0800

    Publish
     - cabloy@4.13.1
     - egg-born-module-a-basefront@4.4.34
     - egg-born-module-a-iconbooster@4.0.2
     - egg-born-module-a-user@4.2.8

--- src/module-system/a-iconbooster/dist/backend.js
+++ src/module-system/a-iconbooster/dist/backend.js
@@ -74,7 +74,7 @@ module.exports = {};
 /***/ ((module) => {
 
 module.exports = {
-  default: 'add,close',
+  default: 'add,attachment-line,close,comment-dots,star-filled',
   flow: 'activity-none,activity-service,activity-user-task,end-event-atom,end-event-none,gateway-exclusive,gateway-inclusive,gateway-parallel,start-event-atom,start-event-none,start-event-timer',
 };
 
