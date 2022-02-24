ba886b4e9fef4ecbe4fab6ab0c5959b8328412cb
zhennann <zhen.nann@icloud.com>
2022-02-24 22:14:26 +0800

    Publish
     - cabloy@4.13.0
     - create-cabloy@2.3.0
     - egg-born-bin@3.5.0
     - egg-born-front@8.5.0
     - egg-born-init-config@2.2.0
     - egg-born-template-cabloy@4.13.0
     - egg-born-template-module-icon@4.0.1
     - egg-born@2.3.0
     - egg-born-module-a-components-sync@4.5.0
     - egg-born-module-a-icon@4.0.1
     - egg-born-module-a-iconbooster@4.0.1

---
+++ src/module-system/a-icon/dist/backend.js
@@ -0,0 +1,382 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 224:
+/***/ ((module) => {
+
+module.exports = app => {
+  const aops = {};
+  return aops;
+};
+
+
+/***/ }),
+
+/***/ 782:
+/***/ ((module) => {
+
+let __icons = null;
+
+module.exports = ctx => {
+  // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class Icon extends ctx.app.meta.BeanModuleBase {
+    constructor(moduleName) {
+      super(ctx, 'icon');
+      this.moduleName = moduleName || ctx.module.info.relativeName;
+    }
+
+    getIcons() {
+      if (!__icons) {
+        __icons = this._prepareIcons();
+      }
+      return __icons;
+    }
+
+    _prepareIcons() {
+      const icons = {};
+      for (const relativeName in ctx.app.meta.modules) {
+        const module = ctx.app.meta.modules[relativeName];
+        const groups = ctx.bean.util.getProperty(module.main.meta, 'icon.groups');
+        if (groups) {
+          icons[relativeName] = groups;
+        }
+      }
+      return icons;
+    }
+  }
+
+  return Icon;
+};
+
+
+/***/ }),
+
+/***/ 899:
+/***/ ((module) => {
+
+module.exports = app => {
+  class Version extends app.meta.BeanBase {
+    async update(options) {}
+
+    async init(options) {}
+
+    async test() {}
+  }
+
+  return Version;
+};
+
+
+/***/ }),
+
+/***/ 187:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const versionManager = __webpack_require__(899);
+const beanIcon = __webpack_require__(782);
+
+module.exports = app => {
+  const beans = {
+    // version
+    'version.manager': {
+      mode: 'app',
+      bean: versionManager,
+    },
+    // global
+    icon: {
+      mode: 'ctx',
+      bean: beanIcon,
+      global: true,
+    },
+  };
+  return beans;
+};
+
+
+/***/ }),
+
+/***/ 76:
+/***/ ((module) => {
+
+// eslint-disable-next-line
+module.exports = appInfo => {
+  const config = {};
+  return config;
+};
+
+
+/***/ }),
+
+/***/ 624:
+/***/ ((module) => {
+
+// error code should start from 1001
+module.exports = {};
+
+
+/***/ }),
+
+/***/ 327:
+/***/ ((module) => {
+
+module.exports = {};
+
+
+/***/ }),
+
+/***/ 72:
+/***/ ((module) => {
+
+module.exports = {
+  Icons: '图标',
+};
+
+
+/***/ }),
+
+/***/ 25:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = {
+  'en-us': __webpack_require__(327),
+  'zh-cn': __webpack_require__(72),
+};
+
+
+/***/ }),
+
+/***/ 429:
+/***/ ((module) => {
+
+module.exports = app => {
+  // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  const resources = [
+    {
+      atomName: 'Icons',
+      atomStaticKey: 'icons',
+      atomRevision: 2,
+      atomCategoryId: 'a-base:menu.Tools',
+      resourceType: 'a-base:menu',
+      resourceConfig: JSON.stringify({
+        actionPath: '/a/icon/icons',
+      }),
+      resourceRoles: 'template.system',
+    },
+  ];
+  // ok
+  return resources;
+};
+
+
+/***/ }),
+
+/***/ 232:
+/***/ ((module) => {
+
+module.exports = app => {
+  const schemas = {};
+  return schemas;
+};
+
+
+/***/ }),
+
+/***/ 828:
+/***/ ((module) => {
+
+module.exports = app => {
+  class iconController extends app.Controller {
+    getIcons() {
+      const res = this.ctx.service.icon.getIcons();
+      this.ctx.success(res);
+    }
+  }
+
+  return iconController;
+};
+
+
+/***/ }),
+
+/***/ 95:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const icon = __webpack_require__(828);
+
+module.exports = app => {
+  const controllers = {
+    icon,
+  };
+  return controllers;
+};
+
+
+/***/ }),
+
+/***/ 421:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const config = __webpack_require__(76);
+const locales = __webpack_require__(25);
+const errors = __webpack_require__(624);
+
+module.exports = app => {
+  // aops
+  const aops = __webpack_require__(224)(app);
+  // beans
+  const beans = __webpack_require__(187)(app);
+  // routes
+  const routes = __webpack_require__(825)(app);
+  // controllers
+  const controllers = __webpack_require__(95)(app);
+  // services
+  const services = __webpack_require__(214)(app);
+  // models
+  const models = __webpack_require__(230)(app);
+  // meta
+  const meta = __webpack_require__(458)(app);
+
+  return {
+    aops,
+    beans,
+    routes,
+    controllers,
+    services,
+    models,
+    config,
+    locales,
+    errors,
+    meta,
+  };
+};
+
+
+/***/ }),
+
+/***/ 458:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = app => {
+  // schemas
+  const schemas = __webpack_require__(232)(app);
+  // static
+  const staticResources = __webpack_require__(429)(app);
+  const meta = {
+    base: {
+      atoms: {},
+      statics: {
+        'a-base.resource': {
+          items: staticResources,
+        },
+      },
+    },
+    validation: {
+      validators: {},
+      keywords: {},
+      schemas,
+    },
+  };
+  return meta;
+};
+
+
+/***/ }),
+
+/***/ 230:
+/***/ ((module) => {
+
+module.exports = app => {
+  const models = {};
+  return models;
+};
+
+
+/***/ }),
+
+/***/ 825:
+/***/ ((module) => {
+
+module.exports = app => {
+  const routes = [
+    // getIcons
+    {
+      method: 'post',
+      path: 'icon/getIcons',
+      controller: 'icon',
+      meta: { right: { type: 'resource', module: 'a-settings', name: 'settings' } },
+    },
+  ];
+  return routes;
+};
+
+
+/***/ }),
+
+/***/ 479:
+/***/ ((module) => {
+
+module.exports = app => {
+  class icon extends app.Service {
+    getIcons() {
+      return this.ctx.bean.icon.getIcons();
+    }
+  }
+
+  return icon;
+};
+
+
+/***/ }),
+
+/***/ 214:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const icon = __webpack_require__(479);
+
+module.exports = app => {
+  const services = {
+    icon,
+  };
+  return services;
+};
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(421);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=backend.js.map
