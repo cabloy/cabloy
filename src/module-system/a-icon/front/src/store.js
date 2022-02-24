f81b4aaa2d1d2407dd4a8ca2dec99d4011ab2f1b
zhennann <zhen.nann@icloud.com>
2022-02-24 09:23:06 +0800

    feat: gitIcon

--- src/module-system/a-icon/front/src/store.js
+++ src/module-system/a-icon/front/src/store.js
@@ -1,9 +1,26 @@
 // eslint-disable-next-line
 export default function (Vue) {
+  function _parseIcon({ icon }) {
+    // support backend api static
+    if (icon.indexOf('/api/static/') === 0) {
+      return Vue.prototype.$meta.util.combineFetchStaticPath(icon);
+    }
+  }
   return {
-    state: {},
+    state: {
+      icons: {},
+    },
     getters: {},
     mutations: {},
-    actions: {},
+    actions: {
+      async getDict({ state, commit }, { icon }) {
+        if (!icon) return null;
+        if (state.icons[icon]) return state.icons[icon];
+        // support backend api static
+        const iconHref = _parseIcon({ icon });
+        commit('setIcon', { icon, iconHref });
+        return iconHref;
+      },
+    },
   };
 }
