cab9bc55937c9d531d4d8b04e7c0ce414d74738f
zhennann <zhen.nann@icloud.com>
2022-02-27 20:35:44 +0800

    feat: editor icons

--- src/module-system/a-iconbooster/front/src/config/icons.js
+++ src/module-system/a-iconbooster/front/src/config/icons.js
@@ -1,9 +1,11 @@
 import _default from '../assets/icons/groups/default.svg';
+import _editor from '../assets/icons/groups/editor.svg';
 import _flow from '../assets/icons/groups/flow.svg';
 import _login from '../assets/icons/groups/login.svg';
 
 export default {
   default: _default,
+  editor: _editor,
   flow: _flow,
   login: _login,
 };
