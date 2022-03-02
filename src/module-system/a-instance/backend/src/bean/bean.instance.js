1b4014d16769ae656e9124138672792393d39fa4
zhennann <zhen.nann@icloud.com>
2022-03-02 18:34:45 +0800

    feat: middleware instance

--- src/module-system/a-instance/backend/src/bean/bean.instance.js
+++ src/module-system/a-instance/backend/src/bean/bean.instance.js
@@ -1,6 +1,10 @@
 const require3 = require('require3');
 const extend = require3('extend2');
 const async = require3('async');
+const chalk = require3('chalk');
+const boxen = require3('boxen');
+
+const boxenOptions = { padding: 1, margin: 1, align: 'center', borderColor: 'yellow', borderStyle: 'round' };
 
 const __queueInstanceStartup = {};
 
@@ -171,6 +175,64 @@ module.exports = ctx => {
         __queueInstanceStartup[subdomain].push({ resolve, reject, subdomain, options });
       });
     }
+
+    async initInstance() {
+      // instance
+      const instance = await ctx.bean.instance.get({ subdomain: ctx.subdomain });
+      if (!instance) {
+        // prompt: should for local/prod
+        // if (ctx.app.meta.isLocal) {
+        const urlInfo =
+          ctx.locale === 'zh-cn'
+            ? 'https://cabloy.com/zh-cn/articles/multi-instance.html'
+            : 'https://cabloy.com/articles/multi-instance.html';
+        let message = `Please add instance in ${chalk.keyword('cyan')('src/backend/config/config.[env].js')}`;
+        message += '\n' + chalk.keyword('orange')(`{ subdomain: '${ctx.subdomain}', password: '', title: '' }`);
+        message += `\nMore info: ${chalk.keyword('cyan')(urlInfo)}`;
+        console.log('\n' + boxen(message, boxenOptions));
+        // }
+        return ctx.fail(423); // not ctx.throw(423)
+      }
+      // check if disabled
+      if (instance.disabled) {
+        // locked
+        console.log('instance disabled: ', ctx.subdomain);
+        return ctx.fail(423); // not ctx.throw(423)
+      }
+
+      // check instance startup ready
+      await this.checkAppReadyInstance();
+
+      // try to save host/protocol to config
+      if (ctxHostValid(ctx)) {
+        if (!instance.config['a-base']) instance.config['a-base'] = {};
+        const aBase = instance.config['a-base'];
+        if (aBase.host !== ctx.host || aBase.protocol !== ctx.protocol) {
+          aBase.host = ctx.host;
+          aBase.protocol = ctx.protocol;
+          // update
+          const modelInstance = ctx.model.module(moduleInfo.relativeName).instance;
+          await modelInstance.update({
+            id: instance.id,
+            config: JSON.stringify(instance.config),
+          });
+          // broadcast
+          ctx.app.meta.broadcast.emit({
+            subdomain: ctx.subdomain,
+            module: 'a-instance',
+            broadcastName: 'resetCache',
+            data: null,
+          });
+        }
+      }
+
+      // ok
+      ctx.instance = instance;
+    }
   }
   return Instance;
 };
+
+function ctxHostValid(ctx) {
+  return !ctx.innerAccess && ctx.host && ctx.protocol && ctx.host !== '127.0.0.1' && ctx.host !== 'localhost';
+}
--- src/module-system/a-instance/backend/src/bean/middleware.instance.js
+++ src/module-system/a-instance/backend/src/bean/middleware.instance.js
@@ -1,72 +1,12 @@
-const require3 = require('require3');
-const chalk = require3('chalk');
-const boxen = require3('boxen');
-
-const boxenOptions = { padding: 1, margin: 1, align: 'center', borderColor: 'yellow', borderStyle: 'round' };
-
 module.exports = ctx => {
-  const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Middleware {
     async execute(options, next) {
-      // instance
-      const instance = await ctx.bean.instance.get({ subdomain: ctx.subdomain });
-      if (!instance) {
-        // prompt: should for local/prod
-        // if (ctx.app.meta.isLocal) {
-        const urlInfo =
-          ctx.locale === 'zh-cn'
-            ? 'https://cabloy.com/zh-cn/articles/multi-instance.html'
-            : 'https://cabloy.com/articles/multi-instance.html';
-        let message = `Please add instance in ${chalk.keyword('cyan')('src/backend/config/config.[env].js')}`;
-        message += '\n' + chalk.keyword('orange')(`{ subdomain: '${ctx.subdomain}', password: '', title: '' }`);
-        message += `\nMore info: ${chalk.keyword('cyan')(urlInfo)}`;
-        console.log('\n' + boxen(message, boxenOptions));
-        // }
-        return ctx.fail(423); // not ctx.throw(423)
-      }
-      // check if disabled
-      if (instance.disabled) {
-        // locked
-        console.log('instance disabled: ', ctx.subdomain);
-        return ctx.fail(423); // not ctx.throw(423)
-      }
-
-      // check instance startup ready
-      await ctx.bean.instance.checkAppReadyInstance();
-
-      // try to save host/protocol to config
-      if (ctxHostValid(ctx)) {
-        if (!instance.config['a-base']) instance.config['a-base'] = {};
-        const aBase = instance.config['a-base'];
-        if (aBase.host !== ctx.host || aBase.protocol !== ctx.protocol) {
-          aBase.host = ctx.host;
-          aBase.protocol = ctx.protocol;
-          // update
-          const modelInstance = ctx.model.module(moduleInfo.relativeName).instance;
-          await modelInstance.update({
-            id: instance.id,
-            config: JSON.stringify(instance.config),
-          });
-          // broadcast
-          ctx.app.meta.broadcast.emit({
-            subdomain: ctx.subdomain,
-            module: 'a-instance',
-            broadcastName: 'resetCache',
-            data: null,
-          });
-        }
-      }
-
-      // ok
-      ctx.instance = instance;
-
+      // init instance
+      await ctx.bean.instance.initInstance();
       // next
       await next();
     }
   }
   return Middleware;
 };
-
-function ctxHostValid(ctx) {
-  return !ctx.innerAccess && ctx.host && ctx.protocol && ctx.host !== '127.0.0.1' && ctx.host !== 'localhost';
-}
