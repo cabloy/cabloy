3a52f223f690d9d9e72aaa36097759842740f1f7
zhennann <zhen.nann@icloud.com>
2022-03-02 18:14:30 +0800

    feat: update

--- packages/egg-born-backend/lib/load/agent.js
+++ packages/egg-born-backend/lib/load/agent.js
@@ -3,9 +3,9 @@ const AgentWorkerLoader = require('egg').AgentWorkerLoader;
 const loadModules = require('../module');
 
 module.exports = class CustomAgentWorkerLoader extends AgentWorkerLoader {
-  constructor(opt) {
-    super(opt);
-  }
+  // constructor(opt) {
+  //   super(opt);
+  // }
   loadConfig() {
     super.loadConfig();
     this.app.subdomainOffset = typeof this.config.subdomainOffset === 'undefined' ? 2 : this.config.subdomainOffset;
--- packages/egg-born-backend/lib/load/app.js
+++ packages/egg-born-backend/lib/load/app.js
@@ -3,9 +3,9 @@ const AppWorkerLoader = require('egg').AppWorkerLoader;
 const loadModules = require('../module');
 
 module.exports = class CustomAppWorkerLoader extends AppWorkerLoader {
-  constructor(opt) {
-    super(opt);
-  }
+  // constructor(opt) {
+  //   super(opt);
+  // }
   loadConfig() {
     super.loadConfig();
     this.app.subdomainOffset = typeof this.config.subdomainOffset === 'undefined' ? 2 : this.config.subdomainOffset;
