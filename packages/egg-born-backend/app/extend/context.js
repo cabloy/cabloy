212c668af3fecb16eaeaf4a3ac7023bded902abe
zhennann <zhen.nann@icloud.com>
2022-03-08 09:38:02 +0800

    feat: context.performAction jsdoc

--- packages/egg-born-backend/app/extend/context.js
+++ packages/egg-born-backend/app/extend/context.js
@@ -186,9 +186,10 @@ module.exports = {
 
   /**
    * perform action of this or that module
+   * @param  {string} options options
    * @param  {string} options.method method
    * @param  {string} options.url    url
-   * @param  {json} options.data   data(optional)
+   * @param  {json} options.body   body(optional)
    * @return {promise}                response.body.data or throw error
    */
   performAction({ innerAccess, subdomain, method, url, query, params, headers, body }) {
