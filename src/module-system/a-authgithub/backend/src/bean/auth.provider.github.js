d755199a7cc122f75e8845aa4042f46fea73e27d
zhennann <zhen.nann@icloud.com>
2022-03-11 21:32:25 +0800

    feat: github bean

--- src/module-system/a-authgithub/backend/src/bean/auth.provider.github.js
+++ src/module-system/a-authgithub/backend/src/bean/auth.provider.github.js
@@ -1,6 +1,11 @@
 module.exports = function (ctx) {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
-  class Provider {}
+  class Provider {
+    getConfig() {
+      const config = ctx.config.module(moduleInfo.relativeName).account.github.apps.default;
+      return { clientID: config.clientID, clientSecret: config.clientSecret };
+    }
+  }
 
   return Provider;
 };
