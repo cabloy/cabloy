464fa89b8e4cd4f7f1a334d65c5086244479c369
zhennann <zhen.nann@icloud.com>
2022-03-12 16:46:09 +0800

    feat: authProvider: scenes/locales

--- src/module-system/a-auth/backend/src/bean/version.manager.js
+++ src/module-system/a-auth/backend/src/bean/version.manager.js
@@ -1,6 +1,16 @@
 module.exports = app => {
   class Version extends app.meta.BeanBase {
-    async update(options) {}
+    async update(options) {
+      if (options.version === 1) {
+        // aAuthProvider: add scenes/locales
+        const sql = `
+      ALTER TABLE aAuthProvider
+        ADD COLUMN scenes JSON DEFAULT NULL,
+        ADD COLUMN locales JSON DEFAULT NULL
+                `;
+        await this.ctx.model.query(sql);
+      }
+    }
 
     async init(options) {}
 
