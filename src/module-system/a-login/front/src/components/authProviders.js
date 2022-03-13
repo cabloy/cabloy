f1bb4ce4a498771b47d88fc83332d64d372eb366
zhennann <zhen.nann@icloud.com>
2022-03-13 20:38:36 +0800

    feat: loadAuthProviders

--- src/module-system/a-login/front/src/components/authProviders.js
+++ src/module-system/a-login/front/src/components/authProviders.js
@@ -26,13 +26,14 @@ export default {
       return await Promise.all(promises);
     },
     async __checkAuthProvider({ ctx, provider, state }) {
+      const meta = provider.meta;
+      if (!meta.render) return null;
       // load module
-      const module = await this.$meta.module.use(provider.module);
+      const module = await this.$meta.module.use(meta.render.module);
       // checkIfDisable
-      if (!provider.meta.component) return null;
-      if (state === 'migrate' && !provider.meta.inline) return null;
-      if (state === 'associate' && provider.meta.disableAssociate) return null;
-      const component = module.options.components[provider.meta.component];
+      if (state === 'migrate' && !meta.inline) return null;
+      if (state === 'associate' && meta.disableAssociate) return null;
+      const component = module.options.components[meta.name];
       const disable = await this.__checkAuthProviderDisable({ ctx, component, provider, state });
       if (disable) return null;
       return { provider, component };
@@ -40,7 +41,19 @@ export default {
     async __checkAuthProviderDisable({ ctx, component, provider, state }) {
       if (!component.meta) return false; // default is false
       if (typeof component.meta.disable !== 'function') return component.meta.disable;
-      return await this.$meta.util.wrapPromise(component.meta.disable({ ctx, state, provider }));
+      // loop scenes
+      const scenes = {};
+      for (const providerScene in provider.scenes) {
+        const disable = await this.$meta.util.wrapPromise(
+          component.meta.disable({ ctx, state, provider, providerScene })
+        );
+        if (!disable) {
+          scenes[providerScene] = provider.scenes[providerScene];
+        }
+      }
+      provider.scenes = scenes;
+      if (Object.keys(scenes).length === 0) return true;
+      return false;
     },
   },
 };
