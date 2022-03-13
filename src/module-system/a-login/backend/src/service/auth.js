860d6b6ad08d9c9e3d61f6810593620ad8577607
zhennann <zhen.nann@icloud.com>
2022-03-13 17:26:39 +0800

    feat: login/auth

--- src/module-system/a-login/backend/src/service/auth.js
+++ src/module-system/a-login/backend/src/service/auth.js
@@ -1,37 +1,7 @@
 module.exports = app => {
   class Auth extends app.Service {
     async list() {
-      const list = this.ctx.bean.authProviderCache.getAuthProvidersConfigForLogin();
-      console.log(list);
-      // list map
-      const listMap = {};
-      // meta
-      const authProviders = this.ctx.bean.base.authProviders();
-      for (const item of list) {
-        const key = `${item.module}:${item.providerName}`;
-        const authProvider = authProviders[key];
-        if (!authProvider) continue;
-        item.meta = authProvider.meta;
-        listMap[key] = item;
-      }
-      // order
-      const res = [];
-      for (const item of this.ctx.config.providers) {
-        const key = `${item.module}:${item.provider}`;
-        const provider = listMap[key];
-        if (provider) {
-          if (item.disable !== true) {
-            res.push(provider);
-          }
-          delete listMap[key];
-        }
-      }
-      // the rest
-      for (const key in listMap) {
-        res.push(listMap[key]);
-      }
-      // ok
-      return res;
+      return this.ctx.bean.authProviderCache.getAuthProvidersConfigForLogin();
     }
   }
 
