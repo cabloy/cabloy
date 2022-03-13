69ee7c578ef1e0c8e744eb09ee382189daeb76eb
zhennann <zhen.nann@icloud.com>
2022-03-13 10:34:01 +0800

    fix: _cacheAuthProvidersConfig

--- src/module-system/a-auth/backend/src/bean/startup.cacheAuthProviders.js
+++ src/module-system/a-auth/backend/src/bean/startup.cacheAuthProviders.js
@@ -2,7 +2,7 @@ module.exports = app => {
   class Startup extends app.meta.BeanBase {
     async execute(/* context*/) {
       // cache all authProviders
-      await this.ctx.bean.authProvider._cacheAuthProviders();
+      await this.ctx.bean.authProvider._cacheAuthProvidersConfig();
     }
   }
 
