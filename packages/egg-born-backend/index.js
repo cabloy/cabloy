b0dfa530f22cf10a276bc051d93ae3b8bc122c40
zhennann <zhen.nann@icloud.com>
2022-03-13 22:38:29 +0800

    feat: passport-oauth2

--- packages/egg-born-backend/index.js
+++ packages/egg-born-backend/index.js
@@ -6,7 +6,6 @@ require('regenerator-runtime/runtime');
 
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('ali-rds', '@zhennann/ali-rds');
-moduleAlias.addAlias('passport-oauth2', '@zhennann/passport-oauth2');
 moduleAlias.addAlias('koa-static-cache', '@zhennann/koa-static-cache');
 
 const Master = require('egg-cluster/lib/master.js');
