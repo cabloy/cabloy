95276ea89483e7414365b329a84d3b2849e138c8
zhennann <zhen.nann@icloud.com>
2022-03-15 20:21:00 +0800

    feat: account migration

--- src/module-system/a-authsms/backend/src/bean/event.accountMigration.js
+++ src/module-system/a-authsms/backend/src/bean/event.accountMigration.js
@@ -10,6 +10,7 @@ module.exports = ctx => {
       });
       // model auth
       const modelAuth = ctx.model.module('a-base').auth;
+      // need not providerScene
       const authItem = await modelAuth.get({ userId: data.userIdFrom, providerId: providerItem.id });
       if (authItem) {
         const user = { id: data.userIdTo, mobile: authItem.profileId };
