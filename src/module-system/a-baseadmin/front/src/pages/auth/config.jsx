3e15ae076edcbabf3c74e9893c6d0fb1eb455910
zhennann <zhen.nann@icloud.com>
2022-03-16 22:06:47 +0800

    feat: auth schema

--- src/module-system/a-baseadmin/front/src/pages/auth/config.jsx
+++ src/module-system/a-baseadmin/front/src/pages/auth/config.jsx
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 const ebClipboard = Vue.prototype.$meta.module.get('a-components').options.mixins.ebClipboard;
 const ebPageContext = Vue.prototype.$meta.module.get('a-components').options.mixins.ebPageContext;
+const ebPageDirty = Vue.prototype.$meta.module.get('a-components').options.mixins.ebPageDirty;
 export default {
   meta: {
     global: false,
   },
-  mixins: [ebClipboard, ebPageContext],
+  mixins: [ebClipboard, ebPageContext, ebPageDirty],
   data() {
     return {
       sceneName: this.$f7route.query.sceneName,
@@ -28,6 +29,22 @@ export default {
     async init() {
       await this._prepareScheme();
       this._prepareData();
+    },
+    onFormSubmit() {
+      this.$refs.buttonSave.onClick();
+    },
+    onPerformSave(event) {
+      return this.$refs.validate.perform(event);
+    },
+    onValidateItemChange() {
+      this.page_setDirty(true);
+    },
+    async onPerformValidate() {
+      await this.$api.post('kitchen-sink/form-schema-validation/saveValidation', {
+        data: this.item,
+      });
+      this.page_setDirty(false);
+      return true; // toast on success
     },
     async _prepareScheme() {
       const meta = this.item.meta;
@@ -102,17 +119,24 @@ export default {
           ref="validate"
           auto
           data={this.data}
-          schema={this.schema}
-          propsOnPerform={this.validate_onPerformValidate}
-          onSubmit={this.validate_onSubmit}
+          meta={{ schema: this.schema }}
+          propsOnPerform={this.onPerformValidate}
+          onSubmit={this.onFormSubmit}
+          onValidateItemChange={this.onValidateItemChange}
         ></eb-validate>
       );
     },
   },
   render() {
+    let domSave;
+    if (this.ready) {
+      domSave = <eb-link ref="buttonSave" iconF7="::save" propsOnPerform={this.onPerformSave}></eb-link>;
+    }
     return (
       <eb-page>
-        <eb-navbar large largeTransparent title={this.$text('Config')} eb-back-link="Back"></eb-navbar>
+        <eb-navbar large largeTransparent title={this.$text('Config')} eb-back-link="Back">
+          <f7-nav-right>{domSave}</f7-nav-right>
+        </eb-navbar>
         {this._renderValidate()}
       </eb-page>
     );
