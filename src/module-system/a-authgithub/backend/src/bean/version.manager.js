77592afc03c69923b2aeeee44de7388067808140
zhennann <zhen.nann@icloud.com>
2022-03-16 11:02:50 +0800

    feat: github version changed

--- src/module-system/a-authgithub/backend/src/bean/version.manager.js
+++ src/module-system/a-authgithub/backend/src/bean/version.manager.js
@@ -1,8 +1,22 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Version extends app.meta.BeanBase {
     async update(options) {
       if (options.version === 1) {
         // empty
+      }
+      if (options.version === 2) {
+        // all instances
+        const instances = await this.ctx.bean.instance.list({ where: {} });
+        for (const instance of instances) {
+          await this.ctx.meta.util.executeBean({
+            subdomain: instance.name,
+            beanModule: moduleInfo.relativeName,
+            beanFullName: `${moduleInfo.relativeName}.version.manager`,
+            context: options,
+            fn: 'update8Auths',
+          });
+        }
       }
     }
 
@@ -10,6 +24,10 @@ module.exports = app => {
       if (options.version === 1) {
         // empty
       }
+    }
+
+    async update8Auths(options) {
+      console.log('---------------         update8Auths');
     }
   }
 
