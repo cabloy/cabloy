d2bf844e365c817c6e49218b666090e6beb4065b
zhennann <zhen.nann@icloud.com>
2022-03-18 09:44:14 +0800

    feat: wechat auth provider

---
+++ src/module-system/a-wechat/backend/src/bean/auth.provider.wechat.js
@@ -0,0 +1,42 @@
+const require3 = require('require3');
+const Strategy = require3('passport-github').Strategy;
+
+module.exports = function (ctx) {
+  const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class Provider extends ctx.app.meta.IAuthProviderBase(ctx) {
+    get configModule() {
+      return ctx.config.module(moduleInfo.relativeName);
+    }
+    async getConfigDefault() {
+      const configGitHub = this.configModule.account.github;
+      return {
+        scenes: configGitHub.scenes,
+        locales: configGitHub.locales,
+      };
+    }
+    checkConfigValid(config) {
+      return !!config.clientID && !!config.clientSecret;
+    }
+    getStrategy() {
+      return Strategy;
+    }
+    async onVerify(accessToken, refreshToken, profile) {
+      return {
+        module: this.providerModule,
+        provider: this.providerName,
+        providerScene: this.providerScene,
+        profileId: profile.id,
+        profile: {
+          userName: profile.username,
+          realName: profile.displayName,
+          avatar: profile.photos && profile.photos[0] && profile.photos[0].value,
+          accessToken,
+          refreshToken,
+          profile,
+        },
+      };
+    }
+  }
+
+  return Provider;
+};
--- src/module-system/a-wechat/backend/src/beans.js
+++ src/module-system/a-wechat/backend/src/beans.js
@@ -2,6 +2,9 @@ const versionManager = require('./bean/version.manager.js');
 const eventLoginInfo = require('./bean/event.loginInfo.js');
 const eventAccountMigration = require('./bean/event.accountMigration.js');
 const middlewareInWechat = require('./bean/middleware.inWechat.js');
+const authProviderWechat = require('./bean/auth.provider.wechat.js');
+const authProviderWechatWeb = require('./bean/auth.provider.wechatWeb.js');
+const authProviderWechatMini = require('./bean/auth.provider.wechatMini.js');
 const beanWechat = require('./bean/bean.wechat.js');
 
 module.exports = app => {
@@ -24,6 +27,19 @@ module.exports = app => {
     'middleware.inWechat': {
       mode: 'ctx',
       bean: middlewareInWechat,
+    },
+    // auth.provider
+    'auth.provider.wechat': {
+      mode: 'ctx',
+      bean: authProviderWechat,
+    },
+    'auth.provider.wechatWeb': {
+      mode: 'ctx',
+      bean: authProviderWechatWeb,
+    },
+    'auth.provider.wechatMini': {
+      mode: 'ctx',
+      bean: authProviderWechatMini,
     },
     // global
     wechat: {
