2e2ea13c4e0472972ea85b48e087aa156217e3bc
zhennann <zhen.nann@icloud.com>
2022-03-18 21:18:07 +0800

    feat: jsconfig

--- src/module-system/a-wechat/backend/src/service/jssdk.js
+++ src/module-system/a-wechat/backend/src/service/jssdk.js
@@ -1,8 +1,16 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class JSSDK extends app.Service {
     async jsconfig({ url }) {
+      // bean provider
+      const beanProvider = this.ctx.bean.authProvider.createAuthProviderBean({
+        module: moduleInfo.relativeName,
+        providerName: 'wechat',
+        providerScene: null,
+      });
+      if (!beanProvider.providerSceneValid) this.ctx.throw(423);
       // config
-      const config = this.ctx.config.account.public;
+      const config = beanProvider.configProviderScene;
       // params
       const params = {
         debug: config.jssdk.debug,
