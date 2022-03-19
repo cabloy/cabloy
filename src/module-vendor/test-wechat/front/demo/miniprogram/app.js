f18941bd617f636e03f2b0b1dc856f3d65f5eb71
zhennann <zhen.nann@icloud.com>
2022-03-19 21:26:55 +0800

    feat: mini baseURL

--- src/module-vendor/test-wechat/front/demo/miniprogram/app.js
+++ src/module-vendor/test-wechat/front/demo/miniprogram/app.js
@@ -16,7 +16,7 @@ App({
         locale: 'en-us',
       },
       api: {
-        baseURL: 'http://yourdomain.com',
+        baseURL: 'https://yourdomain.com',
       },
     };
     this.cabloy = Cabloy(this, cabloyOptions);
