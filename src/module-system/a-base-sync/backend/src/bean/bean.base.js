f6023b9619cce3a74fad1a38e983d72782fa97b5
zhennann <zhen.nann@icloud.com>
2022-03-19 11:25:08 +0800

    feat: wechat sdk

--- src/module-system/a-base-sync/backend/src/bean/bean.base.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.base.js
@@ -309,7 +309,7 @@ module.exports = ctx => {
         let metaAuth = module.main.meta && module.main.meta.auth;
         if (!metaAuth) continue;
         // todo: only check github
-        if (relativeName !== 'a-authgithub' && relativeName !== 'a-authsimple' && relativeName !== 'a-authsms') {
+        if (!['a-authgithub', 'a-authsimple', 'a-authsms', 'a-wechat'].includes(relativeName)) {
           continue;
         }
         if (typeof metaAuth === 'function') {
