cad0ea9e942692ae733994c2254109b59697b581
zhennann <zhen.nann@icloud.com>
2022-03-20 21:36:50 +0800

    feat: middleware inwxwork

--- src/module-system/a-wxwork/backend/src/bean/middleware.inWxwork.js
+++ src/module-system/a-wxwork/backend/src/bean/middleware.inWxwork.js
@@ -2,7 +2,14 @@ module.exports = ctx => {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Middleware {
     async execute(options, next) {
-      if (!ctx.bean.wxwork.util.in(options.scene)) return ctx.throw.module(moduleInfo.relativeName, 1001);
+      if (
+        !ctx.bean.wxwork.util.in({
+          providerName: options.providerName,
+          providerScene: options.providerScene,
+        })
+      ) {
+        return ctx.throw.module(moduleInfo.relativeName, 1001);
+      }
       // next
       await next();
     }
