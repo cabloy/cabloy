ebb65b373e4610023765335cabb4502c44f29317
zhennann <zhen.nann@icloud.com>
2022-03-20 20:51:07 +0800

    feat: wxwork version managerr

--- src/module-system/a-wxwork/backend/src/bean/version.manager.js
+++ src/module-system/a-wxwork/backend/src/bean/version.manager.js
@@ -1,4 +1,5 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Version extends app.meta.BeanBase {
     async update(options) {
       if (options.version === 1) {
@@ -60,11 +61,85 @@ module.exports = app => {
         `;
         await this.ctx.model.query(sql);
       }
+
+      if (options.version === 2) {
+        // all instances
+        const instances = await this.ctx.bean.instance.list({ where: {} });
+        for (const instance of instances) {
+          await this.ctx.meta.util.executeBean({
+            subdomain: instance.name,
+            beanModule: moduleInfo.relativeName,
+            beanFullName: `${moduleInfo.relativeName}.version.manager`,
+            context: options,
+            fn: 'update2Auths',
+          });
+        }
+      }
     }
 
     async init(options) {}
 
     async test() {}
+
+    async update2Auths() {
+      await this.update2Auths_wxwork('wxwork');
+      await this.update2Auths_wxwork('wxworkweb');
+      await this.update2Auths_wxworkmini();
+    }
+
+    async update2Auths_wxwork(providerName) {
+      const provideItem = await this.ctx.bean.authProvider.getAuthProvider({
+        module: moduleInfo.relativeName,
+        providerName,
+      });
+      await this.ctx.model.query('update aAuth a set a.providerScene=? where a.iid=? and a.providerId=?', [
+        'selfBuilt',
+        this.ctx.instance.id,
+        provideItem.id,
+      ]);
+    }
+
+    async update2Auths_wxworkmini() {
+      // wxworkminiXXX
+      const providers = await this.ctx.model.query(
+        `
+        select * from aAuthProvider 
+          where iid=? and providerName like 'wxworkmini%'
+        `,
+        [this.ctx.instance.id]
+      );
+      let providerDefault;
+      for (const provider of providers) {
+        const providerName = provider.providerName;
+        const providerScene = providerName.substring('wxworkmini'.length);
+        if (!providerScene) continue;
+        if (providerScene === 'default') {
+          providerDefault = provider;
+          await this.modelAuthProvider.update({
+            id: provider.id,
+            providerName: 'wxworkmini',
+          });
+        } else {
+          await this.modelAuthProvider.delete({ id: provider.id });
+        }
+      }
+      // auths
+      if (!providerDefault) {
+        // donothing
+      }
+      for (const provider of providers) {
+        const providerName = provider.providerName;
+        const providerScene = providerName.substring('wxworkmini'.length);
+        if (!providerScene) continue;
+        await this.ctx.model.query(
+          `
+          update aAuth set providerId=?, providerScene=?
+            where iid=? and providerId=?
+          `,
+          [providerDefault.id, providerScene, this.ctx.instance.id, provider.id]
+        );
+      }
+    }
   }
 
   return Version;
