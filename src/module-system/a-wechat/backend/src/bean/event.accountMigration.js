ee74e99d9af01d877d1c443a4eb337b4408df56f
zhennann <zhen.nann@icloud.com>
2022-03-20 21:29:12 +0800

    feat: event accountMigration

--- src/module-system/a-wechat/backend/src/bean/event.accountMigration.js
+++ src/module-system/a-wechat/backend/src/bean/event.accountMigration.js
@@ -1,8 +1,8 @@
 module.exports = ctx => {
-  // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class eventBean {
     get modelWechatUser() {
-      return ctx.model.module('a-wechat').wechatUser;
+      return ctx.model.module(moduleInfo.relativeName).wechatUser;
     }
 
     async execute(context, next) {
