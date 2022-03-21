8147ffbb8fb96cd15b302998d94d971039463dcc
zhennann <zhen.nann@icloud.com>
2022-03-21 15:30:10 +0800

    feat: jssdk

--- src/module-system/a-wxwork/backend/src/service/jssdk.js
+++ src/module-system/a-wxwork/backend/src/service/jssdk.js
@@ -1,26 +1,39 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class JSSDK extends app.Service {
     async jsconfig({ url }) {
+      // bean provider
+      const beanProvider = this.ctx.bean.authProvider.createAuthProviderBean({
+        module: moduleInfo.relativeName,
+        providerName: 'wxwork',
+        providerScene: 'selfBuilt',
+      });
+      if (!beanProvider.providerSceneValid) this.ctx.throw(423);
       // config
-      const config = this.ctx.config.account.wxwork;
-      const configAppSelfBuilt = config.apps.selfBuilt;
+      const config = beanProvider.configProviderScene;
       // params
       const params = {
-        debug: configAppSelfBuilt.jssdk.debug,
-        jsApiList: configAppSelfBuilt.jssdk.jsApiList,
+        debug: config.jssdk.debug,
+        jsApiList: config.jssdk.jsApiList,
         url,
       };
       return await this.ctx.bean.wxwork.app.selfBuilt.getJsConfig(params);
     }
 
     async jsconfigAgent({ url }) {
+      // bean provider
+      const beanProvider = this.ctx.bean.authProvider.createAuthProviderBean({
+        module: moduleInfo.relativeName,
+        providerName: 'wxwork',
+        providerScene: 'selfBuilt',
+      });
+      if (!beanProvider.providerSceneValid) this.ctx.throw(423);
       // config
-      const config = this.ctx.config.account.wxwork;
-      const configAppSelfBuilt = config.apps.selfBuilt;
+      const config = beanProvider.configProviderScene;
       // params
       const params = {
-        agentid: configAppSelfBuilt.agentid,
-        jsApiList: configAppSelfBuilt.jssdkAgent.jsApiList,
+        agentid: config.agentId,
+        jsApiList: config.jssdkAgent.jsApiList,
         url,
       };
       return await this.ctx.bean.wxwork.app.selfBuilt.getJsConfigAgent(params);
