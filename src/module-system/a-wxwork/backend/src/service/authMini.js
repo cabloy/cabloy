0b2ee7c0933dd4eeac081f2eb4b6f6a8420dd9ab
zhennann <zhen.nann@icloud.com>
2022-03-21 10:14:06 +0800

    feat: authMini

--- src/module-system/a-wxwork/backend/src/service/authMini.js
+++ src/module-system/a-wxwork/backend/src/service/authMini.js
@@ -1,20 +1,31 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class AuthMini extends app.Service {
     get localHelper() {
       return this.ctx.bean.local.helper;
     }
 
-    async login({ scene, code }) {
+    async login({ providerScene, code }) {
       if (!code) return this.ctx.throw(403);
-      const res = await this.ctx.bean.wxwork.app.mini[scene].code2Session(code);
+      // mini
+      const apiMini = this.ctx.bean.wxwork.mini[providerScene];
+      // session
+      const res = await apiMini.code2Session(code);
       // const res = { errcode: 0, userid: 'YangJian1', session_key: 'kJtdi6RF+Dv67QkbLlPGjw==' };
       if (res.errcode) throw new Error(res.errmsg);
       const session_key = res.session_key;
       const memberId = res.userid;
+      // bean provider
+      const beanProvider = this.ctx.bean.authProvider.createAuthProviderBean({
+        module: moduleInfo.relativeName,
+        providerName: 'wxworkmini',
+        providerScene,
+      });
+      if (!beanProvider.providerSceneValid) this.ctx.throw(423);
       // verify
-      await this.localHelper.verifyAuthUser({ scene: `wxworkmini${scene}`, memberId, needLogin: true });
+      await this.localHelper.verifyAuthUser({ beanProvider, memberId, needLogin: true });
       // save session_key, because ctx.state.user maybe changed
-      await this.ctx.bean.wxwork.mini[scene].saveSessionKey(session_key);
+      await apiMini.saveSessionKey(session_key);
       // echo
       return await this.ctx.bean.auth.echo();
     }
