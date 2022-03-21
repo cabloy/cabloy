085e87014ace2c64b55382d886800f4348fd9134
zhennann <zhen.nann@icloud.com>
2022-03-21 10:07:45 +0800

    feat: remove _createWxworkApiMini

--- src/module-system/a-wxwork/backend/src/bean/bean.wxwork.js
+++ src/module-system/a-wxwork/backend/src/bean/bean.wxwork.js
@@ -7,8 +7,7 @@ module.exports = function (ctx) {
   function getCacheDb() {
     return ctx.cache.db.module(moduleInfo.relativeName);
   }
-  // bean.wxwork.app.mini.{providerScene}
-  // bean.wxwork.app.selfBuilt
+  // bean.wxwork.app.{providerScene}
   // bean.wxwork.mini.{providerScene}
   // bean.wxwork.util
   return function () {
@@ -24,23 +23,8 @@ module.exports = function (ctx) {
               {
                 get(obj, prop) {
                   if (!obj[prop]) {
-                    if (prop === 'mini') {
-                      // app.mini
-                      obj[prop] = new Proxy(
-                        {},
-                        {
-                          get(obj, prop) {
-                            if (!obj[prop]) {
-                              obj[prop] = _createWxworkApiApp({ providerScene: prop, mini: true });
-                            }
-                            return obj[prop];
-                          },
-                        }
-                      );
-                    } else {
-                      // others
-                      obj[prop] = _createWxworkApiApp({ providerScene: prop });
-                    }
+                    // others
+                    obj[prop] = _createWxworkApiApp({ providerScene: prop });
                   }
                   return obj[prop];
                 },
@@ -53,7 +37,7 @@ module.exports = function (ctx) {
               {
                 get(obj, prop) {
                   if (!obj[prop]) {
-                    obj[prop] = _createWxworkApiMini({ providerScene: prop });
+                    obj[prop] = _createWxworkApiApp({ providerScene: prop, mini: true });
                   }
                   return obj[prop];
                 },
@@ -112,60 +96,14 @@ module.exports = function (ctx) {
         }
       }
     );
-    // ready
-    return api;
-  }
-
-  function _createWxworkApiMini({ providerScene }) {
-    // bean provider
-    const beanProvider = ctx.bean.authProvider.createAuthProviderBean({
-      module: moduleInfo.relativeName,
-      providerName: 'wxworkmini',
-      providerScene,
-    });
-    if (!beanProvider.providerSceneValid) ctx.throw(423);
-    // config
-    const config = beanProvider.configProviderScene;
-    // api
-    const api = new WechatAPI(
-      config.appID,
-      config.appSecret,
-      async function () {
-        const cacheKey = `wxworkmini-token:${providerScene}`;
-        return await getCacheDb().get(cacheKey);
-      },
-      async function (token) {
-        const cacheKey = `wxworkmini-token:${providerScene}`;
-        if (token) {
-          await getCacheDb().set(cacheKey, token, token.expireTime - Date.now());
-        } else {
-          await getCacheDb().remove(cacheKey);
-        }
-      }
-    );
-    // registerTicketHandle
-    api.registerTicketHandle(
-      async function (type) {
-        const cacheKey = `wxworkmini-jsticket:${providerScene}:${type}`;
-        return await getCacheDb().get(cacheKey);
-      },
-      async function (type, token) {
-        const cacheKey = `wxworkmini-jsticket:${providerScene}:${type}`;
-        if (token) {
-          await getCacheDb().set(cacheKey, token, token.expireTime - Date.now());
-        } else {
-          await getCacheDb().remove(cacheKey);
-        }
-      }
-    );
     // registerSessionKeyHandle
     api.registerSessionKeyHandle(
       async function () {
-        const cacheKey = `wxworkmini-sessionKey:${providerScene}:${ctx.state.user.agent.id}`;
+        const cacheKey = `wxwork-sessionKey:${appName}:${ctx.state.user.agent.id}`;
         return await getCacheDb().get(cacheKey);
       },
       async function (sessionKey) {
-        const cacheKey = `wxworkmini-sessionKey:${providerScene}:${ctx.state.user.agent.id}`;
+        const cacheKey = `wxwork-sessionKey:${appName}:${ctx.state.user.agent.id}`;
         await getCacheDb().set(cacheKey, sessionKey);
       }
     );
