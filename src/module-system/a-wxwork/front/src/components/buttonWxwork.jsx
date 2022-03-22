08855ef49dd12ab3cc75588c245586c78275222e
zhennann <zhen.nann@icloud.com>
2022-03-22 20:08:32 +0800

    feat: login wxwork

--- src/module-system/a-wxwork/front/src/components/buttonWxwork.jsx
+++ src/module-system/a-wxwork/front/src/components/buttonWxwork.jsx
@@ -1,31 +1,28 @@
 import Vue from 'vue';
 const ebAuthLoginBase = Vue.prototype.$meta.module.get('a-base').options.mixins.ebAuthLoginBase;
-const urlLogin = '/api/a/auth/passport/a-wxwork/wxwork';
 export default {
   meta: {
     global: false,
-    async disable({ ctx, state }) {
-      // in wxwork
-      if (!ctx.$device.wxwork) return true;
-      // associate
-      if (state === 'associate') return false;
-      // reload
-      const reload = ctx.$store.state.auth.reload;
-      if (reload) return false;
-      // login direct for state=login
-      this.login({ ctx, state });
-      // throw error
-      throw new Error();
-    },
-    login({ ctx, state, hash }) {
-      ctx.$meta.vueApp.toLogin({ url: urlLogin, state, hash });
-    },
   },
   mixins: [ebAuthLoginBase],
   data() {
     return {};
   },
   methods: {
+    async disable() {
+      const ctx = this.ctx;
+      // in wxwork
+      if (!ctx.$device.wxwork) return true;
+      // associate
+      if (this.state === 'associate') return false;
+      // reload
+      const reload = ctx.$store.state.auth.reload;
+      if (reload) return false;
+      // login direct for state=login
+      await this.login();
+      // throw error to interrupt
+      throw new Error();
+    },
     onPerformSignIn() {
       return this.login();
     },
--- src/module-system/a-wxwork/front/src/components/buttonWxworkweb.jsx
+++ src/module-system/a-wxwork/front/src/components/buttonWxworkweb.jsx
@@ -1,25 +1,22 @@
 import Vue from 'vue';
 const ebAuthLoginBase = Vue.prototype.$meta.module.get('a-base').options.mixins.ebAuthLoginBase;
-const urlLogin = '/api/a/wxwork/passport/a-wxwork/wxworkweb';
 export default {
   meta: {
     global: false,
-    async disable({ ctx, state }) {
-      // only pc
-      if (ctx.$device.iphone || ctx.$device.android || ctx.$device.wxwork || ctx.$device.wechat) {
-        return true;
-      }
-      return false;
-    },
-    login({ ctx, state, hash }) {
-      ctx.$meta.vueApp.toLogin({ url: urlLogin, state, hash });
-    },
   },
   mixins: [ebAuthLoginBase],
   data() {
     return {};
   },
   methods: {
+    async disable() {
+      const ctx = this.ctx;
+      // only pc
+      if (ctx.$device.iphone || ctx.$device.android || ctx.$device.wxwork || ctx.$device.wechat) {
+        return true;
+      }
+      return false;
+    },
     onPerformSignIn() {
       return this.login();
     },
