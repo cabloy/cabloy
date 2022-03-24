f2ce37261d918dae2811bbd138a99c003be16f18
zhennann <zhen.nann@icloud.com>
2022-03-24 20:05:16 +0800

    feat: jssdk

--- src/module-system/a-dingtalk/backend/src/service/jssdk.js
+++ src/module-system/a-dingtalk/backend/src/service/jssdk.js
@@ -1,16 +1,23 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class JSSDK extends app.Service {
     async jsconfig({ url }) {
+      // bean provider
+      const beanProvider = this.ctx.bean.authProvider.createAuthProviderBean({
+        module: moduleInfo.relativeName,
+        providerName: 'dingtalk',
+        providerScene: 'selfBuilt',
+      });
+      if (!beanProvider.providerSceneValid) this.ctx.throw(423);
       // config
-      const config = this.ctx.config.account.dingtalk;
-      const configAppSelfBuilt = config.apps.selfBuilt;
+      const config = beanProvider.configProviderScene;
       // jsconfig
       const res = await this.ctx.bean.dingtalk.app.selfBuilt.client.getJSApiConfig(url);
       return {
         ...res,
-        agentId: configAppSelfBuilt.agentid,
-        type: configAppSelfBuilt.jssdk.type,
-        jsApiList: configAppSelfBuilt.jssdk.jsApiList,
+        agentId: config.agentId,
+        type: config.jssdk.type,
+        jsApiList: config.jssdk.jsApiList,
       };
     }
   }
