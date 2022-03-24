2f50d124058ac9086b298e928563a79f6c253c05
zhennann <zhen.nann@icloud.com>
2022-03-24 15:27:22 +0800

    feat: wechat message

--- src/module-system/a-wechat/backend/src/controllers.js
+++ src/module-system/a-wechat/backend/src/controllers.js
@@ -1,13 +1,11 @@
 const message = require('./controller/message.js');
 const jssdk = require('./controller/jssdk.js');
-const messageMini = require('./controller/messageMini.js');
 const authMini = require('./controller/authMini.js');
 
 module.exports = app => {
   const controllers = {
     message,
     jssdk,
-    messageMini,
     authMini,
   };
   return controllers;
--- src/module-system/a-wechat/backend/src/services.js
+++ src/module-system/a-wechat/backend/src/services.js
@@ -1,13 +1,11 @@
 const message = require('./service/message.js');
 const jssdk = require('./service/jssdk.js');
-const messageMini = require('./service/messageMini.js');
 const authMini = require('./service/authMini.js');
 
 module.exports = app => {
   const services = {
     message,
     jssdk,
-    messageMini,
     authMini,
   };
   return services;
