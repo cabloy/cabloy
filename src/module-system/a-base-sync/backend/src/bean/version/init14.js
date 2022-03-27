29823095db2851631ef14f4075cd8529a641dbfb
zhennann <zhen.nann@icloud.com>
2022-03-27 19:19:45 +0800

    feat: role right

--- src/module-system/a-base-sync/backend/src/bean/version/init14.js
+++ src/module-system/a-base-sync/backend/src/bean/version/init14.js
@@ -1,6 +1,11 @@
 module.exports = function (ctx) {
   class VersionInit {
     async run() {
+      await this._addRoleRightsResource();
+      await this._addRoleRightsRole();
+    }
+
+    async _addRoleRightsResource() {
       // add role rights
       const roleRights = [
         { roleName: 'system', action: 'create' },
@@ -22,6 +27,30 @@ module.exports = function (ctx) {
         { roleName: 'system', action: 'exportBulk' },
       ];
       await ctx.bean.role.addRoleRightBatch({ atomClassName: 'resource', roleRights });
+    }
+
+    async _addRoleRightsRole() {
+      // add role rights
+      const roleRights = [
+        { roleName: 'system', action: 'create' },
+        { roleName: 'system', action: 'read', scopeNames: 0 },
+        { roleName: 'system', action: 'read', scopeNames: 'root' },
+        { roleName: 'system', action: 'write', scopeNames: 0 },
+        { roleName: 'system', action: 'write', scopeNames: 'root' },
+        { roleName: 'system', action: 'delete', scopeNames: 0 },
+        { roleName: 'system', action: 'delete', scopeNames: 'root' },
+        { roleName: 'system', action: 'clone', scopeNames: 0 },
+        { roleName: 'system', action: 'clone', scopeNames: 'root' },
+        // { roleName: 'system', action: 'enable', scopeNames: 0 },
+        // { roleName: 'system', action: 'enable', scopeNames: 'root' },
+        // { roleName: 'system', action: 'disable', scopeNames: 0 },
+        // { roleName: 'system', action: 'disable', scopeNames: 'root' },
+        { roleName: 'system', action: 'authorize', scopeNames: 0 },
+        { roleName: 'system', action: 'authorize', scopeNames: 'root' },
+        { roleName: 'system', action: 'deleteBulk' },
+        { roleName: 'system', action: 'exportBulk' },
+      ];
+      await ctx.bean.role.addRoleRightBatch({ atomClassName: 'role', roleRights });
     }
   }
 
