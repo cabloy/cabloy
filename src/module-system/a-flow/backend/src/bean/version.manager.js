501a251c922d278c6e9c7b096a415b60e23c5c93
zhennann <zhen.nann@icloud.com>
2022-03-27 10:16:16 +0800

    feat(a-flow): fileVersion->4

--- src/module-system/a-flow/backend/src/bean/version.manager.js
+++ src/module-system/a-flow/backend/src/bean/version.manager.js
@@ -192,21 +192,43 @@ module.exports = app => {
 
     async init(options) {
       if (options.version === 1) {
+        // // add role rights
+        // const roleRights = [
+        //   { roleName: 'system', action: 'create' },
+        //   { roleName: 'system', action: 'read', scopeNames: 0 },
+        //   { roleName: 'system', action: 'read', scopeNames: 'superuser' },
+        //   { roleName: 'system', action: 'write', scopeNames: 0 },
+        //   { roleName: 'system', action: 'write', scopeNames: 'superuser' },
+        //   { roleName: 'system', action: 'delete', scopeNames: 0 },
+        //   { roleName: 'system', action: 'delete', scopeNames: 'superuser' },
+        //   { roleName: 'system', action: 'clone', scopeNames: 0 },
+        //   { roleName: 'system', action: 'clone', scopeNames: 'superuser' },
+        //   { roleName: 'system', action: 'enable', scopeNames: 0 },
+        //   { roleName: 'system', action: 'enable', scopeNames: 'superuser' },
+        //   { roleName: 'system', action: 'disable', scopeNames: 0 },
+        //   { roleName: 'system', action: 'disable', scopeNames: 'superuser' },
+        //   { roleName: 'system', action: 'deleteBulk' },
+        //   { roleName: 'system', action: 'exportBulk' },
+        // ];
+        // await this.ctx.bean.role.addRoleRightBatch({ atomClassName: 'flowDef', roleRights });
+      }
+
+      if (options.version === 4) {
         // add role rights
         const roleRights = [
           { roleName: 'system', action: 'create' },
           { roleName: 'system', action: 'read', scopeNames: 0 },
-          { roleName: 'system', action: 'read', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'read', scopeNames: 'authenticated' },
           { roleName: 'system', action: 'write', scopeNames: 0 },
-          { roleName: 'system', action: 'write', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'write', scopeNames: 'authenticated' },
           { roleName: 'system', action: 'delete', scopeNames: 0 },
-          { roleName: 'system', action: 'delete', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'delete', scopeNames: 'authenticated' },
           { roleName: 'system', action: 'clone', scopeNames: 0 },
-          { roleName: 'system', action: 'clone', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'clone', scopeNames: 'authenticated' },
           { roleName: 'system', action: 'enable', scopeNames: 0 },
-          { roleName: 'system', action: 'enable', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'enable', scopeNames: 'authenticated' },
           { roleName: 'system', action: 'disable', scopeNames: 0 },
-          { roleName: 'system', action: 'disable', scopeNames: 'superuser' },
+          { roleName: 'system', action: 'disable', scopeNames: 'authenticated' },
           { roleName: 'system', action: 'deleteBulk' },
           { roleName: 'system', action: 'exportBulk' },
         ];
