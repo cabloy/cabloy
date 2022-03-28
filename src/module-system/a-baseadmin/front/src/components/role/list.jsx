9b9bad2eee6ffc31be630845f641198a32062ac4
zhennann <zhen.nann@icloud.com>
2022-03-28 20:39:21 +0800

    feat: childrenTop

--- src/module-system/a-baseadmin/front/src/components/role/list.jsx
+++ src/module-system/a-baseadmin/front/src/components/role/list.jsx
@@ -23,29 +23,32 @@ export default {
     },
   },
   methods: {
-    onLoadChildren(node) {
-      const roleId = node.root ? this.roleIdStart : node.id;
-      return this.$api
-        .post('role/children', { roleId, page: { size: 0 } })
-        .then(data => {
-          const list = data.list.map(item => {
-            const node = {
-              id: item.id,
-              attrs: {
-                label: item.roleName || `[${this.$text('New Role')}]`,
-                toggle: item.catalog === 1,
-                loadChildren: item.catalog === 1,
-              },
-              data: item,
-            };
-            return node;
-          });
-          return list;
-        })
-        .catch(err => {
-          this.$view.toast.show({ text: err.message });
-          throw err;
+    async onLoadChildren(node) {
+      try {
+        let data;
+        const roleId = node.root ? this.roleIdStart : node.id;
+        if (roleId === 0) {
+          data = await this.$api.post('role/childrenTop', { roleId, page: { size: 0 } });
+        } else {
+          data = await this.$api.post('role/children', { roleId, page: { size: 0 } });
+        }
+        const list = data.list.map(item => {
+          const node = {
+            id: item.id,
+            attrs: {
+              label: item.roleName || `[${this.$text('New Role')}]`,
+              toggle: item.catalog === 1,
+              loadChildren: item.catalog === 1,
+            },
+            data: item,
+          };
+          return node;
         });
+        return list;
+      } catch (err) {
+        this.$view.toast.show({ text: err.message });
+        throw err;
+      }
     },
     onNodeClick(node) {
       this.$emit('node:click', node);
