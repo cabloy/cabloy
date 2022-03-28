26b8869a69a9c2fc06cf8b6730ed7d0eeed630ff
zhennann <zhen.nann@icloud.com>
2022-03-28 14:39:38 +0800

    feat: roles static atoms

--- src/module-system/a-base-sync/backend/src/config/static/roles.js
+++ src/module-system/a-base-sync/backend/src/config/static/roles.js
@@ -1,14 +1,29 @@
 module.exports = app => {
   // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
-  const roles = [
-    // {
-    //   atomName: 'root',
-    //   atomStaticKey: 'role_root',
-    //   atomRevision: 1,
-    //   description: '1111',
-    //   resourceRoles: 'template.system',
-    // },
+  const __rolesAll = [
+    { atomName: 'root', roleTypeCode: 0 },
+    { atomName: 'anonymous', roleTypeCode: 0 },
+    { atomName: 'authenticated', roleTypeCode: 0 },
+    { atomName: 'template', roleTypeCode: 5 },
+    { atomName: 'system', roleTypeCode: 5 },
+    { atomName: 'registered', roleTypeCode: 0 },
+    { atomName: 'activated', roleTypeCode: 0 },
+    { atomName: 'superuser', roleTypeCode: 0 },
+    { atomName: 'organization', roleTypeCode: 1 },
+    { atomName: 'internal', roleTypeCode: 1 },
+    { atomName: 'external', roleTypeCode: 1 },
   ];
+  const roles = [];
+  for (const __role of __rolesAll) {
+    roles.push({
+      atomName: __role.atomName,
+      atomStaticKey: `role_${__role.atomName}`,
+      atomRevision: 1,
+      description: '',
+      roleTypeCode: __role.roleTypeCode,
+      resourceRoles: 'template.system',
+    });
+  }
   // ok
   return roles;
 };
