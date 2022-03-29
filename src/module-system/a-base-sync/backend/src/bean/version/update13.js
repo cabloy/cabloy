2d0b3d656449db52106ebc61ffc752217b503422
zhennann <zhen.nann@icloud.com>
2022-03-29 11:35:42 +0800

    feat: aViewUserRightRefAtomClass

--- src/module-system/a-base-sync/backend/src/bean/version/update13.js
+++ src/module-system/a-base-sync/backend/src/bean/version/update13.js
@@ -17,6 +17,13 @@ module.exports = function (ctx) {
         Add COLUMN atomId int(11) DEFAULT '0'
                 `;
       await ctx.model.query(sql);
+      // aViewUserRightRefAtomClass
+      sql = `
+      create view aViewUserRightRefAtomClass as
+        select a.iid,a.userId as userIdWho,a.roleExpandId,a.roleId,a.roleIdBase,b.id as roleRightRefId,b.roleRightId,b.atomClassId,b.action,b.roleIdScope as roleIdWhom from aViewUserRoleExpand a
+          inner join aRoleRightRef b on a.roleIdBase=b.roleId
+        `;
+      await ctx.model.query(sql);
     }
   }
 
