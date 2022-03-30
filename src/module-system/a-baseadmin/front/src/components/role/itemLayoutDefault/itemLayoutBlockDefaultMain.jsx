9c66273b05ab504ad333f5c0f05878933fcf3eaa
zhennann <zhen.nann@icloud.com>
2022-03-30 16:41:48 +0800

    feat: role edit toolbar

--- src/module-system/a-baseadmin/front/src/components/role/itemLayoutDefault/itemLayoutBlockDefaultMain.jsx
+++ src/module-system/a-baseadmin/front/src/components/role/itemLayoutDefault/itemLayoutBlockDefaultMain.jsx
@@ -16,7 +16,32 @@ export default {
   data() {
     return {};
   },
+  methods: {
+    _renderToolbar() {
+      return (
+        <f7-segmented tag="p">
+          <f7-button class="display-flex" outline iconF7="::add">
+            Add
+          </f7-button>
+          <f7-button class="display-flex" outline>
+            Outline
+          </f7-button>
+          <f7-button class="display-flex" outline>
+            Active
+          </f7-button>
+        </f7-segmented>
+      );
+    },
+  },
   render() {
-    return this.layoutManager.validate_render();
+    const domToolbar = this._renderToolbar();
+    const domValidate = this.layoutManager.validate_render();
+    if (!domToolbar) return domValidate;
+    return (
+      <div>
+        {domToolbar}
+        {domValidate}
+      </div>
+    );
   },
 };
