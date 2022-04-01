f5a13b2ca4e4d9f8afdb92d8fe56faf24d65fb7f
zhennann <zhen.nann@icloud.com>
2022-04-01 11:34:07 +0800

    feat: role item main

--- src/module-system/a-baseadmin/front/src/components/role/itemLayoutDefault/itemLayoutBlockDefaultMain.jsx
+++ src/module-system/a-baseadmin/front/src/components/role/itemLayoutDefault/itemLayoutBlockDefaultMain.jsx
@@ -16,46 +16,9 @@ export default {
   data() {
     return {};
   },
-  computed: {
-    roleAtomId() {
-      return this.layoutManager.container.atomId;
-    },
-  },
   created() {},
-  methods: {
-    _renderToolbar() {
-      if (!this.roleActions) return null;
-      const children = [];
-      for (const action of this.roleActions) {
-        children.push(
-          <eb-button
-            key={action.name}
-            outline
-            iconF7={action.icon.f7}
-            propsOnPerform={event => this.onPerformAction(event, action)}
-          >
-            {action.titleLocale}
-          </eb-button>
-        );
-      }
-      if (children.length === 0) return null;
-      return <f7-segmented tag="p">{children}</f7-segmented>;
-    },
-  },
-  onPerformAction(event, action) {
-    const method = `onPerformAction_${action.name}`;
-    return this[method](event, action);
-  },
-  onPerformAction_addChildRole(event, action) {},
+  methods: {},
   render() {
-    const domToolbar = this._renderToolbar();
-    const domValidate = this.layoutManager.validate_render();
-    if (!domToolbar) return domValidate;
-    return (
-      <div>
-        {domToolbar}
-        {domValidate}
-      </div>
-    );
+    return this.layoutManager.validate_render();
   },
 };
