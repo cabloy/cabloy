f66b84e214d6d36420188930d5f09c378101ab9f
zhennann <zhen.nann@icloud.com>
2022-04-06 18:07:10 +0800

    feat: user unittest

--- src/module-vendor/test-party/backend/test/controller/test/multilevelAuthorization/user.test.js
+++ src/module-vendor/test-party/backend/test/controller/test/multilevelAuthorization/user.test.js
@@ -1,6 +1,6 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe.only('test/controller/test/multilevelAuthorization/user.test.js', () => {
+describe('test/controller/test/multilevelAuthorization/user.test.js', () => {
   it('action:multilevelAuthorization:user', async () => {
     const result = await app.httpRequest().post(mockUrl('test/multilevelAuthorization/user'));
     assert.equal(result.body.code, 0);
