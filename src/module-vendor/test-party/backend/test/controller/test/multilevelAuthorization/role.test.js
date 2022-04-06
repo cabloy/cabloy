41ab09bca3d0f1afd74f616b4eb548ae50fb84c2
zhennann <zhen.nann@icloud.com>
2022-04-06 09:49:19 +0800

    feat: multilevel authorization unittest

---
+++ src/module-vendor/test-party/backend/test/controller/test/multilevelAuthorization/role.test.js
@@ -0,0 +1,8 @@
+const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
+
+describe.only('test/controller/test/multilevelAuthorization/role.test.js', () => {
+  it('action:multilevelAuthorization:role', async () => {
+    const result = await app.httpRequest().post(mockUrl('test/multilevelAuthorization/role'));
+    assert.equal(result.body.code, 0);
+  });
+});
