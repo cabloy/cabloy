66a6c907f8485265fc622e635e912affa6c4d53c
zhennann <zhen.nann@icloud.com>
2022-04-07 09:51:18 +0800

    enhance: patch.js

--- packages/egg-born-front/src/inject/patch/patch.js
+++ packages/egg-born-front/src/inject/patch/patch.js
@@ -8,10 +8,18 @@ export default function (ctx, router) {
     if (route) return cb(route);
     // info
     const moduleInfo = mparse.parseInfo(url);
-    if (!moduleInfo) return cb(null); // throw new Error('invalid url');
+    if (!moduleInfo) {
+      // should not throw error
+      console.log('invalid page path: ', url);
+      return cb(null);
+    }
     // use module
     ctx.$meta.module.use(moduleInfo, () => {
       route = router.findMatchingRoute(url);
+      if (!route) {
+        // should not throw error
+        console.log('page path not found: ', url);
+      }
       return cb(route);
     });
   }
