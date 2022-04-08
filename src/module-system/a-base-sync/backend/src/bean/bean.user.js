8a97bdb8fb8aaad304233aed833601efde0c3b4d
zhennann <zhen.nann@icloud.com>
2022-04-08 09:08:44 +0800

    refactor: bean.user

--- src/module-system/a-base-sync/backend/src/bean/bean.user.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.user.js
@@ -1,5 +1,5 @@
-const user_0 = require('./bean.user_0.js');
-const user_1 = require('./bean.user_1.js');
+const user_0 = require('./bean.user/bean.user_0.js');
+const user_1 = require('./bean.user/bean.user_1.js');
 
 module.exports = ctx => {
   return ctx.app.meta.util.mixinClasses(user_0, user_1, ctx);
