8a5f8fe1746ac833d6bb1d304b9c3010634e2830
zhennann <zhen.nann@icloud.com>
2022-04-08 09:12:08 +0800

    refactor: bean.atom/atomBase

--- src/module-system/a-base-sync/backend/src/bean/bean.atom.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.atom.js
@@ -1,5 +1,5 @@
-const atom_0 = require('./bean.atom_0.js');
-const atom_1 = require('./bean.atom_1.js');
+const atom_0 = require('./bean.atom/bean.atom_0.js');
+const atom_1 = require('./bean.atom/bean.atom_1.js');
 
 module.exports = ctx => {
   return ctx.app.meta.util.mixinClasses(atom_0, atom_1, ctx);
--- src/module-system/a-base-sync/backend/src/bean/bean.atomBase.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.atomBase.js
@@ -1,5 +1,5 @@
-const atomBase_0 = require('./bean.atomBase_0.js');
-const atomBase_1 = require('./bean.atomBase_1.js');
+const atomBase_0 = require('./bean.atomBase/bean.atomBase_0.js');
+const atomBase_1 = require('./bean.atomBase/bean.atomBase_1.js');
 
 module.exports = app => {
   return app.meta.util.mixinClasses(atomBase_0, atomBase_1, app);
