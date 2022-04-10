501b57c023030eea19b50dfedbf6f24261368af5
zhennann <zhen.nann@icloud.com>
2022-04-10 10:21:45 +0800

    feat: treeviewData

--- src/module-system/a-components-sync/front/src/common/treeviewAdapter.js
+++ src/module-system/a-components-sync/front/src/common/treeviewAdapter.js
@@ -1,11 +1,11 @@
-export default function (ctx, instance) {
+export default function (ctx, treeviewData) {
   return {
     openNode(node) {
       const $el = ctx.getElementByNode(node);
       ctx.$f7.treeview.open($el);
     },
     async onLoadChildren(node) {
-      return await ctx.onLoadChildren(node, instance);
+      return await ctx.onLoadChildren(node, treeviewData);
     },
     onNodeSelect(node) {
       ctx.$emit('node:select', node);
