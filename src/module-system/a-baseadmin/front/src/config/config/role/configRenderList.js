79f2f8ebb45d8a3fad6df6533da8e3d750aa44b1
zhennann <zhen.nann@icloud.com>
2022-04-13 10:46:12 +0800

    feat: role table fields

--- src/module-system/a-baseadmin/front/src/config/config/role/configRenderList.js
+++ src/module-system/a-baseadmin/front/src/config/config/role/configRenderList.js
@@ -30,61 +30,47 @@ const list = {
     treeTable: {
       blocks: {
         items: {
-          // columns: [
-          //   {
-          //     dataIndex: 'atomName',
-          //     title: 'Atom Name',
-          //     align: 'left',
-          //     component: {
-          //       module: 'a-baselayout',
-          //       name: 'listLayoutTableCellAtomName',
-          //     },
-          //   },
-          //   {
-          //     dataIndex: 'resourceType',
-          //     title: 'Resource Type',
-          //     align: 'left',
-          //     params: {
-          //       computed: {
-          //         expression: 'record.resourceTypeLocale',
-          //       },
-          //     },
-          //   },
-          //   {
-          //     dataIndex: 'atomCategoryNameLocale',
-          //     title: 'Category',
-          //     align: 'left',
-          //   },
-          //   {
-          //     dataIndex: 'userName',
-          //     title: 'Creator',
-          //     align: 'left',
-          //     component: {
-          //       module: 'a-baselayout',
-          //       name: 'listLayoutTableCellUserName',
-          //     },
-          //   },
-          //   {
-          //     dataIndex: 'createdAt',
-          //     title: 'Created Time',
-          //     align: 'center',
-          //     params: {
-          //       dateFormat: {
-          //         lines: true,
-          //       },
-          //     },
-          //   },
-          //   {
-          //     dataIndex: 'updatedAt',
-          //     title: 'Modification Time',
-          //     align: 'center',
-          //     params: {
-          //       dateFormat: {
-          //         lines: true,
-          //       },
-          //     },
-          //   },
-          // ],
+          columns: [
+            {
+              dataIndex: 'atomName',
+              title: 'Role Name',
+              align: 'left',
+              component: {
+                module: 'a-baselayout',
+                name: 'listLayoutTableCellAtomName',
+              },
+            },
+            {
+              dataIndex: 'roleTypeCode',
+              title: 'Role Type',
+              align: 'left',
+              params: {
+                computed: {
+                  expression: 'record._roleTypeCodeTitleLocale',
+                },
+              },
+            },
+            {
+              dataIndex: 'createdAt',
+              title: 'Created Time',
+              align: 'center',
+              params: {
+                dateFormat: {
+                  lines: true,
+                },
+              },
+            },
+            {
+              dataIndex: 'updatedAt',
+              title: 'Modification Time',
+              align: 'center',
+              params: {
+                dateFormat: {
+                  lines: true,
+                },
+              },
+            },
+          ],
         },
       },
     },
