4d97696121c8891fb77a323cb13e9df0ba963f0e
zhennann <zhen.nann@icloud.com>
2022-04-16 16:14:38 +0800

    feat: openAuth unittest

---
+++ src/module-vendor/test-party/backend/test/controller/test/feat/openAuth.test.js
@@ -0,0 +1,37 @@
+const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
+const os = require('os');
+const path = require('path');
+const fse = require('fs-extra');
+
+describe.only('test/controller/test/feat/openAuth.test.js', () => {
+  it('action:openAuth', async () => {
+    // init file
+    const { config } = await _readCabloyInitFile();
+    // token
+    const tokenName = `clidev@${app.name}`;
+    const token = config.tokens[tokenName];
+    console.log(token);
+    // login
+    const result = await app
+      .httpRequest()
+      .post(mockUrl('/a/authopen/auth/login'))
+      .send({
+        data: {
+          clientID: token.clientID,
+          clientSecret: token.clientSecret,
+        },
+      });
+    assert.equal(result.body.code, 0);
+    console.log(result.body);
+  });
+});
+
+async function _readCabloyInitFile() {
+  // fileName
+  const fileName = path.join(os.homedir(), '.cabloy', 'openauth.json');
+  // config
+  const content = await fse.readFile(fileName);
+  const config = JSON.parse(content);
+  // ok
+  return { fileName, config };
+}
