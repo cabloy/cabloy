8c46cd0c645db023f29d214d6f2ef81e646f9a48
zhennann <zhen.nann@icloud.com>
2022-04-20 15:50:17 +0800

    feat: cli demo meta

--- src/module-system/a-clibooster/backend/src/config/cli/command/demo.js
+++ src/module-system/a-clibooster/backend/src/config/cli/command/demo.js
@@ -1,6 +1,75 @@
 module.exports = app => {
   return {
-    title: 'Cli Demo',
+    info: {
+      version: '4.0.0',
+      title: 'Cli Demo',
+      usage: 'Usage: npm run :demo [options]',
+    },
+    options: {
+      username: {
+        description: 'Your username',
+        alias: 'u',
+        type: 'string',
+      },
+      role: {
+        description: 'Your role',
+        alias: 'r',
+        type: 'string',
+      },
+      course: {
+        description: 'Your course',
+        alias: 'c',
+        type: 'string',
+      },
+      grade: {
+        description: 'Your grade',
+        alias: 'g',
+        type: 'string',
+      },
+    },
+    groups: {
+      default: {
+        description: 'Specify your basic info',
+        condition: {
+          expression: null,
+        },
+        questions: {
+          username: {
+            type: 'input',
+            message: 'Your username',
+          },
+          role: {
+            type: 'select',
+            message: 'Your role',
+            choices: ['teacher', 'student'],
+          },
+        },
+      },
+      teacher: {
+        description: 'Specify the teacher info',
+        condition: {
+          expression: 'role==="teacher"',
+        },
+        questions: {
+          course: {
+            type: 'input',
+            message: 'Your course',
+          },
+        },
+      },
+      student: {
+        description: 'Specify the student info',
+        condition: {
+          expression: 'role==="student"',
+        },
+        questions: {
+          grade: {
+            type: 'input',
+            message: 'Your grade',
+          },
+        },
+      },
+    },
     bean: 'demo',
   };
 };
