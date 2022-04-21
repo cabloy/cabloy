c7cd4b056fba104d347a85fbb39ea52bc0f0f91e
zhennann <zhen.nann@icloud.com>
2022-04-21 11:19:05 +0800

    feat: prompt run dev:backend

--- packages/egg-born-bin/lib/openAuth.js
+++ packages/egg-born-bin/lib/openAuth.js
@@ -1,4 +1,8 @@
 const urllib = require('urllib');
+const chalk = require('chalk');
+const boxen = require('boxen');
+
+const boxenOptions = { padding: 1, margin: 1, align: 'center', borderColor: 'yellow', borderStyle: 'round' };
 
 module.exports = class OpenAuth {
   constructor({ host }) {
@@ -7,32 +11,40 @@ module.exports = class OpenAuth {
   }
 
   async post({ path, body }) {
-    const accessToken = (this.jwt && this.jwt.accessToken) || '';
-    const httpClient = urllib.create();
-    const url = `${this.host}/api${path}`;
-    const options = {
-      method: 'POST',
-      data: body,
-      contentType: 'json',
-      dataType: 'json',
-      followRedirect: true,
-      maxRedirects: 5,
-      timeout: 20000,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    };
-    const res = await httpClient.request(url, options);
-    // error
-    if (res.data.code !== 0) {
-      throw new Error(res.data.message);
+    try {
+      const accessToken = (this.jwt && this.jwt.accessToken) || '';
+      const httpClient = urllib.create();
+      const url = `${this.host}/api${path}`;
+      const options = {
+        method: 'POST',
+        data: body,
+        contentType: 'json',
+        dataType: 'json',
+        followRedirect: true,
+        maxRedirects: 5,
+        timeout: 20000,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      const res = await httpClient.request(url, options);
+      // error
+      if (res.data.code !== 0) {
+        throw new Error(res.data.message);
+      }
+      // jwt
+      const jwt = res.data['eb-jwt-oauth'];
+      if (jwt) {
+        this.jwt = jwt;
+      }
+      // ok
+      return res.data.data;
+    } catch (err) {
+      if (err.status === -1 && err.address === '127.0.0.1') {
+        const message = `Run ${chalk.keyword('orange')('> npm run dev:backend <')} first!`;
+        console.log('\n' + boxen(message, boxenOptions) + '\n');
+      }
+      throw err;
     }
-    // jwt
-    const jwt = res.data['eb-jwt-oauth'];
-    if (jwt) {
-      this.jwt = jwt;
-    }
-    // ok
-    return res.data.data;
   }
 };
