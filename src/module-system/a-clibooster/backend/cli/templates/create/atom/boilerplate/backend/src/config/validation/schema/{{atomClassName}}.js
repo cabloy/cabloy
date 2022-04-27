cfc2909fae75a7ba078bae15d55062663fc924fd
zhennann <zhen.nann@icloud.com>
2022-04-27 09:11:39 +0800

    feat: cli create atom

---
+++ src/module-system/a-clibooster/backend/cli/templates/create/atom/boilerplate/backend/src/config/validation/schema/{{atomClassName}}.js
@@ -0,0 +1,32 @@
+module.exports = app => {
+  const schemas = {};
+  // <%=argv.atomClassName%>
+  schemas.<%=argv.atomClassName%> = {
+    type: 'object',
+    properties: {
+      atomName: {
+        type: 'string',
+        ebType: 'text',
+        ebTitle: 'Name',
+        notEmpty: true,
+      },
+      description: {
+        type: 'string',
+        ebType: 'text',
+        ebTitle: 'Description',
+      },
+    },
+  };
+  // <%=argv.atomClassName%> search
+  schemas.<%=argv.atomClassName%>Search = {
+    type: 'object',
+    properties: {
+      description: {
+        type: 'string',
+        ebType: 'text',
+        ebTitle: 'Description',
+      },
+    },
+  };
+  return schemas;
+};
