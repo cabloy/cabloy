df3dbcf5af86e79af5eb9757a44466daffb37e28
zhennann <zhen.nann@icloud.com>
2022-04-27 18:16:26 +0800

    feat: template module

--- src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/meta.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/meta.js
@@ -1,13 +1,24 @@
 module.exports = app => {
+  // schemas
   const schemas = require('./config/validation/schemas.js')(app);
+  // static
+  const staticResources = require('./config/static/resources.js')(app);
   const meta = {
     base: {
       atoms: {},
+      statics: {
+        'a-base.resource': {
+          items: staticResources,
+        },
+      },
     },
     validation: {
       validators: {},
       keywords: {},
       schemas,
+    },
+    index: {
+      indexes: {},
     },
   };
   return meta;
