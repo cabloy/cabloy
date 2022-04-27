3501b38aaea470f4c093fcdc2b6d9be497646e14
zhennann <zhen.nann@icloud.com>
2022-04-27 17:48:11 +0800

    feat: meta.js

---
+++ src/module-system/a-clibooster/backend/cli/templates/create/atom/snippets/meta.js
@@ -0,0 +1,42 @@
+const __snippet_atom = `<%=argv.atomClassName%>: {
+  info: {
+    bean: '<%=argv.atomClassName%>',
+    title: '<%=argv.atomClassNameCapitalize%>',
+    tableName: '<%=argv.providerId%><%=argv.atomClassNameCapitalize%>',
+    language: false,
+    category: true,
+    tag: true,
+  },
+  actions: {},
+  validator: '<%=argv.atomClassName%>',
+  search: {
+    validator: '<%=argv.atomClassName%>Search',
+  },
+},`;
+const __snippet_validator = `<%=argv.atomClassName%>: {
+  schemas: '<%=argv.atomClassName%>',
+},
+<%=argv.atomClassName%>Search: {
+  schemas: '<%=argv.atomClassName%>Search',
+},`;
+const __snippet_index = `<%=argv.providerId%><%=argv.atomClassNameCapitalize%>: 'createdAt,updatedAt,atomId',`;
+
+module.exports = {
+  file: 'backend/src/meta.js',
+  async transform({ cli, ast, argv, ctx }) {
+    // atom
+    let code = await cli.template.renderContent({ content: __snippet_atom });
+    ast.replace(
+      `const meta = { base: { atoms: {$$$0},$$$1 },$$$2 }`,
+      `const meta = { base: { atoms: { ${code} \n $$$0},$$$1 },$$$2 }`
+    );
+    // validator
+    code = await cli.template.renderContent({ content: __snippet_validator });
+    ast.replace(`validators: {$$$0}`, `validators: { ${code} \n $$$0}`);
+    // index
+    code = await cli.template.renderContent({ content: __snippet_index });
+    ast.replace(`indexes: {$$$0}`, `indexes: { ${code} \n $$$0}`);
+    // ok
+    return ast;
+  },
+};
