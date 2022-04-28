ddd749ac1e75834ecdd53c15e3fbe0b71917db52
zhennann <zhen.nann@icloud.com>
2022-04-28 15:47:40 +0800

    feat: git-status.sh

---
+++ scripts/git-status.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+cd ./src/module/a-flownodebooster/
+pwd
+git status
+cd ../../../
+
+cd ./src/module/a-flowtask/
+pwd
+git status
+cd ../../../
+
+cd ./src/module/a-markdown/
+pwd
+git status
+cd ../../../
+
+cd ./src/module/test-flow/
+pwd
+git status
+cd ../../../
+
+cd ./src/module/test-note/
+pwd
+git status
+cd ../../../
+
+cd ./src/suite/test-party/
+pwd
+git status
+cd ../../../
