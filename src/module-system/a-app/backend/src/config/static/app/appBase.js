0fe61f589af4dd8e20491fe3b0f4745029d732e8
zhennann <zhen.nann@icloud.com>
2022-05-02 11:19:10 +0800

    feat: appBase config

--- src/module-system/a-app/backend/src/config/static/app/appBase.js
+++ src/module-system/a-app/backend/src/config/static/app/appBase.js
@@ -1,6 +1,61 @@
 module.exports = app => {
   // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
-  const content = {};
+  const content = {
+    presets: {
+      anonymous: {
+        mobile: {
+          layout: 'a-layoutmobile:layoutMobileAnonymous',
+          menu: {
+            layout: '',
+          },
+          home: {
+            mode: 'dashboard', // dashboard,page
+            dashboard: 'a-dashboard:dashboardDefault',
+            page: null,
+          },
+          user: {},
+        },
+        pc: {
+          layout: 'a-layoutpc:layoutPCAnonymous',
+          menu: {
+            layout: '',
+          },
+          home: {
+            mode: 'dashboard', // dashboard,page
+            dashboard: 'a-dashboard:dashboardDefault',
+            page: null,
+          },
+          user: {},
+        },
+      },
+      authenticated: {
+        mobile: {
+          layout: 'a-layoutmobile:layoutMobile',
+          menu: {
+            layout: '',
+          },
+          home: {
+            mode: 'dashboard', // dashboard,page
+            dashboard: 'a-dashboard:dashboardDefault',
+            page: null,
+          },
+          user: {},
+        },
+        pc: {
+          layout: 'a-layoutpc:layoutPC',
+          menu: {
+            layout: '',
+          },
+          home: {
+            mode: 'dashboard', // dashboard,page
+            dashboard: 'a-dashboard:dashboardDefault',
+            page: null,
+          },
+          user: {},
+        },
+      },
+    },
+  };
   const _app = {
     atomName: 'Base',
     atomStaticKey: 'appBase',
