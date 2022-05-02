b005ef150d92befa43237fd80f1d68391cc01896
zhennann <zhen.nann@icloud.com>
2022-05-02 17:42:16 +0800

    feat: a-appbooster

---
+++ src/module-system/a-appbooster/backend/src/config/validation/schemas.js
@@ -0,0 +1,4 @@
+module.exports = app => {
+  const schemas = {};
+  return schemas;
+};
---
+++ src/module-system/a-appbooster/front/src/main.js
@@ -0,0 +1,23 @@
+let Vue;
+
+import './assets/css/module.less';
+
+// install
+function install(_Vue, cb) {
+  if (Vue) return console.error('already installed.');
+
+  Vue = _Vue;
+
+  return cb({
+    routes: require('./routes.js').default,
+    store: require('./store.js').default(Vue),
+    config: require('./config/config.js').default,
+    locales: require('./config/locales.js').default,
+    components: require('./components.js').default,
+  });
+}
+
+// export
+export default {
+  install,
+};
