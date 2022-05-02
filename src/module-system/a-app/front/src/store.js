5d94e4f45a9ef152ff40f7e59729ab520a05d083
zhennann <zhen.nann@icloud.com>
2022-05-02 21:16:03 +0800

    feat: getAppItem

--- src/module-system/a-app/front/src/store.js
+++ src/module-system/a-app/front/src/store.js
@@ -1,9 +1,30 @@
 // eslint-disable-next-line
 export default function (Vue) {
   return {
-    state: {},
+    state: {
+      // global
+      appItems: {},
+    },
     getters: {},
     mutations: {},
-    actions: {},
+    actions: {
+      setAppItem(state, { appKey, appItem }) {
+        state.appItems = {
+          ...state.appItems,
+          [appKey]: appItem,
+        };
+      },
+      async getAppItem({ state, commit }, { appKey }) {
+        let appItem = state.appItems[appKey];
+        if (appItem) return appItem;
+        appItem = await Vue.prototype.$meta.api.post('/a/base/resource/read', {
+          atomStaticKey: appKey,
+          options: { locale: false },
+        });
+        appItem.content = appItem.content ? JSON.parse(appItem.content) : null;
+        commit('setAppItem', { appKey, appItem });
+        return appItem;
+      },
+    },
   };
 }
