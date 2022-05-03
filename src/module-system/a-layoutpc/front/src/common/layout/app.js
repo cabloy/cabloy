4e40b4a0845be975fddb4e1f877d8957ee87d1ae
zhennann <zhen.nann@icloud.com>
2022-05-03 16:26:26 +0800

    feat: apps

--- src/module-system/a-layoutpc/front/src/common/layout/app.js
+++ src/module-system/a-layoutpc/front/src/common/layout/app.js
@@ -1,4 +1,9 @@
 export default {
+  data() {
+    return {
+      appKeyDefault: 'a-app:appDefault',
+    };
+  },
   methods: {
     async app_openHome() {
       await this.app_openAppMenu();
@@ -10,18 +15,33 @@ export default {
       const current = this.$store.getters['a/app/current'];
       const appItemCurrent = this.$store.getters['a/app/appItemCurrent'];
       // app default
-      if (current.appKey !== 'a-app:appDefault' && !appItemCurrent.isolate) {
-        const appItemDefault = await this.$store.dispatch('a/app/getAppItem', { appKey: 'a-app:appDefault' });
+      if (!this.app_isDefault(current.appKey) && !appItemCurrent.isolate) {
+        const appItemDefault = await this.$store.dispatch('a/app/getAppItem', { appKey: this.appKeyDefault });
         this.navigate('/a/app/appMenu', {
           scene: 'sidebar',
-          sceneOptions: { side: 'left', name: 'a-app:appDefault', title: appItemDefault.atomNameLocale },
+          sceneOptions: {
+            side: 'left',
+            name: this.appKeyDefault,
+            title: this.app_openAppMenu_panelTitle(this.appKeyDefault, appItemDefault),
+          },
         });
       }
       // app current
       this.navigate('/a/app/appMenu?1', {
         scene: 'sidebar',
-        sceneOptions: { side: 'left', name: current.appKey, title: appItemCurrent.atomNameLocale },
+        sceneOptions: {
+          side: 'left',
+          name: current.appKey,
+          title: this.app_openAppMenu_panelTitle(current.appKey, appItemCurrent),
+        },
       });
+    },
+    app_isDefault(appKey) {
+      return appKey === this.appKeyDefault;
+    },
+    app_openAppMenu_panelTitle(appKey, appItem) {
+      if (this.app_isDefault(appKey)) return null; // this.$text('Apps');
+      return appItem.atomNameLocale;
     },
     async app_openAppHome() {},
   },
