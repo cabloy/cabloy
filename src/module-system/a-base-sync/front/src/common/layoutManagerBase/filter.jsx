08eecd9bf0cd5168dcdc4c989dd54f034ad0bbb0
zhennann <zhen.nann@icloud.com>
2022-05-05 20:50:04 +0800

    feat: filter

--- src/module-system/a-base-sync/front/src/common/layoutManagerBase/filter.jsx
+++ src/module-system/a-base-sync/front/src/common/layoutManagerBase/filter.jsx
@@ -25,79 +25,8 @@ export default {
       // ok
       return _schema;
     },
-
     filter_prepareSelectParams() {
-      if (!this.filter.data) return null;
-      // options
-      const options = {
-        where: {},
-      };
-      // params
-      const params = {
-        options,
-      };
-      // form
-      const form = this.filter.data.form;
-      if (form) {
-        // atomName、mine、stage、language、star、label、atomClass
-        // category、tag
-        const clause1 = this.$meta.util.combineSearchClause({
-          ctx: this,
-          schema: this.filter.data.schemaBasic,
-          data: form,
-          searchStates: this.filter.data.searchStatesBasic,
-        });
-        const clause2 = this.$meta.util.combineSearchClause({
-          ctx: this,
-          schema: this.filter.data.schemaGeneral,
-          data: form,
-          searchStates: this.filter.data.searchStatesGeneral,
-        });
-        const clause = Object.assign({}, clause1, clause2);
-        // atomClass
-        if (clause.atomClass) {
-          params.atomClass = clause.atomClass.val;
-          delete clause.atomClass;
-        }
-        // atomName、createdAt
-        if (clause.__or__atomNameResource) {
-          options.where.__or__atomNameResource = clause.__or__atomNameResource;
-          delete clause.__or__atomNameResource;
-        }
-        if (clause.__and__createdAt) {
-          options.where.__and__createdAt = clause.__and__createdAt;
-          delete clause.__and__createdAt;
-        }
-        // mine、stage、language、star、label、atomClass
-        for (const key in clause) {
-          options[key] = clause[key].val;
-        }
-        // category、tag
-        if (form.category) {
-          options.category = form.category;
-        }
-        if (form.tag) {
-          options.tag = form.tag;
-        }
-      }
-      // formAtomClass
-      const formAtomClass = this.filter.data.formAtomClass;
-      if (formAtomClass) {
-        const clause = this.$meta.util.combineSearchClause({
-          ctx: this,
-          schema: this.filter.data.schemaSearch,
-          data: formAtomClass,
-          searchStates: this.filter.data.searchStatesSearch,
-        });
-        if (clause) {
-          Object.assign(options.where, clause);
-          if (Object.keys(clause).length > 0) {
-            options.mode = 'search';
-          }
-        }
-      }
-      // ok
-      return params;
+      return this.filter_onCombineSelectParams();
     },
     filter_getSchemaSearch() {
       return this.$meta.util.getProperty(this.filter, 'data.schemaSearch');
--- src/module-system/a-basefront/front/src/common/listLayoutManager/filter.jsx
+++ src/module-system/a-basefront/front/src/common/listLayoutManager/filter.jsx
@@ -69,7 +69,7 @@ export default {
         schema: null,
       });
     },
-    filter_prepareSelectParams() {
+    filter_onCombineSelectParams() {
       if (!this.filter.data) return null;
       // options
       const options = {
@@ -141,52 +141,6 @@ export default {
       }
       // ok
       return params;
-    },
-    filter_getSchemaSearch() {
-      return this.$meta.util.getProperty(this.filter, 'data.schemaSearch');
-    },
-    filter_getSchemaSearchProperty({ schema, key }) {
-      return this.$meta.util.getProperty(schema, `properties.${key}`);
-    },
-    filter_getConfig() {
-      return this.$meta.util.getProperty(this.layout.configFull, 'info.filter');
-    },
-    filter_openTab(tabName) {
-      this.filter.tabNameCurrent = tabName;
-      this.filter_onPerform();
-    },
-    filter_onPerform() {
-      const inPanel = this.$view.inPanel();
-      const immediate = this.$meta.vueApp.layout === 'pc' && !inPanel;
-      const filterConfig = this.filter_getConfig();
-      const actionPath = filterConfig.actionPath;
-      const navigateOptions = {
-        context: {
-          params: {
-            layoutManager: this,
-            filterConfig,
-            immediate,
-          },
-        },
-      };
-      if (immediate) {
-        navigateOptions.scene = 'sidebar';
-        navigateOptions.sceneOptions = { side: 'right', name: 'filter', title: 'Filter' };
-      } else {
-        navigateOptions.target = '_self';
-      }
-      this.$view.navigate(`${actionPath}?t=${Date.now()}`, navigateOptions);
-    },
-    filter_onChanged(value) {
-      this.filter.data = value;
-      // reload
-      this.page_onRefresh();
-    },
-    filter_renderAction() {
-      if (this.container.params && this.container.params.disableFilter === true) return null;
-      return (
-        <eb-link iconF7="::search" tooltip={this.$text('Filter')} propsOnPerform={this.filter_onPerform}></eb-link>
-      );
     },
   },
 };
