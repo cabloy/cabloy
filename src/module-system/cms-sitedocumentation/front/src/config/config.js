8cd68be2b33353fd3cc73b7923fada5c5ad1ac4d
zhennann <zhen.nann@icloud.com>
2022-05-06 11:13:34 +0800

    feat: document layout

--- src/module-system/cms-sitedocumentation/front/src/config/config.js
+++ src/module-system/cms-sitedocumentation/front/src/config/config.js
@@ -1,13 +1,1 @@
-import configDocumentRenderList from './config/configDocumentRenderList.js';
-import configDocumentRenderItem from './config/configDocumentRenderItem.js';
-
-export default {
-  atoms: {
-    document: {
-      render: {
-        list: configDocumentRenderList,
-        item: configDocumentRenderItem,
-      },
-    },
-  },
-};
+export default {};
--- src/module-system/cms-sitedocumentation/front/src/config/config/configDocumentRenderList.js
+++
@@ -1,2 +0,0 @@
-const list = {};
-export default list;
