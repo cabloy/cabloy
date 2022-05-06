f44ca1af51171cb649c1f4c99be41f94c9392075
zhennann <zhen.nann@icloud.com>
2022-05-06 11:52:42 +0800

    feat: module layout

--- src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/meta.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/meta.js
@@ -2,11 +2,15 @@ module.exports = app => {
   // schemas
   const schemas = require('./config/validation/schemas.js')(app);
   // static
+  const staticLayouts = require('./config/static/layouts.js')(app);
   const staticResources = require('./config/static/resources.js')(app);
   const meta = {
     base: {
       atoms: {},
       statics: {
+        'a-baselayout.layout': {
+          items: staticLayouts,
+        },
         'a-base.resource': {
           items: staticResources,
         },
