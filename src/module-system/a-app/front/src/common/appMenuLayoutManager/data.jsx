7a945cc963c6a65e792ecac57a2db1c1147cc633
zhennann <zhen.nann@icloud.com>
2022-05-10 20:27:30 +0800

    feat: data_provider_onLoadItemsAll

--- src/module-system/a-app/front/src/common/appMenuLayoutManager/data.jsx
+++ src/module-system/a-app/front/src/common/appMenuLayoutManager/data.jsx
@@ -14,11 +14,5 @@ export default {
       const list = await this.$store.dispatch('a/app/getAppItemsAll');
       return { list };
     },
-    // appmenu
-    async data_provider_onLoadItemsPage({ page }) {
-      const params = this.base_prepareSelectParams();
-      params.options.page = page;
-      return await this.$api.post('/a/socketio/message/select', params);
-    },
   },
 };
