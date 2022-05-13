6537d100b9fcdad55f359cd05a24b11ab6a79549
zhennann <zhen.nann@icloud.com>
2022-05-13 21:14:12 +0800

    feat: appListLayoutBlockBoxGrid9Items

--- src/module-system/a-app/front/src/components/app/appListLayoutBoxGrid9/appListLayoutBlockBoxGrid9Items.jsx
+++ src/module-system/a-app/front/src/components/app/appListLayoutBoxGrid9/appListLayoutBlockBoxGrid9Items.jsx
@@ -1,33 +1,45 @@
+// export
 export default {
-  data() {
-    return {
-      supportSingle: true,
-    };
-  },
-  methods: {
-    async init_categoriesAll() {
-      this.categoryTree = await this.$store.dispatch('a/base/getCategories', {
-        atomClass: this.layoutManager.base_atomClassApp,
-      });
-    },
-    onItemClick(event, item) {
-      const appKey = item.atomStaticKey;
-      this.layoutManager.$meta.vueLayout.app_openHome({ appKey, force: false });
-    },
-    _renderGroup(group) {
-      const children = [];
-      for (const item of group.items) {
-        const domItem = (
-          <eb-link key={item.atomId} class="box-grid-cell" propsOnPerform={event => this.onItemClick(event, item)}>
-            <div class="box-grid-cell-icon">
-              <f7-icon f7={item.appIcon} size="24"></f7-icon>
-            </div>
-            <div class="box-grid-cell-label">{item.atomNameLocale}</div>
-          </eb-link>
-        );
-        children.push(domItem);
-      }
-      return <div class="eb-box-grid-row">{children}</div>;
-    },
-  },
+  installFactory,
 };
+
+// installFactory
+function installFactory(_Vue) {
+  const Vue = _Vue;
+  const ebAppMenuLayoutBlockListItemsBase =
+    Vue.prototype.$meta.module.get('a-app').options.mixins.ebAppMenuLayoutBlockListItemsBase;
+  return {
+    mixins: [ebAppMenuLayoutBlockListItemsBase],
+    data() {
+      return {
+        supportSingle: true,
+      };
+    },
+    methods: {
+      async init_categoriesAll() {
+        this.categoryTree = await this.$store.dispatch('a/base/getCategories', {
+          atomClass: this.layoutManager.base_atomClassApp,
+        });
+      },
+      onItemClick(event, item) {
+        const appKey = item.atomStaticKey;
+        this.layoutManager.$meta.vueLayout.app_openHome({ appKey, force: false });
+      },
+      _renderGroup(group) {
+        const children = [];
+        for (const item of group.items) {
+          const domItem = (
+            <eb-link key={item.atomId} class="box-grid-cell" propsOnPerform={event => this.onItemClick(event, item)}>
+              <div class="box-grid-cell-icon">
+                <f7-icon f7={item.appIcon} size="24"></f7-icon>
+              </div>
+              <div class="box-grid-cell-label">{item.atomNameLocale}</div>
+            </eb-link>
+          );
+          children.push(domItem);
+        }
+        return <div class="eb-box-grid-row">{children}</div>;
+      },
+    },
+  };
+}
