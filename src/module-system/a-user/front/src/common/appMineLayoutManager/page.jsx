b5b056992a35a776fbf624a872655263cffc205f
zhennann <zhen.nann@icloud.com>
2022-05-15 21:56:18 +0800

    feat: page_renderAppCurrent

--- src/module-system/a-user/front/src/common/appMineLayoutManager/page.jsx
+++ src/module-system/a-user/front/src/common/appMineLayoutManager/page.jsx
@@ -7,7 +7,26 @@ export default {
       return this.$text('Mine');
     },
     page_onGetTitleSub() {
-      return '';
+      return this.page_renderAppCurrent();
+    },
+    page_renderAppCurrent() {
+      const appKey = this.base.appCurrent.appKey;
+      const appItem = this.base.appCurrent.appItem;
+      if (!appItem) return '';
+      if (this.base_isAppDefault(appKey) || appItem.isolate) return '';
+      return (
+        <f7-chip
+          iconF7={appItem.appIcon}
+          iconSize="16"
+          class="eb-app-mine-title-sub eb-cursor-pointer"
+          nativeOnClick={this.page_onClickAppCurrent}
+        >
+          {appItem.atomNameLocale}
+        </f7-chip>
+      );
+    },
+    page_onClickAppCurrent(event) {
+      console.log(event);
     },
   },
 };
