739a4b2064964b6e6ad5beb1cb843f9f0466c4ea
zhennann <zhen.nann@icloud.com>
2022-05-16 21:29:05 +0800

    feat: page_onGetTitle

--- src/module-system/a-app/front/src/common/appMenuLayoutManager/page.jsx
+++ src/module-system/a-app/front/src/common/appMenuLayoutManager/page.jsx
@@ -10,9 +10,14 @@ export default {
       return false;
     },
     page_onGetTitle() {
+      // default title
+      const inPanel = this.$view.inPanel();
+      const titleDefault = inPanel ? this.$text('Apps') : this.$text('Home');
+      // app title
       const __appKeyDefault = this.$config.appKey.default;
-      if (this.container.appKey === __appKeyDefault) return this.$text('Apps');
-      if (!this.base.appItem) return '';
+      if (this.container.appKey === __appKeyDefault) return titleDefault;
+      if (!this.base.appItem) return titleDefault; // default
+      if (this.base.appItem.isolate) return titleDefault;
       return this.base.appItem.atomNameLocale;
     },
     page_onGetTitleSub() {
