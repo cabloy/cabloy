5eff14e6de15fe718312963c2a3861d40838dbee
zhennann <zhen.nann@icloud.com>
2022-05-19 10:19:27 +0800

    feat: appCmsInnerTest

---
+++ src/module-system/a-cms/backend/src/config/static/app/appCmsInnerTest.js
@@ -0,0 +1,49 @@
+module.exports = app => {
+  // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  const info = {
+    home: {
+      mode: 'page',
+      page: '/a/basefront/atom/list?module=a-cms&atomClassName=article',
+    },
+  };
+  const info2 = {
+    'en-us': {
+      ...info,
+      menu: {
+        layout: null,
+      },
+    },
+    'zh-cn': {
+      ...info,
+      menu: {
+        layout: 'a-app:layoutAppMenuDefault',
+      },
+    },
+  };
+  const content = {
+    presets: {
+      anonymous: {
+        mobile: info,
+        pc: info,
+      },
+      authenticated: {
+        mobile: info2,
+        pc: info2,
+      },
+    },
+  };
+  const _app = {
+    atomName: 'CMS(Inner Test)',
+    atomStaticKey: 'appCmsInnerTest',
+    atomRevision: 0,
+    atomCategoryId: 'General',
+    description: '',
+    appIcon: ':outline:article-outline',
+    appIsolate: false,
+    appLanguage: true,
+    content: JSON.stringify(content),
+    resourceRoles: 'root',
+    appSorting: 0,
+  };
+  return _app;
+};
