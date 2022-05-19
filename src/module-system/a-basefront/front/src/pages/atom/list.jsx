54507d300a6aeadf3ace57f7b4038fac932d5f30
zhennann <zhen.nann@icloud.com>
2022-05-19 09:58:05 +0800

    feat: atom list appLanguage

--- src/module-system/a-basefront/front/src/pages/atom/list.jsx
+++ src/module-system/a-basefront/front/src/pages/atom/list.jsx
@@ -6,11 +6,15 @@ export default {
     const module = query && query.module;
     const atomClassName = query && query.atomClassName;
     const atomClass = module && atomClassName ? { module, atomClassName } : null;
-    const options = query && query.options ? JSON.parse(query.options) : null;
+    const options = query && query.options ? JSON.parse(query.options) : {};
     const params = query && query.params ? JSON.parse(query.params) : null;
     const scene = query && query.scene;
     const layout = query && query.layout;
     const resource = query && query.resource;
+    // appLanguage
+    if (query.appLanguage) {
+      options.language = query.appLanguage;
+    }
     return {
       container: {
         atomClass,
