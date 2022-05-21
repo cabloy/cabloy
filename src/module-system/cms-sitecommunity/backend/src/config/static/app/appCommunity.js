c299a7f8ecb740434e268937e2fc12554d1cfaa0
zhennann <zhen.nann@icloud.com>
2022-05-21 17:11:04 +0800

    feat: post/document

--- src/module-system/cms-sitecommunity/backend/src/config/static/app/appCommunity.js
+++ src/module-system/cms-sitecommunity/backend/src/config/static/app/appCommunity.js
@@ -1,15 +1,27 @@
 module.exports = app => {
   // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  const info = {
+    home: {
+      mode: 'page',
+      page: '/a/basefront/atom/list?module=cms-sitecommunity&atomClassName=post',
+    },
+  };
   const content = {
     presets: {
-      anonymous: {},
-      authenticated: {},
+      anonymous: {
+        mobile: info,
+        pc: info,
+      },
+      authenticated: {
+        mobile: info,
+        pc: info,
+      },
     },
   };
   const _app = {
     atomName: 'Community',
     atomStaticKey: 'appCommunity',
-    atomRevision: 1,
+    atomRevision: 2,
     atomCategoryId: 'General',
     description: '',
     appIcon: ':outline:article-outline',
