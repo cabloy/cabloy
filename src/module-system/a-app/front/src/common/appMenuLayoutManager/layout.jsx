7166818f5157d8d3bf92e581001a1c780e321599
zhennann <zhen.nann@icloud.com>
2022-05-21 17:31:35 +0800

    feat: configAppMenuCmsBase

--- src/module-system/a-app/front/src/common/appMenuLayoutManager/layout.jsx
+++ src/module-system/a-app/front/src/common/appMenuLayoutManager/layout.jsx
@@ -10,6 +10,7 @@ export default {
       return `appMenu.${appKey}.render.list.layout.current.${this.$view.size}`;
     },
     async layout_onPrepareConfigFull() {
+      const appItem = this.base.appInfoCurrent.appItem;
       // configAppMenuBase
       if (!this.base.configAppMenuBase) {
         const layoutItem = await this.$store.dispatch('a/baselayout/getLayoutItem', {
@@ -17,8 +18,16 @@ export default {
         });
         this.base.configAppMenuBase = layoutItem.content;
       }
+      // configAppMenuCmsBase
+      if (appItem.appCms && !this.base.configAppMenuCmsBase) {
+        const layoutItem = await this.$store.dispatch('a/baselayout/getLayoutItem', {
+          layoutKey: 'a-cms:layoutAppMenuCmsBase',
+        });
+        this.base.configAppMenuCmsBase = layoutItem.content;
+      }
       // combine
-      return this.$meta.util.extend({}, this.base.configAppMenuBase, this.base.configAppMenu);
+      const configAppMenuCmsBase = appItem.appCms ? this.base.configAppMenuCmsBase : null;
+      return this.$meta.util.extend({}, this.base.configAppMenuBase, configAppMenuCmsBase, this.base.configAppMenu);
     },
   },
 };
