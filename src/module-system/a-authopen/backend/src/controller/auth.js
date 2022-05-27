3121dc72e7ec3229a1fc992043cbd69b37fe3522
zhennann <zhen.nann@icloud.com>
2022-05-27 10:02:39 +0800

    feat: checkDemo

--- src/module-system/a-authopen/backend/src/controller/auth.js
+++ src/module-system/a-authopen/backend/src/controller/auth.js
@@ -1,6 +1,8 @@
 module.exports = app => {
   class AuthController extends app.Controller {
     async signin() {
+      // check demo
+      this.ctx.bean.util.checkDemo();
       // data: { clientID, clientSecret }
       const res = await this.ctx.service.auth.signin({
         data: this.ctx.request.body.data,
--- src/module-system/a-authopen/backend/src/controller/authOpen.js
+++ src/module-system/a-authopen/backend/src/controller/authOpen.js
@@ -1,6 +1,8 @@
 module.exports = app => {
   class AuthOpenController extends app.Controller {
     async hideClientSecret() {
+      // check demo
+      // this.ctx.bean.util.checkDemo();
       const res = await this.ctx.service.authOpen.hideClientSecret({
         key: this.ctx.request.body.key,
         user: this.ctx.state.user.op,
@@ -9,6 +11,8 @@ module.exports = app => {
     }
 
     async resetClientSecret() {
+      // check demo
+      this.ctx.bean.util.checkDemo();
       const res = await this.ctx.service.authOpen.resetClientSecret({
         key: this.ctx.request.body.key,
         user: this.ctx.state.user.op,
