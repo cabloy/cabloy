167f90054111d84b51b4140f9c46dc5c4a18a4ef
zhennann <zhen.nann@icloud.com>
2022-05-29 17:30:07 +0800

    feat: sync store

--- src/module-system/a-clibooster/backend/src/bean/cli.store.sync.js
+++ src/module-system/a-clibooster/backend/src/bean/cli.store.sync.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const require3 = require('require3');
 const AdmZip = require3('adm-zip');
 const shajs = require3('sha.js');
@@ -23,22 +25,76 @@ module.exports = ctx => {
         // not found
         return { code: 1001 };
       }
-      console.log(entityStatus);
-      return;
-      // need official/trial
-      const needOfficial = entityStatus.entity.moduleLicenseFull !== 0;
-      const needTrial = entityStatus.entity.moduleLicenseTrial !== 0;
+      // prepare licenseMeta
+      let licenseMeta;
+      if (entityStatus.licenseFull.download) {
+        licenseMeta = entityStatus.licenseFull;
+      }
+      if (!licenseMeta && entityStatus.licenseTrial.download) {
+        licenseMeta = entityStatus.licenseTrial;
+      }
+      if (!licenseMeta) {
+        licenseMeta = entityStatus.licenseFull;
+      }
+      // handle
+      const res = await this._onExecuteStoreCommandEntity_handle({ entityName, entityStatus, licenseMeta });
+    }
+
+    async _onExecuteStoreCommandEntity_handle({ entityName, entityStatus, licenseMeta }) {
+      // entityVersion
+      const entityVersion = entityStatus.entity.moduleVersion;
+      // entityMeta
+      const entityMeta = this._getEntityMeta({ entityName, entityStatus });
+      // check version
+      if (entityMeta.version && !semver.lt(entityMeta.version, entityVersion)) {
+        // No Changes Found
+        return { code: 2001 };
+      }
+      // check if has download
+      if (!licenseMeta.download) {
+        return licenseMeta;
+      }
+      // download
+      const res = await ctx.curl(licenseMeta.download, { method: 'GET' });
+      if (res.status !== 200) {
+        throw new Error(res.res.statusMessage);
+      }
+      console.log(res.data);
+      throw new Error('ssss');
+      // unzip
+      // copy
       // suite/module
       if (entityStatus.entity.entityTypeCode === 1) {
-        return await this._publishSuite({ suiteName: entityName, entityStatus, entityHash, needOfficial, needTrial });
+        return await this._syncSuite({ suiteName: entityName, entityStatus, entityVersion });
       }
-      return await this._publishModuleIsolate({
-        moduleName: entityName,
-        entityStatus,
-        entityHash,
-        needOfficial,
-        needTrial,
-      });
+      return await this._syncModuleIsolate({ moduleName: entityName, entityStatus, entityVersion });
+    }
+
+    async _syncSuite({ suiteName, entityStatus, entityVersion }) {
+      // Synced
+      return { code: 3000, args: [entityVersion] };
+    }
+
+    _getEntityType({ entityStatus }) {
+      return entityStatus.entity.entityTypeCode;
+    }
+
+    _getEntityMeta({ entityName, entityStatus }) {
+      const { argv } = this.context;
+      // entityMeta
+      const entityType = this._getEntityType({ entityStatus });
+      const entityMeta = {
+        root: path.join(argv.projectPath, entityType === 1 ? 'src/suite-vendor' : 'src/module-vendor', entityName),
+      };
+      // version
+      entityMeta.version = this._getEntityVersion(entityMeta.root);
+      return entityMeta;
+    }
+    _getEntityVersion(entityPath) {
+      const filePkg = path.join(entityPath, 'package.json');
+      if (!fs.existsSync(filePkg)) return null;
+      const _package = require(filePkg);
+      return _package.version;
     }
   }
 
