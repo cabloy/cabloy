5aa6165cbcd08b6e10e4c5bca21452d0438098cb
zhennann <zhen.nann@icloud.com>
2022-05-30 22:31:52 +0800

    feat: remove egg-born-utils

--- packages/egg-born-utils/README.md
+++
@@ -1,1 +0,0 @@
-# `egg-born-utils`
--- packages/egg-born-utils/lib/egg-born-utils.js
+++
@@ -1,9 +0,0 @@
-const OpenAuthClient = require('./common/openAuthClient.js');
-const openAuthConfig = require('./common/openAuthConfig.js');
-const tools = require('./common/tools.js');
-
-module.exports = {
-  OpenAuthClient,
-  openAuthConfig,
-  tools,
-};
