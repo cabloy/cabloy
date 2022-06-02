1b94b2c5cad845098eef0f749a4207e8afb4b1d9
zhennann <zhen.nann@icloud.com>
2022-06-02 14:53:17 +0800

    Publish
     - cabloy@4.21.16
     - egg-born-module-a-cli@4.0.5
     - egg-born-module-a-clibooster@4.0.7

--- src/module-system/a-clibooster/dist/backend.js
+++ src/module-system/a-clibooster/dist/backend.js
@@ -737,7 +737,7 @@ module.exports = ctx => {
 
     async _handleScripts_gitCommit({ entityMeta }) {
       // git add .
-      await this.helper.spawnCmd({
+      await this.helper.spawnExe({
         cmd: 'git',
         args: ['add', '.'],
         options: {
@@ -745,7 +745,7 @@ module.exports = ctx => {
         },
       });
       // git commit
-      await this.helper.spawnCmd({
+      await this.helper.spawnExe({
         cmd: 'git',
         args: ['commit', '-m', `'chore: version ${entityMeta.package.version}'`],
         options: {
@@ -753,7 +753,7 @@ module.exports = ctx => {
         },
       });
       // git push
-      await this.helper.spawnCmd({
+      await this.helper.spawnExe({
         cmd: 'git',
         args: ['push'],
         options: {
