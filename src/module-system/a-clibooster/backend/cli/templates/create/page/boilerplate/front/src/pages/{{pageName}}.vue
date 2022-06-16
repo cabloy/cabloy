cffdad7e2ce5b5ef67205907dcb85a2f0b365cc2
zhennann <zhen.nann@icloud.com>
2022-06-16 11:14:51 +0800

    feat: create page

---
+++ src/module-system/a-clibooster/backend/cli/templates/create/page/boilerplate/front/src/pages/{{pageName}}.vue
@@ -0,0 +1,12 @@
+module.exports = app => {
+  class <%=argv.controllerNameCapitalize%>Controller extends app.Controller {
+    async action() {
+      const res = await this.ctx.service.<%=argv.controllerName%>.action({
+        user: this.ctx.state.user.op,
+      });
+      this.ctx.success(res);
+    }
+  }
+
+  return <%=argv.controllerNameCapitalize%>Controller;
+};
---
+++ src/module-system/a-clibooster/backend/cli/templates/create/page/snippets/routes.js
@@ -0,0 +1,17 @@
+const __snippet = `// <%=argv.controllerName%>
+{ method: 'post', path: '<%=argv.controllerName%>/action', controller: '<%=argv.controllerName%>' },`;
+
+module.exports = {
+  file: 'backend/src/routes.js',
+  async transform({ cli, ast, argv, ctx }) {
+    // code
+    const code = await cli.template.renderContent({ content: __snippet });
+    if (!ast.has(`const routes = [$_$]`)) {
+      ast.replace(`const routes = []`, `const routes = [${code}]`);
+    } else {
+      ast.replace(`const routes = [$_$]`, `const routes = [$_$, \n ${code}]`);
+    }
+    // ok
+    return ast;
+  },
+};
