83b301880ffe03960f1a76c721ecb2b0d5d70b92
zhennann <zhen.nann@icloud.com>
2022-06-29 10:22:50 +0800

    feat: navbar_findViewInfo

--- src/module-system/a-layoutpc/front/src/common/layout/navbar.js
+++ src/module-system/a-layoutpc/front/src/common/layout/navbar.js
@@ -1,13 +1,32 @@
 export default {
   methods: {
+    navbar_findViewDom(ctx) {
+      const $el = ctx.$$(ctx.$el);
+      return $el.parents('.eb-layout-view');
+    },
+    navbar_findViewInfo(ctx) {
+      const $view = this.navbar_findViewDom(ctx);
+      const groupId = $view.parents('.eb-layout-group').data('groupId');
+      const viewId = $view.attr('id');
+      const viewPopup = $view.is('.eb-layout-popup-view');
+      const viewTile = $view.is('.eb-layout-group-view');
+      const viewIndex = parseInt($view.data('index'));
+      return {
+        $view,
+        groupId,
+        viewId,
+        viewPopup,
+        viewTile,
+        viewIndex,
+      };
+    },
     backLink(ctx) {
       let backLink = false;
       const routeDiff = this.$meta.util.viewRouterDid(ctx.$view) ? 2 : 1;
       if (!this.$meta.util.historyUrlEmpty(ctx.$f7router.history[ctx.$f7router.history.length - routeDiff])) {
         backLink = true;
       } else {
-        const $el = ctx.$$(ctx.$el);
-        const $view = $el.parents('.eb-layout-view');
+        const $view = this.navbar_findViewDom(ctx);
         if ($view.is('.eb-layout-group-view') && parseInt($view.data('index')) > 0) {
           backLink = true;
         }
@@ -15,9 +34,12 @@ export default {
       return backLink;
     },
     closeLink(ctx) {
-      return true;
+      const $view = this.navbar_findViewDom(ctx);
+      return $view.is('.eb-layout-popup-view');
     },
     sizeLink(ctx) {
+      const viewInfo = this.navbar_findViewInfo(ctx);
+      console.log(viewInfo);
       return 'restore';
     },
     onCloseClick(ctx) {
