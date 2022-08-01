74903774d731ec144302e20e6b00368002d84c29
zhennann <zhen.nann@icloud.com>
2022-08-01 10:16:42 +0800

    refactor: local.version

--- src/module-system/a-version/backend/src/bean/startup.databaseInit.js
+++ src/module-system/a-version/backend/src/bean/startup.databaseInit.js
@@ -1,9 +1,9 @@
 module.exports = app => {
-  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Startup extends app.meta.BeanBase {
     async execute() {
-      const bean = this.ctx.bean._getBean(`${moduleInfo.relativeName}.local.version`);
-      return await bean.databaseInitStartup();
+      const beanVersion = this.ctx.bean.local.version;
+      return await beanVersion.databaseInitStartup();
     }
   }
 
--- src/module-system/a-version/backend/src/bean/startup.instanceInit.js
+++ src/module-system/a-version/backend/src/bean/startup.instanceInit.js
@@ -1,10 +1,10 @@
 module.exports = app => {
-  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Startup extends app.meta.BeanBase {
     async execute(context) {
       const options = context.options;
-      const bean = this.ctx.bean._getBean(`${moduleInfo.relativeName}.local.version`);
-      return await bean.instanceInitStartup({ options });
+      const beanVersion = this.ctx.bean.local.version;
+      return await beanVersion.instanceInitStartup({ options });
     }
   }
 
