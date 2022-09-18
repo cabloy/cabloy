05914643d768a3c021f2e93ade5013b80f01ac65
zhennann <zhen.nann@icloud.com>
2022-09-18 22:34:43 +0800

    feat: renderTableCellImage

--- src/module-system/a-basefront/front/src/components/render/renderTableCellImage.jsx
+++ src/module-system/a-basefront/front/src/components/render/renderTableCellImage.jsx
@@ -18,27 +18,65 @@ export default {
     size: {
       type: Object,
     },
+    target: {
+      type: String,
+    },
   },
   data() {
     return {};
   },
   created() {},
   methods: {
+    onClickImage() {
+      // target
+      const target = this.target === undefined ? '_popup' : this.target;
+      // url
+      const url = this._getImageUrl(false);
+      // photos
+      const photos = [
+        {
+          url,
+          // caption: title,
+        },
+      ];
+      // pageContext
+      const pageContext = {
+        params: {
+          title: this.$text('View'),
+          photos,
+        },
+      };
+      // navigate
+      this.$view.navigate(`/a/photobrowser/photoBrowser?t=${Date.now()}`, {
+        target,
+        context: pageContext,
+      });
+    },
+    _getImageUrl(fixSize) {
+      let url = this.info.text;
+      if (!url) return null;
+      if (fixSize) {
+        const height = this.size && this.size.height;
+        const width = this.size && this.size.width;
+        url = this.$meta.util.combineImageUrl(url, width, height);
+      }
+      return url;
+    },
     _renderPicture() {
-      // text
-      const text = this.info.text;
-      if (!text) return null;
+      // url
+      const url = this._getImageUrl(true);
+      if (!url) return null;
       // size
       const height = this.size && this.size.height;
       const width = this.size && this.size.width;
       // style
-      const style = {};
+      const style = {
+        cursor: 'pointer',
+      };
       if (height) style.height = height + 'px';
       if (width) style.width = width + 'px';
-      // url
-      const url = this.$meta.util.combineImageUrl(text, width, height);
       // dom
-      return <img src={url} style={style} />;
+      return <img src={url} style={style} onClick={this.onClickImage} />;
     },
   },
   render() {
