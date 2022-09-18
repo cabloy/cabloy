4400d494060acfbaebd7770ae4f4202ba2c27422
zhennann <zhen.nann@icloud.com>
2022-09-18 21:26:32 +0800

    feat: photoBrowser

--- src/module-system/a-photobrowser/front/src/pages/photoBrowser.jsx
+++ src/module-system/a-photobrowser/front/src/pages/photoBrowser.jsx
@@ -1,23 +1,48 @@
+import Vue from 'vue';
+const ebPageContext = Vue.prototype.$meta.module.get('a-components').options.mixins.ebPageContext;
+
 export default {
+  mixins: [ebPageContext],
   data() {
     return {};
   },
+  computed: {
+    title() {
+      return this.contextParams.title;
+    },
+    photos() {
+      return this.contextParams.photos;
+    },
+  },
   methods: {
-    async onPerformDone() {
-      await this.$view.dialog.confirm();
-      return true;
+    _renderPhotos() {
+      if (!this.photos) return null;
+      const domSlides = [];
+      for (const photo of this.photos) {
+        domSlides.push(
+          <f7-swiper-slide key={photo.url}>
+            <div class="caption">{photo.caption}</div>
+            <img class="photo" src={photo.url} />
+          </f7-swiper-slide>
+        );
+      }
+      const params = {
+        pagination: { clickable: true },
+      };
+      const single = this.photos.length === 1;
+      // framework7 UI swiper
+      return (
+        <f7-swiper class="dian-store-banners" pagination={!single} navigation={!single} params={params}>
+          {domSlides}
+        </f7-swiper>
+      );
     },
   },
   render() {
     return (
       <eb-page>
-        <eb-navbar title={this.$text('Demo')} eb-back-link="Back">
-          <f7-nav-right>
-            <eb-link iconF7="::done" ref="buttonDone" propsOnPerform={this.onPerformDone}></eb-link>
-          </f7-nav-right>
-        </eb-navbar>
-        <f7-block-title medium></f7-block-title>
-        <f7-block strong></f7-block>
+        <eb-navbar title={this.title} eb-back-link="Back"></eb-navbar>
+        {this._renderPhotos()}
       </eb-page>
     );
   },
