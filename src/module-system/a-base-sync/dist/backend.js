dbceb6d3ed69fa47ee0ade46229d92a0179afbc8
zhennann <zhen.nann@icloud.com>
2022-09-27 11:56:15 +0800

    chore: update

--- src/module-system/a-base-sync/dist/backend.js
+++ src/module-system/a-base-sync/dist/backend.js
@@ -6274,6 +6274,7 @@ module.exports = ctx => {
     }
 
     async list({ atomClass, options }) {
+      options = options || {};
       atomClass = await ctx.bean.atomClass.get(atomClass);
       if (!options.where) options.where = {};
       options.where.atomClassId = atomClass.id;
