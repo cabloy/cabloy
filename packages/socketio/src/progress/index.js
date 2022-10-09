059885f48626dc994801244871e8da82a0743e23
zhennann <zhen.nann@icloud.com>
2022-10-09 20:00:06 +0800

    feat: progress message checking

--- packages/socketio/src/progress/index.js
+++ packages/socketio/src/progress/index.js
@@ -19,6 +19,16 @@ export default (io, options) => {
       this.ioSimple = simpleFn(io, options);
     };
     this.subscribe = function ({ progressId, options }) {
+      this.progressId = progressId;
+      this.counter = 0;
+      this.queue = [];
+      this.queueHandler = debounce(() => {
+        for (const queueItem of this.queue) {
+          this._checking(queueItem.item);
+        }
+        this.queue = [];
+      }, this.delayTimeout);
+      //
       const path = `/a/progress/update/${progressId}`;
       this.ioSimple.subscribe({ path, options });
     };
@@ -26,18 +36,51 @@ export default (io, options) => {
     this.unsubscribe = function () {
       this.ioSimple.unsubscribe();
     };
-
+    this._queuePush = function (item) {
+      if (!item) return;
+      this.counter = item.counter;
+      const queueItem = { item, timestamp: Date.now() };
+      // push
+      const index = this.queue.findIndex(_queueItem => _queueItem.item.counter > item.counter);
+      if (index === -1) {
+        this.queue.push(queueItem);
+      } else {
+        this.queue.splice(index, 0, queueItem);
+      }
+      // check directly
+      while (this.queue[0] && this.queue[0].timestamp + 2 * this.delayTimeout < queueItem.timestamp) {
+        const queueItem = this.queue.shift();
+        this._checking(queueItem.item);
+      }
+      // queueHandler
+      this.queueHandler();
+    };
+    this._checking = function (item) {
+      // data
+      const data = item.data ? (typeof item.data === 'string' ? JSON.parse(item.data) : item.data) : {};
+      this.onProgress({ data });
+    };
     this._onSubscribedDefault = async function (...args) {
       // custom
       if (this.onSubscribed) {
         const res = await this.onSubscribed(...args);
         if (res) return res;
       }
+      // progress check
+      const item = await io.performAction({
+        url: '/a/progress/progress/check',
+        body: {
+          progressId: this.progressId,
+          counter: this.counter,
+        },
+      });
+      this._queuePush(item);
       // true
       return true;
     };
-
-    this._onMessagePushDefault = function () {};
+    this._onMessagePushDefault = function ({ message }) {
+      this._queuePush(message.content);
+    };
   };
   const progress = new Progress();
   progress.initialize();
