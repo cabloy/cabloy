467958ddeb4c84a15bbee68c4529139335440c3c
zhennann <zhen.nann@icloud.com>
2022-10-10 11:01:13 +0800

    feat: io test

--- packages/socketio/src/io/io_test.js
+++ packages/socketio/src/io/io_test.js
@@ -1,7 +1,7 @@
 export default () => {
   const io = {
-    // __test: false,
-    __test: true,
+    __test: false,
+    // __test: true,
     __testPerformAction(i) {
       for (const method of ['get', 'post']) {
         this.performAction({ method, url: `/a/socketio/test/echo?method=${method}`, body: { echo: i } })
