7aec6035462c70fce715647a829a83c030a9ba03
zhennann <zhen.nann@icloud.com>
2022-10-10 18:47:04 +0800

    feat: socketio

--- packages/socketio/src/main.js
+++
@@ -1,9 +0,0 @@
-import ioFn from './io/index.js';
-import simpleFn from './simple/index.js';
-import progressFn from './progress/index.js';
-
-export default {
-  io: ioFn,
-  simple: simpleFn,
-  progress: progressFn,
-};
