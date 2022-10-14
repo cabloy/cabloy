88ff70d2a44ab4d9f24e7a41ecca913c21173476
zhennann <zhen.nann@icloud.com>
2022-10-14 17:23:26 +0800

    feat: cnpm-sync

---
+++ scripts/cnpm-sync.js
@@ -0,0 +1,180 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const Bagpipe = require('bagpipe');
+const fs = require('fs');
+const querystring = require('querystring');
+const request = require('npm-request');
+const argv = require('./lib/parse_argv')('sync');
+
+const args = argv.args;
+const registrys = [argv.registry];
+const registrywebs = {};
+registrywebs[argv.registry] = argv.registryweb;
+
+const nodeps = !argv.deps;
+const publish = !!argv.syncPublish;
+
+const names = args.slice(1);
+let packageName;
+let dependencies = [];
+let isPrivate = false;
+
+const packagePath = path.join(process.cwd(), 'packages/cabloy/package.json');
+if (!names.length && fs.existsSync(packagePath)) {
+  try {
+    console.log('Parse `package.json` file now...');
+    const pkg = require(packagePath);
+    packageName = pkg.name;
+    const dependenciesMap = {};
+    for (const key in pkg.dependencies || {}) {
+      dependenciesMap[key] = pkg.dependencies[key];
+    }
+    for (const key in pkg.devDependencies || {}) {
+      dependenciesMap[key] = pkg.devDependencies[key];
+    }
+    dependencies = Object.keys(dependenciesMap);
+    isPrivate = !!pkg.private;
+  } catch (err) {
+    console.log('Parse `package.json` file error: %s', err.message);
+    process.exit(1);
+  }
+}
+
+if (names && names.length) {
+  syncByNames(names);
+} else if (packageName) {
+  syncByPackage(packageName);
+} else {
+  console.log(`Usage: $ cnpm sync [moduleName1 moduleName2 ...]
+Options:
+  --publish        sync as publish
+  --no-deps        do not sync dependencies and devDependencies`);
+  process.exit(1);
+}
+
+function showlog(registry, syncInfo, done) {
+  request(
+    {
+      method: 'GET',
+      path: syncInfo.logurl + '?offset=' + syncInfo.lastLines,
+    },
+    {
+      registry,
+      configFile: argv.userconfig,
+    },
+    function (err, info) {
+      if (err) {
+        return done(err);
+      }
+      if (!info || !info.log) {
+        return setTimeout(showlog.bind(null, registry, syncInfo, done), 2000);
+      }
+      const log = info.log.trim();
+      console.log(log);
+      syncInfo.lastLines += log.split('\n').length;
+      if (log.indexOf('[done] Sync ' + syncInfo.name) >= 0) {
+        done();
+      } else {
+        setTimeout(showlog.bind(null, registry, syncInfo, done), 2000);
+      }
+    }
+  );
+}
+
+function sync(registry, name, callback) {
+  let url = name + '/sync?';
+  url += querystring.stringify({
+    publish,
+    nodeps,
+  });
+  console.log('sync %s, PUT %s/%s', name, registry, url);
+  request(
+    {
+      method: 'PUT',
+      path: url,
+      data: {},
+    },
+    {
+      registry,
+      configFile: argv.userconfig,
+    },
+    function (err, result, data, res) {
+      if (err) {
+        return callback(err);
+      }
+      if (res.statusCode === 404 || !result || !result.ok) {
+        if (result.reason) {
+          console.error('[%s] %s: %s', res.statusCode, result.error, result.reason);
+        }
+        return callback(null, {
+          ok: false,
+          statusCode: res.statusCode,
+          result,
+          data,
+        });
+      }
+      const syncInfo = {
+        name,
+        lastLines: 0,
+        logurl: name + '/sync/log/' + result.logId,
+      };
+      console.log('logurl: %s/sync/%s#logid=%s', registrywebs[registry], name, result.logId);
+      showlog(registry, syncInfo, function (err) {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, { ok: true });
+      });
+    }
+  );
+}
+
+function syncByNames(names) {
+  const queue = new Bagpipe(1);
+  let remain = names.length * registrys.length;
+  if (!names) {
+    console.log('Can not find any packages to sync.');
+    process.exit(0);
+  }
+  console.log('Start sync %j.', names);
+  const fail = {};
+  const success = {};
+  registrys.forEach(function (registry) {
+    names.forEach(function (name) {
+      queue.push(sync, registry, name, function (err, data) {
+        remain--;
+        if (err) {
+          console.error(err.message);
+          fail[name] = true;
+        } else if (!data.ok) {
+          fail[name] = true;
+        } else {
+          success[name] = true;
+        }
+        if (!remain) {
+          for (const n in success) {
+            if (fail[n]) {
+              delete success[n];
+            }
+          }
+          console.log('Sync all packages done, successed: %j, failed: %j', Object.keys(success), Object.keys(fail));
+          process.exit(0);
+        }
+      });
+    });
+  });
+}
+
+function syncByPackage(packageName) {
+  if (isPrivate) {
+    return syncByNames(dependencies);
+  }
+
+  if (argv.yes) {
+    dependencies.unshift(packageName);
+    return syncByNames(dependencies);
+  }
+
+  syncByNames([packageName]);
+}
---
+++ scripts/lib/parse_argv.js
@@ -0,0 +1,150 @@
+const os = require('os');
+const fs = require('fs');
+const program = require('commander');
+const config = require('./config');
+const pkg = require('../package.json');
+const help = require('./help');
+
+let argv = null;
+
+module.exports = cmd => {
+  if (!argv) {
+    argv = program
+      .option('-v, --version', 'show full versions')
+      .option('-r, --registry [registry]', 'registry url, default is ' + config.cnpmRegistry)
+      .option('-w, --registryweb [registryweb]', 'web url, default is ' + config.cnpmHost)
+      .option('--disturl [disturl]', 'dist url for node-gyp, default is ' + config.disturl)
+      .option('-c, --cache [cache]', 'cache folder, default is ' + config.cache)
+      .option('-u, --userconfig [userconfig]', 'userconfig file, default is ' + config.userconfig)
+      .option('-y, --yes', 'yes all confirm')
+      .option('--ignore-custom-config', 'ignore custom .cnpmrc')
+      .option('--proxy [proxy]', 'set a http proxy, no default');
+  }
+
+  if (cmd === 'doc') {
+    argv.option('-g, --git', '[doc options] open git url');
+  } else if (cmd === 'sync') {
+    argv
+      .option('--sync-publish', '[sync options] sync as publish')
+      .option('--no-deps', '[sync options] do not sync dependencies and devDependencies');
+  }
+
+  // commander's bug, fix here
+  // https://github.com/visionmedia/commander.js/pull/189
+  let cacheInfo;
+  argv.on('cache', function (cache) {
+    if (typeof cache === 'string') {
+      cacheInfo = cache;
+      return;
+    }
+    argv.args = ['cache'].concat(cache || []);
+  });
+
+  argv.on('version', function () {
+    console.log(
+      'cnpm@%s (%s)%snpm@%s (%s)%snode@%s (%s)%snpminstall@%s (%s)%sprefix=%s %s%s %s %s %sregistry=%s',
+      pkg.version,
+      __filename,
+      os.EOL,
+      require('npm/package.json').version,
+      require.resolve('npm'),
+      os.EOL,
+      process.version.substring(1),
+      process.execPath,
+      os.EOL,
+      require('npminstall/package.json').version,
+      require.resolve('npminstall'),
+      os.EOL,
+      config.prefix,
+      os.EOL,
+      os.platform(),
+      os.arch(),
+      os.release(),
+      os.EOL,
+      config.cnpmRegistry
+    );
+    process.exit(0);
+  });
+
+  // custom help message
+  // output command help, default options help info will output by default
+  argv.on('--help', function () {
+    if (!argv.registry) {
+      if (argv.ignoreCustomConfig) {
+        argv.userconfig = 'none';
+      } else {
+        argv.userconfig = argv.userconfig || config.userconfig;
+      }
+      argv.registry = getDefaultRegistry(argv.userconfig);
+    }
+    help(argv);
+  });
+  argv.parse(process.argv.slice());
+
+  if (argv.ignoreCustomConfig) {
+    argv.userconfig = 'none';
+  }
+
+  argv.userconfig = argv.userconfig || config.userconfig;
+  if (!argv.registry) {
+    // try to use registry in uerconfig
+    argv.registry = getDefaultRegistry(argv.userconfig);
+  }
+  if (!argv.disturl) {
+    const isIOJS = process.execPath.indexOf('iojs') >= 0;
+    argv.disturl = isIOJS ? config.iojsDisturl : config.disturl;
+  }
+  if (!argv.proxy) {
+    argv.proxy = config.proxy;
+  }
+  if (argv.disturl === 'none') {
+    delete argv.disturl;
+  }
+  if (argv.userconfig === 'none') {
+    delete argv.userconfig;
+  }
+  argv.registryweb = argv.registryweb || config.cnpmHost;
+  argv.cache = cacheInfo || config.cache;
+
+  if (!argv.args.length) {
+    help(argv);
+  }
+
+  // filter rawArgs
+  const rawArgs = argv.rawArgs;
+  const needs = [];
+  for (let i = 0; i < rawArgs.length; i++) {
+    const arg = rawArgs[i];
+    if (arg.indexOf('--userconfig=') === 0 || arg.indexOf('-u=') === 0) {
+      continue;
+    }
+    if (arg === '--ignore-custom-config') {
+      continue;
+    }
+    if (arg.indexOf('--disturl=') === 0) {
+      continue;
+    }
+    if (arg.indexOf('--registryweb=') === 0 || arg.indexOf('-w=') === 0) {
+      continue;
+    }
+    if (arg.indexOf('--registry=') === 0 || arg.indexOf('-r=') === 0) {
+      continue;
+    }
+    needs.push(arg);
+  }
+  argv.rawArgs = needs;
+
+  return argv;
+};
+
+function getDefaultRegistry(userconfig) {
+  if (userconfig !== 'none' && fs.existsSync(userconfig)) {
+    const content = fs.readFileSync(userconfig, 'utf8');
+    // registry = {registry-url}
+    const m = /^registry\s*=\s*(.+)$/m.exec(content);
+    if (m) {
+      return m[1];
+    }
+  }
+  return config.cnpmRegistry;
+}
