c48a248ed56de54061c7c7b977a27b0110ed934b
zhennann <zhen.nann@icloud.com>
2022-10-15 20:58:43 +0800

    feat: device.host

--- packages/egg-born-front/src/inject/patch/device.js
+++ packages/egg-born-front/src/inject/patch/device.js
@@ -1,12 +1,24 @@
 export default function (Vue) {
   // device
   const device = Vue.prototype.$device;
+  // host
+  device.host = deviceHost();
+  device.embed = Object.keys(device.host).length > 0;
+}
+
+function deviceHost() {
+  const host = {};
   // ua
   const ua = navigator.userAgent.toLowerCase() || '';
   // wxwork
-  device.wxwork = ua.indexOf('wxwork') > -1;
+  const wxwork = ua.indexOf('wxwork') > -1;
+  if (wxwork) host.wxwork = true;
   // wechat
-  device.wechat = !device.wxwork && (ua.indexOf('micromessenger') > -1 || ua.indexOf('micromessage') > -1);
+  const wechat = !wxwork && (ua.indexOf('micromessenger') > -1 || ua.indexOf('micromessage') > -1);
+  if (wechat) host.wechat = true;
   // dingtalk
-  device.dingtalk = ua.indexOf('dingtalk') > -1;
+  const dingtalk = ua.indexOf('dingtalk') > -1;
+  if (dingtalk) host.dingtalk = true;
+  // ok
+  return host;
 }
