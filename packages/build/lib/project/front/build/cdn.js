fe883aff8a1e8702e5e3cfd3261549d41ff020fe
zhennann <zhen.nann@icloud.com>
2022-10-28 11:38:23 +0800

    feat: webpack oss

--- packages/build/lib/project/front/build/cdn.js
+++ packages/build/lib/project/front/build/cdn.js
@@ -4,6 +4,9 @@ module.exports = () => {
       if (cdn && cdn.cos) {
         return this._prepare_cos({ options: cdn.cos });
       }
+      if (cdn && cdn.alioss) {
+        return this._prepare_alioss({ options: cdn.alioss });
+      }
     },
     _prepare_cos({ options }) {
       const COSPlugin = require('webpack5-cos-plugin');
@@ -20,6 +23,23 @@ module.exports = () => {
       const assetsPublicPath =
         options.assetsPublicPath ||
         `https://${options.bucket.Bucket}.cos.${options.bucket.Region}.myqcloud.com/${options.cosBaseDir}/${options.project}/`;
+      return { plugin, assetsPublicPath };
+    },
+    _prepare_alioss({ options }) {
+      const OSSPlugin = require('webpack-alioss-plugin');
+      options = Object.assign(
+        {},
+        {
+          gzip: true,
+          existCheck: true,
+          removeMode: false,
+        },
+        options
+      );
+      const plugin = new OSSPlugin(options);
+      const assetsPublicPath =
+        options.assetsPublicPath ||
+        `https://${options.auth.bucket}.${options.auth.region}.aliyuncs.com/${options.ossBaseDir}/${options.project}/`;
       return { plugin, assetsPublicPath };
     },
   };
