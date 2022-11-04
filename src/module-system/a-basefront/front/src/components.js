3c5345d6a123b5b9fc56b46d093efb08882d36ad
zhennann <zhen.nann@icloud.com>
2022-11-04 10:46:19 +0800

    feat: move render

--- src/module-system/a-basefront/front/src/components.js
+++ src/module-system/a-basefront/front/src/components.js
@@ -1,22 +1,4 @@
 import combineSearch from './components/action/combineSearch.js';
-import componentAction from './components/render/componentAction.jsx';
-import renderAtom from './components/render/renderAtom.jsx';
-import renderAtomClass from './components/render/renderAtomClass.jsx';
-import renderLanguage from './components/render/renderLanguage.jsx';
-import renderCategory from './components/render/renderCategory.jsx';
-import renderCategoryResource from './components/render/renderCategoryResource.jsx';
-import renderTags from './components/render/renderTags.jsx';
-import renderResourceType from './components/render/renderResourceType.jsx';
-import renderTableCellDefault from './components/render/renderTableCellDefault.jsx';
-import renderTableCellComputed from './components/render/renderTableCellComputed.jsx';
-import renderTableCellDatetime from './components/render/renderTableCellDatetime.jsx';
-import renderTableCellLink from './components/render/renderTableCellLink.jsx';
-import renderTableCellButton from './components/render/renderTableCellButton.jsx';
-import renderTableCellImage from './components/render/renderTableCellImage.jsx';
-import renderUserLabel from './components/render/renderUserLabel.jsx';
-import renderUser from './components/render/renderUser.jsx';
-import renderRole from './components/render/renderRole.jsx';
-import renderMarkdown from './components/render/renderMarkdown.jsx';
 // category
 import categorySelect from './components/category/categorySelect.jsx';
 // tag
@@ -34,24 +16,6 @@ import developerTool from './components/tools/developerTool.js';
 
 export default {
   combineSearch,
-  componentAction,
-  renderAtom,
-  renderAtomClass,
-  renderLanguage,
-  renderCategory,
-  renderCategoryResource,
-  renderTags,
-  renderResourceType,
-  renderTableCellDefault,
-  renderTableCellComputed,
-  renderTableCellDatetime,
-  renderTableCellLink,
-  renderTableCellButton,
-  renderTableCellImage,
-  renderUserLabel,
-  renderUser,
-  renderRole,
-  renderMarkdown,
   // category
   categorySelect,
   // tag
