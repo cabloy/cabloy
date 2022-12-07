8caaf73601c17e57f5964efc2d677fc61fd0bd59
zhennann <zhen.nann@icloud.com>
2022-12-07 15:44:43 +0800

    feat: atomAreaKey/atomAreaValue

--- src/module-system/a-base-sync/backend/src/bean/version/update20.js
+++ src/module-system/a-base-sync/backend/src/bean/version/update20.js
@@ -1,7 +1,15 @@
 module.exports = function (ctx) {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class VersionUpdate20 {
-    async run() {}
+    async run() {
+      // aAtom: add atomAreaKey atomAreaValue
+      const sql = `
+        ALTER TABLE aAtom
+          ADD COLUMN atomAreaKey varchar(255) DEFAULT NULL,
+          ADD COLUMN atomAreaValue varchar(255) DEFAULT NULL
+      `;
+      await ctx.model.query(sql);
+    }
   }
 
   return VersionUpdate20;
