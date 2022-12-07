88a92c21ee10b88a220669aaf537854778dc4431
zhennann <zhen.nann@icloud.com>
2022-12-07 16:55:57 +0800

    feat: _alterViews_aRoleRightRef

--- src/module-system/a-base-sync/backend/src/bean/version/update20.js
+++ src/module-system/a-base-sync/backend/src/bean/version/update20.js
@@ -2,7 +2,7 @@ module.exports = function (ctx) {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class VersionUpdate20 {
     async run() {
-      throw new Error('正在加入新功能，暂时不可用');
+      throw new Error('正在加入新功能，源码模式暂时不可用。请使用项目模式来创建Cabloy项目');
       await this._alterTables();
       await this._alterViews_aRoleRight();
       await this._alterViews_aRoleRightRef();
