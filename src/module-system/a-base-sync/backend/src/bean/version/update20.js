11cfc80e17a03d61f462c26486c23a91d794770c
zhennann <zhen.nann@icloud.com>
2022-12-07 16:29:38 +0800

    feat: _alterTables

--- src/module-system/a-base-sync/backend/src/bean/version/update20.js
+++ src/module-system/a-base-sync/backend/src/bean/version/update20.js
@@ -2,11 +2,31 @@ module.exports = function (ctx) {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class VersionUpdate20 {
     async run() {
+      await this._alterTables();
+    }
+
+    async _alterTables() {
       // aAtom: add atomAreaKey atomAreaValue
-      const sql = `
+      let sql = `
         ALTER TABLE aAtom
           ADD COLUMN atomAreaKey varchar(255) DEFAULT NULL,
           ADD COLUMN atomAreaValue varchar(255) DEFAULT NULL
+      `;
+      await ctx.model.query(sql);
+
+      // aRoleRight: add areaKey areaScope
+      sql = `
+        ALTER TABLE aRoleRight
+          ADD COLUMN areaKey varchar(255) DEFAULT NULL,
+          ADD COLUMN areaScope varchar(255) DEFAULT NULL
+      `;
+      await ctx.model.query(sql);
+
+      // aRoleRightRef: add areaKey areaScope
+      sql = `
+        ALTER TABLE aRoleRightRef
+          ADD COLUMN areaKey varchar(255) DEFAULT NULL,
+          ADD COLUMN areaScope varchar(255) DEFAULT NULL
       `;
       await ctx.model.query(sql);
     }
