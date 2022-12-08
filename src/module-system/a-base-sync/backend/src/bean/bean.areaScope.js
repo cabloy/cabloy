2e350b3c25be141088b5fcb37bc4a6bf00d4aaf9
zhennann <zhen.nann@icloud.com>
2022-12-08 22:40:46 +0800

    feat: adjustKeyAndValue

--- src/module-system/a-base-sync/backend/src/bean/bean.areaScope.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.areaScope.js
@@ -1,6 +1,32 @@
 module.exports = ctx => {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class AreaScope {
+    async adjustKeyAndValue({ atomAreaKey, atomAreaValue }) {
+      // atomAreaValue
+      if (atomAreaValue === null || atomAreaValue === undefined || atomAreaValue === '') {
+        atomAreaValue = null;
+      } else if (Array.isArray(atomAreaValue)) {
+        if (
+          atomAreaValue.length === 0 ||
+          atomAreaValue.some(item => item === null || item === undefined || item === '')
+        ) {
+          atomAreaValue = null;
+        } else {
+          atomAreaValue = atomAreaValue.join('|');
+        }
+      }
+      // atomAreaKey
+      if (!atomAreaValue) {
+        atomAreaKey = null;
+      } else if (!atomAreaKey) {
+        atomAreaKey = null;
+      } else if (Array.isArray(atomAreaKey)) {
+        atomAreaKey = atomAreaKey.join('|');
+      }
+      // ok
+      return { atomAreaKey, atomAreaValue };
+    }
+
     getAreaScopeMeta({ atomClass, escape = true }) {
       if (!atomClass) return null;
       let atomClassBase = ctx.bean.base.atomClass(atomClass);
--- src/module-system/a-base-sync/backend/src/bean/bean.atom/bean.atom_areaScope.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.atom/bean.atom_areaScope.js
@@ -1,28 +1,5 @@
 module.exports = ctx => {
   class Atom {
-    async adjustKeyAndValue({ atomAreaKey, atomAreaValue }) {
-      // atomAreaValue
-      if (atomAreaValue === null || atomAreaValue === undefined || atomAreaValue === '') {
-        atomAreaValue = null;
-      } else if (Array.isArray(atomAreaValue)) {
-        if (
-          atomAreaValue.length === 0 ||
-          atomAreaValue.some(item => item === null || item === undefined || item === '')
-        ) {
-          atomAreaValue = null;
-        } else {
-          atomAreaValue = atomAreaValue.join('|');
-        }
-      }
-      // atomAreaKey
-      if (!atomAreaValue) {
-        atomAreaKey = null;
-      } else if (Array.isArray(atomAreaKey)) {
-        atomAreaKey = atomAreaKey.join('|');
-      }
-      // ok
-      return { atomAreaKey, atomAreaValue };
-    }
     async setAreaScopeValue({ atomId, atomClass, atomAreaValue }) {
       // meta
       const areaScopeMeta = ctx.bean.areaScope.getAreaScopeMeta({ atomClass, escape: true });
@@ -32,7 +9,7 @@ module.exports = ctx => {
       }
       // atomAreaKey
       let atomAreaKey = Object.keys(areaScopeMeta.schemas);
-      const adjustRes = this.adjustKeyAndValue({ atomAreaKey, atomAreaValue });
+      const adjustRes = ctx.bean.areaScope.adjustKeyAndValue({ atomAreaKey, atomAreaValue });
       atomAreaKey = adjustRes.atomAreaKey;
       atomAreaValue = adjustRes.atomAreaValue;
       // update
