3bb621d103692f5e128ef48724e0173be9650245
zhennann <zhen.nann@icloud.com>
2022-12-08 21:43:00 +0800

    feat: getAreaScopeMeta

--- src/module-system/a-base-sync/backend/src/bean/bean.areaScope.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.areaScope.js
@@ -2,7 +2,14 @@ module.exports = ctx => {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class AreaScope {
     getAreaScopeMeta({ atomClass, escape = true }) {
-      if (!atomClass) return false;
+      if (!atomClass) return null;
+      let atomClassBase = ctx.bean.base.atomClass(atomClass);
+      while (true) {
+        const meta = atomClassBase.areaScope;
+        if (!meta) return null;
+        if (!meta.sameAs || !escape) return meta;
+        atomClassBase = ctx.bean.base.atomClass(meta.sameAs);
+      }
     }
   }
   return AreaScope;
