af19ab8b7486671373586ed576d2bd96dca43de7
zhennann <zhen.nann@icloud.com>
2022-12-08 21:58:53 +0800

    feat: setAreaScopeValue

--- src/module-system/a-base-sync/backend/src/bean/bean.atom/bean.atom_areaScope.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.atom/bean.atom_areaScope.js
@@ -1,11 +1,23 @@
 module.exports = ctx => {
   class Atom {
     async setAreaScopeValue({ atomId, atomClass, atomAreaValue }) {
-      const atomClassBase = ctx.bean.base.atomClass(atomClass);
-      if (!atomClassBase.areaScope) return;
-
-      // adjust
-      // if(!atomAreaKey || !atomAreaValue
+      // atomAreaValue
+      if (!atomAreaValue) return;
+      if (Array.isArray(atomAreaValue)) {
+        if (
+          atomAreaValue.length === 0 ||
+          atomAreaValue.some(item => item === null || item === undefined || item === '')
+        ) {
+          return;
+        }
+        atomAreaValue = atomAreaValue.join('|');
+      }
+      // meta
+      const areaScopeMeta = ctx.bean.areaScope.getAreaScopeMeta({ atomClass, escape: true });
+      if (!areaScopeMeta) return;
+      // atomAreaKey
+      const atomAreaKey = Object.keys(areaScopeMeta.schemas).join('|');
+      // update
       await this.modelAtom.update({
         id: atomId,
         atomAreaKey,
