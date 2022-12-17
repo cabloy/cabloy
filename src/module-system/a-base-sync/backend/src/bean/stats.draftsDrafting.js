44183ccd6bcb3857a387c246120988641ae03ea7
zhennann <zhen.nann@icloud.com>
2022-12-17 19:43:58 +0800

    feat: statsDrafts

--- src/module-system/a-base-sync/backend/src/bean/stats.draftsDrafting.js
+++ src/module-system/a-base-sync/backend/src/bean/stats.draftsDrafting.js
@@ -2,14 +2,26 @@ module.exports = ctx => {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Stats {
     async execute(context) {
-      const { user } = context;
-      const modelAtom = ctx.model.module(moduleInfo.relativeName).atom;
-      const count = await modelAtom.count({
+      const { keys, user } = context;
+      let atomClass;
+      if (keys.length > 1) {
+        const [module, atomClassName] = keys[1].split('_');
+        atomClass = await ctx.bean.atomClass.get({
+          module,
+          atomClassName,
+        });
+      }
+      const params = {
         userIdUpdated: user.id,
         atomStage: 0,
         atomClosed: 0,
         atomFlowId: 0,
-      });
+      };
+      if (atomClass) {
+        params.atomClassId = atomClass.id;
+      }
+      const modelAtom = ctx.model.module(moduleInfo.relativeName).atom;
+      const count = await modelAtom.count(params);
       return count;
     }
   }
