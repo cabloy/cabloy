90d349ead2cc98fdbcd7f8e642b543a1b22cc365
zhennann <zhen.nann@icloud.com>
2022-12-23 10:02:39 +0800

    feat: app read

--- src/module-system/a-app/backend/src/service/resource.js
+++ src/module-system/a-app/backend/src/service/resource.js
@@ -1,8 +1,10 @@
 module.exports = app => {
   class Resource extends app.Service {
     async read({ atomStaticKey, options, user }) {
-      // donot check user access right
-      return await this.ctx.bean.resource.readByStaticKey({ atomStaticKey, options /* , user*/ });
+      // donot check user access right, but must check atomClass
+      const appItem = await this.ctx.bean.resource.readByStaticKey({ atomStaticKey, options /* , user*/ });
+      if (appItem.module !== 'a-app' || appItem.atomClassName !== 'app') this.ctx.throw(403);
+      return appItem;
     }
   }
 
