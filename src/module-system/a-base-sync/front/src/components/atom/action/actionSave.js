756c2c1cfee3e1a331bf42dd4f597eb0b4ac03ed
zhennann <zhen.nann@icloud.com>
2022-12-27 16:03:27 +0800

    feat: saveDraftOnly

--- src/module-system/a-base-sync/front/src/components/atom/action/actionSave.js
+++ src/module-system/a-base-sync/front/src/components/atom/action/actionSave.js
@@ -7,9 +7,13 @@ export default {
       if (ctx.layout && ctx.layout.instanceExtend && ctx.layout.instanceExtend.onActionSaveBefore) {
         await ctx.layout.instanceExtend.onActionSaveBefore(this.$props);
       }
+      // options
+      const options = {
+        saveDraftOnly: item.atomStage === 0,
+      };
       // write
       const key = { atomId: item.atomId, itemId: item.itemId };
-      await ctx.$api.post('/a/base/atom/write', { key, item });
+      await ctx.$api.post('/a/base/atom/write', { key, item, options });
       ctx.$meta.eventHub.$emit('atom:action', { key, action, actionSource: ctx });
       // toast
       return ctx.$text('Saved');
