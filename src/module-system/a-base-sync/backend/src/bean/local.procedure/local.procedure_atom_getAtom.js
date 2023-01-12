f637f9316318295c9d12f29293f3b3eaa0957886
zhennann <zhen.nann@icloud.com>
2023-01-12 11:01:46 +0800

    feat: add atomState

--- src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atom_getAtom.js
+++ src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atom_getAtom.js
@@ -87,7 +87,7 @@ module.exports = ctx => {
       const _sql = `select ${_itemField}
                 a.id as atomId,a.itemId,a.atomStage,a.atomFlowId,a.atomClosed,a.atomIdDraft,a.atomIdFormal,a.roleIdOwner,a.atomClassId,a.atomName,
                 a.atomStatic,a.atomStaticKey,a.atomRevision,a.atomLanguage,a.atomCategoryId,j.categoryName as atomCategoryName,a.atomTags,
-                a.atomSimple,a.atomDisabled,a.atomAreaKey,a.atomAreaValue,
+                a.atomSimple,a.atomDisabled,a.atomAreaKey,a.atomAreaValue,a.atomState,
                 a.allowComment,a.starCount,a.commentCount,a.attachmentCount,a.readCount,a.userIdCreated,a.userIdUpdated,a.createdAt as atomCreatedAt,a.updatedAt as atomUpdatedAt,
                 b.module,b.atomClassName,b.atomClassIdParent,
                 ${_userField}
