c84d1b36b175caa41a8d44f25c28899bbda0f0cf
zhennann <zhen.nann@icloud.com>
2023-01-16 20:58:19 +0800

    feat: add fields of aAtomAction: actionMode/flowKey/nodeDefId

--- src/module-system/a-base-sync/backend/src/bean/version/update21.js
+++ src/module-system/a-base-sync/backend/src/bean/version/update21.js
@@ -7,9 +7,18 @@ module.exports = function (ctx) {
 
     async _alterTables() {
       // aAtom: add atomState, default value is null
-      const sql = `
+      let sql = `
         ALTER TABLE aAtom
           ADD COLUMN atomState varchar(255) DEFAULT NULL
+      `;
+      await ctx.model.query(sql);
+      // aAtomAction: for workflow
+      //   actionMode: 0/default 1/workflow
+      sql = `
+        ALTER TABLE aAtomAction
+          ADD COLUMN actionMode int(11) DEFAULT '0',
+          ADD COLUMN flowKey varchar(50) DEFAULT NULL,
+          ADD COLUMN nodeDefId varchar(50) DEFAULT NULL
       `;
       await ctx.model.query(sql);
     }
