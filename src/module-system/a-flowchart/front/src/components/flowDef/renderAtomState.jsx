f29be720e55cb989bbb0341dacee886f506a44bc
zhennann <zhen.nann@icloud.com>
2023-01-31 22:50:23 +0800

    feat: getAtomClassAndStage

--- src/module-system/a-flowchart/front/src/components/flowDef/renderAtomState.jsx
+++ src/module-system/a-flowchart/front/src/components/flowDef/renderAtomState.jsx
@@ -8,8 +8,23 @@ export default {
     return {};
   },
   computed: {},
-  created() {},
-  methods: {},
+  created() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      const atomClassAndStage = await this.getAtomClassAndStage();
+      console.log(atomClassAndStage);
+    },
+    async getAtomClassAndStage() {
+      const action = {
+        name: 'getAtomClassAndStage',
+        actionModule: 'a-flowchart',
+        actionComponent: 'actionUserTask',
+      };
+      return await this.$meta.util.performAction({ ctx: this, action, item: this.context });
+    },
+  },
   render() {
     const { dataPath } = this.context;
     const title = this.context.getTitle();
