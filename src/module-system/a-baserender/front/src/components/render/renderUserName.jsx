c4e63369e2d46986575824ce57151591a91c50d8
zhennann <zhen.nann@icloud.com>
2023-02-08 11:06:09 +0800

    feat: renderUserName

--- src/module-system/a-baserender/front/src/components/render/renderUserName.jsx
+++ src/module-system/a-baserender/front/src/components/render/renderUserName.jsx
@@ -16,12 +16,17 @@ export default {
   render() {
     const { key } = this.context;
     const title = this.context.getTitle();
+    const value = parseInt(this.context.getValue() || 0);
     const userName = this.context.getValue(`_${key}Name`);
     const userAvatar = this.context.getValue(`_${key}Avatar`);
+    let domImg;
+    if (value) {
+      domImg = <img class="avatar avatar16" src={this.getAvatarUrl(userAvatar, 16)} />;
+    }
     return (
       <f7-list-item title={title}>
         <div slot="after" class="display-flex align-items-center">
-          <img class="avatar avatar16" src={this.getAvatarUrl(userAvatar, 16)} />
+          {domImg}
           <span>{userName}</span>
         </div>
       </f7-list-item>
