d01ae1f1cd4abf63d58e1c63a6abd7ed2c10ba44
zhennann <zhen.nann@icloud.com>
2023-02-10 09:50:34 +0800

    feat: menu mode of actionsBar

--- src/module-system/a-components-sync/front/src/components/actionsBar.jsx
+++ src/module-system/a-components-sync/front/src/components/actionsBar.jsx
@@ -56,11 +56,53 @@ export default {
         </div>
       );
     },
+    _render_menu_actions(direction, actions) {
+      if (!actions || actions.length === 0) return null;
+      const domActions = [];
+      for (const action of actions) {
+        domActions.push(
+          <eb-list-item key={action.key} link="#" popoverClose propsOnPerform={event => action.onPerform(event)}>
+            <f7-icon slot="media" color={action.icon.color} f7={action.icon.f7}></f7-icon>
+            {action.title}
+          </eb-list-item>
+        );
+      }
+      return domActions;
+    },
+    _render_menu() {
+      const ready = !!this.right;
+      let children = [];
+      const domLeft = this._render_menu_actions('left', this.left);
+      const domRight = this._render_menu_actions('right', this.right);
+      const domMore = this._render_menu_actions('more', this.more);
+      if (domLeft) {
+        children = children.concat(domLeft);
+      }
+      if (domLeft && (domRight || domMore)) {
+        children.push(<f7-list-item divider></f7-list-item>);
+      }
+      if (domRight) {
+        children = children.concat(domRight);
+      }
+      if (domRight && domMore) {
+        children.push(<f7-list-item divider></f7-list-item>);
+      }
+      if (domMore) {
+        children = children.concat(domMore);
+      }
+      return (
+        <eb-popover ready={ready}>
+          <f7-list inset>{children}</f7-list>
+        </eb-popover>
+      );
+    },
   },
   render() {
     const mode = this.getMode();
     if (mode === 'swipeout') {
       return this._render_swipeout();
+    } else if (mode === 'menu') {
+      return this._render_menu();
     }
     return <div>sssss</div>;
   },
