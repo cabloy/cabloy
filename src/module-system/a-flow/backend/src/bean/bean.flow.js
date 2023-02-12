a124a223eb8cdf37c2c83740471604adcb044b0d
zhennann <zhen.nann@icloud.com>
2023-02-12 10:14:20 +0800

    feat: flow_query

--- src/module-system/a-flow/backend/src/bean/bean.flow.js
+++ src/module-system/a-flow/backend/src/bean/bean.flow.js
@@ -1,10 +1,12 @@
 const flow_0 = require('./bean.flow/bean.flow_0.js');
+const flow_query = require('./bean.flow/bean.flow_query.js');
 
 module.exports = ctx => {
   return ctx.app.meta.util.mixinClasses(
     flow_0,
     [
       //
+      flow_query,
     ],
     ctx
   );
--- src/module-system/a-flow/backend/src/bean/bean.flow/bean.flow_0.js
+++ src/module-system/a-flow/backend/src/bean/bean.flow/bean.flow_0.js
@@ -228,56 +228,6 @@ module.exports = ctx => {
         return typeof item === 'object' ? item.name : item;
       });
     }
-
-    async count({ options, user }) {
-      return await this.select({ options, user, count: 1 });
-    }
-
-    async select({ options, user, pageForce = true, count = 0 }) {
-      const items = await this._list({ options, user, pageForce, count });
-      for (const item of items) {
-        if (item.flowNodeNameCurrent) {
-          item.flowNodeNameCurrentLocale = ctx.text(item.flowNodeNameCurrent);
-        }
-        if (item.flowRemark) {
-          item.flowRemarkLocale = ctx.text(item.flowRemark);
-        }
-      }
-      return items;
-    }
-
-    async get({ flowId, history, user }) {
-      const where = {};
-      if (history) {
-        where['a.flowId'] = flowId;
-      } else {
-        where['a.id'] = flowId;
-      }
-      const flows = await this.select({
-        options: {
-          where,
-          mode: history ? 'history' : 'flowing',
-        },
-        user,
-      });
-      return flows[0];
-    }
-
-    // mode: mine/others/flowing/history
-    async _list({ options: { where, orders, page, mode }, user, pageForce = true, count = 0 }) {
-      page = ctx.bean.util.page(page, pageForce);
-      const sql = this.sqlProcedure.selectFlows({
-        iid: ctx.instance.id,
-        userIdWho: user ? user.id : 0,
-        where,
-        orders,
-        page,
-        count,
-        mode,
-      });
-      const res = await ctx.model.query(sql);
-      return count ? res[0]._count : res;
-    }
   }
 
   return Flow;
