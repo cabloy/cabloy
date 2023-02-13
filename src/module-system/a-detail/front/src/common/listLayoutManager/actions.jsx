422c1df752bf3d11d2dc494914e7eec55e94a80f
zhennann <zhen.nann@icloud.com>
2023-02-13 11:54:12 +0800

    feat: actions_fetchActions

--- src/module-system/a-detail/front/src/common/listLayoutManager/actions.jsx
+++ src/module-system/a-detail/front/src/common/listLayoutManager/actions.jsx
@@ -10,18 +10,14 @@ export default {
     this.actions_fetchActions();
   },
   methods: {
-    actions_fetchActions() {
+    async actions_fetchActions() {
       if (this.actions.list) return;
-      this.$api
-        .post('/a/detail/detail/actions', {
-          flowTaskId: this.container.flowTaskId,
-          atomKey: { atomId: this.container.atomId },
-          detailClass: this.container.detailClass,
-          mode: this.container.mode,
-        })
-        .then(data => {
-          this.actions.list = data;
-        });
+      this.actions.list = await this.$api.post('/a/detail/detail/actions', {
+        flowTaskId: this.container.flowTaskId,
+        atomKey: { atomId: this.container.atomId },
+        detailClass: this.container.detailClass,
+        mode: this.container.mode,
+      });
     },
   },
 };
