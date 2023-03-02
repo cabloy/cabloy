5abd55f7d7bc3d2348f9150a6e956933e160191b
zhennann <zhen.nann@icloud.com>
2023-03-02 17:12:47 +0800

    feat: cli create app

---
+++ src/module-system/a-clibooster/backend/cli/templates/create/app/snippets/meta.js
@@ -0,0 +1,25 @@
+const __snippet_staticApps = `const staticApps = require('./config/static/apps.js')(app);`;
+const __snippet_staticsApp = `
+'a-app.app': {
+  items: staticApps,
+},`;
+
+module.exports = {
+  file: 'backend/src/meta.js',
+  async transform({ cli, ast, argv, ctx }) {
+    // staticApps
+    if (!ast.has(__snippet_staticApps)) {
+      // insert
+      let code = await cli.template.renderContent({ content: __snippet_staticApps });
+      ast.find('const meta = {$$$0}').before(code);
+      // apps
+      code = await cli.template.renderContent({ content: __snippet_staticsApp });
+      ast.replace(
+        `const meta = { base: { statics: {$$$0},$$$1 },$$$2 }`,
+        `const meta = { base: { statics: { ${code} \n $$$0},$$$1 },$$$2 }`
+      );
+    }
+    // ok
+    return ast;
+  },
+};
