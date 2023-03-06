2f087dc3afdc6a530c4d4bd4d1b40ff91ac7492c
zhennann <zhen.nann@icloud.com>
2023-03-06 16:29:40 +0800

    feat: cli create atom action

---
+++ src/module-system/a-clibooster/backend/cli/templates/create/atomAction/snippets/9-local.zh-cn.js
@@ -0,0 +1,12 @@
+const __snippet = `<%=argv.actionNameCapitalize%>: '<%=argv.actionNameCapitalize%>',`;
+
+module.exports = {
+  file: 'backend/src/config/locale/zh-cn.js',
+  async transform({ cli, ast, argv, ctx }) {
+    // code
+    const code = await cli.template.renderContent({ content: __snippet });
+    ast.replace(`module.exports = {$$$0}`, `module.exports = {$$$0, ${code}}`);
+    // ok
+    return ast;
+  },
+};
