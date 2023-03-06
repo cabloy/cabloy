286518069d6543f8bec6639c38cd88e6815a2a68
zhennann <zhen.nann@icloud.com>
2023-03-06 09:39:29 +0800

    feat: cli atom action

--- src/module-system/a-clibooster/backend/cli/templates/create/atomAction/snippets/1-meta.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/atomAction/snippets/1-meta.js
@@ -30,19 +30,6 @@ module.exports = {
       `{ base: { atoms: { ${argv.atomClassName}: {info:{$$$4}, actions:{$$$5}, $$$0 }, $$$1 }, $$$2 }, $$$3 }`,
       `{ base: { atoms: { ${argv.atomClassName}: {info:{$$$4}, actions:{${code} $$$5}, $$$0 }, $$$1 }, $$$2 }, $$$3 }`
     );
-    return ast;
-    // staticApps
-    if (!ast.has(__snippet_staticApps)) {
-      // insert
-      let code = await cli.template.renderContent({ content: __snippet_staticApps });
-      ast.find('const meta = {$$$0}').before(code);
-      // apps
-      code = await cli.template.renderContent({ content: __snippet_staticsApp });
-      ast.replace(
-        `const meta = { base: { statics: {$$$0},$$$1 },$$$2 }`,
-        `const meta = { base: { statics: { ${code} \n $$$0},$$$1 },$$$2 }`
-      );
-    }
     // ok
     return ast;
   },
