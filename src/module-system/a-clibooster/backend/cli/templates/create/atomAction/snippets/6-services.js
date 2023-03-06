adacf0b914e06ac23a493b74eaa95ef25e4b3d89
zhennann <zhen.nann@icloud.com>
2023-03-06 14:55:19 +0800

    feat: cli create atom action

---
+++ src/module-system/a-clibooster/backend/cli/templates/create/atomAction/snippets/6-services.js
@@ -0,0 +1,15 @@
+const __snippet_declare = `const <%=argv.atomClassName%> = require('./service/<%=argv.atomClassName%>.js');`;
+const __snippet_body = `<%=argv.atomClassName%>,`;
+
+module.exports = {
+  file: 'backend/src/services.js',
+  async transform({ cli, ast, argv, ctx }) {
+    // code
+    let code = await cli.template.renderContent({ content: __snippet_declare });
+    ast.before(code);
+    code = await cli.template.renderContent({ content: __snippet_body });
+    ast.replace(`const services = {$$$0}`, `const services = {${code} \n $$$0}`);
+    // ok
+    return ast;
+  },
+};
