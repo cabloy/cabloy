4e38795cb6780d619c9f1ed0ca6dab27a7faec62
zhennann <zhen.nann@icloud.com>
2023-03-11 17:54:19 +0800

    feat: cli release

--- packages/egg-born-bin/lib/cmd/release.js
+++ packages/egg-born-bin/lib/cmd/release.js
@@ -1,6 +1,6 @@
-// const path = require('path');
+const path = require('path');
 const chalk = require('chalk');
-// const fse = require('fs-extra');
+const fse = require('fs-extra');
 const Command = require('@zhennann/egg-bin').Command;
 const mglob = require('egg-born-mglob');
 
@@ -44,9 +44,38 @@ class ReleaseCommand extends Command {
     console.log(chalk.cyan(`  ${description} successfully!`));
   }
 
-  *__releaseSuite(entity, entityRepos) {}
+  *__releaseSuite(entity, entityRepos) {
+    const dirDest = this.__prepareDirectory(entity);
+  }
 
-  *__releaseModule(entity, entityRepos) {}
+  *__releaseModule(entity, entityRepos) {
+    const dirDest = this.__prepareDirectory(entity);
+  }
+
+  __prepareDirectory(entity) {
+    // dirSrc
+    const dirSrc = entity.root;
+    // dirDest
+    let entityPathName;
+    if (entity.modules) {
+      entityPathName = entity.package.name.substring('egg-born-suite-'.length);
+    } else {
+      entityPathName = entity.package.name.substring('egg-born-module-'.length);
+    }
+
+    const dirDest = path.join(
+      this.context.cwd,
+      'dist',
+      'release',
+      this.type || 'all',
+      entity.modules ? 'suite' : 'modue',
+      entityPathName
+    );
+    // clear dirDest
+    fse.emptyDirSync(dirDest);
+    // ok
+    return { dirSrc, dirDest };
+  }
 
   description() {
     return 'release';
