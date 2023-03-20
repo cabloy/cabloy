73798cbcc0048d27eadf2ccc65ed7aae0a7df90b
zhennann <zhen.nann@icloud.com>
2023-03-20 15:51:56 +0800

    feat: getCache

--- src/module-system/a-summer/backend/src/bean/local.redis.js
+++ src/module-system/a-summer/backend/src/bean/local.redis.js
@@ -1,5 +1,16 @@
 module.exports = ctx => {
-  class LocalRedis {}
+  class LocalRedis {
+    constructor() {
+      this._redisSummer = null;
+    }
+
+    get redisSummer() {
+      if (!this._redisSummer) {
+        this._redisSummer = ctx.app.redis.get('summer');
+      }
+      return this._redisSummer;
+    }
+  }
 
   return LocalRedis;
 };
