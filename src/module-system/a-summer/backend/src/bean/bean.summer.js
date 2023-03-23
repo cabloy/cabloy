9528f703382d4474dad675831757603fcc519118
zhennann <zhen.nann@icloud.com>
2023-03-23 16:18:14 +0800

    feat: summer

--- src/module-system/a-summer/backend/src/bean/bean.summer.js
+++ src/module-system/a-summer/backend/src/bean/bean.summer.js
@@ -16,6 +16,36 @@ module.exports = ctx => {
       });
     }
 
+    async get(cacheName, key, options) {
+      const cache = this.getCache(cacheName);
+      return await cache.get(key, options);
+    }
+
+    async mget(cacheName, keys, options) {
+      const cache = this.getCache(cacheName);
+      return await cache.mget(keys, options);
+    }
+
+    async del(cacheName, key, options) {
+      const cache = this.getCache(cacheName);
+      return await cache.del(key, options);
+    }
+
+    async mdel(cacheName, keys, options) {
+      const cache = this.getCache(cacheName);
+      return await cache.mdel(keys, options);
+    }
+
+    async clear(cacheName, options) {
+      const cache = this.getCache(cacheName);
+      return await cache.clear(options);
+    }
+
+    async peek(cacheName, key, options) {
+      const cache = this.getCache(cacheName);
+      return await cache.peek(key, options);
+    }
+
     _findCacheBase({ module, name, fullKey }) {
       module = module || this.moduleName;
       if (!fullKey) {
