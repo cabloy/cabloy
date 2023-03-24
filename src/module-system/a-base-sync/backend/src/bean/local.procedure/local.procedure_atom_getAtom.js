ac12f76c761682906b4faf9f3e4e30028d8be3fd
zhennann <zhen.nann@icloud.com>
2023-03-24 16:01:46 +0800

    feat: remove g aUser

--- src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atom_getAtom.js
+++ src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atom_getAtom.js
@@ -1,6 +1,6 @@
 module.exports = ctx => {
   class Procedure {
-    async getAtom({ iid, userIdWho, tableName, atomId, resource, resourceLocale, mode, cms, forAtomUser }) {
+    async getAtom({ iid, userIdWho, tableName, atomId, resource, resourceLocale, mode, cms /* , forAtomUser*/ }) {
       // -- tables
       // -- a: aAtom
       // -- d: aAtomStar
