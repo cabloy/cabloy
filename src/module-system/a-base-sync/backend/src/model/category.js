9ad2b59efa2706cdb5519031b14ec3bb5f07bb61
zhennann <zhen.nann@icloud.com>
2023-03-24 17:29:22 +0800

    feat: category model

--- src/module-system/a-base-sync/backend/src/model/category.js
+++ src/module-system/a-base-sync/backend/src/model/category.js
@@ -3,6 +3,22 @@ module.exports = app => {
     constructor(ctx) {
       super(ctx, { table: 'aCategory', options: { disableDeleted: false } });
     }
+
+    async update(category, ...args) {
+      const res = await super.update(category, ...args);
+      if (category.id) {
+        await this.ctx.bean.category.deleteCacheCategory({ categoryId: category.id });
+      }
+      return res;
+    }
+
+    async delete(category, ...args) {
+      const res = await super.delete(category, ...args);
+      if (category.id) {
+        await this.ctx.bean.category.deleteCacheCategory({ categoryId: category.id });
+      }
+      return res;
+    }
   }
   return Category;
 };
