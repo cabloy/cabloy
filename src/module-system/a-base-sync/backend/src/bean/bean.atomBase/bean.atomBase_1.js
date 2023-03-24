4bd97e4dc142c363965802f4fb6ab3915d76aac7
zhennann <zhen.nann@icloud.com>
2023-03-24 19:28:40 +0800

    feat: remove atom join aFlow

--- src/module-system/a-base-sync/backend/src/bean/bean.atomBase/bean.atomBase_1.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.atomBase/bean.atomBase_1.js
@@ -244,19 +244,6 @@ module.exports = app => {
         }
       }
     }
-
-    // _flowNodeNameCurrentLocaleTranslate({ items, item }) {
-    //   // items
-    //   if (item) {
-    //     items = [item];
-    //   }
-    //   // set
-    //   for (item of items) {
-    //     if (item.flowNodeNameCurrent) {
-    //       item.flowNodeNameCurrentLocale = this.ctx.text(item.flowNodeNameCurrent);
-    //     }
-    //   }
-    // }
   }
 
   return AtomBase;
