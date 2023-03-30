f43dd41eb19957a0e14ba84ec75b41c0ffd902b4
zhennann <zhen.nann@icloud.com>
2023-03-30 12:20:52 +0800

    feat: formatValue/formatWhere

--- packages/egg-born-backend/lib/base/model.js
+++ packages/egg-born-backend/lib/base/model.js
@@ -63,6 +63,8 @@ module.exports = app => {
     'escape',
     'escapeId',
     'format',
+    '_formatValue',
+    '_formatWhere',
     'query',
     'queryOne',
     '_query',
@@ -183,26 +185,6 @@ module.exports = app => {
     });
   });
 
-  ['_format'].forEach(method => {
-    Object.defineProperty(Model.prototype, method, {
-      get() {
-        return function () {
-          return _formatValue(this.ctx.db, arguments[0]);
-        };
-      },
-    });
-  });
-
-  ['_formatWhere'].forEach(method => {
-    Object.defineProperty(Model.prototype, method, {
-      get() {
-        return function () {
-          return _formatWhere(this.ctx.db, arguments[0]);
-        };
-      },
-    });
-  });
-
   ['_orders'].forEach(method => {
     Object.defineProperty(Model.prototype, method, {
       get() {
@@ -242,127 +224,4 @@ function _whereClause(db, where) {
     return ' WHERE false';
   }
   return ` WHERE (${wheres})`;
-}
-
-function _formatOrAnd(db, ors, orAnd) {
-  // or
-  if (orAnd === 'OR') {
-    return _formatOrAnd_or(db, ors);
-  }
-  // and
-  return _formatOrAnd_and(db, ors);
-}
-
-function _formatOrAnd_or(db, ors) {
-  const wheres = [];
-  for (const or of ors) {
-    const _where = _formatWhere(db, or);
-    if (_where === false) {
-      // ignore
-    } else if (_where === true) {
-      return true;
-    } else {
-      wheres.push(_where);
-    }
-  }
-  if (wheres.length === 0) return false;
-  return wheres.join(' OR ');
-}
-
-function _formatOrAnd_and(db, ors) {
-  const wheres = [];
-  for (const or of ors) {
-    const _where = _formatWhere(db, or);
-    if (_where === true) {
-      // ignore
-    } else if (_where === false) {
-      // deny
-      return false;
-    } else {
-      wheres.push(_where);
-    }
-  }
-  if (wheres.length === 0) return true;
-  return wheres.join(' AND ');
-}
-
-function _formatWhere(db, where) {
-  if (where === true || where === false) return where;
-  if (where === undefined || where === null) {
-    return true;
-  }
-
-  const wheres = [];
-  for (const key in where) {
-    const value = where[key];
-    // check key or/and
-    let keyOrAnd;
-    if (key.indexOf(__whereOrPlaceholder) > -1) {
-      keyOrAnd = 'OR';
-    } else if (key.indexOf(__whereAndPlaceholder) > -1) {
-      keyOrAnd = 'AND';
-    }
-    if (keyOrAnd) {
-      const _where = _formatOrAnd(db, value, keyOrAnd);
-      if (_where === true) {
-        // ignore
-      } else if (_where === false) {
-        // deny
-        return false;
-      } else {
-        wheres.push(`(${_where})`);
-      }
-      continue;
-    }
-    // check value
-    if (Array.isArray(value)) {
-      if (value.length === 0) {
-        return false;
-      }
-      wheres.push(db.format('?? IN (?)', [key, value]));
-    } else if (value === null || value === undefined) {
-      wheres.push(db.format('?? IS ?', [key, value]));
-    } else if (value && !(value instanceof Date) && typeof value === 'object') {
-      // op
-      let op = value.op || '='; // default is =
-      op = op.indexOf('like') > -1 ? 'LIKE' : op;
-      // op: notNull
-      if (op === 'notNull') {
-        wheres.push(db.format('?? IS NOT null', [key]));
-      } else {
-        wheres.push(`${db.format('??', key)} ${_safeOp(op)} ${_formatValue(db, value)}`);
-      }
-    } else {
-      wheres.push(db.format('?? = ?', [key, value]));
-    }
-  }
-  if (wheres.length === 0) return true;
-  return wheres.join(' AND ');
-}
-
-function _formatValue(db, value) {
-  if (typeof value !== 'object' || value instanceof Date) return db.format('?', value);
-  let val;
-  if (value.type === 'Date') {
-    val = db.format('?', moment(value.val).toDate());
-  } else {
-    val = db.format('?', value.val);
-  }
-  // like
-  const val2 = val.substr(1, val.length - 2);
-  if (value.op === 'like') return `'%${val2}%'`;
-  if (value.op === 'likeLeft') return `'%${val2}'`;
-  if (value.op === 'likeRight') return `'${val2}%'`;
-  // in
-  if (value.op === 'in' || value.op === 'notIn') {
-    const arr = typeof value.val === 'string' ? value.val.split(',') : value.val;
-    return `(${db.format('?', [arr])})`;
-  }
-  // others
-  return val;
-}
-
-function _safeOp(op) {
-  if (op === 'notIn') return 'not in';
-  return op.replace(/[\\\.*#%'"`;, ]/g, '');
 }
