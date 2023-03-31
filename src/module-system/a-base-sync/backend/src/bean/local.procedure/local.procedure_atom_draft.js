6f6de454788ba8ba3e05c68640a32fd9ebb3b285
zhennann <zhen.nann@icloud.com>
2023-03-31 11:21:16 +0800

    feat: atom_draft

--- src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atom_draft.js
+++ src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atom_draft.js
@@ -3,12 +3,14 @@ module.exports = ctx => {
     async _selectAtoms_draft({
       iid,
       userIdWho,
+      // atomClass,
+      // atomClassBase,
       tableName,
       where,
       orders,
       page,
-      star,
-      label,
+      // star,
+      // label,
       comment,
       file,
       count,
@@ -33,76 +35,42 @@ module.exports = ctx => {
 
       // for safe
       // tableName = tableName ? ctx.model.format('??', tableName) : null; // not format tableName
-      where = where ? ctx.model._where(where) : null;
-      orders = orders ? ctx.model._orders(orders) : null;
-      const limit = page ? ctx.model._limit(page.size, page.index) : null;
+      const _where = Object.assign({}, where);
+      const _orders = orders ? ctx.model._orders(orders) : '';
+      const _limit = page ? ctx.model._limit(page.size, page.index) : '';
 
       // vars
-      let _languageWhere;
-      let _categoryWhere;
-      let _tagJoin, _tagWhere;
+      let _tagJoin;
 
-      let _starJoin, _starWhere;
+      let _commentField, _commentJoin;
 
-      let _labelJoin, _labelWhere;
-      let _commentField, _commentJoin, _commentWhere;
-
-      let _fileField, _fileJoin, _fileWhere;
+      let _fileField, _fileJoin;
 
       let _itemField, _itemJoin;
-
-      let _atomClassWhere;
+      // let _atomField, _atomJoin;
 
       // cms
       const { _cmsField, _cmsJoin, _cmsWhere } = this._prepare_cms({ tableName, iid, mode, cms });
-
-      //
-      const _where = where ? `${where} AND` : ' WHERE';
-      const _orders = orders || '';
-      const _limit = limit || '';
+      _where.__and__cms = _cmsWhere;
 
       // language
       if (language) {
-        _languageWhere = ctx.model.format(' and a.atomLanguage=?', language);
-      } else {
-        _languageWhere = '';
+        _where['a.atomLanguage'] = language;
       }
 
       // category
       if (category) {
-        _categoryWhere = ` and a.atomCategoryId=${category}`;
-      } else {
-        _categoryWhere = '';
+        _where['a.atomCategoryId'] = category;
       }
 
       // tag
       if (tag) {
         _tagJoin = ' inner join aTagRef k on k.atomId=a.id';
-        _tagWhere = ` and k.iid=${iid} and k.tagId=${tag}`;
+        _where['k.iid'] = iid;
+        _where['k.tagId'] = tag;
       } else {
         _tagJoin = '';
-        _tagWhere = '';
       }
-
-      // star
-      if (star) {
-        _starJoin = ' inner join aAtomStar d on a.id=d.atomId';
-        _starWhere = ` and d.iid=${iid} and d.userId=${userIdWho} and d.star=1`;
-      } else {
-        _starJoin = '';
-        _starWhere = '';
-      }
-      const _starField = `,(select d2.star from aAtomStar d2 where d2.iid=${iid} and d2.atomId=a.id and d2.userId=${userIdWho}) as star`;
-
-      // label
-      if (label) {
-        _labelJoin = ' inner join aAtomLabelRef e on a.id=e.atomId';
-        _labelWhere = ` and e.iid=${iid} and e.userId=${userIdWho} and e.labelId=${label}`;
-      } else {
-        _labelJoin = '';
-        _labelWhere = '';
-      }
-      const _labelField = `,(select e2.labels from aAtomLabel e2 where e2.iid=${iid} and e2.atomId=a.id and e2.userId=${userIdWho}) as labels`;
 
       // comment
       if (comment) {
@@ -110,11 +78,11 @@ module.exports = ctx => {
                (select h2.heart from aCommentHeart h2 where h2.iid=${iid} and h2.commentId=h.id and h2.userId=${userIdWho}) as h_heart`;
 
         _commentJoin = ' inner join aViewComment h on h.atomId=a.id';
-        _commentWhere = ` and h.iid=${iid} and h.deleted=0`;
+        _where['h.iid'] = iid;
+        _where['h.deleted'] = 0;
       } else {
         _commentField = '';
         _commentJoin = '';
-        _commentWhere = '';
       }
 
       // file
@@ -122,11 +90,11 @@ module.exports = ctx => {
         _fileField =
           ',i.id i_id,i.createdAt i_createdAt,i.updatedAt i_updatedAt,i.userId i_userId,i.downloadId i_downloadId,i.mode i_mode,i.fileSize i_fileSize,i.width i_width,i.height i_height,i.filePath i_filePath,i.fileName i_fileName,i.realName i_realName,i.fileExt i_fileExt,i.encoding i_encoding,i.mime i_mime,i.attachment i_attachment,i.flag i_flag,i.userName i_userName,i.avatar i_avatar';
         _fileJoin = ' inner join aViewFile i on i.atomId=a.id';
-        _fileWhere = ` and i.iid=${iid} and i.deleted=0`;
+        _where['i.iid'] = iid;
+        _where['i.deleted'] = 0;
       } else {
         _fileField = '';
         _fileJoin = '';
-        _fileWhere = '';
       }
 
       // tableName
@@ -138,13 +106,21 @@ module.exports = ctx => {
         _itemJoin = '';
       }
 
-      // atomClass inner
-      // eslint-disable-next-line
-      _atomClassWhere = '';
-      // if (atomClass || star || label) {
-      //   _atomClassWhere = '';
-      // } else {
-      //   _atomClassWhere = await this._prepare_atomClassIdsInner();
+      // atom
+      const _atomField = `a.id as atomId,a.itemId,a.atomStage,a.atomFlowId,a.atomClosed,a.atomIdDraft,a.atomIdFormal,a.roleIdOwner,a.atomClassId,a.atomName,
+          a.atomStatic,a.atomStaticKey,a.atomRevision,a.atomLanguage,a.atomCategoryId,a.atomTags,
+          a.atomSimple,a.atomDisabled,a.atomState,
+          a.allowComment,a.starCount,a.commentCount,a.attachmentCount,a.readCount,a.userIdCreated,a.userIdUpdated,a.createdAt as atomCreatedAt,a.updatedAt as atomUpdatedAt`;
+      const _atomJoin = 'from aAtom a';
+      _where['a.deleted'] = 0;
+      _where['a.iid'] = iid;
+      _where['a.atomStage'] = stage;
+      _where['a.atomClosed'] = 0;
+      _where['a.userIdUpdated'] = userIdWho;
+
+      // atomClass inner: need not
+      // if (!atomClass) {
+      //   _where['a.atomClassId'] = await this._prepare_atomClassIdsInner();
       // }
 
       // fields
@@ -152,39 +128,30 @@ module.exports = ctx => {
       if (count) {
         _selectFields = 'count(*) as _count';
       } else {
-        _selectFields = `${_itemField} ${_cmsField}
-                a.id as atomId,a.itemId,a.atomStage,a.atomFlowId,a.atomClosed,a.atomIdDraft,a.atomIdFormal,a.roleIdOwner,a.atomClassId,a.atomName,
-                a.atomStatic,a.atomStaticKey,a.atomRevision,a.atomLanguage,a.atomCategoryId,a.atomTags,
-                a.atomSimple,a.atomDisabled,a.atomState,
-                a.allowComment,a.starCount,a.commentCount,a.attachmentCount,a.readCount,a.userIdCreated,a.userIdUpdated,a.createdAt as atomCreatedAt,a.updatedAt as atomUpdatedAt
-                ${_starField} ${_labelField} ${_commentField}
-                ${_fileField}
-              `;
+        _selectFields = this._combineFields([
+          //
+          _itemField,
+          _cmsField,
+          _atomField,
+          _commentField,
+          _fileField,
+        ]);
       }
 
+      // where clause
+      let _whereClause = ctx.model._formatWhere(_where);
+      if (_whereClause === false) return false;
+      _whereClause = _whereClause === true ? '' : ` WHERE (${_whereClause})`;
+
       // sql
-      const _sql = `select ${_selectFields} from aAtom a
+      const _sql = `select ${_selectFields} ${_atomJoin}
             ${_itemJoin}
             ${_tagJoin}
-            ${_starJoin}
-            ${_labelJoin}
             ${_commentJoin}
             ${_fileJoin}
             ${_cmsJoin}
 
-          ${_where}
-           (
-             a.deleted=0 and a.iid=${iid} and a.atomStage=${stage} and a.atomClosed=0 and a.userIdUpdated=${userIdWho}
-             ${_atomClassWhere}
-             ${_languageWhere}
-             ${_categoryWhere}
-             ${_tagWhere}
-             ${_starWhere}
-             ${_labelWhere}
-             ${_commentWhere}
-             ${_fileWhere}
-             ${_cmsWhere}
-           )
+          ${_whereClause}
 
           ${count ? '' : _orders}
           ${count ? '' : _limit}
