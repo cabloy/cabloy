7b3458d8bc47c408a18599c5cbf8affb18a6df39
zhennann <zhen.nann@icloud.com>
2023-03-31 14:15:05 +0800

    feat: checkRightAction

--- src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atomRight_checkRightAction.js
+++ src/module-system/a-base-sync/backend/src/bean/local.procedure/local.procedure_atomRight_checkRightAction.js
@@ -6,61 +6,53 @@ module.exports = ctx => {
       userIdWho = parseInt(userIdWho);
       atomId = parseInt(atomId);
       action = parseInt(action);
+      // _where
+      const _where = {
+        'a.deleted': 0,
+        'a.iid': iid,
+        'a.id': atomId,
+        'a.atomStage': [1, 2],
+      };
       // _rightWhere
-      let _rightWhere = await this._checkRightAction_rightWhere({ iid, userIdWho, atomClass, action, forAtomUser });
-      if (_rightWhere === false) return false;
-      if (_rightWhere) {
-        _rightWhere = ` and ( ${_rightWhere} )`;
-      }
+      const _rightWhere = await this._checkRightAction_rightWhere({ iid, userIdWho, atomClass, action, forAtomUser });
+      _where.__and__right = _rightWhere;
+
+      // where clause
+      let _whereClause = ctx.model._formatWhere(_where);
+      if (_whereClause === false) return false;
+      _whereClause = _whereClause === true ? '' : ` WHERE (${_whereClause})`;
+
       // sql
       const _sql = `select a.* from aAtom a
-            where
-            (
-              a.deleted=0 and a.iid=${iid} and a.id=${atomId}
-              and a.atomStage>0
-              ${_rightWhere} 
-            )
-        `;
+        ${_whereClause}
+      `;
       return _sql;
     }
 
     async _checkRightAction_rightWhere({ iid, userIdWho, atomClass, action, forAtomUser }) {
-      const _mine = `
+      const _mine = ctx.model.raw(`
         (a.userIdCreated=${userIdWho} and exists(select c.atomClassId from aViewUserRightAtomClass c where c.iid=${iid} and a.atomClassId=c.atomClassId and c.action=${action} and c.scope=0 and c.userIdWho=${userIdWho}))
-      `;
+      `);
       let _others;
       if (forAtomUser) {
-        _others = `
+        _others = ctx.model.raw(`
           exists(
             select c.userIdWhom from aViewUserRightAtomClassUser c where c.iid=${iid} and a.itemId=c.userIdWhom and c.atomClassId=a.atomClassId and c.action=${action} and c.userIdWho=${userIdWho}
           )
-        `;
+        `);
       } else {
         const roleScopes = await this._prepare_roleScopesOfUser({ atomClass, action, userIdWho });
-        if (roleScopes === true) return ''; // pass through
+        if (roleScopes === true) return true; // pass through
         if (roleScopes === false) {
-          _others = ''; // should check mine
+          _others = false; // should check mine
         } else {
-          _others = `
+          _others = ctx.model.raw(`
             a.roleIdOwner in (${roleScopes.join(',')})
-          `;
+          `);
         }
       }
-      //
-      let _rightWhere;
-      if (!_others) {
-        _rightWhere = _mine;
-      } else {
-        _rightWhere = `
-            (
-              ${_mine}
-              or
-              ${_others}
-            )
-          `;
-      }
-      // ok
-      return _rightWhere;
+      // mine or others
+      return { __or__: [_mine, _others] };
     }
   }
   return Procedure;
