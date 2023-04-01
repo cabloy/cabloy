3d815dee4707758d084c0cbd95a920339f0b560b
zhennann <zhen.nann@icloud.com>
2023-04-01 21:53:17 +0800

    feat: test

--- src/module-system/a-useronline/backend/src/bean/version.manager.js
+++ src/module-system/a-useronline/backend/src/bean/version.manager.js
@@ -71,7 +71,18 @@ module.exports = app => {
       }
     }
 
-    async test() {}
+    async test() {
+      const roleRights = [
+        // for testing purposes
+        { roleName: 'system', action: 'create' },
+        { roleName: 'system', action: 'write', scopeNames: 0 },
+        { roleName: 'system', action: 'delete', scopeNames: 0 },
+        { roleName: 'system', action: 'clone', scopeNames: 0 },
+        { roleName: 'system', action: 'deleteBulk' },
+        { roleName: 'system', action: 'exportBulk' },
+      ];
+      await this.ctx.bean.role.addRoleRightBatch({ atomClassName: 'userOnlineHistory', roleRights });
+    }
   }
 
   return Version;
