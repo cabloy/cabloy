77e0b92b25cf7e063590c8527ca953aae488cca3
zhennann <zhen.nann@icloud.com>
2023-04-12 23:11:42 +0800

    chore: auth issue

--- src/module-system/a-auth/backend/src/bean/startup.registerPassport.js
+++ src/module-system/a-auth/backend/src/bean/startup.registerPassport.js
@@ -5,6 +5,10 @@ module.exports = app => {
       app.passport.verify(async (ctx, profileUser) => {
         // state: login/associate
         const state = ctx.request.query.state || 'login';
+        // maybe ready
+        if (profileUser.op && profileUser.agent && profileUser.provider) {
+          return profileUser;
+        }
         // user verify
         console.log('----app.passport.verify:', profileUser.module, profileUser.provider, profileUser.providerScene);
         return await ctx.bean.user.verify({ state, profileUser });
