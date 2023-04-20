2c31558ec4d65a34c7ac87dfd979fa6c765fec56
zhennann <zhen.nann@icloud.com>
2023-04-20 11:56:47 +0800

    feat: atom:action atomClass

--- src/module-system/a-useronline/front/src/components/action/actionKickout.js
+++ src/module-system/a-useronline/front/src/components/action/actionKickout.js
@@ -4,8 +4,12 @@ export default {
       const { ctx, item } = this.$props;
       await ctx.$view.dialog.confirm();
       const key = { atomId: item.atomId, itemId: item.itemId };
+      const atomClass = {
+        module: item.module,
+        atomClassName: item.atomClassName,
+      };
       await ctx.$api.post('/a/useronline/userOnline/kickOut', { key });
-      ctx.$meta.eventHub.$emit('atom:action', { key, action: { name: 'save' } });
+      ctx.$meta.eventHub.$emit('atom:action', { key, atomClass, action: { name: 'save' } });
       return true;
     },
   },
