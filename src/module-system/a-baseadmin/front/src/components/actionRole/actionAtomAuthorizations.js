059db78f0dd4cc744782f505696348ae3959a5dd
zhennann <zhen.nann@icloud.com>
2023-04-25 06:52:57 +0800

    feat: _onActionAtomAuthorizations

--- src/module-system/a-baseadmin/front/src/components/actionRole/actionAtomAuthorizations.js
+++ src/module-system/a-baseadmin/front/src/components/actionRole/actionAtomAuthorizations.js
@@ -3,11 +3,28 @@ export default {
     async _onActionAtomAuthorizations() {
       const { ctx, action, item } = this.$props;
       // atomAuthorizations
+      // options
+      const options = {
+        atomIdMain: item.atomId,
+      };
+      // params
+      const params = {
+        pageTitle: `${this.$text('Atom Right')}: ${item.atomNameLocale || item.atomName}`,
+      };
+      // queries
+      const queries = {
+        module: 'a-base',
+        atomClassName: 'roleRight',
+        options: JSON.stringify(options),
+        params: JSON.stringify(params),
+      };
+      // url
+      const url = ctx.$meta.util.combineQueries('/a/basefront/atom/list', queries);
+      // const url = ctx.$meta.util.replaceTemplate(
+      //   '/a/baseadmin/atomRight/edit?roleAtomId={{atomId}}&roleId={{itemId}}',
+      //   item
+      // );
       // open
-      const url = ctx.$meta.util.replaceTemplate(
-        '/a/baseadmin/atomRight/edit?roleAtomId={{atomId}}&roleId={{itemId}}',
-        item
-      );
       let navigateOptions = action.navigateOptions;
       if (ctx.$pageRoute.path === '/a/basefront/atom/item') {
         navigateOptions = { target: '_self' };
