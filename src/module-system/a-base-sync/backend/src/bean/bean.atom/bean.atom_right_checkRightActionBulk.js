bc8adda6aaffeffab2daae1376f5f8c9132668aa
zhennann <zhen.nann@icloud.com>
2023-04-30 14:10:54 +0800

    feat: __checkRightActionBulk_fetchActions_fromMeta

--- src/module-system/a-base-sync/backend/src/bean/bean.atom/bean.atom_right_checkRightActionBulk.js
+++ src/module-system/a-base-sync/backend/src/bean/bean.atom/bean.atom_right_checkRightActionBulk.js
@@ -24,8 +24,64 @@ module.exports = ctx => {
       // enableRight
       const enableRight = atomClassBase.enableRight;
       if (enableRight) {
+        // from db
         return await this.__checkRightActionBulk_fetchActions_fromDb({ atomClass, atomClassBase, action, user });
       }
+      // from meta
+      return await this.__checkRightActionBulk_fetchActions_fromMeta({
+        atomClass,
+        atomClassBase,
+        action,
+        user,
+        bulk: true,
+      });
+    }
+
+    async __checkRightActionBulk_fetchActions_fromMeta({ atomClass, /* atomClassBase,*/ action, user, bulk }) {
+      // meta
+      const _module = ctx.app.meta.modules[atomClass.module];
+      const metaAtomClass = _module.main.meta.base.atoms[atomClass.atomClassName];
+      const metaActions = metaAtomClass.actions;
+      // actions
+      let actionsRes = [];
+      if (action) {
+        const actionBase = ctx.bean.base.action({
+          module: atomClass.module,
+          atomClassName: atomClass.atomClassName,
+          code: action,
+        });
+        if (actionBase && Boolean(actionBase.bulk) === Boolean(bulk) && metaActions[actionBase.name]) {
+          actionsRes.push(actionBase);
+        }
+      } else {
+        for (const actionName in metaActions) {
+          const actionBase = ctx.bean.base.action({
+            module: atomClass.module,
+            atomClassName: atomClass.atomClassName,
+            name: actionName,
+          });
+          if (actionBase && Boolean(actionBase.bulk) === Boolean(bulk)) {
+            actionsRes.push(actionBase);
+          }
+        }
+      }
+      // map
+      actionsRes = actionsRes.map(item => {
+        return {
+          id: item.code,
+          iid: ctx.instance.id,
+          atomClassId: atomClass.id,
+          code: item.code,
+          name: item.name,
+          bulk: item.bulk,
+          actionMode: 0,
+          module: atomClass.module,
+          atomClassName: atomClass.atomClassName,
+        };
+      });
+      // ok
+      if (action) return actionsRes[0];
+      return actionsRes;
     }
 
     async __checkRightActionBulk_fetchActions_fromDb({ atomClass, atomClassBase, action, user }) {
