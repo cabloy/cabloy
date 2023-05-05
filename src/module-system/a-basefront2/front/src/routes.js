c2f554825e4df70dd6a1ed8764a14191229f0b84
zhennann <zhen.nann@icloud.com>
2023-05-05 09:27:46 +0800

    feat: a-basefront2

---
+++ src/module-system/a-basefront2/front/src/routes.js
@@ -0,0 +1,8 @@
+function load(name) {
+  return require(`./pages/${name}.vue`).default;
+}
+function loadjsx(name) {
+  return require(`./pages/${name}.jsx`).default;
+}
+
+export default [];
---
+++ src/module-system/a-basefront2/front/src/store.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+export default function (Vue) {
+  return {
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+  };
+}
