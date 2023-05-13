66c8f2297f476708d7884986c09f1e70d02e2fb9
zhennann <zhen.nann@icloud.com>
2023-05-13 21:06:34 +0800

    feat: a-basestore

---
+++ src/module-system/a-basestore/backend/src/meta.js
@@ -0,0 +1,29 @@
+module.exports = app => {
+  // schemas
+  const schemas = require('./config/validation/schemas.js')(app);
+  // static
+  const staticLayouts = require('./config/static/layouts.js')(app);
+  const staticResources = require('./config/static/resources.js')(app);
+  const meta = {
+    base: {
+      atoms: {},
+      statics: {
+        'a-baselayout.layout': {
+          items: staticLayouts,
+        },
+        'a-base.resource': {
+          items: staticResources,
+        },
+      },
+    },
+    validation: {
+      validators: {},
+      keywords: {},
+      schemas,
+    },
+    index: {
+      indexes: {},
+    },
+  };
+  return meta;
+};
