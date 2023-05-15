abf10a5682c5c80e7807b608535bfdf6ed9aa945
zhennann <zhen.nann@icloud.com>
2023-05-15 09:56:01 +0800

    feat: bulk delete

--- src/module-system/a-baseaction/front/src/components/atom/actionBulk/actionBulkDelete.js
+++ src/module-system/a-baseaction/front/src/components/atom/actionBulk/actionBulkDelete.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
     async _onActionBulkDelete() {
-      const { ctx, item } = this.$props;
+      const { ctx, action, item } = this.$props;
       // confirm
       await ctx.$view.dialog.confirm();
       // atomClass
@@ -9,13 +9,22 @@ export default {
         module: item.module,
         atomClassName: item.atomClassName,
       };
+      // dataOptions
+      const dataOptions = action.dataOptions || {};
+      // params
+      const params = { atomClass, options: {} };
+      // atomIdMain
+      if (dataOptions.atomIdMain) {
+        params.options.atomIdMain = dataOptions.atomIdMain;
+      }
       // keys
       const selectedAtoms = ctx.bulk.selectedAtoms;
       const keys = selectedAtoms.map(item => {
         return { atomId: item.atomId, itemId: item.itemId };
       });
+      params.keys = keys;
       // deleteBulk
-      const res = await ctx.$api.post('/a/base/atom/deleteBulk', { atomClass, keys });
+      const res = await ctx.$api.post('/a/base/atom/deleteBulk', params);
       // delete
       for (const key of res.keys) {
         // selected
