47ad8226e709f0e14adf2eabfdd32dab22e66be0
zhennann <zhen.nann@icloud.com>
2023-05-17 10:37:50 +0800

    feat: documentVisibility

--- src/module-system/a-base-sync/front/src/stores/documentVisibility.js
+++ src/module-system/a-base-sync/front/src/stores/documentVisibility.js
@@ -1,10 +1,26 @@
-import Vue from 'vue';
-
 export default {
   state() {
     return {
       visibilityState: document.visibilityState === 'visible',
+      initialized: false,
     };
   },
-  actions: {},
+  actions: {
+    initialize() {
+      if (this.initialized) return;
+      this.initialized = true;
+      document.addEventListener('visibilitychange', () => {
+        const visibilityState = document.visibilityState === 'visible';
+        if (this.visibilityState !== visibilityState) {
+          this.visibilityState = visibilityState;
+        }
+      });
+    },
+    subscribe(callback) {
+      this.initialize();
+      this.$subscribe((mutation, state) => {
+        callback(state.visibilityState);
+      });
+    },
+  },
 };
