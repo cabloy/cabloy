17e244e9c00b3d4ec67a35542b1a956415214eb1
zhennann <zhen.nann@icloud.com>
2023-05-19 17:35:50 +0800

    feat: renderAtomItem

--- src/module-system/a-components-sync/front/src/common/validate/render/renderAtomItem.jsx
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderAtomItem.jsx
@@ -3,28 +3,31 @@ export default {
     renderAtomItem(context) {
       const { key, property } = context;
       const title = this.getTitle(context, true);
-      // params
-      const atomClass = property.ebParams.atomClass;
-      let target = property.ebParams.target;
-      if (target === undefined) target = '_self';
       // value format
       let value = context.getValue();
       value = this._formatTextGeneral(property, value);
-      // atomId: maybe from host
-      const atomId = this.getAtomId(context, false);
-
       // props
       const props = {
         title,
         link: '#',
         onPerform: event => {
-          console.log('ssss');
+          this.renderAtomItem_onPerform(event, context);
         },
       };
       if (value) {
         props.after = value;
       }
       return <eb-list-item key={key} {...{ props }}></eb-list-item>;
+    },
+    renderAtomItem_onPerform(event, context) {
+      const { property } = context;
+      // params
+      const atomClass = property.ebParams.atomClass;
+      let target = property.ebParams.target;
+      if (target === undefined) target = '_self';
+      // atomId: maybe from host
+      const atomId = this.getAtomId(context, false);
+      console.log('ssss');
     },
   },
 };
