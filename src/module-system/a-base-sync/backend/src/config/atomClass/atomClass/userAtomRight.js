69e5203402049cd415328cafdb43d83d532a5c36
zhennann <zhen.nann@icloud.com>
2023-05-21 15:13:42 +0800

    feat: select sql

--- src/module-system/a-base-sync/backend/src/config/atomClass/atomClass/userAtomRight.js
+++ src/module-system/a-base-sync/backend/src/config/atomClass/atomClass/userAtomRight.js
@@ -1,19 +1,20 @@
 const tableNameDefault = function () {
   return `
     (select
-      concat_ws(':',__d.id,__a.id) as id,
-      __d.id as roleExpandId,
-      __d.createdAt,__d.updatedAt,__d.deleted,__d.iid,__d.roleId,__d.roleAtomId,__d.roleIdBase,
-      __a.id as roleRightId,__a.atomClassId as atomClassIdTarget,__a.action,__a.scope,
+      concat_ws(':',__a.roleExpandId,__a.roleRightId) as id,
+      __a.iid,__a.roleExpandId,__a.roleRightId,0 as deleted,
+      __a.userIdWho,__a.roleId,__a.roleIdBase, 
+      __a.atomClassId as atomClassIdTarget,__a.action,__a.scope, 
       __b.module as moduleTarget,__b.atomClassName as atomClassNameTarget,
       __c.name as actionName,__c.bulk as actionBulk,__c.actionMode,
       __e.roleName as roleNameBase,__e.atomId as roleAtomIdBase,
+      __g.atomId as userAtomId,
       __f.atomName as flowDefName 
-        from aRoleRight __a
+      from aViewUserRightAtomClass __a
         inner join aAtomClass __b on __a.atomClassId=__b.id
         inner join aAtomAction __c on __a.atomClassId=__c.atomClassId and __a.action=__c.code
-        inner join aRoleExpand __d on __a.roleId=__d.roleIdBase
-        inner join aRole __e on __d.roleIdBase=__e.id
+        inner join aRole __e on __a.roleIdBase=__e.id
+        inner join aUser __g on __g.id=__a.userIdWho
         left join aAtom __f on __c.flowKey=__f.atomStaticKey and __f.atomStage=1
     )
   `;
