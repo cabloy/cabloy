120617cf1696cb2e07ed3ad89004b7d17d357ed9
zhennann <zhen.nann@icloud.com>
2023-06-12 17:15:35 +0800

    enhance: worker alive

--- src/module-system/a-version/backend/src/bean/bean.worker.js
+++ src/module-system/a-version/backend/src/bean/bean.worker.js
@@ -1,22 +1,37 @@
 module.exports = app => {
   const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Worker extends app.meta.BeanBase {
+    constructor(ctx) {
+      super(ctx);
+      this._redisCache = null;
+      this._redisIO = null;
+    }
+
     get id() {
       return app.meta.workerId;
+    }
+
+    get redisCache() {
+      if (!this._redisCache) this._redisCache = app.redis.get('cache');
+      return this._redisCache;
+    }
+
+    get redisIO() {
+      if (!this._redisIO) this._redisIO = app.redis.get('io');
+      return this._redisIO;
     }
 
     async setAlive() {
       const config = app.meta.configs[moduleInfo.relativeName];
       const aliveTimeout = config.worker.alive.timeout;
       const key = `workerAlive:${this.id}`;
-      const redis = app.redis.get('cache');
-      await redis.set(key, JSON.stringify(true), 'PX', aliveTimeout * 2);
+      await this.redisCache.set(key, JSON.stringify(true), 'PX', aliveTimeout * 2);
+      await this.redisIO.set(key, JSON.stringify(true), 'PX', aliveTimeout * 2);
     }
 
     async getAlive({ id }) {
       const key = `workerAlive:${id}`;
-      const redis = app.redis.get('cache');
-      const value = await redis.get(key);
+      const value = await this.redisCache.get(key);
       return value ? JSON.parse(value) : undefined;
     }
   }
