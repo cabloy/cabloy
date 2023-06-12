7f6830823fea8a3dc0469db3c911b7d173c97050
zhennann <zhen.nann@icloud.com>
2023-06-12 17:45:53 +0800

    enhance: worker alive

--- src/module-system/a-version/backend/src/bean/bean.worker.js
+++ src/module-system/a-version/backend/src/bean/bean.worker.js
@@ -4,7 +4,7 @@ module.exports = app => {
     constructor(ctx) {
       super(ctx);
       this._redisCache = null;
-      this._redisIO = null;
+      // this._redisIO = null;
     }
 
     get id() {
@@ -16,17 +16,17 @@ module.exports = app => {
       return this._redisCache;
     }
 
-    get redisIO() {
-      if (!this._redisIO) this._redisIO = app.redis.get('io');
-      return this._redisIO;
-    }
+    // get redisIO() {
+    //   if (!this._redisIO) this._redisIO = app.redis.get('io');
+    //   return this._redisIO;
+    // }
 
     async setAlive() {
       const config = app.meta.configs[moduleInfo.relativeName];
       const aliveTimeout = config.worker.alive.timeout;
       const key = `workerAlive:${this.id}`;
       await this.redisCache.set(key, JSON.stringify(true), 'PX', aliveTimeout * 2);
-      await this.redisIO.set(key, JSON.stringify(true), 'PX', aliveTimeout * 2);
+      // await this.redisIO.set(key, JSON.stringify(true), 'PX', aliveTimeout * 2);
     }
 
     async getAlive({ id }) {
