6ae9403dda2f3757d7b162cc5e3b4d7f255bb98f
zhennann <zhen.nann@icloud.com>
2023-07-25 15:11:50 +0800

    refactor: bean.detail

--- src/module-system/a-detail/backend/src/bean/bean.detail/bean.detail_delete.js
+++ src/module-system/a-detail/backend/src/bean/bean.detail/bean.detail_delete.js
@@ -1,6 +1,38 @@
 module.exports = ctx => {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
-  class Detail {}
+  class Detail {
+    async _deleteDetails({ atomClass, atomKey, user }) {
+      await this._loopDetailClasses({
+        atomClass,
+        fn: async ({ detailClass }) => {
+          await this._deleteDetails_Class({ detailClass, atomClass, atomKey, user });
+        },
+      });
+    }
+
+    async _deleteDetails_Class({ detailClass, atomKey, user }) {
+      // details
+      const details = await this.modelDetail.select({
+        where: {
+          atomId: atomKey.atomId,
+          detailClassId: detailClass.id,
+        },
+      });
+      // loop
+      for (const detail of details) {
+        // delete
+        const key = { detailId: detail.id, detailItemId: detail.detailItemId };
+        await this._delete2({ detailClass, key, user });
+      }
+    }
+
+    async _deleteDetailBase({ atomClass, key, options, user }) {
+      await this.modelDetailBase.delete({
+        detailId: key.atomId,
+        detailClassId: atomClass.id,
+      });
+    }
+  }
 
   return Detail;
 };
