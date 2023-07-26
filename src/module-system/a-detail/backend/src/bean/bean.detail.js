e56129cd92b7b52165332ef0f5f183bd6ffb27fd
zhennann <zhen.nann@icloud.com>
2023-07-26 15:00:04 +0800

    refactor: bean.detail_1

--- src/module-system/a-detail/backend/src/bean/bean.detail.js
+++ src/module-system/a-detail/backend/src/bean/bean.detail.js
@@ -1,5 +1,4 @@
 const detail_0 = require('./bean.detail/bean.detail_0.js');
-const detail_1 = require('./bean.detail/bean.detail_1.js');
 const detail_copy = require('./bean.detail/bean.detail_copy.js');
 const detail_delete = require('./bean.detail/bean.detail_delete.js');
 
@@ -7,8 +6,7 @@ module.exports = ctx => {
   return ctx.app.meta.util.mixinClasses(
     detail_0,
     [
-      detail_1, //
-      detail_copy,
+      detail_copy, //
       detail_delete,
     ],
     ctx
