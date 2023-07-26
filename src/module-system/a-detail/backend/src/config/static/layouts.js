20db2aaa841452aa1ba9344ef9f6039d575a23f7
zhennann <zhen.nann@icloud.com>
2023-07-26 14:55:13 +0800

    feat: remove layoutDetailItemBase

--- src/module-system/a-detail/backend/src/config/static/layouts.js
+++ src/module-system/a-detail/backend/src/config/static/layouts.js
@@ -1,10 +1,8 @@
-const layoutDetailItemBase = require('./layout/layoutDetailItemBase.js');
 const layoutDetailListBase = require('./layout/layoutDetailListBase.js');
 
 module.exports = app => {
   const layouts = [
     //
-    layoutDetailItemBase(app),
     layoutDetailListBase(app),
   ];
   return layouts;
