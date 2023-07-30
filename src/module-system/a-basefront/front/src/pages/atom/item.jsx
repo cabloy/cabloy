b7831b7a41d6aa17982f548d84ea0693c5569a09
zhennann <zhen.nann@icloud.com>
2023-07-30 10:37:13 +0800

    feat: flowTaskId of item

--- src/module-system/a-basefront/front/src/pages/atom/item.jsx
+++ src/module-system/a-basefront/front/src/pages/atom/item.jsx
@@ -10,15 +10,19 @@ export default {
     const atomId = parseInt(query.atomId || 0);
     const itemId = parseInt(query.itemId || 0);
     const layout = query.layout;
-    const module = query && query.module;
-    const atomClassName = query && query.atomClassName;
+    const module = query.module;
+    const atomClassName = query.atomClassName;
     const atomClass = module && atomClassName ? { module, atomClassName } : null;
-    const options = query && query.options ? JSON.parse(query.options) : {};
-    const params = query && query.params ? JSON.parse(query.params) : {};
+    const options = query.options ? JSON.parse(query.options) : {};
+    const params = query.params ? JSON.parse(query.params) : {};
     // atomMain
     const atomMain = this.$f7route.context?.params?.atomMain;
     if (atomMain) {
       options.atomMain = atomMain;
+    }
+    // flowTaskId
+    if (query.flowTaskId) {
+      options.flowTaskId = parseInt(query.flowTaskId);
     }
     return {
       container: {
