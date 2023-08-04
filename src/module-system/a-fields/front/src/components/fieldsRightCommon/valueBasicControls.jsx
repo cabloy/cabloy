ad38b093340f6cba03cd7d0769788e167fc2ec5a
zhennann <zhen.nann@icloud.com>
2023-08-04 10:41:08 +0800

    feat: showBasicControls

--- src/module-system/a-fields/front/src/components/fieldsRightCommon/valueBasicControls.jsx
+++ src/module-system/a-fields/front/src/components/fieldsRightCommon/valueBasicControls.jsx
@@ -1,9 +1,17 @@
 export default {
+  computed: {
+    showBasicControls() {
+      return this.valueMode === 'allowSpecificFields';
+    },
+  },
   methods: {
     _renderListGroupValueBasicControls() {
+      // if (!this.showBasicControls) return null;
       return (
         <f7-list-group>
-          <f7-list-item group-title title={this.$text('FieldsRightBasicControls')}></f7-list-item>
+          <f7-list-item group-title title={this.$text('FieldsRightBasicControls')}>
+            <div slot="after">ssss</div>
+          </f7-list-item>
         </f7-list-group>
       );
     },
