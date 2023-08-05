7b292e1946c0ee7461a9fef0079c5b921eea7823
zhennann <zhen.nann@icloud.com>
2023-08-05 17:15:23 +0800

    feat: bean.fields

---
+++ src/module-system/a-fields/backend/src/bean/bean.fields.js
@@ -0,0 +1,8 @@
+module.exports = ctx => {
+  // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class Fields {
+    async parseSchema({ atomClass, fieldsRight }) {}
+  }
+
+  return Fields;
+};
--- src/module-system/a-fields/backend/src/beans.js
+++ src/module-system/a-fields/backend/src/beans.js
@@ -1,4 +1,5 @@
 const versionManager = require('./bean/version.manager.js');
+const beanFields = require('./bean/bean.fields.js');
 
 module.exports = app => {
   const beans = {
@@ -6,6 +7,12 @@ module.exports = app => {
     'version.manager': {
       mode: 'app',
       bean: versionManager,
+    },
+    // global
+    event: {
+      mode: 'ctx',
+      bean: beanFields,
+      global: true,
     },
   };
   return beans;
