d4526861ad9df21d21b041fcabdadd5c14720f81
zhennann <zhen.nann@icloud.com>
2023-08-08 16:55:50 +0800

    feat: roleFieldsRight

--- src/module-system/a-baseadmin/backend/src/config/validation/schemas.js
+++ src/module-system/a-baseadmin/backend/src/config/validation/schemas.js
@@ -1,9 +1,11 @@
 const roleRight = require('./schema/roleRight.js');
 const roleResourceRight = require('./schema/roleResourceRight.js');
+const roleFieldsRight = require('./schema/roleFieldsRight.js');
 
 module.exports = app => {
   const schemas = {};
   Object.assign(schemas, roleRight(app));
   Object.assign(schemas, roleResourceRight(app));
+  Object.assign(schemas, roleFieldsRight(app));
   return schemas;
 };
