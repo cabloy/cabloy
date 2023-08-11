5e00f8274800c635325e80a17aeb8c6a32cdc967
zhennann <zhen.nann@icloud.com>
2023-08-11 16:05:11 +0800

    feat: model cache of userOnline

--- src/module-system/a-useronline/backend/src/config/config.js
+++ src/module-system/a-useronline/backend/src/config/config.js
@@ -1,6 +1,23 @@
 // eslint-disable-next-line
 module.exports = appInfo => {
   const config = {};
+
+  // summer
+  config.summer = {
+    caches: {
+      modelUserOnline: {
+        bean: null,
+        mode: 'all',
+        mem: {
+          max: 500,
+        },
+        redis: {
+          ttl: 4 * 60 * 60 * 1000, // 4 hours
+        },
+      },
+    },
+  };
+
   // userOnline
   config.userOnline = {
     expired: 20 * 60 * 1000, // 20 minutes
--- src/module-system/a-useronline/backend/src/model/userOnline.js
+++ src/module-system/a-useronline/backend/src/model/userOnline.js
@@ -1,7 +1,14 @@
 module.exports = app => {
-  class UserOnline extends app.meta.Model {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class UserOnline extends app.meta.ModelCache {
     constructor(ctx) {
-      super(ctx, { table: 'aUserOnline', options: { disableDeleted: false } });
+      super(ctx, {
+        table: 'aUserOnline',
+        options: {
+          disableDeleted: false,
+          cacheName: { module: moduleInfo.relativeName, name: 'modelUserOnline' },
+        },
+      });
     }
   }
   return UserOnline;
