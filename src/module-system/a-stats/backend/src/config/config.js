3bd26342f01f303cba0144236b0fefee2c19ab13
zhennann <zhen.nann@icloud.com>
2023-08-11 18:13:27 +0800

    feat: model cache for stats

--- src/module-system/a-stats/backend/src/config/config.js
+++ src/module-system/a-stats/backend/src/config/config.js
@@ -9,5 +9,20 @@ module.exports = appInfo => {
     },
   };
 
+  // summer
+  config.summer = {
+    caches: {
+      modelStats: {
+        mode: 'redis', // only redis
+        // mem: {
+        //   max: 500,
+        // },
+        redis: {
+          ttl: 4 * 60 * 60 * 1000, // 4 hours
+        },
+      },
+    },
+  };
+
   return config;
 };
--- src/module-system/a-stats/backend/src/model/stats.js
+++ src/module-system/a-stats/backend/src/model/stats.js
@@ -1,7 +1,14 @@
 module.exports = app => {
-  class Stats extends app.meta.Model {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class Stats extends app.meta.ModelCache {
     constructor(ctx) {
-      super(ctx, { table: 'aStats', options: { disableDeleted: true } });
+      super(ctx, {
+        table: 'aStats',
+        options: {
+          disableDeleted: true,
+          cacheName: { module: moduleInfo.relativeName, name: 'modelStats' },
+        },
+      });
     }
   }
   return Stats;
