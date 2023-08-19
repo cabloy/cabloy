4e1e597f1a1c8c04858af4ecb905fcd015dd994a
zhennann <zhen.nann@icloud.com>
2023-08-19 09:48:42 +0800

    feat: fieldsRight

--- src/module-system/a-fields/backend/src/bean/bean.fields.js
+++ src/module-system/a-fields/backend/src/bean/bean.fields.js
@@ -1,11 +1,11 @@
 const fields_parseSchema = require('./bean.fields/bean.fields_parseSchema.js');
-// const fields_parseSchema = require('./bean.fields/bean.fields_parseSchema.js');
+const fields_fieldsRight = require('./bean.fields/bean.fields_fieldsRight.js');
 
 module.exports = ctx => {
   return ctx.app.meta.util.mixinClasses(
     fields_parseSchema,
     [
-      // atomAction_flow, //
+      fields_fieldsRight, //
     ],
     ctx
   );
