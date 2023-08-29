6b2ea8ac6571872c9ad1a835ec2461862a756714
zhennann <zhen.nann@icloud.com>
2023-08-29 11:31:57 +0800

    feat: timeline _renderContainer

--- src/module-system/a-flowtimeline/front/src/components/atom/itemLayoutDefault/itemLayoutBlockDefaultTimeline.jsx
+++ src/module-system/a-flowtimeline/front/src/components/atom/itemLayoutDefault/itemLayoutBlockDefaultTimeline.jsx
@@ -25,25 +25,39 @@ export default {
     },
   },
   methods: {
-    _renderTodoList() {
-      return this.timelineInstance.timeline_render();
+    _renderCardHeader() {
+      return (
+        <f7-card-header>
+          <f7-block-title>
+            <f7-icon f7="::play-arrow"></f7-icon>
+            {this.$text('TimeLine_ToDoList')}
+          </f7-block-title>
+        </f7-card-header>
+      );
+    },
+    _renderCardContent() {
+      const domTimeline = this.timelineInstance.timeline_render();
+      return <f7-card-content padding={false}>{domTimeline}</f7-card-content>;
+    },
+    _renderCardFooter() {
+      return (
+        <f7-card-footer>
+          <f7-link>Like</f7-link>
+          <f7-link>Read more</f7-link>
+        </f7-card-footer>
+      );
     },
     _renderContainer() {
       const tasks = this.timelineInstance.base_tasks;
       if (!tasks || tasks.length === 0) return null;
+      const domCardHeader = this._renderCardHeader();
+      const domCardContent = this._renderCardContent();
+      const domCardFooter = this._renderCardFooter();
       return (
         <f7-card class="card-workflow-timeline-currentonly">
-          <f7-card-header>
-            <f7-block-title>
-              <f7-icon f7="::play-arrow"></f7-icon>
-              {this.$text('TimeLine_ToDoList')}
-            </f7-block-title>
-          </f7-card-header>
-          <f7-card-content padding={false}>{this._renderTodoList()}</f7-card-content>
-          <f7-card-footer>
-            <f7-link>Like</f7-link>
-            <f7-link>Read more</f7-link>
-          </f7-card-footer>
+          {domCardHeader}
+          {domCardContent}
+          {domCardFooter}
         </f7-card>
       );
     },
