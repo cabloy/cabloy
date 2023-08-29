41782832e173afb52d81a5df2851bee814c6a1f1
zhennann <zhen.nann@icloud.com>
2023-08-29 22:35:31 +0800

    feat: action components

--- src/module-system/a-flowtimeline/front/src/components.js
+++ src/module-system/a-flowtimeline/front/src/components.js
@@ -1,15 +1,9 @@
 import timeline from './components/timeline/index.jsx';
 // itemLayoutDefault
 import itemLayoutBlockDefaultTimeline from './components/atom/itemLayoutDefault/itemLayoutBlockDefaultTimeline.jsx';
-// action
-import actionCancelFlow from './components/action/actionCancelFlow.jsx';
-import actionHandleTask from './components/action/actionHandleTask.jsx';
 
 export default {
   timeline,
   // itemLayoutDefault
   itemLayoutBlockDefaultTimeline,
-  // action
-  actionCancelFlow,
-  actionHandleTask,
 };
