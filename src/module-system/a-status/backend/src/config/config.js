5683944e13274f0a92edba8c8e8ada82bba40ee1
zhennann <zhen.nann@icloud.com>
2023-09-11 17:16:51 +0800

    feat: modelcache of aStatus

--- src/module-system/a-status/backend/src/config/config.js
+++ src/module-system/a-status/backend/src/config/config.js
@@ -1,5 +1,21 @@
 // eslint-disable-next-line
 module.exports = appInfo => {
   const config = {};
+
+  // summer
+  config.summer = {
+    caches: {
+      modelStatus: {
+        mode: 'redis', // only redis
+        // mem: {
+        //   max: 500,
+        // },
+        redis: {
+          ttl: 2 * 60 * 60 * 1000, // 2 hours
+        },
+      },
+    },
+  };
+
   return config;
 };
--- src/module-system/a-status/backend/src/model/status.js
+++ src/module-system/a-status/backend/src/model/status.js
@@ -1,7 +1,14 @@
 module.exports = app => {
-  class Status extends app.meta.Model {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class Status extends app.meta.ModelCache {
     constructor(ctx) {
-      super(ctx, { table: 'aStatus', options: { disableDeleted: true } });
+      super(ctx, {
+        table: 'aStatus',
+        options: {
+          disableDeleted: true,
+          cacheName: { module: moduleInfo.relativeName, name: 'modelStatus' },
+        },
+      });
     }
   }
 
