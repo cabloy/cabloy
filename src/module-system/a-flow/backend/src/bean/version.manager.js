ada8e80561a08d811b58c39bd5556027a57afaa5
zhennann <zhen.nann@icloud.com>
2023-09-22 10:34:39 +0800

    feat: flow version

--- src/module-system/a-flow/backend/src/bean/version.manager.js
+++ src/module-system/a-flow/backend/src/bean/version.manager.js
@@ -1,8 +1,9 @@
+const fileVersionUpdates = [1, 2, 3, 5];
+const fileVersionInits = [1, 4];
 module.exports = app => {
   class Version extends app.meta.BeanBase {
     async update(options) {
-      const fileVersions = [1, 2, 3, 5];
-      if (fileVersions.includes(options.version)) {
+      if (fileVersionUpdates.includes(options.version)) {
         const VersionUpdateFn = require(`./version.manager/update/update${options.version}.js`);
         const versionUpdate = new (VersionUpdateFn(this.ctx))();
         await versionUpdate.run();
@@ -10,8 +11,7 @@ module.exports = app => {
     }
 
     async init(options) {
-      const fileVersions = [1, 4];
-      if (fileVersions.includes(options.version)) {
+      if (fileVersionInits.includes(options.version)) {
         const VersionInitFn = require(`./version.manager/init/init${options.version}.js`);
         const versionInit = new (VersionInitFn(this.ctx))();
         await versionInit.run();
