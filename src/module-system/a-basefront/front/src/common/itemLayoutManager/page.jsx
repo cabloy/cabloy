036c6c032f180b645b2a374135390c888f1ebb0b
zhennann <zhen.nann@icloud.com>
2023-10-07 18:02:34 +0800

    feat: translateText/getDialogTitle

--- src/module-system/a-basefront/front/src/common/itemLayoutManager/page.jsx
+++ src/module-system/a-basefront/front/src/common/itemLayoutManager/page.jsx
@@ -11,12 +11,23 @@ export default {
       return this.page_getDirtyTitle(title);
     },
     page_onGetTitle_inner() {
+      // form action
+      if (this.base._formActionBase && this.base.item) {
+        const useStoreActionBase = this.$store.useSync('a/base/actionBase');
+        return useStoreActionBase.getDialogTitle({
+          ctx: this,
+          action: this.base._formActionBase,
+          item: this.base.item,
+        });
+      }
+      // item
       if (this.base.item) {
         if (this.container.mode === 'edit') {
           return this.base.item.atomName;
         }
         return this.base.item.atomNameLocale || this.base.item.atomName || this.base.item._meta?.atomName;
       }
+      // atomClass
       return this.base.atomClassBase.titleLocale;
       // return `${name}: ${this.base.item.atomName}`;
       // title = this.container.mode === 'edit' ? this.$text('Edit') : this.$text('View');
