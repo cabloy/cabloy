da2d938db5963dd4f90720160786e3316878b220
zhennann <zhen.nann@icloud.com>
2023-10-08 17:30:24 +0800

    feat: history:true for flowDef

--- src/module-system/a-flow/backend/src/meta.js
+++ src/module-system/a-flow/backend/src/meta.js
@@ -20,6 +20,7 @@ module.exports = app => {
             tag: true,
             comment: false,
             attachment: false,
+            history: true,
           },
           actions: {
             write: {
