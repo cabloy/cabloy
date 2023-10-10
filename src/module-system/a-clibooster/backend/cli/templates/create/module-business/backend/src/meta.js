2a18c1d7d5da397a0d4723e67283a7bd13c25c66
zhennann <zhen.nann@icloud.com>
2023-10-10 20:35:39 +0800

    feat: cli module-business meta

--- src/module-system/a-clibooster/backend/cli/templates/create/module-business/backend/src/meta.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module-business/backend/src/meta.js
@@ -1,12 +1,19 @@
 module.exports = app => {
+  // atomClasses
+  const atomClasses = require('./config/atomClass/atomClasses.js')(app);
   // schemas
   const schemas = require('./config/validation/schemas.js')(app);
+  // keywords
+  const keywords = require('./config/validation/keywords.js')(app);
   // static
+  const staticApps = require('./config/static/apps.js')(app);
+  const staticDicts = require('./config/static/dicts.js')(app);
   const staticLayouts = require('./config/static/layouts.js')(app);
   const staticResources = require('./config/static/resources.js')(app);
   // meta
   const meta = {
     base: {
+      atoms: atomClasses,
       atoms: {
         <%=argv.atomClassName%>: {
           info: {
@@ -33,6 +40,12 @@ module.exports = app => {
         },
       },
       statics: {
+        'a-app.app': {
+          items: staticApps,
+        },
+        'a-dict.dict': {
+          items: staticDicts,
+        },
         'a-baselayout.layout': {
           items: staticLayouts,
         },
@@ -43,7 +56,7 @@ module.exports = app => {
     },
     validation: {
       validators: {},
-      keywords: {},
+      keywords,
       schemas,
     },
     index: {
