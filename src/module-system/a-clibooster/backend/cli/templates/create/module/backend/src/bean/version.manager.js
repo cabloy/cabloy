49e8f74242f269644bb0c714e75b476cab08f179
zhennann <zhen.nann@icloud.com>
2023-10-10 16:57:10 +0800

    feat: cli module

--- src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/bean/version.manager.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/bean/version.manager.js
@@ -1,10 +1,29 @@
+const fileVersionUpdates = [];
+const fileVersionInits = [];
+
 module.exports = app => {
   class Version extends app.meta.BeanBase {
-    async update(options) {}
+    async update(options) {
+      if (fileVersionUpdates.includes(options.version)) {
+        const VersionUpdateFn = require(`./version.manager/update/update${options.version}.js`);
+        const versionUpdate = new (VersionUpdateFn(this.ctx))();
+        await versionUpdate.run(options);
+      }
+    }
 
-    async init(options) {}
+    async init(options) {
+      if (fileVersionInits.includes(options.version)) {
+        const VersionInitFn = require(`./version.manager/init/init${options.version}.js`);
+        const versionInit = new (VersionInitFn(this.ctx))();
+        await versionInit.run(options);
+      }
+    }
 
-    async test() {}
+    async test() {
+      const VersionTestFn = require('./version.manager/test/test.js');
+      const versionTest = new (VersionTestFn(this.ctx))();
+      await versionTest.run();
+    }
   }
 
   return Version;
