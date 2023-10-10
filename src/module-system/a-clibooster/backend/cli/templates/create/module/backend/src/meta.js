d7d96fe7aba9f25a7c1befa0a60197be7570e717
zhennann <zhen.nann@icloud.com>
2023-10-10 20:03:45 +0800

    feat: cli module apps

--- src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/meta.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/meta.js
@@ -6,13 +6,18 @@ module.exports = app => {
   // keywords
   const keywords = require('./config/validation/keywords.js')(app);
   // static
+  const staticApps = require('./config/static/apps.js')(app);
   const staticDicts = require('./config/static/dicts.js')(app);
   const staticLayouts = require('./config/static/layouts.js')(app);
   const staticResources = require('./config/static/resources.js')(app);
+  // meta
   const meta = {
     base: {
       atoms: atomClasses,
       statics: {
+        'a-app.app': {
+          items: staticApps,
+        },
         'a-dict.dict': {
           items: staticDicts,
         },
