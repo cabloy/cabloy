575c751167480789fa595479dc4af4bc73ceb917
zhennann <zhen.nann@icloud.com>
2023-10-11 10:42:53 +0800

    feat: cli module resource

--- src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/config/static/resources.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module/backend/src/config/static/resources.js
@@ -1,4 +1,17 @@
+const functions = require('./resource/functions.js');
+const menus = require('./resource/menus.js');
+const mines = require('./resource/mines.js');
+const widgets = require('./resource/widgets.js');
+const blocks = require('./resource/blocks.js');
+const deprecateds = require('./resource/deprecateds.js');
+
 module.exports = app => {
-  const resources = [];
+  let resources = [];
+  resources = resources.concat(functions(app));
+  resources = resources.concat(menus(app));
+  resources = resources.concat(mines(app));
+  resources = resources.concat(widgets(app));
+  resources = resources.concat(blocks(app));
+  resources = resources.concat(deprecateds(app));
   return resources;
 };
