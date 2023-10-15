ec4a9fa8997bf4eb8803a26cef1fe7502715ebbc
zhennann <zhen.nann@icloud.com>
2023-10-15 21:41:30 +0800

    fix: support modelCache cacheName

--- src/module-system/a-clibooster/backend/cli/templates/create/module-business/backend/src/model/{{atomClassName}}.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module-business/backend/src/model/{{atomClassName}}.js
@@ -1,11 +1,12 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class <%=argv.atomClassNameCapitalize%> extends app.meta.ModelCache {
     constructor(ctx) {
       super(ctx, {
         table: '<%=argv.providerId%><%=argv.atomClassNameCapitalize%>',
         options: {
           disableDeleted: false,
-          cacheName: null,
+          cacheName: { module: moduleInfo.relativeName, name: 'model<%=argv.atomClassNameCapitalize%>' },
         }
       });
     }
