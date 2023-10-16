7beb926c7cea1beabe317f6abf096e1e5a9a7056
zhennann <zhen.nann@icloud.com>
2023-10-16 17:36:47 +0800

    feat: enhance snippet.file

--- src/module-system/a-clibooster/backend/cli/templates/create/atom-detail/snippets/config.schema.{{atomClassMain.atomClassName}}.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/atom-detail/snippets/config.schema.{{atomClassMain.atomClassName}}.js
@@ -21,7 +21,13 @@ const __snippet_body = `
 `;
 
 module.exports = {
-  file: 'backend/src/config/validation/schema/{{atomClassMain.atomClassName}}.js',
+  file({ argv, ctx }) {
+    if (argv.moduleInfo.relativeName !== argv.atomClassMain.module) {
+      // not in the same module
+      return null;
+    }
+    return `backend/src/config/validation/schema/${argv.atomClassMain.atomClassName}.js`;
+  },
   async transform({ cli, ast, argv, ctx }) {
     // code
     const code = await cli.template.renderContent({ content: __snippet_body });
