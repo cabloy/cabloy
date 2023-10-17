2f8174177c6c3e23cb0523039d34b932ac09a771
zhennann <zhen.nann@icloud.com>
2023-10-17 17:37:34 +0800

    fix: ast replace fileVersionUpdates

--- src/module-system/a-clibooster/backend/cli/templates/create/atom/snippets/version.manager.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/atom/snippets/version.manager.js
@@ -2,16 +2,16 @@ module.exports = {
   file: 'backend/src/bean/version.manager.js',
   async transform({ cli, ast, argv, ctx }) {
     // update
-    if (ast.has(`const fileVersionUpdates = []`)) {
-      ast.replace(`const fileVersionUpdates = []`, `const fileVersionUpdates = [${argv.fileVersion}]`);
-    } else {
+    if (ast.has(`const fileVersionUpdates = [$_$]`)) {
       ast.replace(`const fileVersionUpdates = [$_$]`, `const fileVersionUpdates = [$_$, ${argv.fileVersion}]`);
+    } else {
+      ast.replace(`const fileVersionUpdates = []`, `const fileVersionUpdates = [${argv.fileVersion}]`);
     }
     // init
-    if (ast.has(`const fileVersionInits = []`)) {
-      ast.replace(`const fileVersionInits = []`, `const fileVersionInits = [${argv.fileVersion}]`);
-    } else {
+    if (ast.has(`const fileVersionInits = [$_$]`)) {
       ast.replace(`const fileVersionInits = [$_$]`, `const fileVersionInits = [$_$, ${argv.fileVersion}]`);
+    } else {
+      ast.replace(`const fileVersionInits = []`, `const fileVersionInits = [${argv.fileVersion}]`);
     }
     // ok
     return ast;
