8b259f9fa630668b39634362b11109c17f1663c2
zhennann <zhen.nann@icloud.com>
2023-10-18 15:04:00 +0800

    feat: patch renderDetails

--- src/module-system/a-components-sync/front/src/common/validate/render/renderDetails.jsx
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderDetails.jsx
@@ -1,9 +1,6 @@
 export default {
   methods: {
     renderDetails(context) {
-      const { parcel } = context;
-      const atomIdMain = parcel.data.atomId;
-      if (atomIdMain === 0) return null;
       return this._renderComponent(context, {
         module: 'a-detail',
         name: 'renderDetails',
