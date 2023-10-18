e1dc3b469225c2c0511cfc21c7c2910a4e0729f1
zhennann <zhen.nann@icloud.com>
2023-10-18 17:34:32 +0800

    feat: _renderProperties_patchFirstGroup

--- src/module-system/a-components-sync/front/src/common/validate/render/renderProperties.jsx
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderProperties.jsx
@@ -8,11 +8,23 @@ export default {
       const keys = Object.keys(parcel.properties);
       const count = keys.length;
       let index = 0;
+      // patch first group
+      const propertyGroupFirst = this._renderProperties_patchFirstGroup({ parcel });
+      if (propertyGroupFirst) {
+        index = -1;
+      }
       // groupCount
       let groupCount = 0;
       while (index < count) {
-        const key = keys[index];
-        const property = parcel.properties[key];
+        let key;
+        let property;
+        if (index === -1) {
+          key = propertyGroupFirst.key;
+          property = propertyGroupFirst.property;
+        } else {
+          key = keys[index];
+          property = parcel.properties[key];
+        }
         // context
         const context2 = this.getContext({
           parcel,
@@ -43,6 +55,25 @@ export default {
       }
       // ok
       return children;
+    },
+    _renderProperties_patchFirstGroup({ parcel }) {
+      // only for top level
+      if (parcel.pathParent !== '') return null;
+      // check first property
+      const keys = Object.keys(parcel.properties);
+      const property = parcel.properties[keys[0]];
+      if (!property) return null;
+      if (property.ebType === 'group' || property.ebType === 'group-flatten') return null;
+      return {
+        key: '__groupFirst',
+        property: {
+          ebType: 'group-flatten',
+          ebGroupWhole: true,
+          ebParams: {
+            titleHidden: true,
+          },
+        },
+      };
     },
   },
 };
