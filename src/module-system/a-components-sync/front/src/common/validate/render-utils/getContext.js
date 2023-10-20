f8008e3e16dbf98a2be6bce762c7e1d125bd5d5c
zhennann <zhen.nann@icloud.com>
2023-10-20 16:22:33 +0800

    feat: staticClass

--- src/module-system/a-components-sync/front/src/common/validate/render-utils/getContext.js
+++ src/module-system/a-components-sync/front/src/common/validate/render-utils/getContext.js
@@ -87,13 +87,12 @@ export default {
       const params = context.getParams();
       return params.cssStyle;
     },
-    _getClassName({ context }) {
+    _getClassName({ context, classNameDefault }) {
       const params = context.getParams();
       const className = params.className;
-      if (className === undefined) {
-        return this._getClassNameDefault({ context });
-      }
-      return className;
+      if (className !== undefined) return className;
+      if (classNameDefault !== undefined) return classNameDefault;
+      return this._getClassNameDefault({ context });
     },
     _getClassNameDefault({ context }) {
       const paramsDefault = context.getParamsDefault();
@@ -125,15 +124,22 @@ export default {
       if (!item) return;
       const { property } = context;
       if (property.ebType === 'group' || property.ebType === 'group-flatten') return;
-      const className = this._getClassName({ context });
+      // use <tag staticClass="xxx"> as the default value, so as to override the grid-col rules
+      //    use <tag class="xxx"> to support extend rules
+      const className = this._getClassName({ context, classNameDefault: item.data.staticClass });
       const cssStyle = this._getCssStyle({ context });
       const items = Array.isArray(item) ? item : [item];
       for (item of items) {
         if (className) {
-          item.data.staticClass = this.$vuef7.utils.classNames(item.data.staticClass, className);
+          item.data.staticClass = className;
+          // this.$vuef7.utils.classNames(item.data.staticClass, className);
         }
         if (cssStyle) {
-          item.data.style = this.$meta.util.extend({}, item.data.style, cssStyle);
+          if (item.data.style) {
+            item.data.style = this.$meta.util.extend({}, item.data.style, cssStyle);
+          } else {
+            item.data.style = cssStyle;
+          }
         }
       }
     },
--- src/module-system/a-components-sync/front/src/common/validate/render/renderDetails.jsx
+++ src/module-system/a-components-sync/front/src/common/validate/render/renderDetails.jsx
@@ -1,10 +1,14 @@
 export default {
   methods: {
     renderDetails(context) {
-      return this._renderComponent(context, {
-        module: 'a-detail',
-        name: 'renderDetails',
-      });
+      return this._renderComponent(
+        context,
+        {
+          module: 'a-detail',
+          name: 'renderDetails',
+        },
+        'eb-details-container'
+      );
     },
   },
 };
