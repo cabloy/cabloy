bfbbe569920227c3361e1bb7762bba81a175feb3
zhennann <zhen.nann@icloud.com>
2023-10-24 20:04:44 +0800

    feat: validate

--- src/module-system/a-validation/backend/src/service/validation.js
+++ src/module-system/a-validation/backend/src/service/validation.js
@@ -4,9 +4,17 @@ module.exports = app => {
       return this.ctx.bean.validation.getSchema({ module, validator, schema });
     }
     async validate({ params, data }) {
-      await this.ctx.bean.validation.validate({
-        ...params,
+      const { module, validator, schema } = params;
+      await this.ctx.bean.validation._validate({
+        atomClas: null,
         data,
+        options: {
+          schema: { module, validator, schema },
+        },
+        filterOptions: {
+          type: true,
+          ebReadOnly: true,
+        },
       });
     }
   }
