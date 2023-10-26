261549dfc3828c67e305b35224512d65fcf3a48b
zhennann <zhen.nann@icloud.com>
2023-10-26 15:09:03 +0800

    feat: dict of area

--- src/module-system/a-dictarea/backend/src/config/locale/zh-cn.js
+++ src/module-system/a-dictarea/backend/src/config/locale/zh-cn.js
@@ -1,1 +1,5 @@
-module.exports = {};
+module.exports = {
+  Countries: '国家',
+  'Cities USA': '美国城市',
+  'Cities China': '中国城市',
+};
--- src/module-system/a-dictbooster/backend/src/config/locale/zh-cn.js
+++ src/module-system/a-dictbooster/backend/src/config/locale/zh-cn.js
@@ -1,9 +1,6 @@
 module.exports = {
-  Countries: '国家',
   AtomStateDefault: '数据状态(默认)',
   AtomDisabledStatus: '数据禁用状态',
-  'Cities USA': '美国城市',
-  'Cities China': '中国城市',
   'Online Status': '在线状态',
   'Layout Type': '布局类型',
 };
