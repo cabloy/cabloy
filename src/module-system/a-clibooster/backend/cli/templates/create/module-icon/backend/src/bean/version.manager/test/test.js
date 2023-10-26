7de49526c91cf56b3a8c1cec2bb2ff4c4e7cba7a
zhennann <zhen.nann@icloud.com>
2023-10-26 20:34:18 +0800

    feat: cli create module-icon

---
+++ src/module-system/a-clibooster/backend/cli/templates/create/module-icon/backend/src/bean/version.manager/test/test.js
@@ -0,0 +1,8 @@
+module.exports = function (ctx) {
+  // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class VersionTest {
+    async run(options) {}
+  }
+
+  return VersionTest;
+};
