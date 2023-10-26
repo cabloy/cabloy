38753fd2938b2ac883cf5c68b3d16e5665bcafe2
zhennann <zhen.nann@icloud.com>
2023-10-26 15:05:17 +0800

    feat: dict of area

--- src/module-system/a-dictarea/backend/src/meta/static/dicts.js
+++ src/module-system/a-dictarea/backend/src/meta/static/dicts.js
@@ -1,4 +1,12 @@
+const countries = require('./dict/countries.js');
+const citiesUSA = require('./dict/citiesUSA.js');
+const citiesChina = require('./dict/citiesChina.js');
+
 module.exports = app => {
-  const dicts = [];
+  const dicts = [
+    countries(app), //
+    citiesUSA(app),
+    citiesChina(app),
+  ];
   return dicts;
 };
--- src/module-system/a-dictbooster/backend/src/meta/static/dicts.js
+++ src/module-system/a-dictbooster/backend/src/meta/static/dicts.js
@@ -1,6 +1,3 @@
-const countries = require('./dict/countries.js');
-const citiesUSA = require('./dict/citiesUSA.js');
-const citiesChina = require('./dict/citiesChina.js');
 const onlineStatus = require('./dict/onlineStatus.js');
 const layoutType = require('./dict/layoutType.js');
 const atomStateDefault = require('./dict/atomStateDefault.js');
@@ -8,10 +5,7 @@ const atomDisabled = require('./dict/atomDisabled.js');
 
 module.exports = app => {
   const dicts = [
-    countries(app), //
-    citiesUSA(app),
-    citiesChina(app),
-    onlineStatus(app),
+    onlineStatus(app), //
     layoutType(app),
     atomStateDefault(app),
     atomDisabled(app),
