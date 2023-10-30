899486063c2294e3b7751ead62bb3fe1a5012430
zhennann <zhen.nann@icloud.com>
2023-10-30 20:53:38 +0800

    feat: _getPrefferedRole_select

--- src/module-system/a-basestore/front/src/stores/prefferedRole.js
+++ src/module-system/a-basestore/front/src/stores/prefferedRole.js
@@ -1,7 +1,7 @@
 export default function (Vue) {
   return {
     state() {
-      return { userAtomClassRolesPreferred: {} };
+      return { preferredRoles: {} };
     },
     created() {
       Vue.prototype.$meta.eventHub.$on('auth:login', data => {
@@ -10,10 +10,58 @@ export default function (Vue) {
     },
     actions: {
       authLogin() {
-        this.userAtomClassRolesPreferred = {};
+        this.preferredRoles = {};
       },
-      setUserAtomClassRolesPreferred({ atomClassId, roleIdOwner }) {
-        this.userAtomClassRolesPreferred[atomClassId] = roleIdOwner;
+      setPrefferedRole({ atomClassId, prefferedRole }) {
+        this.preferredRoles[atomClassId] = prefferedRole;
+      },
+      async getPrefferedRole({ ctx, atomClass, options }) {
+        const useStoreAtomClasses = await Vue.prototype.$meta.store.use('a/basestore/atomClasses');
+        const atomClassId = await useStoreAtomClasses.getAtomClassId({ atomClass });
+        if (this.preferredRoles[atomClassId]) return this.preferredRoles[atomClassId];
+        // get preferred roles
+        const roles = await ctx.$api.post('/a/base/atom/preferredRoles', {
+          atomClass: { id: atomClassId },
+        });
+        // 0/1
+        if (roles.length === 0) throw new Error('Error');
+        if (roles.length === 1) {
+          const preferredRole = roles[0].roleIdWho;
+          this.setPrefferedRole({ atomClassId, preferredRole });
+          return preferredRole;
+        }
+        // >1
+        const preferredRole = await this._getPrefferedRole_select({ ctx, roles, options });
+        if (preferredRole) {
+          this.setPrefferedRole({ atomClassId, preferredRole });
+        }
+        return preferredRole;
+      },
+      async _getPrefferedRole_select({ ctx, roles, options }) {
+        // buttons
+        const buttons = [
+          {
+            text: ctx.$text('AtomClassSelectRoleTip'),
+            label: true,
+          },
+        ];
+        for (const role of roles) {
+          buttons.push({
+            text: role.roleNameWho,
+            data: role,
+          });
+        }
+        // choose
+        const params = {
+          targetEl: options.targetEl,
+          buttons,
+        };
+        try {
+          const button = await ctx.$view.actions.choose(params);
+          return button.data.roleIdWho;
+        } catch (err) {
+          return null;
+        }
       },
     },
   };
