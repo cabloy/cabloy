1d4dc5b8c3bee4e522ed398646fcb85b298ad0a3
zhennann <zhen.nann@icloud.com>
2023-11-05 17:57:24 +0800

    feat: cli tools.demo

--- src/module-system/a-clibooster/backend/src/controller/tools.js
+++ src/module-system/a-clibooster/backend/src/controller/tools.js
@@ -1,9 +1,16 @@
 module.exports = app => {
+  const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class DemoController extends app.Controller {
+    get localUtils() {
+      return this.ctx.bean.local.module(moduleInfo.relativeName).utils;
+    }
+
     async demo() {
-      const method = this.ctx.params.method;
-      console.log(method);
-      this.ctx.success(method);
+      // method
+      const method = this.ctx.params.method || 'execute';
+      // execute
+      const result = await this.localUtils.demoExecute({ method });
+      this.ctx.success(result);
     }
   }
 
--- src/module-system/a-clibooster/backend/src/routes.js
+++ src/module-system/a-clibooster/backend/src/routes.js
@@ -2,6 +2,16 @@ module.exports = app => {
   const routes = [
     {
       method: 'get',
+      path: 'tools/demo',
+      controller: 'tools',
+      action: 'demo',
+      meta: {
+        right: { type: 'resource', module: 'a-clibooster', name: 'cliTools' },
+        gate: { env: 'local' },
+      },
+    },
+    {
+      method: 'get',
       path: 'tools/demo/:method',
       controller: 'tools',
       action: 'demo',
