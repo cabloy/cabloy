6689e63e50f93f40a3040f3a996ab27e6c740741
zhennann <zhen.nann@icloud.com>
2023-11-05 14:53:22 +0800

    feat: support dependencts of middleware

--- packages/egg-born-backend/lib/module/middleware.js
+++ packages/egg-born-backend/lib/module/middleware.js
@@ -12,6 +12,9 @@ module.exports = function (loader) {
   // load middlewares all
   loadMiddlewaresAll(ebMiddlewaresAll, ebModulesArray, loader);
 
+  // handle dependents
+  handleDependents(ebMiddlewaresAll);
+
   // load middlewares
   loadMiddlewares(
     ebMiddlewaresAll,
@@ -82,6 +85,30 @@ function loadMiddlewaresAll(ebMiddlewaresAll, ebModulesArray, loader) {
         options: middlewareConfig,
         bean,
       });
+    }
+  }
+}
+
+function handleDependents(ebMiddlewaresAll) {
+  for (const middleware of ebMiddlewaresAll) {
+    let dependents = middleware.options.dependents;
+    if (!dependents) continue;
+    if (!Array.isArray(dependents)) {
+      dependents = dependents.split(',');
+    }
+    for (const dep of dependents) {
+      const middleware2 = ebMiddlewaresAll.find(item => item.name === dep);
+      if (!middleware2) {
+        throw new Error(`middleware ${dep} not found for dependents of ${middleware.name}`);
+      }
+      const options = middleware2.options;
+      if (!options.dependencies) options.dependencies = [];
+      if (!Array.isArray(options.dependencies)) {
+        options.dependencies = options.dependencies.split(',');
+      }
+      if (options.dependencies.findIndex(item => item === middleware.name) === -1) {
+        options.dependencies.push(middleware.name);
+      }
     }
   }
 }
