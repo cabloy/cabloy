08528d0200456e014e6841d7804f1223f2ffeb96
zhennann <zhen.nann@icloud.com>
2023-11-12 20:44:35 +0800

    refactor: unit test by performAction

--- src/module-system/a-dict/backend/test/controller/test.test.js
+++ src/module-system/a-dict/backend/test/controller/test.test.js
@@ -2,58 +2,66 @@ const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
 describe('[your tests start from here]', () => {
   it('[atom]', async () => {
-    app.mockSession({});
+    // ctx
+    const ctx = await app.mockCtx();
 
     // atomClass info
     const atomClassModule = mockInfo().relativeName;
     const atomClassName = 'dict';
+    const atomClass = { module: atomClassModule, atomClassName };
 
     // login as root
-    await app
-      .httpRequest()
-      .post(mockUrl('/a/auth/passport/a-authsimple/authsimple'))
-      .send({
-        data: {
-          auth: 'root',
-          password: '123456',
-        },
-      });
+    await ctx.meta.mockUtil.login({ auth: 'root' });
 
     // create
-    let result = await app
-      .httpRequest()
-      .post(mockUrl('/a/base/atom/write'))
-      .send({
-        atomClass: { module: atomClassModule, atomClassName },
+    const keyDraft = await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/write',
+      body: {
+        atomClass,
         item: {
           atomName: 'test',
           description: 'this is a test',
         },
-      });
-    assert(result.body.code === 0);
-    const keyDraft = result.body.data;
+      },
+    });
+    assert(!!keyDraft);
 
     // submit
-    result = await app
-      .httpRequest()
-      .post(mockUrl('/a/base/atom/submit'))
-      .send({
+    let data = await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/submit',
+      body: {
         key: keyDraft,
-        atomClass: { module: atomClassModule, atomClassName },
-      });
-    assert(result.body.code === 0);
-    const keyFormal = result.body.data.formal.key;
+        atomClass,
+      },
+    });
+    const keyFormal = data.formal.key;
+    assert(!!keyFormal);
 
     // read
-    result = await app.httpRequest().post(mockUrl('/a/base/atom/read')).send({
-      key: keyFormal,
+    data = await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/read',
+      body: {
+        key: keyFormal,
+        atomClass,
+      },
     });
-    assert(result.body.code === 0);
+    assert(!!data);
 
     // delete
-    result = await app.httpRequest().post(mockUrl('/a/base/atom/delete')).send({
-      key: keyFormal,
+    await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/delete',
+      body: {
+        key: keyFormal,
+        atomClass,
+      },
     });
-    assert(result.body.code === 0);
   });
 });
