b2bc6852251a9bbb8014c57900c45b168ba01397
zhennann <zhen.nann@icloud.com>
2023-11-14 10:58:00 +0800

    feat: _findNodeTasks

--- src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_find.js
+++ src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_find.js
@@ -1,6 +1,52 @@
 module.exports = ctx => {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
-  class FlowDef {}
+  class FlowDef {
+    _findNodeTasks({ content, nodeStart }) {
+      const nodeTasks = [];
+      const nodeIdCaches = {};
+      // next
+      this._findNodeTasks_next({ content, node: nodeStart, nodeTasks, nodeIdCaches });
+      //
+      return nodeTasks;
+    }
+
+    _findNodeTasks_next({ content, node, nodeTasks, nodeIdCaches }) {
+      // cache
+      nodeIdCaches[node.id] = true;
+      // check if endEvent
+      if (node.type.indexOf('endEvent') > -1) {
+        return;
+      }
+      // check if activityUserTask
+      if (node.type.indexOf('activityUserTask') > -1) {
+        // check if auto
+        let _vars = ctx.bean.util.getProperty(node, 'options.assignees.vars');
+        if (_vars) {
+          if (typeof _vars === 'string') {
+            _vars = _vars.split(',');
+          }
+          if (_vars.includes('auto')) {
+            nodeTasks.push(node);
+          }
+        }
+      }
+      // edges
+      const edges = content.process.edges.filter(item => {
+        return item.source === node.id && !nodeIdCaches[item.target];
+      });
+      // next
+      for (const edge of edges) {
+        const nodeTarget = content.process.nodes.find(item => {
+          return item.id === edge.target;
+        });
+        if (!nodeTarget) {
+          throw new Error(`flow node not found: ${edge.target}`);
+        }
+        // next
+        this._findNodeTasks_next({ content, node: nodeTarget, nodeTasks, nodeIdCaches });
+      }
+    }
+  }
 
   return FlowDef;
 };
