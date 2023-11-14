28a0b1d89191c8f508d2f927925e78338cc27dae
zhennann <zhen.nann@icloud.com>
2023-11-14 09:38:39 +0800

    refactor: bean.flowDef

--- src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_0.js
+++ src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_0.js
@@ -1,6 +1,58 @@
 module.exports = ctx => {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
-  class FlowDef {}
+  class FlowDef {
+    get modelFlowDef() {
+      return ctx.model.module(moduleInfo.relativeName).flowDef;
+    }
+    get modelFlowDefContent() {
+      return ctx.model.module(moduleInfo.relativeName).flowDefContent;
+    }
+    get modelFlowDefFull() {
+      return ctx.model.module(moduleInfo.relativeName).flowDefFull;
+    }
+    get atomClass() {
+      return {
+        module: moduleInfo.relativeName,
+        atomClassName: 'flowDef',
+      };
+    }
+
+    async getByKey({ flowDefKey }) {
+      return await this._getByKey({ flowDefKey, atomStage: 'formal' });
+    }
+
+    async getById({ flowDefId }) {
+      // get
+      return await this._getById({ flowDefId });
+    }
+
+    async getByKeyAndRevision({ flowDefKey, flowDefRevision }) {
+      // get from formal
+      let flowDef = await this._getByKey({ flowDefKey, flowDefRevision, atomStage: 'formal' });
+      if (flowDef) return flowDef;
+      // get from history
+      flowDef = await this._getByKey({ flowDefKey, flowDefRevision, atomStage: 'history' });
+      if (flowDef) return flowDef;
+      // not found
+      return null;
+    }
+
+    async _getById({ flowDefId }) {
+      return await ctx.bean.atom.read({ key: { atomId: flowDefId } });
+    }
+
+    async _getByKey({ flowDefKey, flowDefRevision, atomStage }) {
+      // fullKey
+      const { fullKey } = this._combineFullKey({ flowDefKey });
+      // from db
+      return await ctx.bean.atom.readByStaticKey({
+        atomClass: this.atomClass,
+        atomStaticKey: fullKey,
+        atomRevision: flowDefRevision,
+        atomStage,
+      });
+    }
+  }
 
   return FlowDef;
 };
--- src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_deploy.js
+++ src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_deploy.js
@@ -1,6 +1,54 @@
 module.exports = ctx => {
   const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
-  class FlowDef {}
+  class FlowDef {
+    async deploy({ flowDefId, undeploy, deleting }) {
+      // start event
+      const res = await this._deploy_startEvent({ flowDefId, undeploy, deleting });
+      const atomClass = res?.atomClass;
+      // atom state
+      if (atomClass) {
+        await this._deploy_atomState({ atomClass });
+      }
+    }
+
+    async _deploy_atomState({ atomClass }) {
+      const _nodeBaseBean = ctx.bean._newBean('a-flowtask.flow.node.startEventAtom');
+      const list = await _nodeBaseBean._getAllConditions({ atomClassId: atomClass.id });
+      console.log(list);
+    }
+
+    async _deploy_startEvent({ flowDefId, undeploy, deleting }) {
+      // flowDef
+      const flowDef = await this._getById({ flowDefId });
+      if (!flowDef) return;
+      // content
+      const content = flowDef.content ? JSON.parse(flowDef.content) : null;
+      if (!content || !content.process) return;
+      // all startEvents
+      let atomClass;
+      for (const node of content.process.nodes) {
+        const nodeType = node.type;
+        if (nodeType.indexOf('startEvent') === -1) continue;
+        const _nodeBase = this._getFlowNodeBase(nodeType);
+        const _nodeBaseBean = ctx.bean._newBean(_nodeBase.beanFullName);
+        if (_nodeBaseBean.deploy) {
+          const res = await _nodeBaseBean.deploy({
+            deploy: !undeploy && flowDef.atomDisabled === 0,
+            flowDefId,
+            node,
+            deleting,
+            flowDef,
+            content,
+          });
+          if (res?.atomClass) {
+            atomClass = res?.atomClass;
+          }
+        }
+      }
+      // return
+      return { atomClass };
+    }
+  }
 
   return FlowDef;
 };
