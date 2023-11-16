841776f86398761ff9f175f5f46fc95a33dd48aa
zhennann <zhen.nann@icloud.com>
2023-11-16 11:31:24 +0800

    feat: _deploy_atomState

--- src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_deploy.js
+++ src/module-system/a-flow/backend/src/bean/bean.flowDef/bean.flowDef_deploy.js
@@ -14,35 +14,140 @@ module.exports = ctx => {
     async _deploy_atomState({ atomClass }) {
       // all flowDefs
       const _nodeBaseBean = ctx.bean._newBean('a-flowtask.flow.node.startEventAtom');
-      const list = await _nodeBaseBean._getAllConditions({ atomClassId: atomClass.id });
-      const flowDefs = await ctx.bean.atom.select({
-        atomClass: this.atomClass,
-        options: {
-          stage: 'formal',
-          where: {
-            'a.id': {
-              op: 'in',
-              val: list.map(item => item.flowDefId),
-            },
-          },
-          mode: 'full',
-        },
-        user: null,
-        pageForce: false,
-      });
+      const conditions = await _nodeBaseBean._getAllConditions({ atomClassId: atomClass.id, needFlowContent: true });
       // vars
-      const titleStateStart = null;
-      const titleStateEnd = null;
-      const dictItems = [];
+      let dictItemStart = null;
+      let dictItemEnd = null;
+      let dictItems = [];
+      const dictLocales = {};
+      const needGroup = conditions.length > 1;
       // loop
-      for (const flowDef of flowDefs) {
-        const dictItems_flowDef = this._deploy_atomState_handleFlowDef({ flowDef });
+      for (const condition of conditions) {
+        const flowDefId = condition.flowDefId;
+        const startEventId = condition.startEventId;
+        const content = condition.content ? JSON.parse(condition.content) : null;
+        const flowName = condition.atomName;
+        const { nodeStart, nodeEnd, nodeTasks } = await this._deploy_atomState_findNodes({
+          flowDefId,
+          startEventId,
+          content,
+        });
+        // start
+        if (!dictItemStart) {
+          dictItemStart = this._deploy_atomState_combineDictItem({ flowDefId, node: nodeStart, code: 1, dictLocales });
+        }
+        // end
+        if (!dictItemEnd) {
+          dictItemEnd = this._deploy_atomState_combineDictItem({ flowDefId, node: nodeEnd, code: -1, dictLocales });
+        }
+        // items
+        const dictItemsTask = this._deploy_atomState_combineDictItemsTask({ flowDefId, nodeTasks, dictLocales });
+        if (dictItemsTask.length === 0) {
+          // no tasks
+          continue;
+        }
+        // append
+        if (needGroup) {
+          const nodeGroup = { name: flowName };
+          const dictGroup = this._deploy_atomState_combineDictItem({
+            flowDefId,
+            node: nodeGroup,
+            code: `${flowDefId}:__group__`,
+            title: flowName,
+            dictLocales,
+          });
+          dictGroup.options = {
+            group: true,
+          };
+          dictItems.push(dictGroup);
+        }
+        dictItems = dictItems.concat(dictItemsTask);
       }
-      console.log(flowDefs);
+      // append start
+      dictItems.unshift(dictItemStart);
+      // append end
+      dictItems.push(dictItemEnd);
+      // ok
+      console.log(dictItems);
+      console.log(dictLocales);
     }
 
-    _deploy_atomState_handleFlowDef({ flowDef }) {
+    async _deploy_atomState_findNodes({ startEventId, content }) {
+      let nodeStart = null;
+      let nodeEnd = null;
+      const nodeTasks = await ctx.bean.flowDef._loopNodes({
+        content,
+        nodeIdStart: startEventId,
+        fn: async ({ nodes, node }) => {
+          // check if startEvent
+          if (node.id === startEventId) {
+            nodeStart = node;
+            return;
+          }
+          // check if endEvent
+          if (node.type.indexOf('endEventAtom') > -1) {
+            nodeEnd = node;
+            return;
+          }
+          // check if other endEvent
+          if (node.type.indexOf('endEvent') > -1) {
+            return;
+          }
+          // check if activityUserTask
+          if (node.type.indexOf('activityUserTask') > -1) {
+            nodes.push(node);
+          }
+        },
+      });
+      return { nodeStart, nodeEnd, nodeTasks };
+    }
+
+    _deploy_atomState_combineDictItemsTask({ flowDefId, nodeTasks, dictLocales }) {
       const dictItems = [];
+      const codesCache = {};
+      for (const nodeTask of nodeTasks) {
+        const dictItem = this._deploy_atomState_combineDictItem({ flowDefId, node: nodeTask, dictLocales, codesCache });
+        if (dictItem) {
+          dictItems.push(dictItem);
+        }
+      }
+      return dictItems;
+    }
+
+    _deploy_atomState_combineDictItem({ flowDefId, node, code, title, dictLocales, codesCache }) {
+      // name
+      const name = node.nameState || node.name;
+      // code
+      if (!code) {
+        code = `${flowDefId}:${name}`;
+      }
+      if (codesCache && codesCache[code]) {
+        // exists
+        return null;
+      }
+      // title
+      if (!title) {
+        title = name;
+      }
+      // dict item
+      const dictItem = {
+        code,
+        title,
+      };
+      // dict locales
+      const locales = ctx.bean.base.locales();
+      for (const locale of locales) {
+        const language = locale.value;
+        const text = ctx.text.locale(language, title);
+        if (text !== title) {
+          if (!dictLocales[language]) {
+            dictLocales[language] = {};
+          }
+          dictLocales[language][title] = text;
+        }
+      }
+      // ok
+      return dictItem;
     }
 
     async _deploy_startEvent({ flowDefId, undeploy, deleting }) {
