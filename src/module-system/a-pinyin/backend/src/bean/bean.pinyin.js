d6e2bba3f2915efef4839157492d883251d66ca1
zhennann <zhen.nann@icloud.com>
2023-11-28 14:02:13 +0800

    feat: pinyin

--- src/module-system/a-pinyin/backend/src/bean/bean.pinyin.js
+++ src/module-system/a-pinyin/backend/src/bean/bean.pinyin.js
@@ -3,8 +3,28 @@ const py = require('pinyin-pro');
 module.exports = ctx => {
   // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Pinyin {
-    translate({ word, options }) {
-      return py.pinyin(word, options);
+    translate({ text, options }) {
+      return py.pinyin(text, options);
+    }
+
+    match({ text, pinyin, options }) {
+      return py.match(text, pinyin, options);
+    }
+
+    convert({ pinyin, options }) {
+      return py.convert(pinyin, options);
+    }
+
+    custom({ config }) {
+      return py.customPinyin(config);
+    }
+
+    html({ text, options }) {
+      return py.html(text, options);
+    }
+
+    polyphonic({ text, options }) {
+      return py.polyphonic(text, options);
     }
   }
   return Pinyin;
