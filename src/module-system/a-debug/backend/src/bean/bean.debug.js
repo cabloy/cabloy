853b24489b32354a870e210bf7755d60dae47184
zhennann <zhen.nann@icloud.com>
2023-12-05 22:48:20 +0800

    feat: debug

--- src/module-system/a-debug/backend/src/bean/bean.debug.js
+++ src/module-system/a-debug/backend/src/bean/bean.debug.js
@@ -1,10 +1,15 @@
 const debug = require('debug');
 
+const __debug_caches = {};
+
 module.exports = app => {
   // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
   class Debug {
-    create(namespace) {
-      return debug(namespace);
+    get(namespace) {
+      if (!__debug_caches[namespace]) {
+        __debug_caches[namespace] = debug(namespace);
+      }
+      return __debug_caches[namespace];
     }
 
     instance() {
