f6029305c660fd90c9b41fcf16b06a1864314ca4
zhennann <zhen.nann@icloud.com>
2023-12-05 22:28:44 +0800

    feat: debug bean

---
+++ src/module-system/a-debug/backend/src/bean/bean.debug.js
@@ -0,0 +1,5 @@
+module.exports = ctx => {
+  // const moduleInfo = ctx.app.meta.mockUtil.parseInfoFromPackage(__dirname);
+  class Debug {}
+  return Debug;
+};
--- src/module-system/a-debug/backend/src/beans.js
+++ src/module-system/a-debug/backend/src/beans.js
@@ -1,4 +1,5 @@
 const versionManager = require('./bean/version.manager.js');
+const beanDebug = require('./bean/bean.debug.js');
 
 module.exports = app => {
   const beans = {
@@ -6,6 +7,12 @@ module.exports = app => {
     'version.manager': {
       mode: 'app',
       bean: versionManager,
+    },
+    // global
+    debug: {
+      mode: 'app',
+      bean: beanDebug,
+      global: true,
     },
   };
   return beans;
