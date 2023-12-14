0f72f880e809672125d887db2369d367f9d7e3b0
zhennann <zhen.nann@icloud.com>
2023-12-14 15:43:35 +0800

    refactor: IOChannelBase

--- src/module-system/a-mail/backend/src/bean/io.channel.mail.js
+++ src/module-system/a-mail/backend/src/bean/io.channel.mail.js
@@ -4,75 +4,72 @@ const boxen = require('boxen');
 
 const boxenOptions = { padding: 1, margin: 1, align: 'center', borderColor: 'yellow', borderStyle: 'round' };
 
-module.exports = ctx => {
-  // const moduleInfo = module.info;
-  class IOChannel extends ctx.app.meta.IOChannelBase(ctx) {
-    async onPush({ content /* options, message, messageSync, messageClass*/ }) {
-      // check if content.message
-      // not set content.message.to dynamic for test, which must be set by business
-      if (!content.message || !content.message.to) return false;
-      // scene
-      let scene;
-      let sceneTest = false;
-      // 1. maybe object by dynamic
-      if (content.scene && typeof content.scene === 'object') {
-        scene = content.scene;
-      } else {
-        // 2. from config cache
-        scene = ctx.bean.mailSceneCache.getMailSceneConfigCache(content.scene || 'system');
-      }
-      // 3. test
-      if (!this._sceneValid(scene) && (ctx.app.meta.isTest || ctx.app.meta.isLocal)) {
-        scene = await this._createSceneTest();
-        sceneTest = true;
-      }
-      // check if empty
-      if (!this._sceneValid(scene)) {
-        const message = chalk.keyword('orange')(ctx.text('mailhostNotConfigAlert'));
-        console.log('\n' + boxen(message, boxenOptions));
-        return false;
-      }
-      // transporter
-      const transporter = nodemailer.createTransport(scene.transport, scene.defaults);
-      // send
-      const res = await transporter.sendMail(content.message);
-      // log
-      if (sceneTest) {
-        const url = nodemailer.getTestMessageUrl(res);
-        const message =
-          chalk.keyword('cyan')('Test Mail To: ') +
-          chalk.keyword('yellow')(content.message.to) +
-          chalk.keyword('orange')('\n' + url);
-        console.log('\n' + boxen(message, boxenOptions));
-      }
-      // done
-      return true;
+// const moduleInfo = module.info;
+module.exports = class IOChannel extends module.meta.class.IOChannelBase {
+  async onPush({ content /* options, message, messageSync, messageClass*/ }) {
+    // check if content.message
+    // not set content.message.to dynamic for test, which must be set by business
+    if (!content.message || !content.message.to) return false;
+    // scene
+    let scene;
+    let sceneTest = false;
+    // 1. maybe object by dynamic
+    if (content.scene && typeof content.scene === 'object') {
+      scene = content.scene;
+    } else {
+      // 2. from config cache
+      scene = this.ctx.bean.mailSceneCache.getMailSceneConfigCache(content.scene || 'system');
     }
-
-    async _createSceneTest() {
-      const account = await nodemailer.createTestAccount();
-      return {
-        transport: {
-          host: account.smtp.host,
-          port: account.smtp.port,
-          secure: account.smtp.secure,
-          auth: {
-            user: account.user,
-            pass: account.pass,
-          },
-          logger: false,
-          debug: false,
-        },
-        defaults: {
-          // sender info
-          from: 'Nodemailer <example@nodemailer.com>',
-        },
-      };
+    // 3. test
+    if (!this._sceneValid(scene) && (this.ctx.app.meta.isTest || this.ctx.app.meta.isLocal)) {
+      scene = await this._createSceneTest();
+      sceneTest = true;
     }
-
-    _sceneValid(scene) {
-      return scene && scene.transport && scene.transport.host;
+    // check if empty
+    if (!this._sceneValid(scene)) {
+      const message = chalk.keyword('orange')(this.ctx.text('mailhostNotConfigAlert'));
+      console.log('\n' + boxen(message, boxenOptions));
+      return false;
     }
+    // transporter
+    const transporter = nodemailer.createTransport(scene.transport, scene.defaults);
+    // send
+    const res = await transporter.sendMail(content.message);
+    // log
+    if (sceneTest) {
+      const url = nodemailer.getTestMessageUrl(res);
+      const message =
+        chalk.keyword('cyan')('Test Mail To: ') +
+        chalk.keyword('yellow')(content.message.to) +
+        chalk.keyword('orange')('\n' + url);
+      console.log('\n' + boxen(message, boxenOptions));
+    }
+    // done
+    return true;
   }
-  return IOChannel;
+
+  async _createSceneTest() {
+    const account = await nodemailer.createTestAccount();
+    return {
+      transport: {
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+        logger: false,
+        debug: false,
+      },
+      defaults: {
+        // sender info
+        from: 'Nodemailer <example@nodemailer.com>',
+      },
+    };
+  }
+
+  _sceneValid(scene) {
+    return scene && scene.transport && scene.transport.host;
+  }
 };
