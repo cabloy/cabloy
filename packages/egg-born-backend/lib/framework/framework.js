e3a83d34ecd3409bd0e5ce2f12557ef8a9e54653
zhennann <zhen.nann@icloud.com>
2023-12-14 14:49:53 +0800

    fix: egg framework

--- packages/egg-born-backend/lib/framework/framework.js
+++ packages/egg-born-backend/lib/framework/framework.js
@@ -5,9 +5,11 @@ const EGG_LOADER = Symbol.for('egg#loader');
 const AppWorkerLoader = require('./load/app.js');
 const AgentWorkerLoader = require('./load/agent.js');
 
+const eggPath = path.resolve(__dirname, '../..');
+
 class Application extends egg.Application {
   get [EGG_PATH]() {
-    return path.dirname(__dirname);
+    return eggPath;
   }
   get [EGG_LOADER]() {
     return AppWorkerLoader;
@@ -16,7 +18,7 @@ class Application extends egg.Application {
 
 class Agent extends egg.Agent {
   get [EGG_PATH]() {
-    return path.dirname(__dirname);
+    return eggPath;
   }
   get [EGG_LOADER]() {
     return AgentWorkerLoader;
