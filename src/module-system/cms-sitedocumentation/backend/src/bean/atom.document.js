a474f8840130f8f9c9902ffd79126c37b1cc3c05
zhennann <zhen.nann@icloud.com>
2023-12-14 10:56:45 +0800

    refactor: AtomBase

--- src/module-system/cms-sitedocumentation/backend/src/bean/atom.document.js
+++ src/module-system/cms-sitedocumentation/backend/src/bean/atom.document.js
@@ -1,43 +1,35 @@
-module.exports = ctx => {
-  // const moduleInfo = module.info;
-  class Atom extends ctx.app.meta.AtomCmsBase {
-    constructor() {
-      super(ctx);
-    }
-
-    async default({ atomClass, item, options, user }) {
-      // super
-      return await super.default({ atomClass, item, options, user });
-    }
-
-    async read({ atomClass, options, key, user }) {
-      // super
-      const item = await super.read({ atomClass, options, key, user });
-      if (!item) return null;
-      // ok
-      return item;
-    }
-
-    async select({ atomClass, options, items, user }) {
-      // super
-      await super.select({ atomClass, options, items, user });
-    }
-
-    async create({ atomClass, item, options, user }) {
-      // super
-      return await super.create({ atomClass, item, options, user });
-    }
-
-    async write({ atomClass, target, key, item, options, user }) {
-      // super
-      return await super.write({ atomClass, target, key, item, options, user });
-    }
-
-    async delete({ atomClass, key, options, user }) {
-      // super
-      await super.delete({ atomClass, key, options, user });
-    }
+// const moduleInfo = module.info;
+module.exports = class Atom extends module.meta.class.AtomCmsBase {
+  async default({ atomClass, item, options, user }) {
+    // super
+    return await super.default({ atomClass, item, options, user });
   }
 
-  return Atom;
+  async read({ atomClass, options, key, user }) {
+    // super
+    const item = await super.read({ atomClass, options, key, user });
+    if (!item) return null;
+    // ok
+    return item;
+  }
+
+  async select({ atomClass, options, items, user }) {
+    // super
+    await super.select({ atomClass, options, items, user });
+  }
+
+  async create({ atomClass, item, options, user }) {
+    // super
+    return await super.create({ atomClass, item, options, user });
+  }
+
+  async write({ atomClass, target, key, item, options, user }) {
+    // super
+    return await super.write({ atomClass, target, key, item, options, user });
+  }
+
+  async delete({ atomClass, key, options, user }) {
+    // super
+    await super.delete({ atomClass, key, options, user });
+  }
 };
