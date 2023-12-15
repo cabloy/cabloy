164ec84a4ec994d7a8ed76e800f224d584d2ed09
zhennann <zhen.nann@icloud.com>
2023-12-15 15:02:08 +0800

    refactor: version

--- src/module-system/a-clibooster/backend/cli/templates/create/module-business/backend/src/bean/version.manager/test/test.js
+++ src/module-system/a-clibooster/backend/cli/templates/create/module-business/backend/src/bean/version.manager/test/test.js
@@ -1,8 +1,3 @@
-module.exports = function (ctx) {
-  // const moduleInfo = module.info;
-  class VersionTest {
-    async run(options) {}
-  }
-
-  return VersionTest;
+module.exports = class VersionTest {
+  async run(options) {}
 };
--- src/module-system/a-flow/backend/src/bean/version.manager/test/test.js
+++ src/module-system/a-flow/backend/src/bean/version.manager/test/test.js
@@ -1,17 +1,12 @@
-module.exports = function (ctx) {
-  // const moduleInfo = module.info;
-  class VersionTest {
-    async run(options) {
-      // why add these test codes
-      //   - for force flowHistory.id !== flow.id
-      // flowHistory
-      let res = await ctx.model.flowHistory.insert({});
-      await ctx.model.flowHistory.delete({ id: res.insertId });
-      // flowNodeHistory
-      res = await ctx.model.flowNodeHistory.insert({});
-      await ctx.model.flowNodeHistory.delete({ id: res.insertId });
-    }
+module.exports = class VersionTest {
+  async run(options) {
+    // why add these test codes
+    //   - for force flowHistory.id !== flow.id
+    // flowHistory
+    let res = await this.ctx.model.flowHistory.insert({});
+    await this.ctx.model.flowHistory.delete({ id: res.insertId });
+    // flowNodeHistory
+    res = await this.ctx.model.flowNodeHistory.insert({});
+    await this.ctx.model.flowNodeHistory.delete({ id: res.insertId });
   }
-
-  return VersionTest;
 };
