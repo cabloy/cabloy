a0edbb43cdd7feb8443da163acf89bfd3ef919de
zhennann <zhen.nann@icloud.com>
2023-12-15 16:34:32 +0800

    refactor: summer

--- src/module-system/a-fields/backend/src/bean/summer.cache.fieldsRightOfAtomClass.js
+++ src/module-system/a-fields/backend/src/bean/summer.cache.fieldsRightOfAtomClass.js
@@ -1,9 +1,5 @@
-module.exports = ctx => {
-  class SummerCache {
-    async get(key) {
-      return await ctx.bean.fields.__getFieldsRightOfAtomClassRaw(key);
-    }
+module.exports = class SummerCache {
+  async get(key) {
+    return await this.ctx.bean.fields.__getFieldsRightOfAtomClassRaw(key);
   }
-
-  return SummerCache;
 };
