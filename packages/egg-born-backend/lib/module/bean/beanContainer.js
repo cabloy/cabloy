51a3a2bbb818e2f09991dd1671182dd2e8cb50ee
zhennann <zhen.nann@icloud.com>
2023-12-17 22:32:54 +0800

    Update beanContainer.js

--- packages/egg-born-backend/lib/module/bean/beanContainer.js
+++ packages/egg-born-backend/lib/module/bean/beanContainer.js
@@ -363,6 +363,7 @@ function __aopMatch(match, beanFullName) {
 function __setPropertyValue(obj, prop, value) {
   Object.defineProperty(obj, prop, {
     enumerable: false,
+    configurable: true,
     get() {
       return value;
     },
