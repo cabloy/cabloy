5e96b4c9ae354d74b9c451d6c2748ab601707c88
zhennann <zhen.nann@icloud.com>
2026-06-16 16:34:07 +0800

    feat: update

--- zova/src/module/demo-student/cli/openapi.config.ts
+++
@@ -1,9 +0,0 @@
-import type { ZovaOpenapiConfigModule } from 'zova-openapi';
-
-export default function (): ZovaOpenapiConfigModule {
-  return {
-    operations: {
-      match: [/^DemoStudent_*/],
-    },
-  };
-}
