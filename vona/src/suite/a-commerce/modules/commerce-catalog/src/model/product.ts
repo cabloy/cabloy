488b71c8cf5c3a473e1ae4a8a298bc7dc4940c6c
zhennann <zhen.nann@icloud.com>
2026-07-19 23:57:23 +0800

    feat: update

--- vona/src/suite/a-commerce/modules/commerce-catalog/src/model/product.ts
+++ vona/src/suite/a-commerce/modules/commerce-catalog/src/model/product.ts
@@ -1,10 +1,19 @@
 import type { IDecoratorModelOptions } from 'vona-module-a-orm';
 
-import { BeanModelBase, Model } from 'vona-module-a-orm';
+import { $relation, BeanModelBase, Model } from 'vona-module-a-orm';
 
 import { EntityProduct } from '../entity/product.tsx';
 
 export interface IModelOptionsProduct extends IDecoratorModelOptions<EntityProduct> {}
 
-@Model<IModelOptionsProduct>({ entity: EntityProduct })
+@Model<IModelOptionsProduct>({
+  entity: EntityProduct,
+  relations: {
+    category: $relation.belongsTo(
+      'commerce-catalog:product',
+      'commerce-catalog:category',
+      'categoryId',
+    ),
+  },
+})
 export class ModelProduct extends BeanModelBase<EntityProduct> {}
--- vona/src/suite/a-commerce/modules/commerce-catalog/src/model/sku.ts
+++ vona/src/suite/a-commerce/modules/commerce-catalog/src/model/sku.ts
@@ -1,10 +1,15 @@
 import type { IDecoratorModelOptions } from 'vona-module-a-orm';
 
-import { BeanModelBase, Model } from 'vona-module-a-orm';
+import { $relation, BeanModelBase, Model } from 'vona-module-a-orm';
 
 import { EntitySku } from '../entity/sku.tsx';
 
 export interface IModelOptionsSku extends IDecoratorModelOptions<EntitySku> {}
 
-@Model<IModelOptionsSku>({ entity: EntitySku })
+@Model<IModelOptionsSku>({
+  entity: EntitySku,
+  relations: {
+    product: $relation.belongsTo('commerce-catalog:sku', 'commerce-catalog:product', 'productId'),
+  },
+})
 export class ModelSku extends BeanModelBase<EntitySku> {}
